62cb650fde7fced4d01c7a67b704497acc31452c
Chung Tran <bigearsenal@gmail.com>
2020-10-22 12:16:13 +0700

    add framework NodeMobile

---
+++ p2p wallet/p2p_wallet-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  p2p_wallet-Bridging-Header.h
+//  p2p wallet
+//
+//  Created by Chung Tran on 10/22/20.
+//
+
+#ifndef p2p_wallet_Bridging_Header_h
+#define p2p_wallet_Bridging_Header_h
+
+
+#endif /* p2p_wallet_Bridging_Header_h */
