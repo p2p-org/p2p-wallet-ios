412f0110bc0b8da5a7f3eeaadd30e7174b98eef6
Chung Tran <bigearsenal@gmail.com>
2020-10-26 15:51:15 +0700

    configure project

---
+++ p2p_walletTests/SolanaSwiftSDKTests.swift
@@ -0,0 +1,34 @@
+//
+//  SolanaSwiftSDKTests.swift
+//  p2p_walletTests
+//
+//  Created by Chung Tran on 10/26/20.
+//
+
+import XCTest
+import SolanaSwift
+import RxBlocking
+
+class SolanaSwiftSDKTests: XCTestCase {
+    var solanaSDK: SolanaSDK!
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
