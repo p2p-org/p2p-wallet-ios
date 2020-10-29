8f05f77369884425e7cdbb3905384011015ccfa4
Chung Tran <bigearsenal@gmail.com>
2020-10-29 11:00:03 +0700

    fix color for button

--- p2p_wallet/Extensions/Asset+Generated.swift
+++ p2p_wallet/Extensions/Asset+Generated.swift
@@ -15,6 +15,7 @@ private let bundle = Bundle(for: BundleToken.self)
 extension UIColor {
     static let a4a4a4 = UIColor(named: "a4a4a4", in: bundle, compatibleWith: nil)!
     static let background = UIColor(named: "background", in: bundle, compatibleWith: nil)!
+    static let buttonSub = UIColor(named: "buttonSub", in: bundle, compatibleWith: nil)!
     static let textBlack = UIColor(named: "textBlack", in: bundle, compatibleWith: nil)!
     static let textWhite = UIColor(named: "textWhite", in: bundle, compatibleWith: nil)!
 }
