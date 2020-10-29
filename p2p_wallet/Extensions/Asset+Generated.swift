52281ea05a71a0760ecc99be3e8e183f664b4f76
Chung Tran <bigearsenal@gmail.com>
2020-10-29 10:57:23 +0700

    layout welcome screen

--- p2p_wallet/Extensions/Asset+Generated.swift
+++ p2p_wallet/Extensions/Asset+Generated.swift
@@ -14,8 +14,9 @@ private let bundle = Bundle(for: BundleToken.self)
 
 extension UIColor {
     static let a4a4a4 = UIColor(named: "a4a4a4", in: bundle, compatibleWith: nil)!
-    static let appBlack = UIColor(named: "appBlack", in: bundle, compatibleWith: nil)!
     static let background = UIColor(named: "background", in: bundle, compatibleWith: nil)!
+    static let textBlack = UIColor(named: "textBlack", in: bundle, compatibleWith: nil)!
+    static let textWhite = UIColor(named: "textWhite", in: bundle, compatibleWith: nil)!
 }
 
 // MARK: - Images -
---
+++ p2p_wallet/Scenes/IntroVC.swift
@@ -0,0 +1,43 @@
+//
+//  IntroVC.swift
+//  p2p_wallet
+//
+//  Created by Chung Tran on 10/29/20.
+//
+
+import Foundation
+
+class IntroVC: BaseVC, BEPageVCType {
+    var index: Int {0}
+    
+    // MARK: - Subviews
+    lazy var stackView = UIStackView(axis: .vertical, spacing: 31, alignment: .center, distribution: .fill)
+    
+    lazy var descriptionLabel = UILabel(text: "For athletes, high altitude produces two contradictory effects on performance. For explosive events (sprints up to 400 metres, long jump, triple jump) the reduction in atmospheric pressure means there is", textSize: 17, weight: .medium, textColor: UIColor.textBlack.withAlphaComponent(0.6), numberOfLines: 0, textAlignment: .center)
+    
+    // MARK: - Methods
+    override func setUp() {
+        super.setUp()
+        
+        let imageView = UIImageView(width: 220, height: 220, cornerRadius: 110)
+        imageView.image = .walletIntro
+        
+        stackView.addArrangedSubview(imageView)
+        stackView.addArrangedSubview(UILabel(text: L10n.wowletForPeopleNotForTokens, textSize: 32, weight: .bold, numberOfLines: 0, textAlignment: .center))
+        
+        // FIXME: - Change text
+        stackView.addArrangedSubview(descriptionLabel)
+        
+        view.addSubview(stackView)
+        stackView.autoCenterInSuperview()
+        stackView.autoPinEdge(toSuperviewEdge: .leading, withInset: 30)
+        stackView.autoPinEdge(toSuperviewEdge: .trailing, withInset: 30)
+    }
+    
+    #if DEBUG
+    override func injected() {
+        stackView.arrangedSubviews.forEach {$0.removeFromSuperview()}
+        super.injected()
+    }
+    #endif
+}
