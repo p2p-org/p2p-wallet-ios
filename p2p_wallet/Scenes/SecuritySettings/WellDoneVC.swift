79c076355158dcd395cc82932036d1dd0d4d514a
Chung Tran <bigearsenal@gmail.com>
2020-10-30 12:52:16 +0700

    Completed WellDoneVC

--- p2p_wallet/Scenes/SecuritySettings/WellDoneVC.swift
+++ p2p_wallet/Scenes/SecuritySettings/WellDoneVC.swift
@@ -8,5 +8,19 @@
 import Foundation
 
 class WellDoneVC: SecuritySettingVC {
-    
+    override func setUp() {
+        super.setUp()
+        acceptButton.setTitle(L10n.finishSetup, for: .normal)
+        doThisLaterButton.isHidden = true
+        
+        stackView.spacing = 30
+        
+        let imageView = UIImageView(width: 219, height: 209, image: .walletIntro)
+        let titleLabel = UILabel(text: L10n.wellDone, textSize: 32, weight: .bold, textAlignment: .center)
+        let descriptionLabel = UILabel(text: L10n.exploreWowletAndDepositFundsWhenYouReReady, textSize: 17, weight: .medium, textColor: UIColor.textBlack.withAlphaComponent(0.6), numberOfLines: 0, textAlignment: .center)
+        
+        stackView.insertArrangedSubview(imageView, at: 1)
+        stackView.insertArrangedSubview(titleLabel, at: 2)
+        stackView.insertArrangedSubview(descriptionLabel, at: 3)
+    }
 }
