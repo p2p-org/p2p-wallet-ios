8827735787c7cc8bf7c11b6531e82be35ba54a26
Chung Tran <bigearsenal@gmail.com>
2020-11-02 10:55:59 +0700

    Add CompositionalLayout

--- p2p_wallet/AppDelegate.swift
+++ p2p_wallet/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         #if DEBUG
 //        KeychainStorage.shared.clear()
         
-        Bundle(path: "/Applications/InjectionIII.app/Contents/Resources/iOSInjection.bundle")!.load()
+        Bundle(path: "/Applications/InjectionIII.app/Contents/Resources/iOSInjection.bundle")?.load()
 //        //for tvOS:
 //        Bundle(path: "/Applications/InjectionIII.app/Contents/Resources/tvOSInjection.bundle")?.load()
 //        //Or for macOS:
--- p2p_wallet/Extensions/Asset+Generated.swift
+++ p2p_wallet/Extensions/Asset+Generated.swift
@@ -19,6 +19,7 @@ extension UIColor {
     static let lightGrayBackground = UIColor(named: "lightGrayBackground", in: bundle, compatibleWith: nil)!
     static let textBlack = UIColor(named: "textBlack", in: bundle, compatibleWith: nil)!
     static let textWhite = UIColor(named: "textWhite", in: bundle, compatibleWith: nil)!
+    static let vcBackground = UIColor(named: "vcBackground", in: bundle, compatibleWith: nil)!
 }
 
 // MARK: - Images -
