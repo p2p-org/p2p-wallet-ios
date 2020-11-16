dd3f3d44eedd41ec46528a051dae82a6e0295391
Chung Tran <bigearsenal@gmail.com>
2020-11-16 16:44:35 +0700

    some fixes

---
+++ p2p_wallet/Extensions/Double+Extensions.swift
@@ -0,0 +1,36 @@
+//
+//  Double+Extensions.swift
+//  p2p_wallet
+//
+//  Created by Chung Tran on 16/11/2020.
+//
+
+import Foundation
+
+extension Double {
+    public var readableString: String {
+        let formatter = NumberFormatter()
+        formatter.usesGroupingSeparator = false
+        formatter.numberStyle = .decimal
+        formatter.maximumFractionDigits = (self < 1000) ? 4 : 2
+        return formatter.string(from: self as NSNumber) ?? "0"
+    }
+    
+    public var currencyValueFormatted: String {
+        let formatter = NumberFormatter()
+        formatter.groupingSize = 3
+        formatter.numberStyle = .decimal
+        formatter.usesGroupingSeparator = true
+        formatter.locale = Locale(identifier: "en")
+
+        if self > 1000 {
+            formatter.maximumFractionDigits = 2
+        } else if self < 100 {
+            formatter.maximumFractionDigits = 3
+        } else {
+            formatter.maximumFractionDigits = 2
+        }
+        
+        return (formatter.string(from: self as NSNumber) ?? "0").replacingOccurrences(of: ",", with: " ")
+    }
+}
