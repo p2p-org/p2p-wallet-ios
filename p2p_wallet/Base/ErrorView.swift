9f261a3b353eab2b3f19d02083e8d54d24ee6e8f
Chung Tran <bigearsenal@gmail.com>
2020-11-16 12:15:47 +0700

    handle state with EmptyView and ErrorView

--- p2p_wallet/Base/ErrorView.swift
+++ p2p_wallet/Base/ErrorView.swift
@@ -8,16 +8,40 @@
 import Foundation
 import Action
 
-class ErrorView: BEView {
+class MessageView: BEView {
     lazy var stackView = UIStackView(axis: .vertical, spacing: 8, alignment: .center, distribution: .fill)
     lazy var titleLabel = UILabel(text: L10n.error.uppercaseFirst, textSize: 17, weight: .semibold, textAlignment: .center)
     lazy var descriptionLabel = UILabel(text: L10n.somethingWentWrongPleaseTryAgainLater, textSize: 17, textColor: .secondary, numberOfLines: 0, textAlignment: .center)
-    lazy var tryAgainButton = WLButton.stepButton(type: .main, label: L10n.tryAgain)
+    lazy var actionButton = WLButton.stepButton(type: .main, label: L10n.tryAgain)
     
     override func commonInit() {
         super.commonInit()
         addSubview(stackView)
         stackView.autoPinEdgesToSuperviewEdges()
+        
+        stackView.addArrangedSubviews([
+            titleLabel,
+            descriptionLabel,
+            actionButton
+        ])
+        actionButton.isHidden = true
+    }
+    
+    var buttonAction: CocoaAction? {
+        didSet {
+            guard let action = buttonAction else {
+                actionButton.isHidden = true
+                return
+            }
+            actionButton.isHidden = false
+            actionButton.rx.action = action
+        }
+    }
+}
+
+class ErrorView: MessageView {
+    override func commonInit() {
+        super.commonInit()
         
         let iconView: UILabel = {
             let label = UILabel(text: "!", textSize: 50, weight: .medium, textAlignment: .center)
@@ -29,29 +53,11 @@ class ErrorView: BEView {
             return label
         }()
         
-        stackView.addArrangedSubviews([
-            iconView,
-            titleLabel,
-            descriptionLabel,
-            tryAgainButton
-        ])
-        
+        stackView.insertArrangedSubview(iconView, at: 0)
         stackView.setCustomSpacing(56.adaptiveHeight, after: iconView)
         stackView.setCustomSpacing(8, after: titleLabel)
         stackView.setCustomSpacing(56.adaptiveHeight, after: descriptionLabel)
         
-        tryAgainButton.isHidden = true
-    }
-    
-    var tryAgainAction: CocoaAction? {
-        didSet {
-            guard let action = tryAgainAction else {
-                tryAgainButton.isHidden = true
-                return
-            }
-            tryAgainButton.isHidden = false
-            tryAgainButton.rx.action = action
-        }
     }
     
     func setUpWithError(_ error: Error) {
