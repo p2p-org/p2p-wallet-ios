c9028a018ec7e4a8d2cb11e940ca2b4c87fb9068
Chung Tran <bigearsenal@gmail.com>
2020-11-18 12:08:48 +0700

    completed layout for ReceiveTokenVC

--- p2p_wallet/Extensions/UIImageView+Extensions.swift
+++ p2p_wallet/Extensions/UIImageView+Extensions.swift
@@ -9,6 +9,8 @@ import Foundation
 import SDWebImage
 
 extension UIImageView {
+    static let qrCodeCache = NSCache<NSString, UIImage>()
+    
     func setImage(urlString: String?) {
         guard let urlString = urlString, let url = URL(string: urlString) else {
             image = UIColor.gray.image(frame.size)
@@ -17,6 +19,34 @@ extension UIImageView {
         sd_setImage(with: url) { [weak self] (image, _, _, _) in
             if image == nil {
                 self?.image = UIColor.gray.image(self?.frame.size ?? .zero)
+            }
+        }
+    }
+    
+    func setQrCode(string: String?) {
+        guard let string = string else {return}
+        
+        if let imageFromCache = UIImageView.qrCodeCache.object(forKey: string as NSString) {
+            image = imageFromCache
+            return
+        }
+        
+        let data = string.data(using: String.Encoding.ascii)
+
+        DispatchQueue.global().async {
+            var image: UIImage?
+            if let filter = CIFilter(name: "CIQRCodeGenerator") {
+                filter.setValue(data, forKey: "inputMessage")
+                let transform = CGAffineTransform(scaleX: 3, y: 3)
+
+                if let output = filter.outputImage?.transformed(by: transform) {
+                    let qrCode = UIImage(ciImage: output)
+                    image = qrCode
+                    UIImageView.qrCodeCache.setObject(qrCode, forKey: string as NSString)
+                }
+            }
+            DispatchQueue.main.async {
+                self.image = image
             }
         }
     }
