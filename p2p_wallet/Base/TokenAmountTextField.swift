4b2feb75fa2e20f9a42178cda1aabd38e02f2e00
Chung Tran <bigearsenal@gmail.com>
2020-11-23 15:07:58 +0700

    Completed layout for SwapTokenVC

---
+++ p2p_wallet/Base/TokenAmountTextField.swift
@@ -0,0 +1,37 @@
+//
+//  TokenAmountTextField.swift
+//  p2p_wallet
+//
+//  Created by Chung Tran on 23/11/2020.
+//
+
+import Foundation
+
+class TokenAmountTextField: BEDecimalTextField {
+    var wallet: Wallet?
+    
+    override func shouldChangeCharactersInRange(_ range: NSRange, replacementString string: String) -> Bool {
+        // get the current text, or use an empty string if that failed
+        let currentText = text ?? ""
+        
+        guard super.shouldChangeCharactersInRange(range, replacementString: string),
+              let stringRange = Range(range, in: currentText)
+        else {
+            return false
+        }
+        // add their new text to the existing text
+        var updatedText = currentText.replacingCharacters(in: stringRange, with: string)
+        if let dotIndex = updatedText.firstIndex(of: Locale.current.decimalSeparator?.first ?? ".") {
+            let offset = updatedText.distance(from: dotIndex, to: updatedText.endIndex) - 1
+            let decimals = wallet?.decimals ?? 9
+            if offset > decimals {
+                let endIndex = updatedText.index(dotIndex, offsetBy: decimals)
+                updatedText = String(updatedText[updatedText.startIndex...endIndex])
+                text = updatedText
+                return false
+            }
+        }
+        
+        return true
+    }
+}
