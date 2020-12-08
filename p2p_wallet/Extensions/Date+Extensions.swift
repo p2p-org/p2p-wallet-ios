52ac1b3c0e30d14594f0c0952cbc39506bc23fa3
Chung Tran <bigearsenal@gmail.com>
2020-12-08 10:29:56 +0700

    add Date extension

---
+++ p2p_wallet/Extensions/Date+Extensions.swift
@@ -0,0 +1,17 @@
+//
+//  Date+Extensions.swift
+//  p2p_wallet
+//
+//  Created by Chung Tran on 08/12/2020.
+//
+
+import Foundation
+
+extension Date {
+    func string(withFormat format: String) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale.current
+        dateFormatter.dateFormat = format
+        return dateFormatter.string(from: self)
+    }
+}
