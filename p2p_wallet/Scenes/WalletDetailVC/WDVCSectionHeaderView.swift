d68da96ded725d4c18f79add78c601f23fba9054
Chung Tran <bigearsenal@gmail.com>
2020-12-11 16:33:03 +0700

    add Charts

--- p2p_wallet/Scenes/WalletDetailVC/WDVCSectionHeaderView.swift
+++ p2p_wallet/Scenes/WalletDetailVC/WDVCSectionHeaderView.swift
@@ -6,17 +6,51 @@
 //
 
 import Foundation
+import Charts
 
 class WDVCSectionHeaderView: SectionHeaderView {
     lazy var amountLabel = UILabel(text: "$120,00", textSize: 25, weight: .semibold, textColor: .textBlack, textAlignment: .center)
     lazy var changeLabel = UILabel(text: "+ 0,16 US$ (0,01%) 24 hrs", textSize: 15, textColor: .secondary, textAlignment: .center)
+    lazy var lineChartView: LineChartView = {
+        let chartView = LineChartView(height: 257)
+        chartView.chartDescription?.enabled = false
+        chartView.leftAxis.drawAxisLineEnabled = false
+        chartView.leftAxis.drawLabelsEnabled = false
+        chartView.rightAxis.drawAxisLineEnabled = false
+        chartView.rightAxis.drawLabelsEnabled = false
+        chartView.xAxis.enabled = false
+        return chartView
+    }()
     override func commonInit() {
         super.commonInit()
+        stackView.alignment = .fill
         stackView.insertArrangedSubview(amountLabel, at: 0)
         stackView.insertArrangedSubview(changeLabel, at: 1)
+        stackView.insertArrangedSubview(lineChartView, at: 2)
         
-        // TODO: - GraphView
-//        stackView.insertArrangedSubview(UIImageView(width: 374, height: 256, image: .graphDetailDemo), at: 2)
+        let values = (0..<10).map { (i) -> ChartDataEntry in
+            let val = Double(arc4random_uniform(10) + 3)
+            return ChartDataEntry(x: Double(i), y: val)
+        }
+        
+        let set1 = LineChartDataSet(entries: values)
+        let gradientColors = [UIColor.clear.cgColor,
+                              UIColor.secondary.cgColor]
+        let gradient = CGGradient(colorsSpace: nil, colors: gradientColors as CFArray, locations: nil)!
+        
+        set1.fillAlpha = 1
+        set1.fill = Fill(linearGradient: gradient, angle: 90)
+        set1.drawFilledEnabled = true
+        set1.drawCirclesEnabled = false
+        set1.drawIconsEnabled = false
+        set1.drawValuesEnabled = false
+        
+        set1.drawCirclesEnabled = false
+        set1.lineWidth = 0
+        set1.circleRadius = 0
+        
+        let data = LineChartData(dataSet: set1)
+        lineChartView.data = data
     }
     
     func setUp(wallet: Wallet) {
