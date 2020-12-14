ab022d2c77df8abb90eb7d08e664d445ead0d300
Chung Tran <bigearsenal@gmail.com>
2020-12-14 10:26:47 +0700

    completed adding graph

--- p2p_wallet/Scenes/WalletDetailVC/WDVCSectionHeaderView.swift
+++ p2p_wallet/Scenes/WalletDetailVC/WDVCSectionHeaderView.swift
@@ -11,8 +11,8 @@ import Charts
 class WDVCSectionHeaderView: SectionHeaderView {
     lazy var amountLabel = UILabel(text: "$120,00", textSize: 25, weight: .semibold, textColor: .textBlack, textAlignment: .center)
     lazy var changeLabel = UILabel(text: "+ 0,16 US$ (0,01%) 24 hrs", textSize: 15, textColor: .secondary, textAlignment: .center)
-    lazy var lineChartView: LineChartView = {
-        let chartView = LineChartView(height: 257)
+    lazy var lineChartView: ChartView = {
+        let chartView = ChartView(height: 257)
         chartView.chartDescription?.enabled = false
         chartView.leftAxis.drawAxisLineEnabled = false
         chartView.leftAxis.drawLabelsEnabled = false
@@ -22,6 +22,8 @@ class WDVCSectionHeaderView: SectionHeaderView {
         chartView.rightAxis.gridLineWidth = 0
         chartView.xAxis.enabled = false
         chartView.legend.enabled = false
+        chartView.pinchZoomEnabled = false
+        chartView.doubleTapToZoomEnabled = false
         
         // marker
         var marker = createMarker()
@@ -43,33 +45,6 @@ class WDVCSectionHeaderView: SectionHeaderView {
         changeLabel.text = "\(wallet.price?.change24h?.value.toString(showPlus: true) ?? "") US$ (\((wallet.price?.change24h?.percentage * 100).toString(maximumFractionDigits: 2, showPlus: true)) %) 24 hrs"
     }
     
-    func setUp(prices: [PriceRecord]) {
-        var x = 0
-        let values = prices.map { price -> ChartDataEntry in
-            x += 1
-            return ChartDataEntry(x: Double(x), y: price.close)
-        }
-        
-        let set1 = LineChartDataSet(entries: values)
-        let gradientColors = [UIColor.clear.cgColor,
-                              UIColor.secondary.cgColor]
-        let gradient = CGGradient(colorsSpace: nil, colors: gradientColors as CFArray, locations: nil)!
-        
-        set1.fillAlpha = 1
-        set1.fill = Fill(linearGradient: gradient, angle: 90)
-        set1.drawFilledEnabled = true
-        set1.drawCirclesEnabled = false
-        set1.drawIconsEnabled = false
-        
-        set1.drawCirclesEnabled = false
-        set1.lineWidth = 0
-        set1.circleRadius = 0
-        
-        let data = LineChartData(dataSet: set1)
-        data.setDrawValues(false)
-        lineChartView.data = data
-    }
-    
     private func createMarker() -> ChartMarker {
         ChartMarker(
             color: .textBlack,
@@ -77,5 +52,41 @@ class WDVCSectionHeaderView: SectionHeaderView {
             textColor: .textWhite,
             insets: UIEdgeInsets(top: 8, left: 8, bottom: 20, right: 8)
         )
+    }
+}
+
+extension WDVCSectionHeaderView {
+    class ChartView: LineChartView, LazyView {
+        func handleDataLoaded(_ prices: [PriceRecord]) {
+            var x = 0
+            let values = prices.map { price -> ChartDataEntry in
+                x += 1
+                return ChartDataEntry(x: Double(x), y: price.close)
+            }
+            
+            let set1 = LineChartDataSet(entries: values)
+            let gradientColors = [UIColor.clear.cgColor,
+                                  UIColor.secondary.cgColor]
+            let gradient = CGGradient(colorsSpace: nil, colors: gradientColors as CFArray, locations: nil)!
+            
+            set1.fillAlpha = 1
+            set1.fill = Fill(linearGradient: gradient, angle: 90)
+            set1.drawFilledEnabled = true
+            set1.drawCirclesEnabled = false
+            set1.drawIconsEnabled = false
+            
+            set1.drawCirclesEnabled = false
+            set1.lineWidth = 0
+            set1.circleRadius = 0
+            
+            let data = LineChartData(dataSet: set1)
+            data.setDrawValues(false)
+            self.data = data
+        }
+        
+        func handleError(_ error: Error) {
+            // TODO: Error
+            
+        }
     }
 }
