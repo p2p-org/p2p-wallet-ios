16e109867ddb2a77fade3b4853367630b77540b5
Chung Tran <bigearsenal@gmail.com>
2020-12-16 09:51:44 +0700

    add some scenes

---
+++ p2p_wallet/Scenes/ProfileVC/SelectNetworkVC.swift
@@ -0,0 +1,18 @@
+//
+//  SelectNetworkVC.swift
+//  p2p_wallet
+//
+//  Created by Chung Tran on 15/12/2020.
+//
+
+import Foundation
+
+class SelectNetworkVC: ProfileVCBase {
+    override func setUp() {
+        title = L10n.network
+        super.setUp()
+        stackView.addArrangedSubviews([
+            
+        ])
+    }
+}
