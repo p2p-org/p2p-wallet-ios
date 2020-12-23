8be87f7e9112cd387fef77f09be88cc38c34287a
Chung Tran <bigearsenal@gmail.com>
2020-12-23 10:47:23 +0700

    clean MainVC

--- p2p_wallet/Scenes/MainVC/MainVC+UICollectionReusableViews.swift
+++ p2p_wallet/Scenes/MainVC/MainVC+UICollectionReusableViews.swift
@@ -9,12 +9,11 @@ import Foundation
 import Action
 
 extension MainVC {
-    class FirstSectionHeaderView: SectionHeaderView {
-        lazy var avatarImageView = UIImageView(width: 30, height: 30, backgroundColor: .c4c4c4, cornerRadius: 15)
-            .onTap(self, action: #selector(avatarImageViewDidTouch))
-        lazy var activeStatusView = UIView(width: 8, height: 8, backgroundColor: .red, cornerRadius: 4)
-            .onTap(self, action: #selector(avatarImageViewDidTouch))
-        var openProfileAction: CocoaAction?
+    class FirstSectionHeaderView: SectionHeaderView, LoadableView {
+        lazy var equityValueLabel = UILabel(text: " ", textSize: 21, weight: .bold, textColor: .white)
+        lazy var changeLabel = UILabel(text: " ", textSize: 13, textColor: .white)
+        
+        var loadingViews: [UIView] {[equityValueLabel, changeLabel]}
         
         override func commonInit() {
             super.commonInit()
@@ -22,20 +21,52 @@ extension MainVC {
             stackView.arrangedSubviews.forEach {$0.removeFromSuperview()}
             
             // add header
-            let headerView = UIStackView(axis: .horizontal, spacing: 10, alignment: .center, distribution: .fill, arrangedSubviews: [
-                avatarImageView,
-                .spacer
+            stackView.addArrangedSubviews([
+                UIView.row([
+                    .col([
+                        UILabel(text: L10n.totalBalance, textSize: 15, textColor: .white),
+                        equityValueLabel,
+                        changeLabel
+                    ]),
+                    UIImageView(width: 75, height: 75, image: .totalBalanceGraph)
+                ])
+                    .with(spacing: 16, alignment: .center, distribution: .fill)
+                    .padding(.init(x: 16, y: 14), backgroundColor: .h2b2b2b, cornerRadius: 12)
+                    .padding(.init(x: .defaultPadding, y: 0))
             ])
             
-            headerView.addSubview(activeStatusView)
-            activeStatusView.autoPinEdge(.top, to: .top, of: avatarImageView)
-            activeStatusView.autoPinEdge(.trailing, to: .trailing, of: avatarImageView)
-            
-            stackView.addArrangedSubview(headerView.padding(.init(x: 20, y: 0)))
+            stackView.constraintToSuperviewWithAttribute(.top)?.constant = 30
+            stackView.constraintToSuperviewWithAttribute(.bottom)?.constant = -30
         }
         
-        @objc func avatarImageViewDidTouch() {
-            openProfileAction?.execute()
+        func setUp(with state: FetcherState<[Wallet]>) {
+            switch state {
+            case .initializing:
+                equityValueLabel.text = " "
+                changeLabel.text = " "
+                hideLoading()
+            case .loading:
+                equityValueLabel.text = L10n.loading + "..."
+                changeLabel.text = L10n.loading + "..."
+                showLoading()
+            case .loaded(let wallets):
+                let equityValue = wallets.reduce(0) { $0 + $1.amountInUSD }
+                equityValueLabel.text = "$ \(equityValue.toString(maximumFractionDigits: 2))"
+                let changeValue = PricesManager.shared.solPrice?.change24h?.percentage * 100
+                var color = UIColor.attentionGreen
+                if changeValue < 0 {
+                    color = .red
+                }
+                changeLabel.attributedText = NSMutableAttributedString()
+                    .text("\(changeValue.toString(maximumFractionDigits: 2, showPlus: true))%", size: 13, color: color)
+                    .text(" \(L10n.forTheLast24Hours)", size: 13, color: .white)
+                hideLoading()
+            case .error(let error):
+                debugPrint(error)
+                equityValueLabel.text = L10n.error.uppercaseFirst
+                changeLabel.text = L10n.error.uppercaseFirst
+                hideLoading()
+            }
         }
     }
     
@@ -60,79 +91,6 @@ extension MainVC {
         
         @objc func buttonDidTouch() {
             showProductsAction?.execute()
-        }
-    }
-    
-    class FirstSectionBackgroundView: SectionBackgroundView {
-        override func commonInit() {
-            super.commonInit()
-            backgroundColor = .h1b1b1b
-        }
-    }
-    
-    class SecondSectionBackgroundView: SectionBackgroundView {
-        lazy var backgroundView = UIView(backgroundColor: .white)
-        
-        override func commonInit() {
-            super.commonInit()
-            backgroundColor = .h1b1b1b
-            
-            addSubview(backgroundView)
-            backgroundView.autoPinEdgesToSuperviewEdges()
-        }
-        
-        override func layoutSubviews() {
-            super.layoutSubviews()
-            backgroundView.roundCorners([.topLeft, .topRight], radius: 20)
-        }
-    }
-    
-    class SecondSectionHeaderView: SectionHeaderView {
-        var receiveAction: CocoaAction?
-        var sendAction: CocoaAction?
-        var exchangeAction: CocoaAction?
-        
-        override func commonInit() {
-            super.commonInit()
-            stackView.insertArrangedSubviews([
-                UILabel(text: L10n.payments, textSize: 17, weight: .semibold)
-                    .padding(.init(x: 20, y: 0)),
-                UIView.row([
-                    .spacer,
-//                    createButton(image: .walletAdd, title: L10n.buy),
-                    createButton(image: .walletReceive, title: L10n.receive)
-                        .onTap(self, action: #selector(buttonReceiveDidTouch)),
-                    createButton(image: .walletSend, title: L10n.send)
-                        .onTap(self, action: #selector(buttonSendDidTouch)),
-                    createButton(image: .walletSwap, title: L10n.exchange)
-                        .onTap(self, action: #selector(buttonExchangeDidTouch)),
-                    .spacer
-                ])
-                    .padding(.init(x: 20, y: 0))
-            ], at: 0, withCustomSpacings: [20, 30])
-        }
-        
-        private func createButton(image: UIImage, title: String) -> UIStackView {
-            let button = UIButton(width: 56, height: 56, backgroundColor: .f4f4f4, cornerRadius: 12, label: title, contentInsets: .init(all: 16))
-            button.setImage(image, for: .normal)
-            button.isUserInteractionEnabled = false
-            button.tintColor = .textBlack
-            return UIStackView(axis: .vertical, spacing: 8, alignment: .center, distribution: .fill, arrangedSubviews: [
-                button,
-                UILabel(text: title, textSize: 12)
-            ])
-        }
-        
-        @objc func buttonReceiveDidTouch() {
-            receiveAction?.execute()
-        }
-        
-        @objc func buttonSendDidTouch() {
-            sendAction?.execute()
-        }
-        
-        @objc func buttonExchangeDidTouch() {
-            exchangeAction?.execute()
         }
     }
 }
