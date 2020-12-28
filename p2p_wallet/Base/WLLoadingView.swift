72c9c8ba05ffc3dbb3fce2d00bc2a64b727e9aff
Chung Tran <bigearsenal@gmail.com>
2020-12-28 09:53:56 +0700

    add WLLoadingView

---
+++ p2p_wallet/Base/WLLoadingView.swift
@@ -0,0 +1,51 @@
+//
+//  WLLoadingView.swift
+//  p2p_wallet
+//
+//  Created by Chung Tran on 28/12/2020.
+//
+
+import Foundation
+
+open class WLLoadingView: BEView {
+    open override var tintColor: UIColor? {
+        didSet { filledView.backgroundColor = tintColor }
+    }
+    
+    lazy var filledView = UIView(forAutoLayout: ())
+    
+    var filledViewTrailingConstraint: NSLayoutConstraint!
+    
+    open override func commonInit() {
+        super.commonInit()
+        filledView.backgroundColor = backgroundColor
+        addSubview(filledView)
+        filledView.autoPinEdgesToSuperviewEdges(with: .zero, excludingEdge: .trailing)
+        filledViewTrailingConstraint = filledView.widthAnchor.constraint(equalTo: widthAnchor)
+        
+        filledViewTrailingConstraint.isActive = true
+    }
+    
+    func setUp(state: FetcherState<Bool>) {
+        filledView.layer.removeAllAnimations()
+        switch state {
+        case .loading:
+            filledViewTrailingConstraint.constant = -bounds.width
+            layoutIfNeeded()
+            animate()
+            isUserInteractionEnabled = false
+        case .initializing, .loaded, .error:
+            filledViewTrailingConstraint.constant = 0
+            layoutIfNeeded()
+            isUserInteractionEnabled = true
+        }
+    }
+    
+    private func animate() {
+        
+        UIView.animate(withDuration: TimeInterval(bounds.width / 100), delay: 0, options: .repeat) {
+            self.filledViewTrailingConstraint.constant = 0
+            self.layoutIfNeeded()
+        }
+    }
+}
