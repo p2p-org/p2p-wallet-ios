499554808986515468dfcfce72a607563ed32911
Chung Tran <bigearsenal@gmail.com>
2020-12-30 09:53:53 +0700

    fix ReceiveTokenVC

--- p2p_wallet/Extensions/Asset+Generated.swift
+++ p2p_wallet/Extensions/Asset+Generated.swift
@@ -19,6 +19,7 @@ extension UIColor {
     static let background2 = UIColor(named: "background-2", in: bundle, compatibleWith: nil)!
     static let background3 = UIColor(named: "background-3", in: bundle, compatibleWith: nil)!
     static let background4 = UIColor(named: "background-4", in: bundle, compatibleWith: nil)!
+    static let background5 = UIColor(named: "background-5", in: bundle, compatibleWith: nil)!
     static let background = UIColor(named: "background", in: bundle, compatibleWith: nil)!
     static let barChart1 = UIColor(named: "bar-chart-1", in: bundle, compatibleWith: nil)!
     static let barChart2 = UIColor(named: "bar-chart-2", in: bundle, compatibleWith: nil)!
