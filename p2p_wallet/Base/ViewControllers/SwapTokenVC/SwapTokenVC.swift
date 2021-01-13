864b5ab2c8fec30de95d6b52ca3cfb7961e3ca8c
Chung Tran <bigearsenal@gmail.com>
2021-01-13 15:44:06 +0700

    new design for swap

--- p2p_wallet/Base/ViewControllers/SwapTokenVC/SwapTokenVC.swift
+++ p2p_wallet/Base/ViewControllers/SwapTokenVC/SwapTokenVC.swift
@@ -38,7 +38,7 @@ class SwapTokenVC: WLModalWrapperVC {
 
 class _SwapTokenVC: BaseVStackVC {
     override var preferredNavigationBarStype: BEViewController.NavigationBarStyle { .normal(backgroundColor: .vcBackground) }
-    override var padding: UIEdgeInsets { UIEdgeInsets(top: 44, left: 16, bottom: 0, right: 16) }
+    override var padding: UIEdgeInsets { UIEdgeInsets(top: .defaultPadding, left: 16, bottom: 0, right: 16) }
     
     var wallets: [Wallet]
     var fromWallet = BehaviorRelay<Wallet?>(value: nil)
@@ -47,10 +47,10 @@ class _SwapTokenVC: BaseVStackVC {
     lazy var fromWalletView = SwapTokenItemView(forAutoLayout: ())
     lazy var toWalletView = SwapTokenItemView(forAutoLayout: ())
     
-    lazy var reverseButton = UIImageView(width: 44, height: 44, cornerRadius: 22, image: .reverseButton)
+    lazy var reverseButton = UIImageView(width: 44, height: 44, cornerRadius: 12, image: .reverseButton)
         .onTap(self, action: #selector(buttonReverseDidTouch))
     
-    lazy var swapButton = WLButton.stepButton(type: .black, label: L10n.swapNow)
+    lazy var swapButton = WLButton.stepButton(type: .blue, label: L10n.swapNow)
         .onTap(self, action: #selector(buttonSwapDidTouch))
     
     init(wallets: [Wallet]) {
@@ -67,43 +67,42 @@ class _SwapTokenVC: BaseVStackVC {
         view.backgroundColor = .vcBackground
         title = L10n.swap
         
-        scrollView.contentView.backgroundColor = .textWhite
-        scrollView.contentView.layer.cornerRadius = 16
-        scrollView.contentView.layer.masksToBounds = true
-        
-        stackView.constraintToSuperviewWithAttribute(.top)?.constant = 20
+        stackView.spacing = 30
+        stackView.constraintToSuperviewWithAttribute(.leading)?.constant = 16
+        stackView.constraintToSuperviewWithAttribute(.trailing)?.constant = -16
         stackView.constraintToSuperviewWithAttribute(.bottom)?.constant = -20
         
         let reverseView: UIView = {
             let view = UIView(forAutoLayout: ())
-            let separator = UIView.separator(height: 2, color: .vcBackground)
+            let separator = UIView.separator(height: 1, color: .separator)
             view.addSubview(separator)
             separator.autoPinEdge(toSuperviewEdge: .leading)
             separator.autoPinEdge(toSuperviewEdge: .trailing)
             separator.autoAlignAxis(toSuperviewAxis: .horizontal)
             
             view.addSubview(reverseButton)
-            reverseButton.autoPinEdgesToSuperviewEdges(with: UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 20), excludingEdge: .leading)
+            reverseButton.autoPinEdgesToSuperviewEdges(with: .zero, excludingEdge: .leading)
             
             return view
         }()
         
-        stackView.addArrangedSubviews(
-            [
-                UILabel(text: L10n.from, weight: .medium).padding(UIEdgeInsets(x: 16, y: 0)),
-                fromWalletView.padding(UIEdgeInsets(x: 16, y: 0)),
-                reverseView,
-                UILabel(text: L10n.to, weight: .medium).padding(UIEdgeInsets(x: 16, y: 0)),
-                toWalletView.padding(UIEdgeInsets(x: 16, y: 0)),
-                swapButton.padding(UIEdgeInsets(x: 16, y: 0))
-            ]
-        )
-        
-        stackView.setCustomSpacing(35, after: toWalletView.wrapper!)
+        stackView.addArrangedSubviews([
+            UIStackView(axis: .horizontal, spacing: 10, alignment: .fill, distribution: .fill, arrangedSubviews: [
+                UILabel(text: L10n.from),
+                UILabel(text: "Available", textColor: .h5887ff)
+            ]),
+            fromWalletView,
+            BEStackViewSpacing(8),
+            reverseView,
+            BEStackViewSpacing(8),
+            UILabel(text: L10n.to),
+            toWalletView,
+            UIView.separator(height: 1, color: .separator),
+            swapButton
+        ])
         
         fromWalletView.amountTextField.delegate = self
         toWalletView.amountTextField.isUserInteractionEnabled = false
-        
     }
     
     override func bind() {
