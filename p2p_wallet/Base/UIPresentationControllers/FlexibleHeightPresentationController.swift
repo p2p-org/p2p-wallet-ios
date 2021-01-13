2bf36df00dca93f160cff495a636a76750029f1a
Chung Tran <bigearsenal@gmail.com>
2021-01-13 14:58:31 +0700

    some fixes

--- p2p_wallet/Base/UIPresentationControllers/FlexibleHeightPresentationController.swift
+++ p2p_wallet/Base/UIPresentationControllers/FlexibleHeightPresentationController.swift
@@ -7,24 +7,31 @@
 //
 
 import Foundation
-class FlexibleHeightPresentationController: DimmingPresentationController {
+
+class FlexibleHeightPresentationController: DimmingPresentationController, ResizablePresentationController {
     // MARK: - Nested type
     enum Position {
         case bottom, center
     }
     
+    // MARK: - Properties
     let position: Position
+    var presentedViewFixedFrame: CGRect?
+    var presentedViewCurrentTop: CGFloat?
+    
+    // MARK: - Initializer
     init(position: Position, presentedViewController: UIViewController, presenting presentingViewController: UIViewController?) {
         self.position = position
         super.init(presentedViewController: presentedViewController, presenting: presentingViewController)
     }
     
+    // MARK: - Methods
     override var frameOfPresentedViewInContainerView: CGRect {
         guard var frame = containerView?.bounds else { return .zero }
         
         let targetWidth = frame.width
         
-        let targetHeight = calculateFittingHeightOfPresentedView(targetWidth: targetWidth)
+        var targetHeight = calculateFittingHeightOfPresentedView(targetWidth: targetWidth)
         
         if targetHeight > frame.size.height {
             return frame
@@ -32,7 +39,16 @@ class FlexibleHeightPresentationController: DimmingPresentationController {
         
         switch position {
         case .bottom:
-            frame.origin.y += frame.size.height - targetHeight
+            // swipe down
+            if let currentTop = presentedViewCurrentTop,
+               let fixedFrame = presentedViewFixedFrame,
+               currentTop > fixedFrame.origin.y
+            {
+                frame.origin.y = currentTop
+                targetHeight = fixedFrame.height
+            } else {
+                frame.origin.y += frame.size.height - targetHeight
+            }
         case .center:
             frame.origin.y += (frame.size.height - targetHeight) / 2
         }
@@ -43,6 +59,49 @@ class FlexibleHeightPresentationController: DimmingPresentationController {
     }
     
     func calculateFittingHeightOfPresentedView(targetWidth: CGFloat) -> CGFloat {
-        presentedView!.fittingHeight(targetWidth: targetWidth)
+        if let height = presentedViewFixedFrame?.height {return height}
+        return presentedView!.fittingHeight(targetWidth: targetWidth)
+    }
+    
+    func presentedViewDidSwipe(gestureRecognizer: UIPanGestureRecognizer) {
+        guard let view = gestureRecognizer.view else {return}
+        
+        // Get the changes in the X and Y directions relative to
+        // the superview's coordinate space.
+        let translation = gestureRecognizer.translation(in: containerView)
+        
+        switch gestureRecognizer.state {
+        case .began:
+            // save original state
+            presentedViewFixedFrame = view.frame
+            
+        case .changed:
+            // on gesture changed
+            presentedViewCurrentTop = presentedViewFixedFrame!.origin.y + translation.y
+            animateResizing = false
+            (presentedViewController as? BaseVC)?.forceResizeModal()
+        case .ended:
+            // calculate distances
+            let distanceToBottom = abs(containerView!.bounds.height - presentedViewCurrentTop!)
+            let distanceToTop = abs(presentedViewFixedFrame!.origin.y - presentedViewCurrentTop!)
+            
+            // Dismiss when presentedView is close to bottom
+            if distanceToBottom < distanceToTop {
+                presentedViewController
+                    .dismiss(animated: true, completion: nil)
+                return
+            }
+            
+            presentedViewCurrentTop = nil
+            animateResizing = true
+            (presentedViewController as? BaseVC)?.forceResizeModal()
+            
+            presentedViewFixedFrame = nil
+        default:
+            presentedViewCurrentTop = nil
+            animateResizing = true
+            (presentedViewController as? BaseVC)?.forceResizeModal()
+            presentedViewFixedFrame = nil
+        }
     }
 }
