5127f3da639b4aa84561b808bc0cc0f543bdd154
Chung Tran <bigearsenal@gmail.com>
2021-01-13 13:35:58 +0700

    refactor ResizablePresentationController

--- p2p_wallet/Base/UIPresentationControllers/ResizablePresentationController.swift
+++ p2p_wallet/Base/UIPresentationControllers/ResizablePresentationController.swift
@@ -7,81 +7,6 @@
 
 import Foundation
 
-class WLResizableModalVC: WLModalVC, UIViewControllerTransitioningDelegate {
-    override init() {
-        super.init()
-        modalPresentationStyle = .custom
-        transitioningDelegate = self
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func setUp() {
-        super.setUp()
-        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(viewDidSwipe(_:)))
-        view.addGestureRecognizer(panGesture)
-        view.isUserInteractionEnabled = true
-    }
-    
-    func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
-        ResizablePresentationController(presentedViewController: presented, presenting: presenting)
-    }
-    
-    var originalTop: CGFloat?
-    @IBAction func viewDidSwipe(_ gestureRecognizer: UIPanGestureRecognizer) {
-        guard gestureRecognizer.view != nil else {return}
-        
-        let presentationController = self.presentationController as! ResizablePresentationController
-        
-        // Get the changes in the X and Y directions relative to
-        // the superview's coordinate space.
-        let translation = gestureRecognizer.translation(in: presentationController.containerView)
-        
-        switch gestureRecognizer.state {
-        case .began:
-            // save original state
-            originalTop = view.frame.origin.y
-        case .changed:
-            // on gesture changed
-            presentationController.currentTop = originalTop! + translation.y
-            presentationController.animateResizing = false
-            forceResizeModal()
-        case .ended:
-            // on gesture ended
-            originalTop = nil
-            
-            // calculate distances
-            let distanceToTop = abs(presentationController.minTop - presentationController.currentTop!)
-            let distanceToCenter = abs(presentationController.containerView!.frame.size.height / 2 - presentationController.currentTop!)
-            let distanceToBottom = abs(presentationController.containerView!.bounds.height - presentationController.currentTop!)
-            
-            // Dismiss when presentedView is close to bottom
-            if distanceToBottom < distanceToCenter {
-                dismiss(animated: true, completion: nil)
-                return
-            }
-            
-            // Define coverType
-            if distanceToTop > distanceToCenter{
-                presentationController.coverType = .haft
-            } else {
-                presentationController.coverType = .full
-            }
-            
-            presentationController.currentTop = nil
-            presentationController.animateResizing = true
-            forceResizeModal()
-        default:
-            originalTop = nil
-            presentationController.currentTop = nil
-            presentationController.animateResizing = true
-            forceResizeModal()
-        }
-    }
-}
-
 class ResizablePresentationController: DimmingPresentationController {
     // MARK: - Nested types
     enum CoverType {
@@ -89,7 +14,6 @@ class ResizablePresentationController: DimmingPresentationController {
     }
     
     // MARK: - Properties
-    var resizable = true
     var coverType = CoverType.haft
     var padding: UIEdgeInsets = .zero {
         didSet {
@@ -124,5 +48,55 @@ class ResizablePresentationController: DimmingPresentationController {
         
         frame.size.height = frame.height - frame.origin.y
         return frame
+    }
+    
+    func presentedViewDidSwipe(_ gestureRecognizer: UIPanGestureRecognizer, originalTop: inout CGFloat?) {
+        guard let view = gestureRecognizer.view else {return}
+        
+        // Get the changes in the X and Y directions relative to
+        // the superview's coordinate space.
+        let translation = gestureRecognizer.translation(in: containerView)
+        
+        switch gestureRecognizer.state {
+        case .began:
+            // save original state
+            originalTop = view.frame.origin.y
+        case .changed:
+            // on gesture changed
+            currentTop = originalTop! + translation.y
+            animateResizing = false
+            (presentedViewController as? BaseVC)?.forceResizeModal()
+        case .ended:
+            // on gesture ended
+            originalTop = nil
+            
+            // calculate distances
+            let distanceToTop = abs(minTop - currentTop!)
+            let distanceToCenter = abs(containerView!.frame.size.height / 2 - currentTop!)
+            let distanceToBottom = abs(containerView!.bounds.height - currentTop!)
+            
+            // Dismiss when presentedView is close to bottom
+            if distanceToBottom < distanceToCenter {
+                presentedViewController
+                    .dismiss(animated: true, completion: nil)
+                return
+            }
+            
+            // Define coverType
+            if distanceToTop > distanceToCenter{
+                coverType = .haft
+            } else {
+                coverType = .full
+            }
+            
+            currentTop = nil
+            animateResizing = true
+            (presentedViewController as? BaseVC)?.forceResizeModal()
+        default:
+            originalTop = nil
+            currentTop = nil
+            animateResizing = true
+            (presentedViewController as? BaseVC)?.forceResizeModal()
+        }
     }
 }
