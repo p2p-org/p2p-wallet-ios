20572eaa5de0261cb99fa190a2392fcf399f4812
Chung Tran <bigearsenal@gmail.com>
2021-01-13 14:58:31 +0700

    some fixes

--- p2p_wallet/Base/ViewControllers/WLResizableModalVC.swift
+++ p2p_wallet/Base/ViewControllers/WLResizableModalVC.swift
@@ -18,20 +18,7 @@ class WLResizableModalVC: WLModalVC, UIViewControllerTransitioningDelegate {
         fatalError("init(coder:) has not been implemented")
     }
     
-    override func setUp() {
-        super.setUp()
-        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(viewDidSwipe(_:)))
-        view.addGestureRecognizer(panGesture)
-        view.isUserInteractionEnabled = true
-    }
-    
     func presentationController(forPresented presented: UIViewController, presenting: UIViewController?, source: UIViewController) -> UIPresentationController? {
-        ResizablePresentationController(presentedViewController: presented, presenting: presenting)
-    }
-    
-    var originalTop: CGFloat?
-    @objc func viewDidSwipe(_ gestureRecognizer: UIPanGestureRecognizer) {
-        let presentationController = self.presentationController as! ResizablePresentationController
-        presentationController.presentedViewDidSwipe(gestureRecognizer, originalTop: &originalTop)
+        ExpandablePresentationController(presentedViewController: presented, presenting: presenting)
     }
 }
