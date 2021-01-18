49c3fb7e779b0397d10f428dba48a5049ae2a812
Chung Tran <bigearsenal@gmail.com>
2021-01-18 11:44:43 +0700

    some fixes

--- p2p_wallet/AppDelegate.swift
+++ p2p_wallet/AppDelegate.swift
@@ -24,6 +24,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     static var shared: AppDelegate {
         UIApplication.shared.delegate as! AppDelegate
     }
+    
+    func changeThemeTo(_ style: UIUserInterfaceStyle) {
+        Defaults.appearance = style
+        if #available(iOS 13.0, *) {
+            window?.overrideUserInterfaceStyle = style
+        }
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         UserDefaults.standard.set(false, forKey: "_UIConstraintBasedLayoutLogUnsatisfiable")
