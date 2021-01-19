5b8420dfd5cd0b4cbf9c39f2ca507ed4a62b5192
Chung Tran <bigearsenal@gmail.com>
2021-01-19 10:01:07 +0700

    Fix IntroVC type

--- p2p_wallet/Scenes/Welcome/WelcomeBackVC.swift
+++ p2p_wallet/Scenes/Welcome/WelcomeBackVC.swift
@@ -6,9 +6,10 @@
 //
 
 import Foundation
+import SwiftUI
 
-class WelcomeBackVC: IntroVCWithButtons {
-    lazy var goToWalletButton = WLButton.stepButton(type: .black, label: L10n.goToWallet)
+class WelcomeBackVC: WLIntroVC {
+    lazy var goToWalletButton = WLButton.stepButton(type: .blue, label: L10n.goToWallet)
         .onTap(self, action: #selector(buttonGoToWalletDidTouch))
     
     let phrases: [String]
@@ -23,10 +24,25 @@ class WelcomeBackVC: IntroVCWithButtons {
     
     override func setUp() {
         super.setUp()
-        descriptionLabel.isHidden = true
-        titleLabel.text = L10n.welcomeBackToTheWowletSpace
         
-        buttonStackView.addArrangedSubview(goToWalletButton)
+        // add center image
+        var i = 3
+        let logoImageView = UIImageView(width: 200, height: 200, image: .welcomeBack)
+        let spacer2 = UIView.spacer
+        stackView.insertArrangedSubviewsWithCustomSpacing([
+            logoImageView.centeredHorizontallyView,
+            spacer2
+        ], at: &i)
+        
+        spacer2.heightAnchor.constraint(equalTo: spacer1.heightAnchor)
+            .isActive = true
+        
+        titleLabel.text = L10n.welcomeBack
+        
+        buttonsStackView.addArrangedSubviews([
+            goToWalletButton,
+            UIView(height: 56)
+        ])
     }
     
     @objc func buttonGoToWalletDidTouch() {
@@ -45,6 +61,18 @@ class WelcomeBackVC: IntroVCWithButtons {
                     self.showError(error)
                 }
             }
+        }
+    }
+}
+
+@available(iOS 13, *)
+struct WelcomeBackVC_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            UIViewControllerPreview {
+                WelcomeBackVC(phrases: [])
+            }
+            .previewDevice("iPhone SE (2nd generation)")
         }
     }
 }
