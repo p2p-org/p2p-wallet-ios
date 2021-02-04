9d67e9a09be9c9d04af013b1cd0658736fb36ead
Chung Tran <bigearsenal@gmail.com>
2021-02-04 12:28:16 +0700

    add apikey

--- p2p_wallet/Services/PriceAPIs/CryptoCompare/CryptoComparePricesFetcher.swift
+++ p2p_wallet/Services/PriceAPIs/CryptoCompare/CryptoComparePricesFetcher.swift
@@ -10,9 +10,10 @@ import RxSwift
 
 class CryptoComparePricesFetcher: PricesFetcher {
     let endpoint = "https://min-api.cryptocompare.com/data"
+    let apikey = "887b4e547fd87338984f7298d0b25dffee49522df82c0b4ed0a301be9ca688c9"
     
     func getCurrentPrices(coins: [String], toFiat fiat: String) -> Single<[String: CurrentPrice?]> {
-        send("/pricemulti?fsyms=\(coins.joined(separator: ","))&tsyms=\(fiat)", decodedTo: [String: [String: Double]].self)
+        send("/pricemulti?api_key=\(apikey)&fsyms=\(coins.joined(separator: ","))&tsyms=\(fiat)", decodedTo: [String: [String: Double]].self)
             .map {dict in
                 var result = [String: CurrentPrice?]()
                 for key in dict.keys {
