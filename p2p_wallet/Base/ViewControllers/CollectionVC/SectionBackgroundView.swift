76c89f0a251970b2799cbbee7339b6085f8b6e19
Chung Tran <bigearsenal@gmail.com>
2021-02-07 00:29:31 +0700

    update lib

--- p2p_wallet/Base/ViewControllers/CollectionVC/SectionBackgroundView.swift
+++ p2p_wallet/Base/ViewControllers/CollectionVC/SectionBackgroundView.swift
@@ -13,6 +13,9 @@ class SectionBackgroundView: UICollectionReusableView {
         commonInit()
     }
     
+    @available(*, unavailable,
+    message: "Loading this view from a nib is unsupported in favor of initializer dependency injection."
+    )
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
