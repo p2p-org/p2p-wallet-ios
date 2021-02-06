4d90cc205a7c30cdc1e0613db6dd6960861f98b1
Chung Tran <bigearsenal@gmail.com>
2021-02-07 00:29:31 +0700

    update lib

--- p2p_wallet/Base/BaseCollectionViewCell.swift
+++ p2p_wallet/Base/BaseCollectionViewCell.swift
@@ -13,6 +13,9 @@ class BaseCollectionViewCell: UICollectionViewCell {
         commonInit()
     }
     
+    @available(*, unavailable,
+    message: "Loading this view from a nib is unsupported in favor of initializer dependency injection."
+    )
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
