ec21d9a7394257adc74d9a52b6a868005935e120
Chung Tran <bigearsenal@gmail.com>
2021-02-07 17:39:45 +0700

    some fixes

--- p2p_wallet/Scenes/AddWallet/Wrapped/_AddNewWalletVC.swift
+++ p2p_wallet/Scenes/AddWallet/Wrapped/_AddNewWalletVC.swift
@@ -23,6 +23,28 @@ class _AddNewWalletVC: WalletsVC {
         collectionView.keyboardDismissMode = .onDrag
     }
     
+    override func bind() {
+        super.bind()
+        let viewModel = self.viewModel as! _AddNewWalletVM
+        
+        viewModel.navigatorSubject
+            .subscribe(onNext: {navigator in
+                switch navigator {
+                case .present(let vc):
+                    self.present(vc, animated: true, completion: nil)
+                case .show(let vc):
+                    self.show(vc, sender: nil)
+                }
+            })
+            .disposed(by: disposeBag)
+        
+        viewModel.clearSearchBarSubject
+            .subscribe(onNext: {
+                (self.parent as? AddNewWalletVC)?.searchBar.clear()
+            })
+            .disposed(by: disposeBag)
+    }
+    
     override var sections: [Section] {
         [
             Section(
@@ -34,6 +56,7 @@ class _AddNewWalletVC: WalletsVC {
     
     override func configureCell(collectionView: UICollectionView, indexPath: IndexPath, item: Wallet) -> UICollectionViewCell {
         let cell = super.configureCell(collectionView: collectionView, indexPath: indexPath, item: item)
+        let viewModel = self.viewModel as! _AddNewWalletVM
         if let cell = cell as? _AddNewWalletCell, let wallet = itemAtIndexPath(indexPath)
         {
             cell.viewInBlockchainExplorerButton.rx.action = CocoaAction {_ in
@@ -41,91 +64,14 @@ class _AddNewWalletVC: WalletsVC {
                 return .just(())
             }
             
-            cell.createWalletAction = createTokenAccountAction(newWallet: wallet)
-            
-            cell.setUp(feeSubject: (viewModel as! _AddNewWalletVM).feeSubject)
+            cell.createWalletAction = viewModel.addNewToken(newWallet: wallet)
+            cell.setUp(feeSubject: viewModel.feeSubject)
         }
         return cell
     }
     
-    func createTokenAccountAction(newWallet: Wallet) -> CocoaAction {
-        CocoaAction {
-            let viewModel = self.viewModel as! _AddNewWalletVM
-            
-            // catching error
-            if viewModel.feeSubject.value > (WalletsVM.ofCurrentUser.solWallet?.amount ?? 0)
-            {
-                viewModel.updateItem(where: {$0.mintAddress == newWallet.mintAddress}, transform: {
-                    var wallet = $0
-                    wallet.isBeingCreated = nil
-                    wallet.creatingError = L10n.insufficientFunds
-                    return wallet
-                })
-                return .just(())
-            }
-            
-            // remove existing error
-            viewModel.updateItem(where: {$0.mintAddress == newWallet.mintAddress}, transform: {
-                var wallet = $0
-                wallet.isBeingCreated = true
-                wallet.creatingError = nil
-                return wallet
-            })
-            
-            // request
-            return SolanaSDK.shared.createTokenAccount(mintAddress: newWallet.mintAddress)
-//            return Single<(String, String)>.just(("", "")).delay(.seconds(5), scheduler: MainScheduler.instance)
-//                .map {_ -> (String, String) in
-//                    throw SolanaSDK.Error.other("example")
-//                }
-                .do(
-                    afterSuccess: { (signature, newPubkey) in
-                        // remove suggestion from the list
-                        self.viewModel.removeItem(where: {$0.mintAddress == newWallet.mintAddress})
-                        
-                        // cancel search if search result is empty
-                        if self.viewModel.searchResult?.isEmpty == true
-                        {
-                            (self.parent as? AddNewWalletVC)?.searchBar.clear()
-                        }
-                        
-                        // process transaction
-                        var newWallet = newWallet
-                        newWallet.pubkey = newPubkey
-                        newWallet.isProcessing = true
-                        let transaction = Transaction(
-                            signatureInfo: .init(signature: signature),
-                            type: .createAccount,
-                            amount: -(viewModel.feeSubject.value ?? 0),
-                            symbol: "SOL",
-                            status: .processing,
-                            newWallet: newWallet
-                        )
-                        TransactionsManager.shared.process(transaction)
-                        
-                        // present wallet
-                        self.present(WalletDetailVC(wallet: newWallet), animated: true, completion: nil)
-                    },
-                    afterError: { (error) in
-                        viewModel.updateItem(where: {$0.mintAddress == newWallet.mintAddress}, transform: {
-                            var wallet = $0
-                            wallet.isBeingCreated = nil
-                            wallet.creatingError = error.localizedDescription
-                            return wallet
-                        })
-                    }
-                )
-                .map {_ in ()}
-                .asObservable()
-        }
-    }
-    
     override func itemDidSelect(_ item: Wallet) {
         parent?.view.endEditing(true)
-        viewModel.updateItem(where: {item.mintAddress == $0.mintAddress}, transform: {
-            var wallet = $0
-            wallet.isExpanded = !(wallet.isExpanded ?? false)
-            return wallet
-        })
+        (viewModel as! _AddNewWalletVM).tokenDidSelect(item)
     }
 }
--- p2p_wallet/Scenes/AddWallet/Wrapped/_AddNewWalletVM.swift
+++ p2p_wallet/Scenes/AddWallet/Wrapped/_AddNewWalletVM.swift
@@ -6,7 +6,9 @@
 //
 
 import Foundation
+import RxSwift
 import LazySubject
+import Action
 
 class _AddNewWalletVM: ListViewModel<Wallet> {
     let feeSubject = LazySubject(
@@ -17,6 +19,9 @@ class _AddNewWalletVM: ListViewModel<Wallet> {
                 return Double($0) * pow(Double(10), -Double(decimals))
             }
     )
+    
+    let navigatorSubject = PublishSubject<Navigator>()
+    let clearSearchBarSubject = PublishSubject<Void>()
     
     override func reload() {
         // get static data
@@ -43,5 +48,83 @@ class _AddNewWalletVM: ListViewModel<Wallet> {
     override func offlineSearchPredicate(item: Wallet, lowercasedQuery query: String) -> Bool {
         item.name.lowercased().contains(query) ||
         item.symbol.lowercased().contains(query)
+    }
+    
+    func tokenDidSelect(_ token: Wallet) {
+        updateItem(where: {token.mintAddress == $0.mintAddress}, transform: {
+            var wallet = $0
+            wallet.isExpanded = !(wallet.isExpanded ?? false)
+            return wallet
+        })
+    }
+    
+    func addNewToken(newWallet: Wallet) -> CocoaAction {
+        CocoaAction {
+            // catching error
+            if self.feeSubject.value > (WalletsVM.ofCurrentUser.solWallet?.amount ?? 0)
+            {
+                self.updateItem(where: {$0.mintAddress == newWallet.mintAddress}, transform: {
+                    var wallet = $0
+                    wallet.isBeingCreated = nil
+                    wallet.creatingError = L10n.insufficientFunds
+                    return wallet
+                })
+                return .just(())
+            }
+            
+            // remove existing error
+            self.updateItem(where: {$0.mintAddress == newWallet.mintAddress}, transform: {
+                var wallet = $0
+                wallet.isBeingCreated = true
+                wallet.creatingError = nil
+                return wallet
+            })
+            
+            // request
+            return SolanaSDK.shared.createTokenAccount(mintAddress: newWallet.mintAddress)
+//            return Single<(String, String)>.just(("", "")).delay(.seconds(5), scheduler: MainScheduler.instance)
+//                .map {_ -> (String, String) in
+//                    throw SolanaSDK.Error.other("example")
+//                }
+                .do(
+                    afterSuccess: { (signature, newPubkey) in
+                        // remove suggestion from the list
+                        self.removeItem(where: {$0.mintAddress == newWallet.mintAddress})
+                        
+                        // cancel search if search result is empty
+                        if self.searchResult?.isEmpty == true
+                        {
+                            self.clearSearchBarSubject.onNext(())
+                        }
+                        
+                        // process transaction
+                        var newWallet = newWallet
+                        newWallet.pubkey = newPubkey
+                        newWallet.isProcessing = true
+                        let transaction = Transaction(
+                            signatureInfo: .init(signature: signature),
+                            type: .createAccount,
+                            amount: -(self.feeSubject.value ?? 0),
+                            symbol: "SOL",
+                            status: .processing,
+                            newWallet: newWallet
+                        )
+                        TransactionsManager.shared.process(transaction)
+                        
+                        // present wallet
+                        self.navigatorSubject.onNext(.present(WalletDetailVC(wallet: newWallet)))
+                    },
+                    afterError: { (error) in
+                        self.updateItem(where: {$0.mintAddress == newWallet.mintAddress}, transform: {
+                            var wallet = $0
+                            wallet.isBeingCreated = nil
+                            wallet.creatingError = error.localizedDescription
+                            return wallet
+                        })
+                    }
+                )
+                .map {_ in ()}
+                .asObservable()
+        }
     }
 }
