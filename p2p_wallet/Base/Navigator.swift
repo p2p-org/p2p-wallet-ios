fb26205c817c79d73c0385006a95ab4dcc5f8769
Chung Tran <bigearsenal@gmail.com>
2021-02-07 17:39:45 +0700

    some fixes

---
+++ p2p_wallet/Base/Navigator.swift
@@ -0,0 +1,13 @@
+//
+//  Navigator.swift
+//  p2p_wallet
+//
+//  Created by Chung Tran on 07/02/2021.
+//
+
+import Foundation
+
+enum Navigator {
+    case present(UIViewController)
+    case show(UIViewController)
+}
