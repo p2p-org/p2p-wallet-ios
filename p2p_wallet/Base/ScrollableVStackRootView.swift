389653fbeceff16b759464227a05e0975c1b1c46
Chung Tran <bigearsenal@gmail.com>
2021-02-09 17:16:37 +0700

    Refactor SendVC

---
+++ p2p_wallet/Base/ScrollableVStackRootView.swift
@@ -0,0 +1,25 @@
+//
+//  ScrollableVStackRootView.swift
+//  p2p_wallet
+//
+//  Created by Chung Tran on 09/02/2021.
+//
+
+import Foundation
+
+class ScrollableVStackRootView: BEView {
+    lazy var scrollView = ContentHuggingScrollView(scrollableAxis: .vertical, contentInset: UIEdgeInsets(top: .defaultPadding, left: .defaultPadding, bottom: 0, right: .defaultPadding))
+    lazy var stackView = UIStackView(axis: .vertical, spacing: 16, alignment: .fill, distribution: .fill)
+    
+    override func commonInit() {
+        super.commonInit()
+        // scroll view for flexible height
+        addSubview(scrollView)
+        scrollView.autoPinEdgesToSuperviewSafeArea(with: .zero, excludingEdge: .bottom)
+        scrollView.autoPinBottomToSuperViewSafeAreaAvoidKeyboard(inset: 16)
+        
+        // stackView
+        scrollView.contentView.addSubview(stackView)
+        stackView.autoPinEdgesToSuperviewEdges()
+    }
+}
