47c019fd80f9c6a3aa85718748280f15f3ad55d9
Chung Tran <bigearsenal@gmail.com>
2021-02-10 12:02:37 +0700

    fix EnterPhrasesVC

--- p2p_wallet/Extensions/UIViewController+Extensions.swift
+++ p2p_wallet/Extensions/UIViewController+Extensions.swift
@@ -116,10 +116,10 @@ extension UIViewController {
     }
     
     // MARK: - Custom modal
-    func presentCustomModal(vc: UIViewController, title: String? = nil, titleImage: UIImage? = nil) {
+    func presentCustomModal(vc: UIViewController, title: String? = nil, titleImageView: UIView? = nil) {
         let vc = WLModalWrapperVC(wrapped: vc)
         vc.title = title
-        vc.titleImage = titleImage
+        vc.titleImageView = titleImageView
         present(vc, animated: true, completion: nil)
     }
 }
