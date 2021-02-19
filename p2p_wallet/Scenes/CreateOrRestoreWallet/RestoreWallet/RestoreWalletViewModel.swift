e481293f38ea4efca5c951f2ad8d6d06a3efde82
Chung Tran <bigearsenal@gmail.com>
2021-02-19 16:00:47 +0700

    some fixes

--- p2p_wallet/Scenes/CreateOrRestoreWallet/RestoreWallet/RestoreWalletViewModel.swift
+++ p2p_wallet/Scenes/CreateOrRestoreWallet/RestoreWallet/RestoreWalletViewModel.swift
@@ -10,33 +10,55 @@ import RxSwift
 import RxCocoa
 
 enum RestoreWalletNavigatableScene {
-//    case detail
+    case enterPhrases
+    case welcomeBack(phrases: [String])
 }
 
-struct RestoreWalletViewModel {
+class RestoreWalletViewModel {
     // MARK: - Constants
     
     // MARK: - Properties
     let bag = DisposeBag()
+    let completion: () -> Void
+    let accountStorage: KeychainAccountStorage
     
     // MARK: - Subjects
     let navigationSubject = PublishSubject<RestoreWalletNavigatableScene>()
-    
-    // MARK: - Input
-//    let textFieldInput = BehaviorRelay<String?>(value: nil)
+    let errorMessage = PublishSubject<String?>()
     
     // MARK: - Initializer
-    init() {
-        bind()
+    init(accountStorage: KeychainAccountStorage, completion: @escaping () -> Void) {
+        self.accountStorage = accountStorage
+        self.completion = completion
     }
     
-    // MARK: - Binding
-    func bind() {
-        
+    func finish() {
+        completion()
     }
     
     // MARK: - Actions
-//    @objc func showDetail() {
-//        
-//    }
+    @objc func restoreFromICloud() {
+        guard let phrases = accountStorage.phrasesFromICloud() else
+        {
+            errorMessage.onNext(L10n.thereIsNoP2PWalletSavedInYourICloud)
+            return
+        }
+        handlePhrases(phrases)
+    }
+    
+    @objc func restoreManually() {
+        navigationSubject.onNext(.enterPhrases)
+    }
+    
+    private func handlePhrases(_ text: String)
+    {
+        do {
+            let phrases = text.components(separatedBy: " ")
+            _ = try Mnemonic(phrase: phrases.filter {!$0.isEmpty})
+            navigationSubject.onNext(.welcomeBack(phrases: phrases))
+        } catch {
+            let message = (error as? SolanaSDK.Error)?.errorDescription ?? error.localizedDescription
+            errorMessage.onNext(message)
+        }
+    }
 }
