6de4898911d9bcca196c6a1ed2d73760e16f702f
Chung Tran <bigearsenal@gmail.com>
2021-02-22 10:21:19 +0700

    Move authentication to MainViewController

---
+++ p2p_wallet/Scenes/Main/MainViewController.swift
@@ -0,0 +1,91 @@
+//
+//  MainViewController.swift
+//  p2p_wallet
+//
+//  Created by Chung Tran on 22/02/2021.
+//
+
+import Foundation
+import UIKit
+import Action
+
+protocol _MainScenesFactory {
+    func makeTabBarVC() -> TabBarVC
+    func makeLocalAuthVC() -> LocalAuthVC
+}
+
+class MainViewController: BaseVC {
+    
+    // MARK: - Properties
+    let viewModel: MainViewModel
+    let scenesFactory: _MainScenesFactory
+    var shouldAuthenticate = true
+    
+    // MARK: - Initializer
+    init(viewModel: MainViewModel, scenesFactory: _MainScenesFactory)
+    {
+        self.viewModel = viewModel
+        self.scenesFactory = scenesFactory
+        super.init()
+    }
+    
+    // MARK: - Methods
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if shouldAuthenticate {
+            shouldAuthenticate = false
+            viewModel.authenticationSubject.onNext(())
+        }
+    }
+    
+    override func setUp() {
+        super.setUp()
+        add(child: scenesFactory.makeTabBarVC())
+    }
+    
+    override func bind() {
+        super.bind()
+        viewModel.authenticationSubject
+            .subscribe(onNext: {self.authenticate()})
+            .disposed(by: disposeBag)
+    }
+    
+    private func authenticate() {
+        let localAuthVC = scenesFactory.makeLocalAuthVC()
+        localAuthVC.completion = {[weak self] didSuccess in
+            self?.viewModel.isAuthenticating = false
+            self?.viewModel.lastAuthenticationTimestamp = Date().timeIntervalSince1970
+            if !didSuccess {
+                // show error
+                self?.showErrorView()
+                
+                // Count down to next
+                Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [weak self] timer in
+                    guard let strongSelf = self else {return}
+                    
+                    let secondsLeft = strongSelf.viewModel.secondsLeftToNextAuthentication()
+                    
+                    strongSelf.errorView?.descriptionLabel.text =
+                        L10n.authenticationFailed +
+                        "\n" +
+                        L10n.retryAfter + " \(secondsLeft) " + L10n.seconds
+                    
+                    if strongSelf.viewModel.isSessionExpired {
+                        strongSelf.errorView?.descriptionLabel.text = L10n.tapButtonToRetry
+                        strongSelf.errorView?.buttonAction = CocoaAction {
+                            strongSelf.viewModel.authenticationSubject.onNext(())
+                            return .just(())
+                        }
+                        timer.invalidate()
+                    }
+                }
+
+            } else {
+                self?.removeErrorView()
+            }
+        }
+        localAuthVC.modalPresentationStyle = .fullScreen
+        present(localAuthVC, animated: true, completion: nil)
+        viewModel.isAuthenticating = true
+    }
+}
