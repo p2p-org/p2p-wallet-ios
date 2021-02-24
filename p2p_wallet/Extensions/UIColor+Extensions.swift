018e80d19444589e40641e0a9b84cab0ce185c93
Chung Tran <bigearsenal@gmail.com>
2021-02-24 10:26:48 +0700

    fix Send scene

--- p2p_wallet/Extensions/UIColor+Extensions.swift
+++ p2p_wallet/Extensions/UIColor+Extensions.swift
@@ -8,10 +8,6 @@
 import Foundation
 
 extension UIColor {
-    static var textSecondary: UIColor {
-        UIColor.textBlack.withAlphaComponent(0.5)
-    }
-    
     func image(_ size: CGSize = CGSize(width: 1, height: 1)) -> UIImage {
         return UIGraphicsImageRenderer(size: size).image { rendererContext in
             self.setFill()
