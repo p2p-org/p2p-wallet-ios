d0c57e65cdc01239a31cd8c83de8096d629bcba4
Chung Tran <bigearsenal@gmail.com>
2021-02-25 16:22:10 +0700

    add token_settings

--- p2p_wallet/Base/ViewControllers/WalletsVC/WalletsVC.swift
+++ p2p_wallet/Base/ViewControllers/WalletsVC/WalletsVC.swift
@@ -6,10 +6,12 @@
 //
 
 import Foundation
+import Action
 
 protocol MyWalletsScenesFactory {
     func makeWalletDetailVC(wallet: Wallet) -> WalletDetailVC
     func makeAddNewTokenVC() -> AddNewWalletVC
+    func makeTokenSettingsViewController() -> TokenSettingsViewController
 }
 
 class MyWalletsVC: CollectionVC<Wallet> {
@@ -23,5 +25,14 @@ class MyWalletsVC: CollectionVC<Wallet> {
     override func itemDidSelect(_ item: Wallet) {
         let vc = scenesFactory.makeWalletDetailVC(wallet: item)
         present(vc, animated: true, completion: nil)
+    }
+    
+    override func setUpCell(cell: UICollectionViewCell, withItem item: Wallet) {
+        super.setUpCell(cell: cell, withItem: item)
+        (cell as? EditableWalletCell)?.editAction = CocoaAction {
+            let vc = self.scenesFactory.makeTokenSettingsViewController()
+            self.present(vc, animated: true, completion: nil)
+            return .just(())
+        }
     }
 }
--- p2p_wallet/Scenes/Main/MainVC/MainWalletCell.swift
+++ p2p_wallet/Scenes/Main/MainVC/MainWalletCell.swift
@@ -6,8 +6,66 @@
 //
 
 import Foundation
+import Action
 
-class MainWalletCell: WalletCell {
+class EditableWalletCell: WalletCell {
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView(forAutoLayout: ())
+        scrollView.isPagingEnabled = true
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.showsHorizontalScrollIndicator = false
+        return scrollView
+    }()
+    lazy var buttonStackView = UIStackView(axis: .horizontal, spacing: 16, alignment: .fill, distribution: .fill)
+    lazy var editButton = UIImageView(width: 24, height: 24, image: .walletEdit, tintColor: .textBlack)
+        .padding(.init(all: 10), backgroundColor: UIColor.a3a5ba.withAlphaComponent(0.3), cornerRadius: 12)
+        .onTap(self, action: #selector(buttonEditDidTouch))
+    lazy var hideButton = UIImageView(width: 24, height: 24, image: .visibilityHide, tintColor: .textBlack)
+        .padding(.init(all: 10), backgroundColor: UIColor.a3a5ba.withAlphaComponent(0.3), cornerRadius: 12)
+        .onTap(self, action: #selector(buttonHideDidTouch))
+    
+    var editAction: CocoaAction?
+    var hideAction: CocoaAction?
+    
+    override func commonInit() {
+        super.commonInit()
+        stackView.removeFromSuperview()
+        
+        let wrappedStackView = UIStackView(axis: .horizontal, spacing: 16, alignment: .center, arrangedSubviews: [
+            stackView,
+            buttonStackView
+        ])
+        
+        addSubview(scrollView)
+        scrollView.autoPinEdgesToSuperviewEdges()
+        scrollView.addSubview(wrappedStackView)
+        wrappedStackView.autoPinEdgesToSuperviewEdges()
+        stackView.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
+        stackView.heightAnchor.constraint(equalTo: scrollView.heightAnchor).isActive = true
+        
+        buttonStackView.addArrangedSubviews([
+            editButton,
+            hideButton
+        ])
+    }
+    
+    override func setUp(with item: Wallet) {
+        super.setUp(with: item)
+        if item.symbol == "SOL" {
+            buttonStackView.isHidden = true
+        }
+    }
+    
+    @objc func buttonEditDidTouch() {
+        editAction?.execute()
+    }
+    
+    @objc func buttonHideDidTouch() {
+        hideAction?.execute()
+    }
+}
+
+class MainWalletCell: EditableWalletCell {
     lazy var addressLabel = UILabel(text: "public key", textSize: 13, textColor: .textSecondary, numberOfLines: 1)
     lazy var indicatorColorView = UIView(width: 3, cornerRadius: 1.5)
     
