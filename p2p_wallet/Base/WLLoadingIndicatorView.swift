57e394e7ca38d68e3647b81a780328c4cba4a163
Chung Tran <bigearsenal@gmail.com>
2021-03-18 13:57:04 +0700

    add convenience methods for showing spinner

--- p2p_wallet/Base/WLLoadingIndicatorView.swift
+++ p2p_wallet/Base/WLLoadingIndicatorView.swift
@@ -7,25 +7,36 @@
 
 import Foundation
 
+extension UIView {
+    @discardableResult
+    func showLoadingIndicatorView() -> WLLoadingIndicatorView {
+        hideLoadingIndicatorView()
+        let indicator = WLLoadingIndicatorView(forAutoLayout: ())
+        addSubview(indicator)
+        indicator.autoPinEdgesToSuperviewEdges()
+        indicator.animate()
+        return indicator
+    }
+    
+    func hideLoadingIndicatorView() {
+        subviews.first(where: {$0 is WLLoadingIndicatorView})?.removeFromSuperview()
+    }
+}
+
 class WLLoadingIndicatorView: BEView {
     // MARK: - Subviews
     private lazy var spinner: BESpinnerView = {
-        let spinner = BESpinnerView(forAutoLayout: ())
+        let spinner = BESpinnerView(width: 65, height: 65, cornerRadius: 65/2)
         spinner.endColor = .h5887ff
         return spinner
     }()
     
     // MARK: - Methods
-    init() {
-        super.init(frame: .zero)
-        configureForAutoLayout()
-        autoSetDimensions(to: CGSize(width: 65, height: 65))
-    }
-    
     override func commonInit() {
         super.commonInit()
+        backgroundColor = UIColor.textWhite.withAlphaComponent(0.8)
         addSubview(spinner)
-        spinner.autoPinEdgesToSuperviewEdges()
+        spinner.autoCenterInSuperview()
         
         let imageView = UIImageView(width: 45, height: 45, cornerRadius: 45 / 2, image: .spinnerIcon)
         addSubview(imageView)
