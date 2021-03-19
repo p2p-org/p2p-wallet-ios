986280acdf73aa3b1b985d51accb31bae716ec2d
Chung Tran <bigearsenal@gmail.com>
2021-03-19 17:55:49 +0700

    fix origin y

--- p2p_wallet/Scenes/CreateOrRestoreWallet/RestoreWallet/EnterPhrasesVC.swift
+++ p2p_wallet/Scenes/CreateOrRestoreWallet/RestoreWallet/EnterPhrasesVC.swift
@@ -123,7 +123,7 @@ extension EnterPhrasesVC: UITextViewDelegate {
     class Attachment: SubviewTextAttachment {
         override func attachmentBounds(for textContainer: NSTextContainer?, proposedLineFragment lineFrag: CGRect, glyphPosition position: CGPoint, characterIndex charIndex: Int) -> CGRect {
             var bounds = super.attachmentBounds(for: textContainer, proposedLineFragment: lineFrag, glyphPosition: position, characterIndex: charIndex)
-            bounds.origin.y -= 16
+            bounds.origin.y -= 15
             return bounds
         }
     }
