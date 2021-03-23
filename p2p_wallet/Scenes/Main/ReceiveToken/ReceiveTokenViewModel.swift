907c8777b87516ecc6076831ecf4e68a7ef772c7
Chung Tran <bigearsenal@gmail.com>
2021-03-23 10:06:15 +0700

    add new ReceiveToken screen

---
+++ p2p_wallet/Scenes/Main/ReceiveToken/ReceiveTokenViewModel.swift
@@ -0,0 +1,51 @@
+//
+//  ReceiveTokenViewModel.swift
+//  p2p_wallet
+//
+//  Created by Chung Tran on 23/03/2021.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+enum ReceiveTokenNavigatableScene {
+//    case detail
+}
+
+protocol WalletsRepository {
+    
+}
+
+class ReceiveTokenViewModel {
+    // MARK: - Constants
+    
+    // MARK: - Properties
+    let disposeBag = DisposeBag()
+    let repository: WalletsRepository
+    
+    // MARK: - Subjects
+    let navigationSubject = PublishSubject<ReceiveTokenNavigatableScene>()
+    
+    // MARK: - Input
+//    let textFieldInput = BehaviorRelay<String?>(value: nil)
+    
+    // MARK: - Initializers
+    init(walletsRepository: WalletsRepository) {
+        self.repository = walletsRepository
+    }
+    
+    
+    // MARK: - Actions
+    @objc func selectWallet() {
+        
+    }
+    
+    @objc func copyToClipboard() {
+        
+    }
+    
+    @objc func share() {
+        
+    }
+}
