51eb0ee013f9772eb5e685c46e6be5e39d73c754
Chung Tran <bigearsenal@gmail.com>
2021-03-23 14:36:17 +0700

    some fixes

--- p2p_wallet/Scenes/Main/ReceiveToken/ReceiveTokenViewModel.swift
+++ p2p_wallet/Scenes/Main/ReceiveToken/ReceiveTokenViewModel.swift
@@ -10,10 +10,12 @@ import RxSwift
 import RxCocoa
 
 enum ReceiveTokenNavigatableScene {
-//    case detail
+    case chooseWallet
+    case explorer(url: String)
 }
 
 protocol WalletsRepository {
+    var wallets: [Wallet] {get}
     var stateObservable: Observable<FetcherState<[Wallet]>> {get}
 }
 
@@ -21,11 +23,13 @@ class ReceiveTokenViewModel {
     // MARK: - Constants
     
     // MARK: - Properties
-    let disposeBag = DisposeBag()
+    private let disposeBag = DisposeBag()
     let repository: WalletsRepository
     
     // MARK: - Subjects
     let navigationSubject = PublishSubject<ReceiveTokenNavigatableScene>()
+    let wallet = BehaviorRelay<Wallet?>(value: nil)
+    let alert = PublishSubject<String>()
     
     // MARK: - Input
 //    let textFieldInput = BehaviorRelay<String?>(value: nil)
@@ -33,15 +37,40 @@ class ReceiveTokenViewModel {
     // MARK: - Initializers
     init(walletsRepository: WalletsRepository) {
         self.repository = walletsRepository
+        self.wallet.accept(repository.wallets.first)
+        bind()
+    }
+    
+    private func bind() {
+        // bind wallet
+        repository.stateObservable
+            .filter { state in
+                switch state {
+                case .loaded:
+                    return true
+                default:
+                    return false
+                }
+            }
+            .map { [weak self] _ -> Wallet? in
+                if let currentWallet = self?.wallet.value {
+                    return self?.repository.wallets.first(where: {$0.pubkey == currentWallet.pubkey})
+                }
+                return self?.repository.wallets.first
+            }
+            .bind(to: wallet)
+            .disposed(by: disposeBag)
     }
     
     // MARK: - Actions
     @objc func selectWallet() {
-        
+        navigationSubject.onNext(.chooseWallet)
     }
     
-    @objc func copyToClipboard() {
-        
+    @objc func showMintInExplorer() {
+        guard let mint = wallet.value?.mintAddress else {return}
+        let url = "https://explorer.solana.com/address/\(mint)"
+        navigationSubject.onNext(.explorer(url: url))
     }
     
     @objc func share() {
