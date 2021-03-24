e818fc08ce64cbab6315d14db338990341c3c64b
Chung Tran <bigearsenal@gmail.com>
2021-03-24 13:19:59 +0700

    add createWalletButton

--- p2p_wallet/Scenes/Main/ReceiveToken/ReceiveTokenViewModel.swift
+++ p2p_wallet/Scenes/Main/ReceiveToken/ReceiveTokenViewModel.swift
@@ -8,6 +8,7 @@
 import UIKit
 import RxSwift
 import RxCocoa
+import LazySubject
 
 enum ReceiveTokenNavigatableScene {
     case chooseWallet
@@ -21,18 +22,27 @@ class ReceiveTokenViewModel {
     // MARK: - Properties
     private let disposeBag = DisposeBag()
     let repository: WalletsRepository
+    let handler: CreateTokenHandler
     
     // MARK: - Subjects
     let navigationSubject = PublishSubject<ReceiveTokenNavigatableScene>()
     let wallet = BehaviorRelay<Wallet?>(value: nil)
     let alert = PublishSubject<String>()
+    lazy var feeSubject = LazySubject(
+        value: Double(0),
+        request: handler.getCreatingTokenAccountFee()
+            .map {
+                Double($0) * pow(Double(10), -Double(9))
+            }
+    )
     
     // MARK: - Input
 //    let textFieldInput = BehaviorRelay<String?>(value: nil)
     
     // MARK: - Initializers
-    init(walletsRepository: WalletsRepository, pubkey: String? = nil) {
+    init(handler: CreateTokenHandler, walletsRepository: WalletsRepository, pubkey: String? = nil) {
         self.repository = walletsRepository
+        self.handler = handler
         self.wallet.accept(repository.getWallets().first(where: {$0.pubkey == pubkey}) ?? repository.getWallets().first)
         bind()
     }
