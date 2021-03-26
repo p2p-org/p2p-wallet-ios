7acd98fbf9ad6e13dfff83354e6a749f1b13cab3
Chung Tran <bigearsenal@gmail.com>
2021-03-26 11:58:03 +0700

    some fixes

--- p2p_wallet/Scenes/Main/Home/HomeCollectionView/Sections/HiddenWalletsSection/HiddenWalletsSection.swift
+++ p2p_wallet/Scenes/Main/Home/HomeCollectionView/Sections/HiddenWalletsSection/HiddenWalletsSection.swift
@@ -9,101 +9,64 @@ import Foundation
 import BECollectionView
 import Action
 
-class HiddenWalletsSection: HomeWalletsSection {
-    var showAllProductsAction: CocoaAction?
-    var showHideHiddenWalletsAction: CocoaAction?
-    
-    init(index: Int, viewModel: WalletsListViewModelType) {
-        super.init(
-            index: index,
-            layout: .init(
-                header: .init(
-                    identifier: "HiddenWalletsSectionHeaderView",
-                    viewClass: HeaderView.self
+extension HomeCollectionView {
+    class HiddenWalletsSection: BaseHiddenWalletsSection {
+        var showAllProductsAction: CocoaAction?
+        
+        init(index: Int, viewModel: WalletsListViewModelType) {
+            super.init(
+                index: index,
+                layout: .init(
+                    header: .init(
+                        identifier: "HiddenWalletsSectionHeaderView",
+                        viewClass: HeaderView.self
+                    ),
+                    footer: .init(
+                        identifier: "HiddenWalletsSectionFooterView",
+                        viewClass: FooterView.self
+                    ),
+                    cellType: HomeWalletCell.self,
+                    interGroupSpacing: 30,
+                    itemHeight: .absolute(45),
+                    contentInsets: NSDirectionalEdgeInsets(top: 0, leading: .defaultPadding, bottom: .defaultPadding, trailing: .defaultPadding),
+                    horizontalInterItemSpacing: .fixed(16),
+                    background: BackgroundView.self
                 ),
-                footer: .init(
-                    identifier: "HiddenWalletsSectionFooterView",
-                    viewClass: FooterView.self
-                ),
-                cellType: HomeWalletCell.self,
-                interGroupSpacing: 30,
-                itemHeight: .absolute(45),
-                contentInsets: NSDirectionalEdgeInsets(top: 0, leading: .defaultPadding, bottom: .defaultPadding, trailing: .defaultPadding),
-                horizontalInterItemSpacing: .fixed(16),
-                background: BackgroundView.self
-            ),
-            viewModel: viewModel,
-            customFilter: { item in
-                guard let wallet = item as? Wallet else {return false}
-                return wallet.isHidden
-            },
-            limit: {
-                Array($0.prefix(4))
-            }
-        )
-    }
-    
-    override func configureHeader(indexPath: IndexPath) -> UICollectionReusableView? {
-        let view = super.configureHeader(indexPath: indexPath) as? HeaderView
-        view?.showHideHiddenWalletsAction = showHideHiddenWalletsAction
-        return view
-    }
-    
-    override func configureFooter(indexPath: IndexPath) -> UICollectionReusableView? {
-        let view = super.configureFooter(indexPath: indexPath) as? FooterView
-        view?.showProductsAction = showAllProductsAction
-        return view
-    }
-    
-    override func mapDataToCollectionViewItems() -> [BECollectionViewItem] {
-        let viewModel = self.viewModel as? WalletsListViewModelType
-        if viewModel?.isHiddenWalletsShown.value == true {
-            return super.mapDataToCollectionViewItems()
-        } else {
-            return []
-        }
-    }
-    
-    override func dataDidLoad() {
-        super.dataDidLoad()
-        let viewModel = self.viewModel as! WalletsListViewModelType
-        if let headerView = self.headerView() as? HeaderView {
-            if viewModel.isHiddenWalletsShown.value {
-                headerView.imageView.tintColor = .textBlack
-                headerView.imageView.image = .visibilityHide
-                headerView.headerLabel.textColor = .textBlack
-                headerView.headerLabel.text = L10n.hide
-            } else {
-                headerView.imageView.tintColor = .textSecondary
-                headerView.imageView.image = .visibilityShow
-                headerView.headerLabel.textColor = .textSecondary
-                headerView.headerLabel.text = L10n.dHiddenWallet(viewModel.hiddenWallets().count)
-            }
-            if viewModel.hiddenWallets().isEmpty {
-                headerView.removeStackView { [weak self] in
-                    self?.collectionViewLayout?.invalidateLayout()
+                viewModel: viewModel,
+                customFilter: { item in
+                    guard let wallet = item as? Wallet else {return false}
+                    return wallet.isHidden
+                },
+                limit: {
+                    Array($0.prefix(4))
                 }
-            } else {
-                headerView.addStackView { [weak self] in
-                    self?.collectionViewLayout?.invalidateLayout()
-                }
-            }
+            )
         }
-
-        if let footerView = footerView() as? FooterView {
-            if let topConstraint = footerView.button.constraintToSuperviewWithAttribute(.top)
-            {
-                if !viewModel.hiddenWallets().isEmpty && !viewModel.isHiddenWalletsShown.value {
-                    if topConstraint.constant != 0 {
-                        topConstraint.constant = 0
-                        footerView.setNeedsLayout()
-                        collectionViewLayout?.invalidateLayout()
-                    }
-                } else {
-                    if topConstraint.constant != 30 {
-                        topConstraint.constant = 30
-                        footerView.setNeedsLayout()
-                        collectionViewLayout?.invalidateLayout()
+        
+        override func configureFooter(indexPath: IndexPath) -> UICollectionReusableView? {
+            let view = super.configureFooter(indexPath: indexPath) as? FooterView
+            view?.showProductsAction = showAllProductsAction
+            return view
+        }
+        
+        override func dataDidLoad() {
+            super.dataDidLoad()
+            let viewModel = self.viewModel as! WalletsListViewModelType
+            if let footerView = footerView() as? FooterView {
+                if let topConstraint = footerView.button.constraintToSuperviewWithAttribute(.top)
+                {
+                    if !viewModel.hiddenWallets().isEmpty && !viewModel.isHiddenWalletsShown.value {
+                        if topConstraint.constant != 0 {
+                            topConstraint.constant = 0
+                            footerView.setNeedsLayout()
+                            collectionViewLayout?.invalidateLayout()
+                        }
+                    } else {
+                        if topConstraint.constant != 30 {
+                            topConstraint.constant = 30
+                            footerView.setNeedsLayout()
+                            collectionViewLayout?.invalidateLayout()
+                        }
                     }
                 }
             }
