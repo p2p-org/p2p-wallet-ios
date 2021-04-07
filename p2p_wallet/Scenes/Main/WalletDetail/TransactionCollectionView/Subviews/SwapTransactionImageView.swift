0bd75f957e35b814132f0d8b422b052d55c9e8db
Chung Tran <bigearsenal@gmail.com>
2021-04-07 11:40:28 +0700

    fix TransactionCell

---
+++ p2p_wallet/Scenes/Main/WalletDetail/TransactionCollectionView/Subviews/SwapTransactionImageView.swift
@@ -0,0 +1,14 @@
+//
+//  SwapTransactionImageView.swift
+//  p2p_wallet
+//
+//  Created by Chung Tran on 07/04/2021.
+//
+
+import Foundation
+
+class SwapTransactionImageView: BEView {
+    override func commonInit() {
+        
+    }
+}
