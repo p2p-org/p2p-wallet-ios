f814cab57f36d48385aeb728cd4a5e88bb2babff
Chung Tran <bigearsenal@gmail.com>
2021-04-16 16:26:48 +0700

    some fixes

---
+++ p2p_wallet/Repositories/AccountRepository.swift
@@ -0,0 +1,14 @@
+//
+//  AccountRepository.swift
+//  p2p_wallet
+//
+//  Created by Chung Tran on 16/04/2021.
+//
+
+import Foundation
+
+protocol AccountRepository {
+    func phrasesFromICloud() -> String?
+}
+
+extension KeychainAccountStorage: AccountRepository {}
