28f5319a3b970c88a77477cfcb59f32d0fc4e80d
Chung Tran <bigearsenal@gmail.com>
2021-04-27 09:16:35 +0700

    Remove unused FetcherState

--- p2p_wallet/Base/Views/BalancesOverviewView.swift
+++ p2p_wallet/Base/Views/BalancesOverviewView.swift
@@ -53,38 +53,6 @@ class BalancesOverviewView: BERoundedCornerShadowView, LoadableView {
         ])
     }
     
-    /// deprecated
-    func setUp(with state: FetcherState<[Wallet]>, solPrice: CurrentPrice?) {
-        switch state {
-        case .initializing:
-            equityValueLabel.text = " "
-            changeLabel.text = " "
-            hideLoading()
-        case .loading:
-            equityValueLabel.text = L10n.loading + "..."
-            changeLabel.text = L10n.loading + "..."
-            showLoading()
-        case .loaded(let wallets):
-            let equityValue = wallets.reduce(0) { $0 + $1.amountInCurrentFiat }
-            equityValueLabel.text = "\(Defaults.fiat.symbol) \(equityValue.toString(maximumFractionDigits: 2))"
-            let changeValue = solPrice?.change24h?.percentage * 100
-            var color = UIColor.attentionGreen
-            if changeValue < 0 {
-                color = .red
-            }
-            changeLabel.attributedText = NSMutableAttributedString()
-                .text("\(changeValue.toString(maximumFractionDigits: 2, showPlus: true))%", size: 13, color: color)
-                .text(" \(L10n.forTheLast24Hours)", size: 13, color: textColor)
-            setUpChartView(wallets: wallets)
-            hideLoading()
-        case .error(let error):
-            debugPrint(error)
-            equityValueLabel.text = L10n.error.uppercaseFirst
-            changeLabel.text = L10n.error.uppercaseFirst
-            hideLoading()
-        }
-    }
-    
     func setUp(state: BEFetcherState, data: [Wallet]) {
         switch state {
         case .initializing:
