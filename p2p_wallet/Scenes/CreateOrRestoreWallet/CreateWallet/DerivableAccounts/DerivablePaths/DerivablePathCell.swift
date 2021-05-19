adf9bf26e086d8c7053c33d6318e9b0deaf74089
Chung Tran <bigearsenal@gmail.com>
2021-05-19 11:21:40 +0700

    account list: first

---
+++ p2p_wallet/Scenes/CreateOrRestoreWallet/CreateWallet/DerivableAccounts/DerivablePaths/DerivablePathCell.swift
@@ -0,0 +1,32 @@
+//
+//  DerivablePathCell.swift
+//  p2p_wallet
+//
+//  Created by Chung Tran on 19/05/2021.
+//
+
+import Foundation
+import BECollectionView
+
+class DerivablePathCell: BaseCollectionViewCell, LoadableView, BECollectionViewCell {
+    var loadingViews: [UIView] {[radioButton, titleLabel]}
+    
+    private lazy var radioButton = WLRadioButton()
+    private lazy var titleLabel = UILabel(textSize: 17, numberOfLines: 0)
+    
+    override func commonInit() {
+        super.commonInit()
+        let stackView = UIStackView(axis: .horizontal, spacing: 20, alignment: .center, distribution: .fill) {
+            radioButton
+            titleLabel
+        }
+        contentView.addSubview(stackView)
+        stackView.autoPinEdgesToSuperviewEdges(with: .init(all: 22))
+    }
+    
+    func setUp(with item: AnyHashable?) {
+        guard let path = item as? SelectableDerivablePath else {return}
+        radioButton.isSelected = path.isSelected
+        titleLabel.text = path.path.rawValue
+    }
+}
