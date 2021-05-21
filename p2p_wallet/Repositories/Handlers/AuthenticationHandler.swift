2715ccdec711177bce2d97337114aef87bc722e6
Chung Tran <bigearsenal@gmail.com>
2021-05-21 13:11:57 +0700

    add authentication when sending

---
+++ p2p_wallet/Repositories/Handlers/AuthenticationHandler.swift
@@ -0,0 +1,20 @@
+//
+//  AuthenticationHandler.swift
+//  p2p_wallet
+//
+//  Created by Chung Tran on 21/05/2021.
+//
+
+import Foundation
+
+struct AuthenticationPresentationStyle {
+    var title: String = L10n.enterPINCode
+    let isRequired: Bool
+    let isFullScreen: Bool
+    var useBiometry: Bool
+    var completion: (() -> Void)?
+}
+
+protocol AuthenticationHandler {
+    func authenticate(presentationStyle: AuthenticationPresentationStyle)
+}
