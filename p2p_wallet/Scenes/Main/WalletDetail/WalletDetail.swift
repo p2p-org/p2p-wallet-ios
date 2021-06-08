3ad8b71a24ba3c1dd896e6d35137519ef034e68a
Chung Tran <bigearsenal@gmail.com>
2021-06-08 12:48:12 +0700

    completed refactoring WalletDetail

---
+++ p2p_wallet/Scenes/Main/WalletDetail/WalletDetail.swift
@@ -0,0 +1,26 @@
+//
+//  WalletDetail.swift
+//  p2p_wallet
+//
+//  Created by Chung Tran on 08/06/2021.
+//
+
+import Foundation
+
+protocol WalletDetailScenesFactory {
+    func makeReceiveTokenViewController(tokenWalletPubkey: String?) -> ReceiveToken.ViewController?
+    func makeSendTokenViewController(walletPubkey: String?, destinationAddress: String?) -> SendToken.ViewController
+    func makeSwapTokenViewController(fromWallet wallet: Wallet?) -> SwapToken.ViewController
+    func makeTokenSettingsViewController(pubkey: String) -> TokenSettingsViewController
+    func makeTransactionInfoViewController(transaction: SolanaSDK.AnyTransaction) -> TransactionInfoViewController
+}
+
+struct WalletDetail {
+    enum NavigatableScene {
+        case settings(walletPubkey: String)
+        case send(wallet: Wallet)
+        case receive(walletPubkey: String)
+        case swap(fromWallet: Wallet)
+        case transactionInfo(_ transaction: SolanaSDK.AnyTransaction)
+    }
+}
