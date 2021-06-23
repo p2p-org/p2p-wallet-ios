02b693aee6e410e7839173e135549ec6c38ea0e7
Chung Tran <bigearsenal@gmail.com>
2021-06-23 12:21:15 +0700

    fix color on main

--- p2p_wallet/Scenes/Main/Home/HomeRootView.swift
+++ p2p_wallet/Scenes/Main/Home/HomeRootView.swift
@@ -53,7 +53,7 @@ class HomeRootView: BEView {
     
     lazy var tabBar: TabBar = {
         let tabBar = TabBar(cornerRadius: .defaultPadding, contentInset: UIEdgeInsets(top: 20, left: 0, bottom: 8, right: 0))
-        tabBar.backgroundColor = .background2
+        tabBar.backgroundColor = .white.onDarkMode(.h2a2a2a)
         tabBar.stackView.addArrangedSubviews([
             .spacer,
 //                    createButton(image: .walletAdd, title: L10n.buy),
