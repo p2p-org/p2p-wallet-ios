de98f4db55c13df4ae938d6f4f35370dce3916c4
Chung Tran <bigearsenal@gmail.com>
2021-07-01 12:16:05 +0700

    move method fittingHeight to BEPureLayout

--- p2p_wallet/Extensions/UIView+Extensions.swift
+++ p2p_wallet/Extensions/UIView+Extensions.swift
@@ -9,16 +9,6 @@ import Foundation
 import Action
 
 extension UIView {
-    func fittingHeight(targetWidth: CGFloat) -> CGFloat {
-        let fittingSize = CGSize(
-            width: targetWidth,
-            height: UIView.layoutFittingCompressedSize.height
-        )
-        return systemLayoutSizeFitting(fittingSize, withHorizontalFittingPriority: .required,
-                                verticalFittingPriority: .defaultLow)
-            .height
-    }
-    
     static func copyToClipboardButton(spacing: CGFloat = 10, tintColor: UIColor = .textSecondary) -> UIStackView {
         UIStackView(axis: .horizontal, spacing: spacing, alignment: .center, distribution: .fill, arrangedSubviews: [
             UIImageView(width: 24, height: 24, image: .copyToClipboard, tintColor: tintColor),
