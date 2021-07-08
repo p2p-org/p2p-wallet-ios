93e9ffb9051ab27859fef63221a9e3ba287d5c27
Chung Tran <bigearsenal@gmail.com>
2021-07-08 12:54:02 +0700

    move header to collectionview independently

--- p2p_wallet/Scenes/Main/WalletDetail/TransactionCollectionView/TransactionsCollectionView.swift
+++ p2p_wallet/Scenes/Main/WalletDetail/TransactionCollectionView/TransactionsCollectionView.swift
@@ -8,48 +8,53 @@
 import Foundation
 import BECollectionView
 import Action
+import RxCocoa
 
 class TransactionsCollectionView: BECollectionView {
     let transactionsSection: DefaultSection
+    let graphViewModel: WalletGraphViewModel
     let analyticsManager: AnalyticsManagerType
+    let scanQrCodeAction: CocoaAction
+    let wallet: Driver<Wallet?>
+    let solPubkey: Driver<String?>
     
-    var scanQrCodeAction: CocoaAction? {
-        get {
-            transactionsSection.scanQrCodeAction
-        }
-        set {
-            transactionsSection.scanQrCodeAction = newValue
-        }
-    }
-    
-    var wallet: Wallet? {
-        get {
-            transactionsSection.wallet
-        }
-        set {
-            transactionsSection.wallet = newValue
-        }
-    }
-    
-    var solPubkey: String? {
-        get {
-            transactionsSection.solPubkey
-        }
-        set {
-            transactionsSection.solPubkey = newValue
-        }
-    }
-    
-    init(transactionViewModel: BEListViewModelType, graphViewModel: WalletGraphViewModel, analyticsManager: AnalyticsManagerType) {
+    init(
+        transactionViewModel: BEListViewModelType,
+        graphViewModel: WalletGraphViewModel,
+        analyticsManager: AnalyticsManagerType,
+        scanQrCodeAction: CocoaAction,
+        wallet: Driver<Wallet?>,
+        solPubkey: Driver<String?>
+    ) {
         self.analyticsManager = analyticsManager
+        self.graphViewModel = graphViewModel
+        self.scanQrCodeAction = scanQrCodeAction
+        self.wallet = wallet
+        self.solPubkey = solPubkey
+        
         transactionsSection = .init(
             index: 0,
             viewModel: transactionViewModel,
-            graphViewModel: graphViewModel,
-            analyticsManager: analyticsManager
+            graphViewModel: graphViewModel
         )
-        super.init(sections: [
-            transactionsSection
-        ])
+        super.init(
+            header: .init(
+                viewType: TransactionsCollectionView.HeaderView.self,
+                heightDimension: .estimated(557)
+            ),
+            sections: [transactionsSection]
+        )
+    }
+    
+    override func configureHeaderView(kind: String, indexPath: IndexPath) -> UICollectionReusableView? {
+        let headerView = super.configureHeaderView(kind: kind, indexPath: indexPath) as? HeaderView
+        headerView?.setUp(
+            graphViewModel: graphViewModel,
+            analyticsManager: analyticsManager,
+            scanQrCodeAction: scanQrCodeAction,
+            wallet: wallet,
+            solPubkey: solPubkey
+        )
+        return headerView
     }
 }
