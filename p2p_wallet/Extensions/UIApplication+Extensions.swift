ad2266ae7d0b2e0aa6b928d7f1dae6e7a132e381
Chung Tran <bigearsenal@gmail.com>
2021-07-14 15:51:41 +0700

    show toast instead of hud

--- p2p_wallet/Extensions/UIApplication+Extensions.swift
+++ p2p_wallet/Extensions/UIApplication+Extensions.swift
@@ -32,25 +32,6 @@ extension UIApplication {
         kWindow?.hideLoadingIndicatorView()
     }
     
-    func showDone(_ message: String, completion: (() -> Void)? = nil) {
-        guard let keyWindow = kWindow else {return}
-        
-        // Hide all previous hud
-        hideHud()
-        
-        // show new hud
-        let hud = MBProgressHUD.showAdded(to: keyWindow, animated: false)
-        hud.mode = .customView
-        let imageView = UIImageView(width: 100, height: 100, image: .checkMark)
-        imageView.tintColor = .textBlack
-        hud.customView = imageView
-        hud.label.text = message
-        hud.hide(animated: true, afterDelay: 1)
-        if let completion = completion {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1, execute: completion)
-        }
-    }
-    
     func openAppSettings() {
         if let bundleIdentifier = Bundle.main.bundleIdentifier, let appSettings = URL(string: UIApplication.openSettingsURLString + bundleIdentifier) {
             if canOpenURL(appSettings) {
