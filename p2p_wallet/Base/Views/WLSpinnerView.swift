49c9e16777e1aa8668795977706c528155959c08
Chung Tran <bigearsenal@gmail.com>
2021-07-30 11:54:45 +0700

    successfully added Transak

---
+++ p2p_wallet/Base/Views/WLSpinnerView.swift
@@ -0,0 +1,18 @@
+//
+//  WLSpinnerView.swift
+//  p2p_wallet
+//
+//  Created by Chung Tran on 30/07/2021.
+//
+
+import Foundation
+
+class WLSpinnerView: BESpinnerView {
+    override init(size: CGFloat, endColor: UIColor = .black) {
+        super.init(size: size, endColor: endColor)
+        let padding: CGFloat = 10
+        let imageView = UIImageView(width: size - 2 * padding, height: size - 2 * padding, cornerRadius: (size - 2 * padding)/2, image: .spinnerIcon)
+        addSubview(imageView)
+        imageView.autoCenterInSuperview()
+    }
+}
