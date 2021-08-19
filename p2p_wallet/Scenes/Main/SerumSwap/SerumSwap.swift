96b7a3cf792ec3118ed4c3c953517d7f6f26c939
Chung Tran <bigearsenal@gmail.com>
2021-08-19 11:20:20 +0700

    first try implementing serum-swap ui

---
+++ p2p_wallet/Scenes/Main/SerumSwap/SerumSwap.swift
@@ -0,0 +1,122 @@
+//
+//  SerumSwap.swift
+//  p2p_wallet
+//
+//  Created by Chung Tran on 19/08/2021.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+struct SerumSwap {
+    enum NavigatableScene {
+        case chooseSourceWallet
+        case chooseDestinationWallet
+        case settings
+        case chooseSlippage
+        case swapFees
+        case processTransaction(request: Single<ProcessTransactionResponseType>, transactionType: ProcessTransaction.TransactionType)
+    }
+    
+    struct ExchangeRate {
+        let from: String
+        let to: String
+        let rate: Double
+    }
+    
+    // MARK: - Helpers
+    static func createSectionView(
+        title: String? = nil,
+        label: UIView? = nil,
+        contentView: UIView,
+        rightView: UIView = UIImageView(width: 6, height: 12, image: .nextArrow, tintColor: .h8b94a9.onDarkMode(.white)
+        )
+            .padding(.init(x: 9, y: 6)),
+        addSeparatorOnTop: Bool = true
+    ) -> UIStackView {
+        let stackView = UIStackView(axis: .horizontal, spacing: 5, alignment: .center, distribution: .fill) {
+            UIStackView(axis: .vertical, spacing: 5, alignment: .fill, distribution: .fill) {
+                label ?? UILabel(
+                    text: title,
+                    textSize: 13,
+                    weight: .medium,
+                    textColor: .textSecondary
+                )
+                contentView
+            }
+            
+            rightView
+        }
+        
+        if !addSeparatorOnTop {
+            return stackView
+        } else {
+            return UIStackView(axis: .vertical, spacing: 16, alignment: .fill, distribution: .fill) {
+                UIView.defaultSeparator()
+                stackView
+            }
+        }
+    }
+    
+    static func slippageAttributedText(
+        slippage: Double
+    ) -> NSAttributedString {
+        if slippage > .maxSlippage {
+            return NSMutableAttributedString()
+                .text((slippage * 100).toString(maximumFractionDigits: 9) + "%", weight: .medium)
+                .text(" ", weight: .medium)
+                .text(L10n.slippageExceedsMaximum, weight: .medium, color: .red)
+        } else if slippage > .frontrunSlippage && slippage <= .maxSlippage {
+            return NSMutableAttributedString()
+                .text((slippage * 100).toString(maximumFractionDigits: 9) + "%", weight: .medium)
+                .text(" ", weight: .medium)
+                .text(L10n.yourTransactionMayBeFrontrun, weight: .medium, color: .attentionGreen)
+        } else {
+            return NSMutableAttributedString()
+                .text((slippage * 100).toString(maximumFractionDigits: 9) + "%", weight: .medium)
+        }
+    }
+}
+
+protocol SerumSwapScenesFactory {
+    func makeChooseWalletViewController(customFilter: ((Wallet) -> Bool)?, showOtherWallets: Bool, handler: WalletDidSelectHandler) -> ChooseWallet.ViewController
+    func makeProcessTransactionViewController(transactionType: ProcessTransaction.TransactionType, request: Single<ProcessTransactionResponseType>) -> ProcessTransaction.ViewController
+}
+
+protocol SerumSwapViewModelType {
+    // Input
+    var inputAmountSubject: PublishRelay<String?> {get}
+    var estimatedAmountSubject: PublishRelay<String?> {get}
+    
+    // Drivers
+    var navigationDriver: Driver<SerumSwap.NavigatableScene?> {get}
+    var isLoadingDriver: Driver<Bool> {get}
+    
+    var sourceWalletDriver: Driver<Wallet?> {get}
+    var availableAmountDriver: Driver<Double?> {get}
+    var inputAmountDriver: Driver<Double?> {get}
+    
+    var destinationWalletDriver: Driver<Wallet?> {get}
+    var estimatedAmountDriver: Driver<Double?> {get}
+    
+    var exchangeRateDriver: Driver<SerumSwap.ExchangeRate?> {get}
+    
+    var slippageDriver: Driver<Double?> {get}
+    
+    var errorDriver: Driver<String?> {get}
+    
+    var isSwappableDriver: Driver<Bool> {get}
+    
+    // Signals
+    var useAllBalanceDidTapSignal: Signal<Double?> {get}
+    
+    // Actions
+    func navigate(to: SerumSwap.NavigatableScene)
+    func useAllBalance()
+    func log(_ event: AnalyticsEvent)
+    func swapSourceAndDestination()
+    func reverseExchangeRate()
+    func authenticateAndSwap()
+    func changeSlippage(to slippage: Double)
+}
