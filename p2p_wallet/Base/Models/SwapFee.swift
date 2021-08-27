9d555f3e357a687be91ce0d6fa398f4b2ec76af2
Chung Tran <bigearsenal@gmail.com>
2021-08-27 10:50:35 +0700

    some fixes

---
+++ p2p_wallet/Base/Models/SwapFee.swift
@@ -0,0 +1,13 @@
+//
+//  SwapFee.swift
+//  p2p_wallet
+//
+//  Created by Chung Tran on 27/08/2021.
+//
+
+import Foundation
+
+struct SwapFee {
+    let lamports: SolanaSDK.Lamports
+    let symbol: String
+}
--- p2p_wallet/Services/SwapProvider/SwapProviderType.swift
+++ p2p_wallet/Services/SwapProvider/SwapProviderType.swift
@@ -9,15 +9,40 @@ import Foundation
 import RxSwift
 
 protocol SwapProviderType {
-    func loadPrice(fromMint: String, toMint: String) -> Single<Double>
+    /// Price loading for a single pair of token
+    func loadPrice(
+        fromMint: String,
+        toMint: String
+    ) -> Single<Double>
+    
+    /// Define if fee relayer is enabled for current wallet pair
+    func isFeeRelayerEnabled(
+        source: Wallet?,
+        destination: Wallet?
+    ) -> Bool
+    
+    /// Calculate fee for swapping
+    func calculateFee(
+        sourceWallet: Wallet?,
+        destinationWallet: Wallet?,
+        lamportsPerSignature: SolanaSDK.Lamports?,
+        creatingAccountFee: SolanaSDK.Lamports?
+    ) -> Single<SwapFee?>
+    
+    /// Maximum amount that user can use for swapping
     func calculateAvailableAmount(
-        sourceWallet: Wallet?
+        sourceWallet: Wallet?,
+        fee: SwapFee?
     ) -> Double?
+    
+    /// Estimated amount that user can get after swapping
     func calculateEstimatedAmount(
         inputAmount: Double?,
         rate: Double?,
         slippage: Double?
     ) -> Double?
+    
+    /// Input amount needed for receiving an estimated amount
     func calculateNeededInputAmount(
         forReceivingEstimatedAmount estimatedAmount: Double?,
         rate: Double?,
