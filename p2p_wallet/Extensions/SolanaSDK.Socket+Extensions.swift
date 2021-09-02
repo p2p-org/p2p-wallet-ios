8a711ee0810d80325a242869baed6ec0b1536e9d
Chung Tran <bigearsenal@gmail.com>
2021-09-02 23:34:08 +0700

    implement swapTransitive fixes

---
+++ p2p_wallet/Extensions/SolanaSDK.Socket+Extensions.swift
@@ -0,0 +1,10 @@
+//
+//  SolanaSDK.Socket+Extensions.swift
+//  p2p_wallet
+//
+//  Created by Chung Tran on 02/09/2021.
+//
+
+import Foundation
+
+extension SolanaSDK.Socket: SerumSwapSignatureNotificationHandler {}
