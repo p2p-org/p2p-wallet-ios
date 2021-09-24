60ca9b491ba4e18f9dce56c44969c5afe341ee9e
Chung Tran <bigearsenal@gmail.com>
2021-09-24 17:56:14 +0700

    some fixes

--- p2p_wallet/Scenes/CreateOrRestoreWallet/RestoreWallet/DerivableAccounts/DerivableAccounts.ListViewModel.swift
+++ p2p_wallet/Scenes/CreateOrRestoreWallet/RestoreWallet/DerivableAccounts/DerivableAccounts.ListViewModel.swift
@@ -49,6 +49,7 @@ extension DerivableAccounts {
                         self?.fetchBalances(account: account.info.publicKey.base58EncodedString)
                     }
                 })
+                .subscribe(on: ConcurrentDispatchQueueScheduler(qos: .background))
                 .observe(on: MainScheduler.instance)
         }
         
