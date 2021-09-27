ef40775dbbda72b122afe4e21b8d8ed8ce96aa95
Chung Tran <bigearsenal@gmail.com>
2021-09-27 09:56:42 +0700

    separate SendTokenAPIClient

--- p2p_wallet/Scenes/Main/SendToken/SendToken.ViewModel.swift
+++ p2p_wallet/Scenes/Main/SendToken/SendToken.ViewModel.swift
@@ -9,54 +9,6 @@ import Foundation
 import RxSwift
 import RxCocoa
 import LazySubject
-import FeeRelayerSwift
-
-protocol SendTokenAPIClient {
-    func getFees() -> Single<SolanaSDK.Fee>
-    func checkAccountValidation(account: String) -> Single<Bool>
-    func sendNativeSOL(
-        to destination: String,
-        amount: UInt64,
-        withoutFee: Bool,
-        isSimulation: Bool
-    ) -> Single<SolanaSDK.TransactionID>
-    func sendSPLTokens(
-        mintAddress: String,
-        decimals: SolanaSDK.Decimals,
-        from fromPublicKey: String,
-        to destinationAddress: String,
-        amount: UInt64,
-        withoutFee: Bool,
-        isSimulation: Bool
-    ) -> Single<SolanaSDK.TransactionID>
-}
-
-extension SolanaSDK: SendTokenAPIClient {
-    func sendNativeSOL(to destination: String, amount: UInt64, withoutFee: Bool, isSimulation: Bool) -> Single<TransactionID> {
-        sendNativeSOL(
-            to: destination,
-            amount: amount,
-            isSimulation: isSimulation,
-            customProxy: withoutFee ? FeeRelayer(): nil
-        )
-    }
-    
-    func sendSPLTokens(mintAddress: String, decimals: Decimals, from fromPublicKey: String, to destinationAddress: String, amount: UInt64, withoutFee: Bool, isSimulation: Bool) -> Single<TransactionID> {
-        sendSPLTokens(
-            mintAddress: mintAddress,
-            decimals: decimals,
-            from: fromPublicKey,
-            to: destinationAddress,
-            amount: amount,
-            isSimulation: isSimulation,
-            customProxy: withoutFee ? FeeRelayer(): nil
-        )
-    }
-    
-    func getFees() -> Single<Fee> {
-        getFees(commitment: nil)
-    }
-}
 
 extension SendToken {
     class ViewModel: ViewModelType {
