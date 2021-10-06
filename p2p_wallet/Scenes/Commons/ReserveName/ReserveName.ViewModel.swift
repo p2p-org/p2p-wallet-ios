ca173d2fe73b1f5a72b04266108abd6951c20f26
Chung Tran <bigearsenal@gmail.com>
2021-10-06 12:42:51 +0700

    some fixes in viewModel

--- p2p_wallet/Scenes/Commons/ReserveName/ReserveName.ViewModel.swift
+++ p2p_wallet/Scenes/Commons/ReserveName/ReserveName.ViewModel.swift
@@ -10,9 +10,18 @@ import RxSwift
 import RxCocoa
 
 protocol ReserveNameViewModelType {
+    var currentName: String? {get}
+    
     var navigationDriver: Driver<ReserveName.NavigatableScene?> {get}
+    var initializingStateDriver: Driver<LoadableState> {get}
+    var isNameValidLoadableDriver: Driver<Loadable<Bool>> {get}
+    
+    func reload()
+    func userDidEnter(name: String?)
     
     func showCaptcha()
+    
+    func nameDidReserve(_ name: String)
 }
 
 extension ReserveName {
@@ -22,15 +31,34 @@ extension ReserveName {
         private let owner: String
         
         // MARK: - Properties
+        private let disposeBag = DisposeBag()
+        var currentName: String?
         
         // MARK: - Subject
         private let navigationSubject = BehaviorRelay<NavigatableScene?>(value: nil)
+        private let initializingStateSubject = BehaviorRelay<LoadableState>(value: .notRequested)
+        private let isNameValidLoadableSubject = LoadableRelay<Bool>(request: .just(false))
         
         // MARK: - Initializer
         init(owner: String) {
             self.owner = owner
+            
+            reload()
         }
         
+        private func checkIfOwnerHasAlreadyRegistered() {
+            initializingStateSubject.accept(.loading)
+            nameService.getName(owner)
+                .subscribe(onSuccess: {[weak self] names in
+                    self?.initializingStateSubject.accept(.loaded)
+                    if !names.isEmpty {
+                        self?.nameDidReserve(names.first!.name)
+                    }
+                }, onFailure: {[weak self] error in
+                    self?.initializingStateSubject.accept(.error(error.readableDescription))
+                })
+                .disposed(by: disposeBag)
+        }
     }
 }
 
@@ -39,8 +67,35 @@ extension ReserveName.ViewModel: ReserveNameViewModelType {
         navigationSubject.asDriver()
     }
     
+    var initializingStateDriver: Driver<LoadableState> {
+        initializingStateSubject.asDriver()
+    }
+    
+    var isNameValidLoadableDriver: Driver<Loadable<Bool>> {
+        isNameValidLoadableSubject.asDriver()
+    }
+    
     // MARK: - Actions
+    func reload() {
+        checkIfOwnerHasAlreadyRegistered()
+    }
+    
+    func userDidEnter(name: String?) {
+        currentName = name
+        // check for availability
+        if let name = name {
+            isNameValidLoadableSubject.request = nameService.isNameAvailable(name, owner: owner)
+        } else {
+            isNameValidLoadableSubject.request = .just(false)
+        }
+        isNameValidLoadableSubject.reload()
+    }
+    
     func showCaptcha() {
         navigationSubject.accept(.showCaptcha)
+    }
+    
+    func nameDidReserve(_ name: String) {
+        
     }
 }
