a2b0c3e83f106465d07be5c2e14207f5a9aaa118
Chung Tran <bigearsenal@gmail.com>
2021-10-06 15:27:39 +0700

    fix skipLabelDidTouch

--- p2p_wallet/Scenes/Commons/ReserveName/ReserveName.RootView.swift
+++ p2p_wallet/Scenes/Commons/ReserveName/ReserveName.RootView.swift
@@ -178,13 +178,13 @@ extension ReserveName {
                 .disposed(by: disposeBag)
         }
         
-        @objc func skipLabelDidTouch() {
-//            let skipRange = (skipLabel.text! as NSString).range(of: L10n.skip)
-//            if didTapAttributedTextInLabel(label: skipLabel, inRange: skipRange) {
-//                skipButtonDidTouch()
-//            } else {
-//                viewDidTap()
-//            }
+        @objc func skipLabelDidTouch(gesture: UITapGestureRecognizer) {
+            let skipRange = (skipLabel.text! as NSString).range(of: L10n.skip)
+            if gesture.didTapAttributedTextInLabel(label: skipLabel, inRange: skipRange) {
+                skipButtonDidTouch()
+            } else {
+                viewDidTap()
+            }
         }
         
         @objc func continueButtonDidTouch() {
@@ -192,7 +192,7 @@ extension ReserveName {
         }
         
         @objc func skipButtonDidTouch() {
-            
+            viewModel.skip()
         }
         
         @objc func viewDidTap() {
@@ -225,4 +225,37 @@ private func semiboldText(_ texts: [String], in label: UILabel) {
         aStr.addAttribute(.foregroundColor, value: UIColor.h5887ff, range: range)
     }
     label.attributedText = aStr
+}
+
+private extension UITapGestureRecognizer {
+    func didTapAttributedTextInLabel(label: UILabel, inRange targetRange: NSRange) -> Bool {
+         // Create instances of NSLayoutManager, NSTextContainer and NSTextStorage
+         let layoutManager = NSLayoutManager()
+         let textContainer = NSTextContainer(size: CGSize.zero)
+         let textStorage = NSTextStorage(attributedString: label.attributedText!)
+
+         // Configure layoutManager and textStorage
+         layoutManager.addTextContainer(textContainer)
+         textStorage.addLayoutManager(layoutManager)
+
+         // Configure textContainer
+         textContainer.lineFragmentPadding = 0.0
+         textContainer.lineBreakMode = label.lineBreakMode
+         textContainer.maximumNumberOfLines = label.numberOfLines
+         let labelSize = label.bounds.size
+         textContainer.size = labelSize
+
+         // Find the tapped character location and compare it to the specified range
+         let locationOfTouchInLabel = self.location(in: label)
+         let textBoundingBox = layoutManager.usedRect(for: textContainer)
+         //let textContainerOffset = CGPointMake((labelSize.width - textBoundingBox.size.width) * 0.5 - textBoundingBox.origin.x,
+                                               //(labelSize.height - textBoundingBox.size.height) * 0.5 - textBoundingBox.origin.y);
+         let textContainerOffset = CGPoint(x: (labelSize.width - textBoundingBox.size.width) * 0.5 - textBoundingBox.origin.x, y: (labelSize.height - textBoundingBox.size.height) * 0.5 - textBoundingBox.origin.y)
+
+         //let locationOfTouchInTextContainer = CGPointMake(locationOfTouchInLabel.x - textContainerOffset.x,
+                                                         // locationOfTouchInLabel.y - textContainerOffset.y);
+         let locationOfTouchInTextContainer = CGPoint(x: locationOfTouchInLabel.x - textContainerOffset.x, y: locationOfTouchInLabel.y - textContainerOffset.y)
+         let indexOfCharacter = layoutManager.characterIndex(for: locationOfTouchInTextContainer, in: textContainer, fractionOfDistanceBetweenInsertionPoints: nil)
+         return NSLocationInRange(indexOfCharacter, targetRange)
+     }
 }
