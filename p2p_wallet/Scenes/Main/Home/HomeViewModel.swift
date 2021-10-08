22b583d924013a2f6deba5cd1d64186799d49970
Chung Tran <bigearsenal@gmail.com>
2021-10-08 22:36:23 +0700

    some fixes

--- p2p_wallet/Scenes/Main/Home/HomeViewModel.swift
+++ p2p_wallet/Scenes/Main/Home/HomeViewModel.swift
@@ -37,6 +37,10 @@ class HomeViewModel {
     var navigationDriver: Driver<HomeNavigatableScene> {
         navigationSubject.asDriver(onErrorJustReturn: .profile)
     }
+    private let nameDidReserveSubject = PublishRelay<Void>()
+    var nameDidReserveSignal: Signal<Void> {
+        nameDidReserveSubject.asSignal()
+    }
     
     // MARK: - Input
 //    let textFieldInput = BehaviorRelay<String?>(value: nil)
@@ -83,5 +87,6 @@ extension HomeViewModel: ReserveNameHandler {
             Defaults.forceCloseNameServiceBanner = true
             UIApplication.shared.showToast(message: "✅ \(L10n.usernameIsSuccessfullyReserved(name.withNameServiceSuffix()))")
         }
+        nameDidReserveSubject.accept(())
     }
 }
