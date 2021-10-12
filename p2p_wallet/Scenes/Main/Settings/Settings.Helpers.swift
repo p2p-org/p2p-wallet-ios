caba1a5d7bb451086a20de4a9c9d3d2a0be048ee
Chung Tran <bigearsenal@gmail.com>
2021-10-12 12:48:24 +0700

    add SingleSelectionVC

--- p2p_wallet/Scenes/Main/Settings/Settings.Helpers.swift
+++ p2p_wallet/Scenes/Main/Settings/Settings.Helpers.swift
@@ -42,4 +42,86 @@ extension Settings {
             stackView.autoPinEdgesToSuperviewEdges()
         }
     }
+    
+    class SingleSelectionVC<T: Hashable>: BaseViewController {
+        // MARK: - Data
+        var data: [T: Bool] = [T: Bool]()
+        var cells: [Cell<T>] {stackView.arrangedSubviews.filter {$0 is Cell<T>} as! [Cell<T>]}
+        var selectedItem: T? {data.first(where: {$0.value})?.key}
+        
+        // MARK: - Subviews
+        private lazy var scrollView = ContentHuggingScrollView(scrollableAxis: .vertical, contentInset: .zero)
+        private lazy var innerStackView = UIStackView(axis: .vertical, spacing: 10, alignment: .fill, distribution: .fill)
+        
+        override func setUp() {
+            super.setUp()
+            stackView.addArrangedSubview(scrollView)
+            
+            // stackView
+            scrollView.contentView.addSubview(innerStackView)
+            innerStackView.autoPinEdgesToSuperviewEdges()
+            
+            // views
+            stackView.addArrangedSubviews(data.keys.sorted(by: {(data[$0] ?? data[$1] ?? false)}).map {self.createCell(item: $0)})
+            
+            // reload
+            reloadData()
+        }
+        
+        func reloadData() {
+            for cell in cells {
+                guard let item = cell.item else {continue}
+                cell.radioButton.isSelected = data[item] ?? false
+            }
+        }
+        
+        func createCell(item: T) -> Cell<T> {
+            let cell = Cell<T>(height: 66)
+            cell.item = item
+            cell.onTap(self, action: #selector(rowDidSelect(_:)))
+            return cell
+        }
+        
+        @objc private func rowDidSelect(_ gesture: UIGestureRecognizer) {
+            guard let cell = gesture.view as? Cell<T>,
+                  let item = cell.item,
+                  let isCellSelected = data[item],
+                  isCellSelected == false
+            else {return}
+            itemDidSelect(item)
+        }
+        
+        func itemDidSelect(_ item: T) {
+            data[item] = true
+            
+            // deselect all other networks
+            data.keys.filter {$0 != item}.forEach {data[$0] = false}
+            
+            reloadData()
+        }
+    }
+}
+
+extension Settings.SingleSelectionVC {
+    class Cell<T>: BEView {
+        var item: T?
+        
+        lazy var label = UILabel(text: nil)
+        
+        lazy var radioButton: WLRadioButton = {
+            let checkBox = WLRadioButton()
+            checkBox.isUserInteractionEnabled = false
+            return checkBox
+        }()
+        
+        override func commonInit() {
+            super.commonInit()
+            backgroundColor = .contentBackground
+            let stackView = UIStackView(axis: .horizontal, spacing: 16, alignment: .center, distribution: .fill, arrangedSubviews: [
+                radioButton, label
+            ])
+            addSubview(stackView)
+            stackView.autoPinEdgesToSuperviewEdges(with: .init(all: 20))
+        }
+    }
 }
