dab0db94add0641cf7fa36503f6e245d60ec3b32
Chung Tran <bigearsenal@gmail.com>
2021-10-12 16:11:18 +0700

    some fixes

---
+++ p2p_wallet/Models/LocalizedLanguage.swift
@@ -0,0 +1,20 @@
+//
+//  LocalizedLanguage.swift
+//  p2p_wallet
+//
+//  Created by Chung Tran on 12/10/2021.
+//
+
+import Foundation
+
+struct LocalizedLanguage: Hashable, Codable, DefaultsSerializable {
+    var code: String
+    
+    var localizedName: String? {
+        Locale.shared.localizedString(forLanguageCode: code)
+    }
+    
+    var originalName: String? {
+        Locale(identifier: code).localizedString(forLanguageCode: code)
+    }
+}
--- p2p_wallet/Scenes/Main/ProfileVC/SelectLanguageVC.swift
+++ p2p_wallet/Scenes/Main/ProfileVC/SelectLanguageVC.swift
@@ -7,18 +7,6 @@
 
 import Foundation
 
-struct LocalizedLanguage: Hashable, Codable, DefaultsSerializable {
-    var code: String
-    
-    var localizedName: String? {
-        Locale.shared.localizedString(forLanguageCode: code)
-    }
-    
-    var originalName: String? {
-        Locale(identifier: code).localizedString(forLanguageCode: code)
-    }
-}
-
 protocol ChangeLanguageResponder {
     func languageDidChange(to: LocalizedLanguage)
 }
