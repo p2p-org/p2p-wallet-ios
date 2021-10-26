5091c14fc6373d53984c2f0f840845ce71bc21fc
Chung Tran <bigearsenal@gmail.com>
2021-10-26 16:17:35 +0700

    refactor(orcaswap): separate logic to reduce OrcaSwapV2.ViewModel's size

--- p2p_wallet/Scenes/Main/SwapToken/OrcaSwapV2/OrcaSwapV2.ViewModel/OrcaSwapV2.ViewModel.swift
+++ p2p_wallet/Scenes/Main/SwapToken/OrcaSwapV2/OrcaSwapV2.ViewModel/OrcaSwapV2.ViewModel.swift
@@ -9,41 +9,6 @@ import Foundation
 import RxSwift
 import RxCocoa
 
-protocol OrcaSwapV2ViewModelType: WalletDidSelectHandler, SwapTokenSettingsViewModelType, SwapTokenSwapFeesViewModelType {
-    var navigationDriver: Driver<OrcaSwapV2.NavigatableScene?> {get}
-    var loadingStateDriver: Driver<LoadableState> {get}
-    var sourceWalletDriver: Driver<Wallet?> {get}
-    var destinationWalletDriver: Driver<Wallet?> {get}
-    var isTokenPairValidDriver: Driver<Loadable<Bool>> {get}
-    var bestPoolsPairDriver: Driver<OrcaSwap.PoolsPair?> {get}
-    var inputAmountDriver: Driver<Double?> {get}
-    var estimatedAmountDriver: Driver<Double?> {get}
-    var feesDriver: Driver<Loadable<[PayingFee]>> {get}
-    var availableAmountDriver: Driver<Double?> {get}
-    var slippageDriver: Driver<Double> {get}
-    var minimumReceiveAmountDriver: Driver<Double?> {get}
-    var exchangeRateDriver: Driver<Double?> {get}
-    var isExchangeRateReversed: Driver<Bool> {get}
-    var payingTokenDriver: Driver<PayingToken> {get}
-    var errorDriver: Driver<OrcaSwapV2.VerificationError?> {get}
-    
-    func reload()
-    func log(_ event: AnalyticsEvent)
-    func navigate(to scene: OrcaSwapV2.NavigatableScene)
-    func chooseSourceWallet()
-    func chooseDestinationWallet()
-    func retryLoadingRoutes()
-    func swapSourceAndDestination()
-    func useAllBalance()
-    func enterInputAmount(_ amount: Double?)
-    func enterEstimatedAmount(_ amount: Double?)
-    func changeSlippage(to slippage: Double)
-    func reverseExchangeRate()
-    func changePayingToken(to payingToken: PayingToken)
-    
-    func authenticateAndSwap()
-}
-
 extension OrcaSwapV2 {
     class ViewModel {
         // MARK: - Dependencies
