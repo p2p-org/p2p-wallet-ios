6ffe123cd31af1ac487298acccedcd0a782586d7
Chung Tran <bigearsenal@gmail.com>
2021-10-26 10:25:14 +0700

    add(feature): WLTermsAndServiceView

---
+++ p2p_wallet/UIKit/Views/WLTermsAndServiceView.swift
@@ -0,0 +1,45 @@
+//
+//  WLTermsAndServiceView.swift
+//  p2p_wallet
+//
+//  Created by Chung Tran on 26/10/2021.
+//
+
+import Foundation
+import BEPureLayout
+import Down
+import UIKit
+
+class WLTermsAndServiceView: BEView {
+    private let scrollView = ContentHuggingScrollView(scrollableAxis: .vertical, contentInset: .init(top: 20, left: 20, bottom: 0, right: 20))
+    private let label = UILabel(text: nil, textSize: 15, numberOfLines: 0)
+    
+    override func commonInit() {
+        super.commonInit()
+        
+        addSubview(scrollView)
+        scrollView.autoPinEdgesToSuperviewEdges()
+        
+        scrollView.contentView.addSubview(label)
+        label.autoPinEdgesToSuperviewEdges()
+    }
+    
+    func load() {
+        // markdown
+        showIndetermineHud()
+        
+        DispatchQueue.global(qos: .userInteractive).async {
+            // TODO: - Localization
+            let filepath = Bundle.main.path(forResource: "Terms_of_service", ofType: "txt")!
+            let contents = try! String(contentsOfFile: filepath)
+            
+            let down = Down(markdownString: contents)
+            let attributedString = try! down.toAttributedString(.default, stylesheet: "* {font-family: Helvetica; font-size: 15px } code, pre { font-family: Menlo; font-size: 15px }")
+            
+            DispatchQueue.main.async { [weak self] in
+                self?.hideHud()
+                self?.label.attributedText = attributedString
+            }
+        }
+    }
+}
