a5f4addeb8f23257d3e5abb822a67bdc00d6953f
Chung Tran <bigearsenal@gmail.com>
2021-10-27 11:54:22 +0700

    writting documentation

---
+++ p2p_wallet/Scenes/Main/SelectRecepient/RecipientsCollectionView/SelectRecipient.RecipientsListViewModel.swift
@@ -0,0 +1,40 @@
+//
+//  RecipientsListViewModel.swift
+//  p2p_wallet
+//
+//  Created by Chung Tran on 27/10/2021.
+//
+
+import Foundation
+import RxSwift
+import BECollectionView
+
+extension SelectRecipient {
+    class RecipientsListViewModel: BEListViewModel<Recipient> {
+        // MARK: - Dependencies
+        @Injected private var nameService: NameServiceType
+        @Injected private var addressFormatter: AddressFormatterType
+        
+        // MARK: - Properties
+        var name: String = ""
+        
+        // MARK: - Methods
+        /// The only methods that MUST be inheritted
+        override func createRequest() -> Single<[Recipient]> {
+            guard !name.isEmpty else {return .just([])}
+            return nameService
+                .getOwners(name)
+                .map { [weak addressFormatter] in
+                    guard let addressFormatter = addressFormatter else { return [] }
+
+                    return $0.map {
+                        Recipient(
+                            address: $0.owner,
+                            shortAddress: addressFormatter.shortAddress(of: $0.owner),
+                            name: $0.name
+                        )
+                    }
+                }
+        }
+    }
+}
