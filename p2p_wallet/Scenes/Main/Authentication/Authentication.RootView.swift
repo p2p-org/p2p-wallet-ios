6f9478939b48025f0c8e5bdf78bf352c15b5734b
Chung Tran <bigearsenal@gmail.com>
2021-11-12 23:20:11 +0700

    Add authentication scene

---
+++ p2p_wallet/Scenes/Main/Authentication/Authentication.RootView.swift
@@ -0,0 +1,42 @@
+//
+//  Authentication.RootView.swift
+//  p2p_wallet
+//
+//  Created by Chung Tran on 12/11/2021.
+//
+
+import UIKit
+import RxSwift
+
+extension Authentication {
+    class RootView: BEView {
+        // MARK: - Constants
+        let disposeBag = DisposeBag()
+        
+        // MARK: - Properties
+        @Injected private var viewModel: AuthenticationViewModelType
+        
+        // MARK: - Subviews
+        
+        // MARK: - Methods
+        override func commonInit() {
+            super.commonInit()
+            layout()
+            bind()
+        }
+        
+        override func didMoveToWindow() {
+            super.didMoveToWindow()
+            
+        }
+        
+        // MARK: - Layout
+        private func layout() {
+            
+        }
+        
+        private func bind() {
+            
+        }
+    }
+}
---
+++ p2p_wallet/Scenes/Main/Authentication/Authentication.ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  Authentication.ViewController.swift
+//  p2p_wallet
+//
+//  Created by Chung Tran on 12/11/2021.
+//
+
+import Foundation
+import UIKit
+
+extension Authentication {
+    class ViewController: BaseVC {
+        // MARK: - Dependencies
+        @Injected private var viewModel: AuthenticationViewModelType
+        
+        // MARK: - Properties
+        
+        // MARK: - Methods
+        override func loadView() {
+            view = RootView()
+        }
+        
+        override func setUp() {
+            super.setUp()
+            
+        }
+        
+        override func bind() {
+            super.bind()
+            viewModel.navigationDriver
+                .drive(onNext: {[weak self] in self?.navigate(to: $0)})
+                .disposed(by: disposeBag)
+        }
+        
+        // MARK: - Navigation
+        private func navigate(to scene: NavigatableScene?) {
+            switch scene {
+            case .resetPincodeWithASeedPhrase:
+                break
+            default:
+                break
+            }
+        }
+    }
+}
