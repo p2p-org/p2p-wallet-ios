16e131738ea0e383ec35f8faaae0d4ab20bebd9f
Chung Tran <bigearsenal@gmail.com>
2021-11-16 14:36:19 +0700

    feat: completed EnableNotificationsVC

--- p2p_wallet/Scenes/Onboarding/Onboarding.EnableNotificationsVC.swift
+++ p2p_wallet/Scenes/Onboarding/Onboarding.EnableNotificationsVC.swift
@@ -7,9 +7,14 @@
 
 import Foundation
 import UIKit
+import BEPureLayout
 
 extension Onboarding {
     class EnableNotificationsVC: BaseVC {
+        override var preferredNavigationBarStype: BEViewController.NavigationBarStyle {
+            .hidden
+        }
+        
         // MARK: - Dependencies
         @Injected private var viewModel: OnboardingViewModelType
         
@@ -19,8 +24,9 @@ extension Onboarding {
             // navigation bar
             let navigationBar = WLNavigationBar(forAutoLayout: ())
             navigationBar.backButton.isHidden = true
+            navigationBar.titleLabel.text = L10n.letSStayInTouch
             
-            let skipButton = UIButton(label: L10n.skip, textColor: .h5887ff)
+            let skipButton = UIButton(label: L10n.skip.uppercaseFirst, textColor: .h5887ff)
                 .onTap(self, action: #selector(buttonSkipDidTouch))
             navigationBar.rightItems.addArrangedSubview(skipButton)
             
@@ -35,14 +41,71 @@ extension Onboarding {
                 numberOfLines: 0
             )
                 .padding(.init(all: 18), backgroundColor: .fafafc, cornerRadius: 12)
+            view.addSubview(explanationView)
+            explanationView.autoPinEdge(.top, to: .bottom, of: navigationBar, withOffset: 18)
+            explanationView.autoPinEdge(toSuperviewSafeArea: .leading, withInset: 18)
+            explanationView.autoPinEdge(toSuperviewSafeArea: .trailing, withInset: 18)
+            
+            // collection
+            let scrollView = ContentHuggingScrollView(scrollableAxis: .vertical, contentInset: .init(x: 0, y: 20))
+            view.addSubview(scrollView)
+            scrollView.autoPinEdge(.top, to: .bottom, of: explanationView)
+            scrollView.autoPinEdge(toSuperviewEdge: .leading, withInset: 18)
+            scrollView.autoPinEdge(toSuperviewEdge: .trailing, withInset: 18)
+            
+            // button
+            let allowButton = WLStepButton.main(
+                image: .notificationsButtonSmall,
+                text: L10n.allowNotifications
+            )
+                .onTap(self, action: #selector(buttonAllowDidTouch))
+            view.addSubview(allowButton)
+            allowButton.autoPinEdgesToSuperviewSafeArea(with: .init(all: 18), excludingEdge: .top)
+            allowButton.autoPinEdge(.top, to: .bottom, of: scrollView)
+            
+            // items
+            let stackView = UIStackView(axis: .vertical, spacing: 8, alignment: .fill, distribution: .fill) {
+                createCell(image: .appIconSmall, title: L10n.newStakingOptionAvailable, subtitle: L10n.getUpTo8APYOnStakingUSDC, timeLabelText: L10n.justNow)
+                createCell(image: .renbtcPlaceholder, subimage: .appIconSmall, title: L10n.receivedRenBTC(0.01), subtitle: L10n.from + " bc1qa5wkgaew2dk...9hz6", timeLabelText: L10n.mAgo(2))
+                createCell(image: .ethPlaceholder, subimage: .appIconSmall, title: L10n.successfullySent("1.2 ETH"), subtitle: L10n.to + " 0xc377814e01DB2ed4...f1cB", timeLabelText: L10n.mAgo(6))
+                createCell(image: .swapPlaceholder, subimage: .appIconSmall, title: L10n.received("1.23 SOL"), subtitle: L10n.swappedSuccessfully("USDC", "SOL"), timeLabelText: L10n.hAgo(1))
+            }
+            scrollView.contentView.addSubview(stackView)
+            stackView.autoPinEdgesToSuperviewEdges()
         }
         
-        @objc private func buttonAcceptDidTouch() {
+        @objc private func buttonAllowDidTouch() {
             viewModel.requestRemoteNotifications()
         }
         
         @objc private func buttonSkipDidTouch() {
             viewModel.markNotificationsAsSet()
+        }
+        
+        private func createCell(image: UIImage, subimage: UIImage? = nil, title: String, subtitle: String, timeLabelText: String?) -> UIView {
+            let view = BERoundedCornerShadowView(shadowColor: .textBlack.withAlphaComponent(0.05), radius: 8, offset: .init(width: 0, height: 1), opacity: 1, cornerRadius: 16, contentInset: .init(all: 10))
+            view.stackView.axis = .horizontal
+            view.stackView.spacing = 10
+            view.stackView.alignment = .center
+            let imageView = UIImageView(width: 40, height: 40, image: image)
+            view.stackView.addArrangedSubviews {
+                imageView
+                UIStackView(axis: .vertical, spacing: 0, alignment: .fill, distribution: .fill) {
+                    UIStackView(axis: .horizontal, spacing: 10, alignment: .fill, distribution: .fill) {
+                        UILabel(text: title, textSize: 13, weight: .semibold, numberOfLines: 0)
+                        UILabel(text: timeLabelText, textSize: 13, textColor: .textSecondary, textAlignment: .right)
+                    }
+                    
+                    UILabel(text: subtitle, textSize: 13, numberOfLines: 0)
+                }
+            }
+            if let subimage = subimage {
+                let subImageView = UIImageView(width: 16, height: 16, image: subimage)
+                view.addSubview(subImageView)
+                subImageView.autoPinEdge(.trailing, to: .trailing, of: imageView, withOffset: 5.33)
+                subImageView.autoPinEdge(.bottom, to: .bottom, of: imageView, withOffset: 1.33)
+            }
+            return view
         }
     }
 }
