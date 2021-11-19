4960563a59b8516dd0dc4b95890a89caf7c10d80
Chung Tran <bigearsenal@gmail.com>
2021-11-19 11:54:14 +0700

    feat(analytics): auto-generate params

---
+++ p2p_wallet/Models/MirrorableEnum.swift
@@ -0,0 +1,27 @@
+//
+//  MirrorableEnum.swift
+//  p2p_wallet
+//
+//  Created by Chung Tran on 19/11/2021.
+//
+
+import Foundation
+
+protocol MirrorableEnum {}
+extension MirrorableEnum {
+    var mirror: (label: String, params: [String: Any]) {
+        get {
+            let reflection = Mirror(reflecting: self)
+            guard reflection.displayStyle == .enum,
+                let associated = reflection.children.first else {
+                    return ("\(self)", [:])
+            }
+            let values = Mirror(reflecting: associated.value).children
+            var valuesArray = [String: Any]()
+            for case let item in values where item.label != nil {
+                valuesArray[item.label!] = item.value
+            }
+            return (associated.label!, valuesArray)
+        }
+    }
+}
