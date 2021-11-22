723217f96d852cc268d55ddaeda9179cf7f29738
Chung Tran <bigearsenal@gmail.com>
2021-11-22 16:07:13 +0700

    feat: InfoOverviewView

--- p2p_wallet/Scenes/Main/WalletDetail/Info/WalletDetail.InfoOverviewView.swift
+++ p2p_wallet/Scenes/Main/WalletDetail/Info/WalletDetail.InfoOverviewView.swift
@@ -11,16 +11,49 @@ import UIKit
 
 extension WalletDetail {
     class InfoOverviewView: WLOverviewView {
+        // MARK: - Properties
+        private let viewModel: WalletDetailViewModelType
+        
         // MARK: - Subviews
         private lazy var coinImageView = CoinLogoImageView(size: 44, cornerRadius: 12)
         private lazy var amountLabel = UILabel(text: "<amount>", textSize: 20, weight: .bold)
         private lazy var equityValueLabel = UILabel(text: "<equity value>", textSize: 13, weight: .semibold)
-        private lazy var change24hLabel = UILabel(text: "<change 24h>", textSize: 13, weight: .semibold)
+        private lazy var change24hLabel = UILabel(text: "<change 24h>", textSize: 13, weight: .semibold, textColor: .h5887ff)
         
         private lazy var sendButton = createButton(image: .buttonSend, title: L10n.send)
             .onTap(self, action: #selector(buttonSendDidTouch))
         private lazy var swapButton = createButton(image: .buttonSwap, title: L10n.swap)
             .onTap(self, action: #selector(buttonSwapDidTouch))
+        
+        // MARK: - Initializer
+        init(viewModel: WalletDetailViewModelType) {
+            self.viewModel = viewModel
+            super.init()
+            bind()
+        }
+        
+        // MARK: - Methods
+        override func createTopView() -> UIView {
+            UIStackView(axis: .horizontal, spacing: 18, alignment: .center, distribution: .fill) {
+                coinImageView
+                UIStackView(axis: .horizontal, spacing: 8, alignment: .fill, distribution: .fill) {
+                    equityValueLabel
+                    change24hLabel
+                }
+            }
+                .padding(.init(x: 18, y: 21))
+        }
+        
+        override func createButtonsView() -> UIView {
+            UIStackView(axis: .horizontal, spacing: 0, alignment: .fill, distribution: .fillEqually) {
+                sendButton
+                swapButton
+            }
+        }
+        
+        func bind() {
+            
+        }
         
         // MARK: - Actions
         @objc private func buttonSendDidTouch() {
@@ -31,20 +64,4 @@ extension WalletDetail {
             
         }
     }
-}
-
-private func createButton(image: UIImage, title: String) -> UIView {
-    let view = UIView(forAutoLayout: ())
-    
-    let stackView = UIStackView(axis: .horizontal, spacing: 4, alignment: .center, distribution: .fill)
-        {
-            UIImageView(width: 24, height: 24, image: image, tintColor: .h5887ff)
-            UILabel(text: title, textSize: 15, weight: .medium, textColor: .h5887ff)
-        }
-    
-    view.addSubview(stackView)
-    stackView.autoAlignAxis(toSuperviewAxis: .vertical)
-    stackView.autoPinEdge(toSuperviewEdge: .top, withInset: 18)
-    stackView.autoPinEdge(toSuperviewEdge: .bottom, withInset: 18)
-    return view
 }
