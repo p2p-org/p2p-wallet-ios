0d72f718292b439e110626dbf9acfe86171ce8c4
Chung Tran <bigearsenal@gmail.com>
2021-11-22 16:56:18 +0700

    feat: Info.Chart

--- p2p_wallet/Scenes/Main/WalletDetail/Info/WalletDetail.InfoViewController.swift
+++ p2p_wallet/Scenes/Main/WalletDetail/Info/WalletDetail.InfoViewController.swift
@@ -19,6 +19,14 @@ extension WalletDetail {
         
         // MARK: - Subviews
         private lazy var overviewView = InfoOverviewView(viewModel: viewModel)
+        lazy var lineChartView = ChartView()
+        lazy var chartPicker: HorizontalPicker = {
+            let chartPicker = HorizontalPicker(forAutoLayout: ())
+            chartPicker.labels = Period.allCases.map {$0.shortString}
+            chartPicker.selectedIndex = Period.allCases.firstIndex(where: {$0 == .last1h})!
+            chartPicker.delegate = self
+            return chartPicker
+        }()
         
         // MARK: - Initializers
         init(viewModel: WalletDetailViewModelType) {
@@ -31,10 +39,29 @@ extension WalletDetail {
             super.setUp()
             let stackView = UIStackView(axis: .vertical, spacing: 8, alignment: .fill, distribution: .fill) {
                 overviewView
+                lineChartView
+                chartPicker
                 UIView.spacer
             }
             view.addSubview(stackView)
             stackView.autoPinEdgesToSuperviewEdges(with: .init(all: 18, excludingEdge: .top))
+            
+            viewModel.graphViewModel.reload()
         }
+        
+        override func bind() {
+            super.bind()
+            lineChartView
+                .subscribed(to: viewModel.graphViewModel)
+                .disposed(by: disposeBag)
+        }
+    }
+}
+
+extension WalletDetail.InfoViewController: HorizontalPickerDelegate {
+    func picker(_ picker: HorizontalPicker, didSelectOptionAtIndex index: Int) {
+        guard index < Period.allCases.count else {return}
+        viewModel.graphViewModel.period = Period.allCases[index]
+        viewModel.graphViewModel.reload()
     }
 }
