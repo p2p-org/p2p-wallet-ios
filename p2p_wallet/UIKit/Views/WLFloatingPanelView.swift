60d6d02b777db06d67a2b2d8264dfdc21c58e4b3
Chung Tran <bigearsenal@gmail.com>
2021-11-23 10:49:14 +0700

    feat: WLFloatingPanelView

---
+++ p2p_wallet/UIKit/Views/WLFloatingPanelView.swift
@@ -0,0 +1,22 @@
+//
+//  WLRoundedCornerShadowView.swift
+//  p2p_wallet
+//
+//  Created by Chung Tran on 23/11/2021.
+//
+
+import Foundation
+import UIKit
+import BEPureLayout
+
+class WLFloatingPanelView: BERoundedCornerShadowView {
+    // MARK: - Initializer
+    init(contentInset: UIEdgeInsets = .zero) {
+        super.init(shadowColor: UIColor.black.withAlphaComponent(0.05), radius: 8, offset: CGSize(width: 0, height: 1), opacity: 1, cornerRadius: 8, contentInset: contentInset)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        border(width: 1, color: .f2f2f7.onDarkMode(.white.withAlphaComponent(0.1)))
+    }
+}
