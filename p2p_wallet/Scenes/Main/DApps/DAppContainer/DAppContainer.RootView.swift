dbe625e121bcd2c37b44b0064ab0427277dd0314
Chung Tran <bigearsenal@gmail.com>
2021-11-26 10:56:56 +0700

    fix: change to appropriate method

--- p2p_wallet/Scenes/Main/DApps/DAppContainer/DAppContainer.RootView.swift
+++ p2p_wallet/Scenes/Main/DApps/DAppContainer/DAppContainer.RootView.swift
@@ -29,7 +29,7 @@ extension DAppContainer {
         
         override func didMoveToWindow() {
             super.didMoveToWindow()
-            reload()
+            load()
         }
         
         // MARK: - Layout
