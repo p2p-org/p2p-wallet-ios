9c104d7094b44d11e653c0669d53f0552d3db7e8
Chung Tran <bigearsenal@gmail.com>
2021-11-30 15:29:57 +0700

    feat: find recipient

--- p2p_wallet/Scenes/Main/SendToken/Subscenes/ChooseRecipientAndNetwork/SelectAddress/Subviews/RecipientsCollectionView/SendToken.ChooseRecipientAndNetwork.SelectAddress.RecipientCell.swift
+++ p2p_wallet/Scenes/Main/SendToken/Subscenes/ChooseRecipientAndNetwork/SelectAddress/Subviews/RecipientsCollectionView/SendToken.ChooseRecipientAndNetwork.SelectAddress.RecipientCell.swift
@@ -1,12 +1,57 @@
 //
-//  RecipientView.swift
+//  SelectRecipient.RecipientCell.swift
 //  p2p_wallet
 //
-//  Created by Andrew Vasiliev on 25.10.2021.
+//  Created by Andrew Vasiliev on 24.10.2021.
 //
 
 import UIKit
-import BEPureLayout
+import BECollectionView
+
+extension SendToken.ChooseRecipientAndNetwork.SelectAddress {
+    final class RecipientCell: UICollectionViewCell, BECollectionViewCell {
+        private let recipientView = RecipientView()
+        
+        override init(frame: CGRect) {
+            super.init(frame: frame)
+            addSubviews()
+            setConstraints()
+        }
+
+        @available(*, unavailable)
+        required init?(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+
+        func setRecipient(_ recipient: Recipient) {
+            recipientView.setRecipient(recipient)
+        }
+
+        private func addSubviews() {
+            [recipientView].forEach(addSubview)
+        }
+
+        private func setConstraints() {
+            recipientView.autoAlignAxis(toSuperviewAxis: .horizontal)
+            recipientView.autoPinEdge(toSuperviewEdge: .leading, withInset: 20)
+            recipientView.autoPinEdge(toSuperviewEdge: .trailing, withInset: 20)
+        }
+        
+        // MARK: - BECollectionViewCell implementation
+        func setUp(with item: AnyHashable?) {
+            guard let recipient = item as? Recipient else {return}
+            recipientView.setRecipient(recipient)
+        }
+        
+        func hideLoading() {
+            recipientView.hideLoader()
+        }
+        
+        func showLoading() {
+            recipientView.showLoader()
+        }
+    }
+}
 
 final class RecipientView: UIStackView {
     private let recipientIcon = UIImageView()
@@ -41,7 +86,7 @@ final class RecipientView: UIStackView {
 
     private func configureSelf() {
         axis = .horizontal
-        spacing = 15
+        spacing = 12
         alignment = .center
         distribution = .fill
     }
