02cba2caee10608b9488c024e54658b6e4490ef7
Chung Tran <bigearsenal@gmail.com>
2021-11-30 14:25:45 +0700

    feat: inputState

--- p2p_wallet/Scenes/Main/SendToken/Subscenes/ChooseRecipientAndNetwork/SelectAddress/SendToken.ChooseRecipientAndNetwork.SelectAddress.swift
+++ p2p_wallet/Scenes/Main/SendToken/Subscenes/ChooseRecipientAndNetwork/SelectAddress/SendToken.ChooseRecipientAndNetwork.SelectAddress.swift
@@ -13,5 +13,24 @@ extension SendToken.ChooseRecipientAndNetwork {
         enum NavigatableScene {
             case detail
         }
+        
+        enum AddressType: Equatable {
+            case raw(address: String, hasFunds: Bool)
+            case resolvedName(name: String, address: String)
+        }
+        
+        enum InputState: Equatable {
+            case entering(String?)
+            case selected(AddressType)
+            
+            var isEntering: Bool {
+                switch self {
+                case .entering:
+                    return true
+                default:
+                    return false
+                }
+            }
+        }
     }
 }
--- p2p_wallet/Scenes/Main/SendToken/Subscenes/ChooseRecipientAndNetwork/SelectAddress/Subviews/SendToken.ChooseRecipientAndNetwork.SelectAddress.AddressInputView.swift
+++ p2p_wallet/Scenes/Main/SendToken/Subscenes/ChooseRecipientAndNetwork/SelectAddress/Subviews/SendToken.ChooseRecipientAndNetwork.SelectAddress.AddressInputView.swift
@@ -10,17 +10,39 @@ import UIKit
 
 extension SendToken.ChooseRecipientAndNetwork.SelectAddress {
     final class AddressInputView: UIStackView {
+        // MARK: - Dependencies
+        private let viewModel: SendTokenChooseRecipientAndNetworkSelectAddressViewModelType
+        
+        // MARK: - Subviews
         lazy var textField = createTextField()
         lazy var clearButton = UIImageView(width: 17, height: 17, image: .crossIcon)
         
-        convenience init(forConvenience: Void) {
-            self.init(axis: .horizontal, spacing: 8, alignment: .center, distribution: .fill)
+        // MARK: - Initializers
+        init(viewModel: SendTokenChooseRecipientAndNetworkSelectAddressViewModelType) {
+            self.viewModel = viewModel
+            super.init(frame: .zero)
+            self.set(axis: .horizontal, spacing: 8, alignment: .center, distribution: .fill)
             self.addArrangedSubviews {
                 UIImageView(width: 24, height: 24, image: .buttonSearch, tintColor: .a3a5ba)
-                    .padding(.init(all: 14), backgroundColor: .f6f6f8, cornerRadius: 20)
+                    .padding(.init(all: 10), backgroundColor: .f6f6f8, cornerRadius: 16)
                 textField
                 clearButton
             }
+            bind()
+        }
+        
+        required init(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+        
+        // MARK: - Methods
+        override func didMoveToWindow() {
+            super.didMoveToWindow()
+            textField.becomeFirstResponder()
+        }
+        
+        private func bind() {
+            
         }
         
         // MARK: - Helpers
