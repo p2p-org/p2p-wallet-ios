6a37f7240863c745cf44d1e321c89f0f95a5c1b6
Chung Tran <bigearsenal@gmail.com>
2021-12-01 16:08:20 +0700

    feat: select network

--- p2p_wallet/Scenes/Main/SendToken/Subscenes/ChooseRecipientAndNetwork/SelectAddress/SendToken.ChooseRecipientAndNetwork.SelectAddress.SelectNetworkViewController.swift
+++ p2p_wallet/Scenes/Main/SendToken/Subscenes/ChooseRecipientAndNetwork/SelectAddress/SendToken.ChooseRecipientAndNetwork.SelectAddress.SelectNetworkViewController.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import BEPureLayout
+import UIKit
 
 extension SendToken.ChooseRecipientAndNetwork.SelectAddress {
     final class SelectNetworkViewController: SendToken.BaseViewController {
@@ -14,16 +15,18 @@ extension SendToken.ChooseRecipientAndNetwork.SelectAddress {
         private let viewModel: SendTokenChooseRecipientAndNetworkSelectAddressViewModelType
         
         // MARK: - Subviews
-        private lazy var networkViews: [NetworkView] = {
+        private lazy var networkViews: [_NetworkView] = {
             var networkViews = viewModel.getSelectableNetwork()
-                .map {network -> NetworkView in
-                    let view = NetworkView()
+                .map {network -> _NetworkView in
+                    let view = _NetworkView()
+                    view.network = network
                     view.setUp(network: network, fee: network.defaultFee, renBTCPrice: viewModel.getRenBTCPrice())
-                    return view
+                    return view.onTap(self, action: #selector(networkViewDidTouch(_:)))
                 }
             
             return networkViews
         }()
+        private lazy var tickView = UIImageView(width: 14.3, height: 14.19, image: .tick, tintColor: .h5887ff)
         
         // MARK: - Initializer
         init(viewModel: SendTokenChooseRecipientAndNetworkSelectAddressViewModelType) {
@@ -52,12 +55,34 @@ extension SendToken.ChooseRecipientAndNetwork.SelectAddress {
             }
             
             // networks
-            for index, view in networkViews.enumerated() {
+            for (index, view) in networkViews.enumerated() {
                 rootView.stackView.addArrangedSubview(view.padding(.init(x: 0, y: 26)))
                 if index < networkViews.count - 1 {
                     rootView.stackView.addArrangedSubview(.separator(height: 1, color: .separator))
                 }
             }
         }
+        
+        override func bind() {
+            super.bind()
+            viewModel.networkDriver
+                .drive(onNext: {[weak self] network in
+                    guard let self = self else {return}
+                    self.tickView.removeFromSuperview()
+                    self.networkViews.first(where: {$0.network == network})?.addArrangedSubview(self.tickView)
+                })
+                .disposed(by: disposeBag)
+        }
+        
+        @objc private func networkViewDidTouch(_ gesture: UITapGestureRecognizer) {
+            guard let view = gesture.view as? _NetworkView, let network = view.network else {
+                return
+            }
+            viewModel.selectNetwork(network)
+        }
+    }
+    
+    private class _NetworkView: NetworkView {
+        fileprivate var network: SendToken.Network?
     }
 }
