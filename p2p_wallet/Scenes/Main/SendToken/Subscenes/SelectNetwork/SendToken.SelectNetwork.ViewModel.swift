de852d70321b7ebcb33c28b1350e9300817829e2
Chung Tran <bigearsenal@gmail.com>
2021-12-10 10:43:43 +0700

    feat: new SendToken.SelectNetwork scene

---
+++ p2p_wallet/Scenes/Main/SendToken/Subscenes/SelectNetwork/SendToken.SelectNetwork.ViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  SendToken.SelectNetwork.ViewModel.swift
+//  p2p_wallet
+//
+//  Created by Chung Tran on 10/12/2021.
+//
+
+import Foundation
+
+protocol SendTokenSelectNetworkViewModelType {
+    func getSelectableNetworks() -> [SendToken.Network]
+    func getSelectedRecipient() -> SendToken.Recipient?
+    func getAPIClient() -> SendTokenAPIClient
+    func getSOLAndRenBTCPrices() -> [String: Double]
+    func getSelectedNetwork() -> SendToken.Network
+    func selectRecipient(_ recipient: SendToken.Recipient?)
+    func selectNetwork(_ network: SendToken.Network)
+    func navigateToChooseRecipientAndNetworkWithPreSelectedNetwork(_ network: SendToken.Network)
+}
