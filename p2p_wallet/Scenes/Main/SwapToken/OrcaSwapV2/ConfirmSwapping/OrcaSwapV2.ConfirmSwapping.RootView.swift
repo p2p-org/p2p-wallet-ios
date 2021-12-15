ac4b6aa3d9b335c04fa2efd3284245b17d61ddbc
Chung Tran <bigearsenal@gmail.com>
2021-12-15 14:56:53 +0700

    feat(confirm-swapping): WalletView

--- p2p_wallet/Scenes/Main/SwapToken/OrcaSwapV2/ConfirmSwapping/OrcaSwapV2.ConfirmSwapping.RootView.swift
+++ p2p_wallet/Scenes/Main/SwapToken/OrcaSwapV2/ConfirmSwapping/OrcaSwapV2.ConfirmSwapping.RootView.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 import UIKit
+import RxSwift
+import RxCocoa
 
 extension OrcaSwapV2.ConfirmSwapping {
     final class RootView: ScrollableVStackRootView {
@@ -36,10 +38,16 @@ extension OrcaSwapV2.ConfirmSwapping {
         private func setUp() {
             stackView.addArrangedSubviews {
                 UIView.floatingPanel(contentInset: .init(x: 8, y: 16), axis: .horizontal, spacing: 8, alignment: .center, distribution: .equalCentering) {
+                    WalletView(viewModel: viewModel, type: .source)
+                        .centered(.horizontal)
+                    
                     UIImageView(width: 11.88, height: 9.74, image: .arrowForward, tintColor: .h8e8e93)
                         .withContentHuggingPriority(.required, for: .horizontal)
                         .padding(.init(all: 10), backgroundColor: .fafafc, cornerRadius: 12)
                         .withContentHuggingPriority(.required, for: .horizontal)
+                    
+                    WalletView(viewModel: viewModel, type: .destination)
+                        .centered(.horizontal)
                 }
             }
             
@@ -54,6 +62,96 @@ extension OrcaSwapV2.ConfirmSwapping {
                 self.bannerView.isHidden = true
             }
             viewModel.closeBanner()
+        }
+    }
+}
+
+extension OrcaSwapV2.ConfirmSwapping {
+    private final class WalletView: UIStackView {
+        enum WalletType {
+            case source, destination
+        }
+        
+        // MARK: - Properties
+        private let viewModel: OrcaSwapV2ConfirmSwappingViewModelType
+        private let type: WalletType
+        private let disposeBag = DisposeBag()
+        
+        // MARK: - Subviews
+        private lazy var coinLogoImageView = CoinLogoImageView(size: 44, cornerRadius: 12)
+        private lazy var amountLabel = UILabel(text: nil, textSize: 15, textAlignment: .center)
+        private lazy var equityAmountLabel = UILabel(text: nil, textSize: 13, textColor: .textSecondary, textAlignment: .center)
+        
+        // MARK: - Initializers
+        init(viewModel: OrcaSwapV2ConfirmSwappingViewModelType, type: WalletType) {
+            self.viewModel = viewModel
+            self.type = type
+            super.init(frame: .zero)
+            set(axis: .vertical, spacing: 0, alignment: .center, distribution: .fill)
+            layout()
+            bind()
+        }
+        
+        required init(coder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+        
+        // MARK: - Methods
+        private func layout() {
+            addArrangedSubviews {
+                coinLogoImageView
+                BEStackViewSpacing(8)
+                amountLabel
+                equityAmountLabel
+            }
+        }
+        
+        private func bind() {
+            let walletDriver = type == .source ? viewModel.sourceWalletDriver: viewModel.destinationWalletDriver
+            
+            walletDriver
+                .drive(coinLogoImageView.rx.wallet)
+                .disposed(by: disposeBag)
+            
+            let tokenAmountDriver = type == .source ? viewModel.inputAmountDriver: viewModel.estimatedAmountDriver
+            
+            Driver.combineLatest(
+                walletDriver,
+                tokenAmountDriver
+            )
+                .map {wallet, amount in
+                    amount.toString(maximumFractionDigits: 9) + " " + wallet?.token.symbol
+                }
+                .drive(amountLabel.rx.text)
+                .disposed(by: disposeBag)
+            
+            if type == .source {
+                Driver.combineLatest(
+                    walletDriver,
+                    tokenAmountDriver
+                )
+                    .map {wallet, amount in
+                        "~ " +
+                            (amount * wallet?.priceInCurrentFiat).toString(maximumFractionDigits: 2) +
+                            " " +
+                            Defaults.fiat.code
+                    }
+                    .drive(equityAmountLabel.rx.text)
+                    .disposed(by: disposeBag)
+            } else if type == .destination {
+                Driver.combineLatest(
+                    walletDriver,
+                    viewModel.minimumReceiveAmountDriver
+                )
+                    .map {wallet, amount in
+                        "≥ " +
+                        amount?.toString(maximumFractionDigits: 9) +
+                        " " +
+                        (wallet?.token.symbol ?? "")
+                    }
+                    .drive(equityAmountLabel.rx.text)
+                    .disposed(by: disposeBag)
+            }
         }
     }
 }
