bb35116d1399730c0e58c5ee4f557ae0b1e03c4f
Tran Giang Long <trangianglong4003@gmail.com>
2021-12-15 13:50:12 +0100

    feat(moonpay)

--- p2p_wallet/Scenes/Main/ReceiveToken/ReceiveToken.Scene.swift
+++ p2p_wallet/Scenes/Main/ReceiveToken/ReceiveToken.Scene.swift
@@ -61,5 +61,15 @@ extension ReceiveToken {
                 }
             }
         }
+        
+        override func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(animated)
+            self.tabBarController?.tabBar.isHidden = false
+        }
+    
+        override func viewWillDisappear(_ animated: Bool) { // As soon as vc disappears
+            super.viewWillDisappear(true)
+            self.tabBarController?.tabBar.isHidden = true
+        }
     }
 }
