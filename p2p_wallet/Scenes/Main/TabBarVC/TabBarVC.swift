455e8416269850e1349ef55efb571b25c7e7b20f
Chung Tran <bigearsenal@gmail.com>
2021-12-16 16:30:05 +0700

    refactor: remove unused code

--- p2p_wallet/Scenes/Main/TabBarVC/TabBarVC.swift
+++ p2p_wallet/Scenes/Main/TabBarVC/TabBarVC.swift
@@ -6,8 +6,6 @@
 //
 
 import Foundation
-import UIKit
-import RxSwift
 
 protocol TabBarScenesFactory {
     func makeHomeViewController() -> Home.ViewController
@@ -17,7 +15,6 @@ protocol TabBarScenesFactory {
 
 class TabBarVC: BEPagesVC {
     lazy var tabBar = NewTabBar()
-    private let disposeBag = DisposeBag()
     private var tabBarTopConstraint: NSLayoutConstraint!
     
     let scenesFactory: TabBarScenesFactory
