423bca6981755063eaae7e9c1b6a588e408679dc
Chung Tran <bigearsenal@gmail.com>
2021-12-22 10:56:41 +0700

    feat: remove testing

--- p2p_wallet/Scenes/Main/SendToken/Subscenes/ChooseRecipientAndNetwork/SendToken.ChooseRecipientAndNetwork.ViewController.swift
+++ p2p_wallet/Scenes/Main/SendToken/Subscenes/ChooseRecipientAndNetwork/SendToken.ChooseRecipientAndNetwork.ViewController.swift
@@ -16,7 +16,6 @@ extension SendToken.ChooseRecipientAndNetwork {
         private let viewModel: SendTokenChooseRecipientAndNetworkViewModelType
         
         // MARK: - Properties
-        override var preferredTabBarHidden: Bool? {false}
         
         // MARK: - Subviews
         private lazy var pagesVC = WLSegmentedPagesVC(items: [
