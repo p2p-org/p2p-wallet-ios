ba3c6f0442f9d7421f29a332185837d64267d5cd
Chung Tran <bigearsenal@gmail.com>
2021-12-26 11:38:19 +0700

    fix(memory-leaks): ChooseWallet + TokenSettingsViewModel

--- p2p_wallet/Scenes/Main/ChooseWallet/ChooseWallet.ViewModel.swift
+++ p2p_wallet/Scenes/Main/ChooseWallet/ChooseWallet.ViewModel.swift
@@ -53,8 +53,9 @@ extension ChooseWallet {
                                 Wallet(pubkey: nil, lamports: nil, token: $0)
                             }
                     }
-                    .map {
-                        self.myWallets + $0.filter {otherWallet in !self.myWallets.contains(where: {$0.token.symbol == otherWallet.token.symbol})}
+                    .map { [weak self] in
+                        guard let self = self else {return []}
+                        return self.myWallets + $0.filter {otherWallet in !self.myWallets.contains(where: {$0.token.symbol == otherWallet.token.symbol})}
                     }
             }
             return .just(myWallets)
