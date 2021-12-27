d536a29a510f7cd8333af94fd6f7b49179b6e309
Tran Giang Long <trangianglong4003@gmail.com>
2021-12-27 09:23:47 +0300

    feat(receive-screen): improve confirmation screen

--- p2p_wallet/Scenes/Main/ReceiveToken/NetworkSelection/NetworkSelection.BitcoinConfirm.ViewController.swift
+++ p2p_wallet/Scenes/Main/ReceiveToken/NetworkSelection/NetworkSelection.BitcoinConfirm.ViewController.swift
@@ -21,30 +21,51 @@ extension ReceiveToken {
         
         override var preferredNavigationBarStype: NavigationBarStyle { .hidden }
         
+        override var padding: UIEdgeInsets { .zero }
+        
         override func build() -> UIView? {
             UIStackView(axis: .vertical, alignment: .fill) {
                 
+                // Title
                 UIStackView(axis: .vertical, alignment: .center) {
                     UILabel(text: L10n.receivingViaBitcoinNetwork, textSize: 20, weight: .semibold)
                         .padding(.init(only: .bottom, inset: 4))
                     UILabel(text: L10n.makeSureYouUnderstandTheAspects, textColor: .textSecondary)
-                }
+                }.padding(.init(all: 18, excludingEdge: .bottom))
                 
-                UIStackView(axis: .vertical, alignment: .center) {
+                // Icon
+                BEZStack() {
+                    UIView.defaultSeparator().withTag(1)
                     UIImageView(width: 44, height: 44, image: .squircleAlert)
+                        .centered(.horizontal)
+                        .withTag(2)
+                }.setup { view in
+                    if let subview = view.viewWithTag(1) {
+                        subview.autoPinEdge(toSuperviewEdge: .left)
+                        subview.autoPinEdge(toSuperviewEdge: .right)
+                        subview.autoCenterInSuperView(leftInset: 0, rightInset: 0)
+                    }
+                    if let subview = view.viewWithTag(2) {
+                        subview.autoPinEdgesToSuperviewEdges()
+                    }
                 }.padding(.init(x: 0, y: 18))
                 
-                ReceiveToken.textBuilder(text: L10n.ThisAddressAccepts.youMayLoseAssetsBySendingAnotherCoin(L10n.onlyBitcoin))
-                ReceiveToken.textBuilder(text: L10n.minimumTransactionAmountOf("0.000112 BTC"))
-                ReceiveToken.textBuilder(text: L10n.isTheRemainingTimeToSafelySendTheAssets("35:59:59"))
+                // Description
+                UIStackView(axis: .vertical, spacing: 12, alignment: .fill) {
+                    ReceiveToken.textBuilder(text: L10n.ThisAddressAcceptsOnly.youMayLoseAssetsBySendingAnotherCoin(L10n.onlyBitcoin).asMarkdown())
+                    ReceiveToken.textBuilder(text: L10n.minimumTransactionAmountOf("0.000112 BTC").asMarkdown())
+                    ReceiveToken.textBuilder(text: L10n.isTheRemainingTimeToSafelySendTheAssets("35:59:59").asMarkdown())
+                }.padding(.init(x: 18, y: 0))
                 
-                WLStepButton.main(image: .checkBoxIOS, text: L10n.iUnderstand)
+                // Accept button
+                WLStepButton.main(image: .check.withTintColor(.white), text: L10n.iUnderstand)
                     .padding(.init(only: .top, inset: 36))
                     .onTap { [unowned self] in
                         self.back()
                         self.onAccept?()
                     }
-            }.padding(.init(all: 18))
+                    .padding(.init(x: 18, y: 4))
+            }
         }
     }
 }
