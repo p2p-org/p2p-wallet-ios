dfa4bc4f87b0a252a38d26c4192ec4dbbabb657d
Tran Giang Long <trangianglong4003@gmail.com>
2022-01-12 11:28:43 +0300

    fix(settings)

--- p2p_wallet/Scenes/Main/Settings/Settings.ViewController.swift
+++ p2p_wallet/Scenes/Main/Settings/Settings.ViewController.swift
@@ -25,7 +25,7 @@ extension Settings {
         
         override func build() -> UIView {
             UIStackView(axis: .vertical, alignment: .fill) {
-                NewWLNavigationBar(title: L10n.settings, separatorEnable: false)
+                NewWLNavigationBar(initialTitle: L10n.settings, separatorEnable: false)
                     .onBack { [unowned self] in self.back() }
                 
                 BEScrollView(contentInsets: .init(x: 18, y: 18), spacing: 36) {
