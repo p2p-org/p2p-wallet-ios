4fbb4333634c20e6613f2c9e750bc68e6a4b4c8b
Chung Tran <bigearsenal@gmail.com>
2022-01-25 15:03:06 +0700

    feat: add swap manager

---
+++ p2p_wallet/Services/SwapManager.swift
@@ -0,0 +1,16 @@
+//
+//  SwapManager.swift
+//  p2p_wallet
+//
+//  Created by Chung Tran on 25/01/2022.
+//
+
+import Foundation
+
+protocol SwapManagerType {
+    
+}
+
+class SwapManager: SwapManagerType {
+    
+}
