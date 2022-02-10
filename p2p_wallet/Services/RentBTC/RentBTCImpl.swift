1eb3f48b77584468d00387a284b8514427aaff20
Giang Long Tran <trangianglong4003@gmail.com>
2022-02-10 15:37:31 +0300

    feat(rent-btc): improve

--- p2p_wallet/Services/RentBTC/RentBTCImpl.swift
+++ p2p_wallet/Services/RentBTC/RentBTCImpl.swift
@@ -2,30 +2,88 @@
 // Created by Giang Long Tran on 07.02.2022.
 //
 
-import Foundation
-import RxSwift
 import FeeRelayerSwift
+import Foundation
+import OrcaSwapSwift
+import RxSwift
 
-//class RentBtcServiceImpl: RentBTC.Service {
-//    private let solanaSDK: SolanaSDK
-//    private let feeRelayer: FeeRelayer.Relay
-//
-//    init(solanaSDK: SolanaSDK) { self.solanaSDK = solanaSDK }
-//
-//    func hasAssociatedTokenAccountBeenCreated() -> Single<Bool> {
-//        solanaSDK.hasAssociatedTokenAccountBeenCreated(tokenMint: .renBTCMint)
-//    }
-//
-//    func createAssociatedTokenAccount() -> Single<SolanaSDK.TransactionID> {
-//        guard let account = solanaSDK.accountStorage.account else { return .error(SolanaSDK.Error.unauthorized) }
-//        solanaSDK.createAssociatedTokenAccount(
-//            for: account.publicKey,
-//            tokenMint: .renBTCMint,
-//            payer: nil,
-//            isSimulation: false
-//        )
-//
-//    return  .just("")
-//    }
-//
-//}
+class RentBtcServiceImpl: RentBTC.Service {
+    private let solanaSDK: SolanaSDK
+    private let feeRelayerApi: FeeRelayerAPIClientType
+    private let accountStorage: AccountStorageType
+    private let orcaSwap: OrcaSwapType
+
+    private var feeRelayer: FeeRelayer.Relay? = nil
+
+    init(
+        solanaSDK: SolanaSDK,
+        feeRelayerApi: FeeRelayerAPIClientType,
+        accountStorage: AccountStorageType,
+        orcaSwap: OrcaSwapType
+    ) {
+        self.solanaSDK = solanaSDK
+        self.feeRelayerApi = feeRelayerApi
+        self.accountStorage = accountStorage
+        self.orcaSwap = orcaSwap
+    }
+
+    func load() -> Completable {
+        do {
+            feeRelayer = try FeeRelayer.Relay(
+                apiClient: feeRelayerApi,
+                solanaClient: solanaSDK,
+                accountStorage: accountStorage,
+                orcaSwapClient: orcaSwap
+            )
+        } catch {
+            return .error(error)
+        }
+        return .empty()
+    }
+
+    func hasAssociatedTokenAccountBeenCreated() -> Single<Bool> {
+        solanaSDK.hasAssociatedTokenAccountBeenCreated(tokenMint: .renBTCMint)
+    }
+
+    func createAssociatedTokenAccount(payingFeeAddress: String, payingFeeMintAddress: String) -> Single<SolanaSDK.TransactionID> {
+        guard let account = accountStorage.account else { return .error(SolanaSDK.Error.unauthorized) }
+        
+        return Single.zip(
+            solanaSDK.getMinimumBalanceForRentExemption(span: 165),
+            solanaSDK.getLamportsPerSignature(),
+            feeRelayerApi.getFeePayerPubkey()
+        ).flatMap { minimumBalanceForRentExemption, lamports, payer in
+            var transaction = SolanaSDK.Transaction()
+            transaction.instructions.append(
+                try solanaSDK.createAssociatedTokenAccountInstruction(
+                    for: account.publicKey,
+                    tokenMint: .renBTCMint,
+                    payer: try SolanaSDK.PublicKey(string: payer)
+                )
+            )
+
+            let transactionFee = try transaction.calculateTransactionFee(lamportsPerSignatures: lamports)
+            let accountCreationFee = minimumBalanceForRentExemption
+
+            let expectedFee = SolanaSDK.FeeAmount(
+                transaction: transactionFee,
+                accountBalances: accountCreationFee
+            )
+
+            let preparedTransaction = SolanaSDK.PreparedTransaction(
+                transaction: transaction,
+                signers: [account],
+                expectedFee: expectedFee
+            )
+
+            feeRelayer?.topUpAndRelayTransaction(
+                preparedTransaction: preparedTransaction,
+                payingFeeToken: FeeRelayer.Relay.TokenInfo(
+                    address: payingFeeAddress,
+                    mint: payingFeeMintAddress
+                )
+            )
+        }
+    }
+
+}
