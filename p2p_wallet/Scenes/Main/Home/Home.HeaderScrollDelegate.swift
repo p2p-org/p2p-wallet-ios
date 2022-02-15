2ebca4153c4eb0a3e2568b2b87d7b2e26301d1e1
Giang Long Tran <trangianglong4003@gmail.com>
2022-02-15 12:26:56 +0300

    feat(home): fix

--- p2p_wallet/Scenes/Main/Home/Home.HeaderScrollDelegate.swift
+++ p2p_wallet/Scenes/Main/Home/Home.HeaderScrollDelegate.swift
@@ -1,5 +1,5 @@
 //
-//  Home.BalancesScrollDelegate.swift
+//  Home.HeaderScrollDelegate.swift
 //  p2p_wallet
 //
 //  Created by Andrew Vasiliev on 24.11.2021.
@@ -22,25 +22,18 @@ extension Home {
             let offset = (scrollView.contentOffset.y + scrollView.contentInset.top)
             let value = min(max(offset, 0), maxValue)
             
-            print([value.truncatingRemainder(dividingBy: 2)])
-            
             guard value > 0 else {
                 headerView.collapseConstraint.constant = 0
                 headerView.bottomCollapseConstraint.constant = headerView.bottomMaxHeight
                 return
             }
             
-            if (value < headerView.preferredTopHeight) {
-                // collapse top
-                headerView.collapseConstraint.constant = value
-            } else {
-                // collapse bottom
-                let bottomValue = headerView.bottomMaxHeight - (value - headerView.preferredTopHeight)
-                print("=== \(bottomValue)")
-                headerView.bottomCollapseConstraint.constant = min(max(headerView.bottomMinHeight, bottomValue), headerView.bottomMaxHeight)
-            }
+            // collapse top
+            headerView.collapseConstraint.constant = min(value, headerView.preferredTopHeight)
             
-            
+            // collapse bottom
+            // let bottomValue = headerView.bottomMaxHeight - (value - headerView.preferredTopHeight)
+            // headerView.bottomCollapseConstraint.constant = min(max(headerView.bottomMinHeight, bottomValue), headerView.bottomMaxHeight)
         }
     }
 }
