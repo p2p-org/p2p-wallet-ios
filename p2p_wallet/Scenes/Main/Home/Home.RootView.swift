3dbd3ca4187edba446a79ffda12c96a5a316cf6a
Giang Long Tran <trangianglong4003@gmail.com>
2022-02-15 15:09:08 +0300

    feat(home): improve

--- p2p_wallet/Scenes/Main/Home/Home.RootView.swift
+++ p2p_wallet/Scenes/Main/Home/Home.RootView.swift
@@ -9,6 +9,7 @@ import Action
 import BECollectionView
 import BEPureLayout
 import RxSwift
+import RxCocoa
 import UIKit
 
 extension Home {
@@ -26,93 +27,102 @@ extension Home {
         init(viewModel: HomeViewModelType) {
             self.viewModel = viewModel
             super.init(frame: .zero)
+            
+            viewModel.walletsRepository.reload()
         }
         
         override func build() -> UIView {
             BESafeArea {
                 BEVStack {
-                    BEVStack {
-                        // Title
-                        BEHStack {
-                            UILabel(textAlignment: .center)
-                                .setupWithType(UILabel.self) { label in
-                                    let p2pWallet = NSMutableAttributedString()
-                                        .text(L10n.p2PWallet, size: 17, weight: .semibold)
-                                        .text(" ")
-                                        .text(L10n.beta, size: 17, weight: .semibold, color: .secondaryLabel)
-                                    label.attributedText = p2pWallet
+                    // Title
+                    BEHStack {
+                        UILabel(textAlignment: .center)
+                            .setupWithType(UILabel.self) { label in
+                                let p2pWallet = NSMutableAttributedString()
+                                    .text(L10n.p2PWallet, size: 17, weight: .semibold)
+                                    .text(" ")
+                                    .text(L10n.beta, size: 17, weight: .semibold, color: .secondaryLabel)
+                                label.attributedText = p2pWallet
+                            }
+                    }.padding(.init(x: 0, y: 12))
+                    
+                    // Indicator
+                    WLStatusIndicatorView(forAutoLayout: ()).setupWithType(WLStatusIndicatorView.self) { view in
+                        viewModel.currentPricesDriver
+                            .map { $0.state }
+                            .drive(onNext: { [weak view] state in
+                                switch state {
+                                case .notRequested:
+                                    view?.isHidden = true
+                                case .loading:
+                                    view?.setUp(state: .loading, text: L10n.updatingPrices)
+                                case .loaded:
+                                    view?.setUp(state: .success, text: L10n.pricesUpdated)
+                                case .error:
+                                    view?.setUp(state: .error, text: L10n.errorWhenUpdatingPrices)
                                 }
-                        }.padding(.init(x: 0, y: 12))
-                        
-                        // Indicator
-                        WLStatusIndicatorView(forAutoLayout: ()).setupWithType(WLStatusIndicatorView.self) { view in
-                            viewModel.currentPricesDriver
-                                .map { $0.state }
-                                .drive(onNext: { [weak view] state in
-                                    switch state {
-                                    case .notRequested:
-                                        view?.isHidden = true
-                                    case .loading:
-                                        view?.setUp(state: .loading, text: L10n.updatingPrices)
-                                    case .loaded:
-                                        view?.setUp(state: .success, text: L10n.pricesUpdated)
-                                    case .error:
-                                        view?.setUp(state: .error, text: L10n.errorWhenUpdatingPrices)
-                                    }
-                                })
-                                .disposed(by: disposeBag)
-                        }
+                            })
+                            .disposed(by: disposeBag)
                     }
                     
-                    BEZStack {
-                        // Tokens
-                        BEZStackPosition(mode: .fill) {
-                            WalletsCollectionView(
-                                walletsRepository: viewModel.walletsRepository,
-                                activeWalletsSection: .init(
-                                    index: 0,
-                                    viewModel: viewModel.walletsRepository,
-                                    cellType: WalletCell.self
-                                ),
-                                hiddenWalletsSection: HiddenWalletsSection(
-                                    index: 1,
-                                    viewModel: viewModel.walletsRepository,
-                                    header: .init(viewClass: HiddenWalletsSectionHeaderView.self)
-                                )
-                            ).setupWithType(WalletsCollectionView.self) { collectionView in
-                                self.collectionView = collectionView
-                                collectionView.delegate = self
-                                collectionView.scrollDelegate = headerViewScrollDelegate
-                                collectionView.walletCellEditAction = Action<Wallet, Void> { [weak self] wallet in
-                                    self?.viewModel.navigate(to: .walletSettings(wallet: wallet))
-                                    return .just(())
-                                }
-                                collectionView.showHideHiddenWalletsAction = CocoaAction { [weak self] in
-                                    self?.viewModel.walletsRepository.toggleIsHiddenWalletShown()
-                                    return .just(())
-                                }
-                                collectionView.contentInset.modify(dTop: 220, dBottom: 50)
-                                collectionView.refresh()
-                            }
-                        }
-    
-                        BEZStackPosition(mode: .pinEdges(top: true, left: true, bottom: false, right: true)) {
-                            FloatingHeaderView()
-                                .setupWithType(FloatingHeaderView.self) { view in
-                                    headerViewScrollDelegate.headerView = view
-                                    let walletsRepository = viewModel.walletsRepository
-                                    walletsRepository
-                                        .dataObservable
-                                        .withLatestFrom(walletsRepository.stateObservable, resultSelector: { ($0 ?? [], $1) })
-                                        .asDriver(onErrorJustReturn: ([], .loaded))
-                                        .drive(view.balanceView.rx.balance)
-                                        .disposed(by: disposeBag)
-                                }
-                                .padding(.init(x: 18, y: 0))
-                        }
+                    BEBuilder(driver: viewModel.isWalletReadyDriver) { [weak self] state in
+                        guard let self = self else { return UIView() }
+                        return state ? self.content() : self.emptyScreen()
                     }
                 }
             }
+        }
+        
+        func emptyScreen() -> UIView {
+            EmptyView(viewModel: viewModel)
+        }
+        
+        func content() -> UIView {
+            BEZStack {
+                // Tokens
+                BEZStackPosition(mode: .fill) {
+                    WalletsCollectionView(
+                        walletsRepository: viewModel.walletsRepository,
+                        activeWalletsSection: .init(
+                            index: 0,
+                            viewModel: viewModel.walletsRepository,
+                            cellType: WalletCell.self
+                        ),
+                        hiddenWalletsSection: HiddenWalletsSection(
+                            index: 1,
+                            viewModel: viewModel.walletsRepository,
+                            header: .init(viewClass: HiddenWalletsSectionHeaderView.self)
+                        )
+                    ).setupWithType(WalletsCollectionView.self) { collectionView in
+                        self.collectionView = collectionView
+                        collectionView.delegate = self
+                        collectionView.scrollDelegate = headerViewScrollDelegate
+                        collectionView.walletCellEditAction = Action<Wallet, Void> { [weak self] wallet in
+                            self?.viewModel.navigate(to: .walletSettings(wallet: wallet))
+                            return .just(())
+                        }
+                        collectionView.showHideHiddenWalletsAction = CocoaAction { [weak self] in
+                            self?.viewModel.walletsRepository.toggleIsHiddenWalletShown()
+                            return .just(())
+                        }
+                        collectionView.contentInset.modify(dTop: 180, dBottom: 120)
+                        collectionView.clipsToBounds = true
+                        
+                        viewModel
+                            .isWalletReadyDriver
+                            .map { !$0 }
+                            .drive(collectionView.rx.isHidden)
+                            .disposed(by: disposeBag)
+                    }.padding(.init(only: .top, inset: 10))
+                }
+                
+                // Action bar
+                BEZStackPosition(mode: .pinEdges(top: true, left: true, bottom: false, right: true)) {
+                    FloatingHeaderView(viewModel: viewModel)
+                        .setupWithType(FloatingHeaderView.self) { view in headerViewScrollDelegate.headerView = view }
+                        .padding(.init(x: 18, y: 0))
+                }
+            }.padding(.init(only: .top, inset: 30))
         }
     }
 }
@@ -121,5 +131,26 @@ extension Home.RootView: BECollectionViewDelegate {
     func beCollectionView(collectionView: BECollectionViewBase, didSelect item: AnyHashable) {
         guard let wallet = item as? Wallet else { return }
         viewModel.navigate(to: .walletDetail(wallet: wallet))
+    }
+}
+
+private extension HomeViewModelType {
+    var isWalletReadyDriver: Driver<Bool> {
+        Observable.zip(
+                walletsRepository.stateObservable,
+                walletsRepository.dataObservable
+            )
+            .map { ($0, $1 ?? []) }
+            .map { (state, wallets) in
+                (state, wallets.reduce(0, { (
+                    partialResult,
+                    wallet
+                ) in
+                    partialResult + wallet.amountInCurrentFiat
+                }))
+            }.map { (state, amount) in
+                if state != .loaded { return true }
+                return amount > 0
+            }.asDriver(onErrorJustReturn: true)
     }
 }
