c92a2c4e452c93bfdc3fae79671cb80a2185df4b
Chung Tran <bigearsenal@gmail.com>
2022-03-05 19:37:04 +0700

    feat: TransactionsViewModel

--- p2p_wallet/Services/TransactionHandler/TransactionHandler.swift
+++ p2p_wallet/Services/TransactionHandler/TransactionHandler.swift
@@ -14,7 +14,9 @@ protocol TransactionHandlerType {
     func sendTransaction(_ processingTransaction: ProcessingTransactionType) -> TransactionIndex
     func observeTransaction(transactionIndex: TransactionIndex) -> Observable<PT.TransactionInfo?>
     func areSomeTransactionsInProgress() -> Bool
-    func processingTransactionsObservable() -> Observable<[PT.TransactionInfo]>
+    
+    func observeProcessingTransactions(forAccount account: String) -> Observable<[SolanaSDK.ParsedTransaction]>
+    func getProccessingTransactions(of account: String) -> [SolanaSDK.ParsedTransaction]
 }
 
 class TransactionHandler: TransactionHandlerType {
@@ -35,7 +37,7 @@ class TransactionHandler: TransactionHandlerType {
         // add to processing
         var value = transactionsSubject.value
         value.append(
-            .init(transactionId: nil, rawTransaction: processingTransaction, status: .sending)
+            .init(transactionId: nil, sentAt: Date(), rawTransaction: processingTransaction, status: .sending)
         )
         transactionsSubject.accept(value)
         
@@ -54,8 +56,89 @@ class TransactionHandler: TransactionHandlerType {
         transactionsSubject.value.contains(where: {$0.status.isProcessing})
     }
     
-    func processingTransactionsObservable() -> Observable<[PT.TransactionInfo]> {
-        transactionsSubject.asObservable()
+    func observeProcessingTransactions(forAccount account: String) -> Observable<[SolanaSDK.ParsedTransaction]> {
+        transactionsSubject
+            .map {[weak self] _ in self?.getProccessingTransactions(of: account) ?? []}
+            .asObservable()
+    }
+    
+    func getProccessingTransactions(of account: String) -> [SolanaSDK.ParsedTransaction] {
+        let pendingTransactions = transactionsSubject.value
+            .filter {$0.status.isProcessing}
+            .filter { pt in
+                switch pt.rawTransaction {
+                case let transaction as PT.SendTransaction:
+                    if transaction.sender.pubkey == account ||
+                        transaction.receiver.address == account ||
+                        transaction.authority == account
+                    {
+                        return true
+                    }
+                case let transaction as PT.OrcaSwapTransaction:
+                    if transaction.sourceWallet.pubkey == account ||
+                        transaction.destinationWallet.pubkey == account ||
+                        transaction.authority == account
+                    {
+                        return true
+                    }
+                default:
+                    break
+                }
+                return false
+            }
+        
+        return pendingTransactions
+            .map { pt in
+                // status
+                let status: SolanaSDK.ParsedTransaction.Status
+                
+                switch pt.status {
+                case .sending:
+                    status = .requesting
+                case .confirmed:
+                    status = .processing(percent: 0)
+                case .finalized:
+                    status = .confirmed
+                case .error(let error):
+                    status = .error(error.readableDescription)
+                }
+            
+                let signature = pt.transactionId
+                
+                var value: AnyHashable?
+                let amountInFiat: Double?
+                let fee: UInt64?
+                
+                switch pt.rawTransaction {
+                case let transaction as PT.SendTransaction:
+                    let amount = transaction.amount.convertToBalance(decimals: transaction.sender.token.decimals)
+                    value = SolanaSDK.TransferTransaction(
+                        source: transaction.sender,
+                        destination: Wallet(pubkey: transaction.receiver.address, lamports: 0, token: transaction.sender.token),
+                        authority: walletsRepository.nativeWallet?.pubkey,
+                        destinationAuthority: nil,
+                        amount: amount,
+                        myAccount: transaction.sender.pubkey
+                    )
+                    amountInFiat = amount * pricesService.currentPrice(for: transaction.sender.token.symbol)?.value
+                    fee = transaction.feeInSOL
+                case let transaction as PT.OrcaSwapTransaction:
+                    value = SolanaSDK.SwapTransaction(
+                        source: transaction.sourceWallet,
+                        sourceAmount: transaction.amount,
+                        destination: transaction.destinationWallet,
+                        destinationAmount: transaction.estimatedAmount,
+                        myAccountSymbol: nil
+                    )
+                    amountInFiat = transaction.amount * pricesService.currentPrice(for: transaction.sourceWallet.token.symbol)?.value
+                    fee = transaction.fees.networkFees?.total
+                default:
+                    amountInFiat = nil
+                    fee = 0
+                }
+                
+                return .init(status: status, signature: signature, value: value, amountInFiat: amountInFiat, slot: 0, blockTime: pt.sentAt, fee: fee, blockhash: nil)
+            }
     }
     
     // MARK: - Helpers
@@ -70,6 +153,7 @@ class TransactionHandler: TransactionHandlerType {
                 self.updateTransactionAtIndex(index) { _ in
                     .init(
                         transactionId: transactionID,
+                        sentAt: Date(),
                         rawTransaction: processingTransaction,
                         status: .confirmed(0)
                     )
