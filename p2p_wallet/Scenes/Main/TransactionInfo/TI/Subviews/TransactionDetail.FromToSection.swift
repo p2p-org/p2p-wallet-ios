094c54f3eff5122c5a32cf0fabe1a6c6983caf7e
Chung Tran <bigearsenal@gmail.com>
2022-03-09 14:29:29 +0700

    feat: Bind FromToSection

--- p2p_wallet/Scenes/Main/TransactionInfo/TI/Subviews/TransactionDetail.FromToSection.swift
+++ p2p_wallet/Scenes/Main/TransactionInfo/TI/Subviews/TransactionDetail.FromToSection.swift
@@ -7,9 +7,13 @@
 
 import Foundation
 import UIKit
+import RxSwift
+import RxCocoa
 
 extension TransactionDetail {
     final class FromToSection: UIStackView {
+        private let disposeBag = DisposeBag()
+        
         private let fromTitleLabel = titleLabel()
         private let fromAddressLabel = addressLabel()
         private let fromNameLabel = nameLabel()
@@ -52,6 +56,86 @@ extension TransactionDetail {
         
         required init(coder: NSCoder) {
             fatalError("init(coder:) has not been implemented")
+        }
+        
+        func driven(with driver: Driver<SolanaSDK.ParsedTransaction?>) -> TransactionDetail.FromToSection {
+            let isSwapDriver = driver.map {$0?.value is SolanaSDK.SwapTransaction}
+            
+            isSwapDriver
+                .drive(fromNameLabel.rx.isHidden, toNameLabel.rx.isHidden)
+                .disposed(by: disposeBag)
+            
+            isSwapDriver
+                .map {$0 ? L10n.from: L10n.senderSAddress}
+                .drive(fromTitleLabel.rx.text)
+                .disposed(by: disposeBag)
+            
+            isSwapDriver
+                .map {$0 ? L10n.to: L10n.recipientSAddress}
+                .drive(toTitleLabel.rx.text)
+                .disposed(by: disposeBag)
+            
+            driver
+                .map {$0?.value}
+                .map { transaction -> String? in
+                    switch transaction {
+                    case let transaction as SolanaSDK.SwapTransaction:
+                        return transaction.source?.pubkey
+                    case let transaction as SolanaSDK.TransferTransaction:
+                        return transaction.source?.pubkey
+                    default:
+                        return nil
+                    }
+                }
+                .drive(fromAddressLabel.rx.text)
+                .disposed(by: disposeBag)
+            
+            driver
+                .map {$0?.value}
+                .map { transaction -> String? in
+                    switch transaction {
+                    case let transaction as SolanaSDK.SwapTransaction:
+                        return transaction.source?.userInfo as? String
+                    case let transaction as SolanaSDK.TransferTransaction:
+                        return transaction.source?.userInfo as? String
+                    default:
+                        return nil
+                    }
+                }
+                .drive(fromNameLabel.rx.text)
+                .disposed(by: disposeBag)
+            
+            driver
+                .map {$0?.value}
+                .map { transaction -> String? in
+                    switch transaction {
+                    case let transaction as SolanaSDK.SwapTransaction:
+                        return transaction.destination?.userInfo as? String
+                    case let transaction as SolanaSDK.TransferTransaction:
+                        return transaction.destination?.userInfo as? String
+                    default:
+                        return nil
+                    }
+                }
+                .drive(toNameLabel.rx.text)
+                .disposed(by: disposeBag)
+            
+            driver
+                .map {$0?.value}
+                .map { transaction -> String? in
+                    switch transaction {
+                    case let transaction as SolanaSDK.SwapTransaction:
+                        return transaction.destination?.pubkey
+                    case let transaction as SolanaSDK.TransferTransaction:
+                        return transaction.destination?.pubkey
+                    default:
+                        return nil
+                    }
+                }
+                .drive(toAddressLabel.rx.text)
+                .disposed(by: disposeBag)
+            
+            return self
         }
     }
 }
