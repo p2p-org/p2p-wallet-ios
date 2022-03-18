5c6cd48f7fdf0da35fa00d94f9787ca852e67e0c
Chung Tran <bigearsenal@gmail.com>
2022-03-18 14:31:57 +0700

    fix: processing transaction

--- p2p_wallet/Scenes/Main/TransactionDetail/Subviews/TransactionDetail.AmountSection.swift
+++ p2p_wallet/Scenes/Main/TransactionDetail/Subviews/TransactionDetail.AmountSection.swift
@@ -28,26 +28,24 @@ extension TransactionDetail {
                 BEVStack(spacing: 8) {
                     // Swap
                     swapSection()
-                        .setup { receivedView in
-                            viewModel.parsedTransactionDriver
-                                .map {$0?.value is SolanaSDK.SwapTransaction}
-                                .map {!$0}
-                                .drive(receivedView.rx.isHidden)
-                                .disposed(by: disposeBag)
+                        .setup { view in
+                            showView(view, onlyWhenTransactionIs: SolanaSDK.SwapTransaction.self)
                         }
                     
                     // Transfer
                     transferSection()
-                        .setup { receivedView in
-                            viewModel.parsedTransactionDriver
-                                .map {$0?.value is SolanaSDK.TransferTransaction}
-                                .map {!$0}
-                                .drive(receivedView.rx.isHidden)
-                                .disposed(by: disposeBag)
+                        .setup { view in
+                            showView(view, onlyWhenTransactionIs: SolanaSDK.TransferTransaction.self)
                         }
                     
                     // Fee
                     feesSection()
+                    
+                    // Total
+                    totalSectionForTransfer()
+                        .setup { view in
+                            showView(view, onlyWhenTransactionIs: SolanaSDK.TransferTransaction.self)
+                        }
                 }
             }
         }
@@ -160,11 +158,13 @@ extension TransactionDetail {
                                     let payingWallet = self.getPayingFeeWallet()
                                     let amount = feeAmount?.accountBalances
                                         .convertToBalance(decimals: payingWallet.token.decimals)
-                                        .toString(maximumFractionDigits: 9)
                                     
-                                    return NSMutableAttributedString()
-                                        .text(amount + " " + payingWallet.token.symbol + " ", size: 15, color: .textBlack)
-                                        .text("(\(L10n.accountCreation(""))", size: 15, color: .textSecondary)
+                                    return self.getAttributedString(
+                                        amount: amount,
+                                        symbol: payingWallet.token.symbol,
+                                        withFiatValue: false
+                                    )
+                                        .text(" (\(L10n.accountCreation("")))", size: 15, color: .textSecondary)
                                 }
                                 .drive(accountCreationLabel.rx.attributedText)
                                 .disposed(by: disposeBag)
@@ -188,13 +188,16 @@ extension TransactionDetail {
                                             .convertToBalance(decimals: payingWallet.token.decimals)
                                             
                                         if amount > 0 {
-                                            return NSMutableAttributedString()
-                                                .text(amount.toString(maximumFractionDigits: 9) + " " + payingWallet.token.symbol + " ", size: 15, color: .textBlack)
-                                                .text("(\(L10n.transferFee))", size: 15, color: .textSecondary)
+                                            return self.getAttributedString(
+                                                amount: amount,
+                                                symbol: payingWallet.token.symbol,
+                                                withFiatValue: false
+                                            )
+                                                .text(" (\(L10n.transferFee))", size: 15, color: .textSecondary)
                                         } else {
                                             return NSMutableAttributedString()
-                                                .text(L10n.free + " ", size: 15, weight: .semibold)
-                                                .text("(\(L10n.PaidByP2p.org))", size: 15, color: .h34c759)
+                                                .text(L10n.free, size: 15, weight: .semibold)
+                                                .text(" (\(L10n.PaidByP2p.org))", size: 15, color: .h34c759)
                                         }
                                     }
                                     .drive(accountCreationLabel.rx.attributedText)
@@ -208,22 +211,110 @@ extension TransactionDetail {
                                     .disposed(by: disposeBag)
                             }
                     }
-                    .onTap { [weak self] in
-                        self?.viewModel.navigate(to: .freeFeeInfo)
+                        .onTap { [weak self] in
+                            self?.viewModel.navigate(to: .freeFeeInfo)
+                        }
+                    
+                    // total
+                    BEVStack(spacing: 8, alignment: .trailing) {
+                        UIView.defaultSeparator()
+                            .frame(width: 266)
+                        
+                        UILabel(text: "0.02 SOL (Transfer fee)", textSize: 15, textAlignment: .right)
+                            .setup { totalFeeLabel in
+                                feesDriver
+                                    .map { [weak self] feeAmount -> NSAttributedString? in
+                                        guard let self = self else {return nil}
+                                        let payingWallet = self.getPayingFeeWallet()
+                                        let transactionFee = feeAmount?.transaction
+                                            .convertToBalance(decimals: payingWallet.token.decimals)
+                                        
+                                        let accountCreationFee = feeAmount?.accountBalances
+                                            .convertToBalance(decimals: payingWallet.token.decimals)
+                                        
+                                        let attrStr = NSMutableAttributedString()
+                                            
+                                        if transactionFee > 0 {
+                                            // transaction fee
+                                            attrStr.append(
+                                                self.getAttributedString(
+                                                    amount: transactionFee,
+                                                    symbol: payingWallet.token.symbol,
+                                                    withFiatValue: false
+                                                )
+                                            )
+                                            
+                                            // separator if needed
+                                            if accountCreationFee > 0 {
+                                                attrStr.text(" + ", size: 15, color: .textBlack)
+                                            }
+                                        }
+                                        
+                                        if accountCreationFee > 0 {
+                                            // account creation fee
+                                            attrStr.append(
+                                                self.getAttributedString(
+                                                    amount: accountCreationFee,
+                                                    symbol: payingWallet.token.symbol,
+                                                    withFiatValue: false
+                                                )
+                                            )
+                                        }
+                                        
+                                        attrStr
+                                            .text(" (\(L10n.totalFee))", size: 15, color: .textSecondary)
+                                        return attrStr
+                                    }
+                                    .drive(totalFeeLabel.rx.attributedText)
+                                    .disposed(by: disposeBag)
+                            }
                     }
+                        .setup { view in
+                            showView(view, onlyWhenTransactionIs: SolanaSDK.SwapTransaction.self)
+                        }
                 }
             }
         }
         
-        private func getAttributedString(amount: Double?, symbol: String?) -> NSAttributedString? {
-            NSMutableAttributedString()
+        private func totalSectionForTransfer() -> BEHStack {
+            BEHStack(spacing: 4, alignment: .top) {
+                titleLabel(text: L10n.total)
+                UILabel(text: "0.00227631 renBTC (~$150)", textSize: 15, textAlignment: .right)
+                    .setup { label in
+                        viewModel.parsedTransactionDriver
+                            .map { [weak self] transaction -> NSAttributedString? in
+                                guard let self = self else {return nil}
+                                return self.getAttributedString(
+                                    amount: abs(transaction?.amount ?? 0),
+                                    symbol: transaction?.symbol
+                                )
+                            }
+                            .drive(label.rx.attributedText)
+                            .disposed(by: disposeBag)
+                    }
+            }
+        }
+        
+        private func getAttributedString(amount: Double?, symbol: String?, withFiatValue: Bool = true) -> NSMutableAttributedString {
+            let attStr = NSMutableAttributedString()
                 .text(amount.toString(maximumFractionDigits: 9) + " " + symbol, size: 15, color: .textBlack)
-                .text(" ")
-                .text("(~\(Defaults.fiat.symbol)\(viewModel.getAmountInCurrentFiat(amountInToken: amount, symbol: symbol)?.toString(maximumFractionDigits: 9) ?? "0"))", size: 15, color: .textSecondary)
+            if withFiatValue {
+                attStr.text(" ")
+                    .text("(~\(Defaults.fiat.symbol)\(viewModel.getAmountInCurrentFiat(amountInToken: amount, symbol: symbol)?.toString(maximumFractionDigits: 9) ?? "0"))", size: 15, color: .textSecondary)
+            }
+            return attStr
         }
         
         private func getPayingFeeWallet() -> Wallet {
             viewModel.getPayingFeeWallet() ?? .nativeSolana(pubkey: nil, lamport: 0)
+        }
+        
+        private func showView<T: Hashable>(_ view: UIView, onlyWhenTransactionIs type: T.Type) {
+            viewModel.parsedTransactionDriver
+                .map {$0?.value is T}
+                .map {!$0}
+                .drive(view.rx.isHidden)
+                .disposed(by: disposeBag)
         }
     }
 }
