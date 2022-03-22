4c5fa95535850b1fbb109ef897eb1e70678a75af
Chung Tran <bigearsenal@gmail.com>
2022-03-22 15:33:44 +0700

    feat: remove unused code

--- p2p_wallet/Scenes/Main/SendToken/SendToken.NetworkView.swift
+++ p2p_wallet/Scenes/Main/SendToken/SendToken.NetworkView.swift
@@ -12,10 +12,6 @@ import UIKit
 
 extension SendToken {
     class NetworkView: UIStackView {
-        // MARK: - Dependencies
-
-        private let disposeBag = DisposeBag()
-
         // MARK: - Subviews
 
         private lazy var coinImageView = UIImageView(width: 44, height: 44, image: nil)
