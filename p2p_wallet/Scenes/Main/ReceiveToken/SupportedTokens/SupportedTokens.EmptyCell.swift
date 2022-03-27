0fb3eaac071ccef84b6142f6dcd062970acb1524
Giang Long Tran <trangianglong4003@gmail.com>
2022-03-27 19:36:55 +0200

    feat(receive-screen): improve error

---
+++ p2p_wallet/Scenes/Main/ReceiveToken/SupportedTokens/SupportedTokens.EmptyCell.swift
@@ -0,0 +1,41 @@
+//
+// Created by Giang Long Tran on 27.03.2022.
+//
+
+import BEPureLayout
+import UIKit
+
+extension SupportedTokens {
+    class EmptyCell: BECollectionCell {
+        var searchKey: String = "" {
+            didSet {
+                textLabel.text = L10n
+                    .ifThereIsATokenNamedWeDonTRecommendSendingItToYourSolanaAddressSinceItWillMostLikelyBeLostForever(
+                        searchKey
+                    )
+            }
+        }
+
+        private let textLabel = BERef<UILabel>()
+
+        override func build() -> UIView {
+            BEContainer {
+                BEHStack(alignment: .center) {
+                    UIImageView(width: 44, height: 44, image: .squircleTransactionError)
+                    UILabel(
+                        text: L10n
+                            .ifThereIsATokenNamedWeDonTRecommendSendingItToYourSolanaAddressSinceItWillMostLikelyBeLostForever(
+                                searchKey
+                            ),
+                        numberOfLines: 10
+                    )
+                    .bind(textLabel)
+                    .padding(.init(x: 14, y: 18))
+                }
+            }
+            .padding(.init(x: 18, y: 0), cornerRadius: 12.0, borderColor: .ff3b30)
+            .backgroundColor(color: .fff5f5)
+            .padding(.init(x: 18, y: 0))
+        }
+    }
+}
