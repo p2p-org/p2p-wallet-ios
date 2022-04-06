6ad5dc3bb8d5b7680b3fb0a961db7cb33bfc1c44
Ivan Babich <ivan.babich@p2p.org>
2022-04-06 11:15:05 +0300

    [iOS] Decimal separation

--- p2p_wallet/Extensions/String+Extensions.swift
+++ p2p_wallet/Extensions/String+Extensions.swift
@@ -97,3 +97,36 @@ extension String {
         ))) ?? NSAttributedString()
     }
 }
+
+extension Collection {
+    func unfoldSubSequences(limitedTo maxLength: Int) -> UnfoldSequence<SubSequence, Index> {
+        sequence(state: startIndex) { start in
+            guard start < endIndex else { return nil }
+            let end = index(start, offsetBy: maxLength, limitedBy: endIndex) ?? endIndex
+            defer { start = end }
+            return self[start ..< end]
+        }
+    }
+
+    func every(n: Int) -> UnfoldSequence<Element, Index> {
+        sequence(state: startIndex) { index in
+            guard index < endIndex else { return nil }
+            defer { _ = formIndex(&index, offsetBy: n, limitedBy: endIndex) }
+            return self[index]
+        }
+    }
+
+    var pairs: [SubSequence] { .init(unfoldSubSequences(limitedTo: 2)) }
+}
+
+extension StringProtocol where Self: RangeReplaceableCollection {
+    mutating func insert<S: StringProtocol>(separator: S, every n: Int) {
+        for index in indices.every(n: n).dropFirst().reversed() {
+            insert(contentsOf: separator, at: index)
+        }
+    }
+
+    func inserting<S: StringProtocol>(separator: S, every n: Int) -> Self {
+        .init(unfoldSubSequences(limitedTo: n).joined(separator: separator))
+    }
+}
