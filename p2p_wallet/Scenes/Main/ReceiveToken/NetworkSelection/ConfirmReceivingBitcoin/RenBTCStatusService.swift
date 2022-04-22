ff5d45ec2c34e33ff9f0332bf0bfc0436234c8b2
Chung Tran <bigearsenal@gmail.com>
2022-04-22 11:40:08 +0700

    feat: RenBTC status service

---
+++ p2p_wallet/Scenes/Main/ReceiveToken/NetworkSelection/ConfirmReceivingBitcoin/RenBTCStatusService.swift
@@ -0,0 +1,50 @@
+//
+//  RenBTCStatusService.swift
+//  p2p_wallet
+//
+//  Created by Chung Tran on 22/04/2022.
+//
+
+import Foundation
+import RxSwift
+import SolanaSwift
+
+/// The `RenBTCServiceType` helps to prepare with renBTC service.
+///
+/// The protocol contains creating account, calculate fee for creation and check availability of balance.
+protocol RenBTCStatusServiceType {
+    /// Checks the associated account has been created.
+    ///
+    /// RenBTC works, when account has been created.
+    ///
+    /// - Returns: the status
+    func hasRenBTCAccountBeenCreated() -> Bool
+
+    /// Checks the associated account is creatable.
+    ///
+    /// This methods calculate all factors, that may affect to fee like account creation fee, trx fee and fee relayer fee.
+    ///
+    /// - Returns: true if account is creatable or false. The false happens when wallet balance is not enough for creating.
+    func isRenBTCAccountCreatable() async throws -> Bool
+
+    /// Creates a associated account.
+    ///
+    /// - Parameters:
+    ///   - payingFeeAddress: the address that will pay a fee.
+    ///   - payingFeeMintAddress: the mint address that will pay a fee.
+    /// - Returns: transaction signature
+    func createAccount(
+        payingFeeAddress: String,
+        payingFeeMintAddress: String
+    ) async throws -> SolanaSDK.TransactionID
+
+    /// Get amount of feed needed to create renBTC account.
+    ///
+    /// The amount will be in spl currency.
+    ///
+    /// - Parameter payingFeeMintAddress: the mint address that will be used as fee paying.
+    /// - Returns: the amount of fee
+    func getCreationFee(
+        payingFeeMintAddress: String
+    ) async throws -> SolanaSDK.Lamports
+}
