e26365540390262d176e3cb42258c7043b34d394
Ivan Babich <ivan.babich@p2p.org>
2022-04-22 17:58:05 +0300

    bug-fixing
    PWN-3428
    PWN-3422
    PWN-3430

--- p2p_wallet/Scenes/Main/History/History.Scene.swift
+++ p2p_wallet/Scenes/Main/History/History.Scene.swift
@@ -9,9 +9,7 @@ import Resolver
 import UIKit
 
 extension History {
-    class Scene: BEScene {
-        override var preferredNavigationBarStype: NavigationBarStyle { .hidden }
-
+    final class Scene: BEScene {
         @Injected private var clipboardManager: ClipboardManagerType
         @Injected private var pricesService: PricesServiceType
 
@@ -20,6 +18,7 @@ extension History {
         override init() {
             super.init()
 
+            navigationItem.title = L10n.history
             // Start loading when wallets are ready.
             Resolver.resolve(WalletsRepository.self)
                 .dataObservable
@@ -31,13 +30,9 @@ extension History {
         }
 
         override func build() -> UIView {
-            BEVStack {
-                NewWLNavigationBar(initialTitle: L10n.history, separatorEnable: false)
-                    .backIsHidden(true)
-                BEBuilder(driver: viewModel.showItems) { [weak self] show in
-                    guard let self = self else { return UIView() }
-                    return show ? self.content : EmptyTransactionsView()
-                }
+            BEBuilder(driver: viewModel.showItems) { [weak self] show in
+                guard let self = self else { return UIView() }
+                return show ? self.content : EmptyTransactionsView()
             }
         }
 
