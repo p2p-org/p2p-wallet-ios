b2df9e5cec6bbc19f93a49bcbc6645ff698f8d2b
Chung Tran <bigearsenal@gmail.com>
2022-04-22 11:34:08 +0700

    fix: ui

---
+++ p2p_wallet/Scenes/Main/ReceiveToken/NetworkSelection/ConfirmReceivingBitcoin/ConfirmReceivingBitcoin.ViewController+Extensions.swift
@@ -0,0 +1,137 @@
+//
+//  ConfirmReceivingBitcoin.ViewController+Extensions.swift
+//  p2p_wallet
+//
+//  Created by Chung Tran on 22/04/2022.
+//
+
+import Foundation
+import RxSwift
+
+extension ConfirmReceivingBitcoin.ViewController {
+    func topUpRequiredView() -> BEVStack {
+        BEVStack(spacing: 12) {
+            createExplanationView(
+                mainString: L10n.aToReceiveBitcoinsOverTheBitcoinNetwork(L10n.renBTCAccountIsRequired),
+                boldString: L10n.renBTCAccountIsRequired
+            )
+
+            createExplanationView(
+                mainString: L10n
+                    .yourWalletListDoesNotContainARenBTCAccountAndToCreateOne(L10n
+                        .youNeedToMakeATransaction),
+                boldString: L10n
+                    .youNeedToMakeATransaction
+            )
+
+            createExplanationView(
+                mainString: L10n
+                    .youToPayForAccountCreationButIfSomeoneSendsRenBTCToYourAddressItWillBeCreatedForYou(L10n
+                        .donTHaveFunds),
+                boldString: L10n
+                    .donTHaveFunds
+            )
+        }
+    }
+
+    func createRenBTCView() -> BEVStack {
+        BEVStack(spacing: 12) {
+            createExplanationView(
+                mainString: L10n
+                    .yourWalletListDoesNotContainARenBTCAccountAndToCreateOne(L10n
+                        .youNeedToMakeATransaction) + " " + L10n.youCanChooseWhichCurrencyToPayInBelow,
+                boldString: L10n
+                    .youNeedToMakeATransaction
+            )
+
+            WLCard {
+                BEHStack(spacing: 12, alignment: .center) {
+                    CoinLogoImageView(size: 44)
+
+                    BEVStack(spacing: 4) {
+                        UILabel(
+                            text: "Account creation fee:",
+                            textSize: 13,
+                            textColor: .textSecondary,
+                            numberOfLines: 0
+                        )
+                        UILabel(text: "0.509 USDC", textSize: 17, weight: .semibold)
+                    }
+
+                    UIView.defaultNextArrow()
+                }
+                .padding(.init(x: 18, y: 14))
+            }
+            .padding(.init(only: .bottom, inset: 12))
+
+            createExplanationView(
+                mainString: L10n
+                    .ThisAddressAccepts.youMayLoseAssetsBySendingAnotherCoin(L10n.onlyBitcoin),
+                boldString: L10n.onlyBitcoin
+            )
+
+            createExplanationView(
+                mainString: L10n
+                    .minimumTransactionAmountOf("0.000112 BTC"),
+                boldString: "0.000112 BTC"
+            )
+
+            createExplanationView(
+                mainString: L10n.isTheRemainingTimeToSafelySendTheAssets("35:59:59"),
+                boldString: "35:59:59"
+            )
+        }
+    }
+
+    func buttonsView() -> UIView {
+        BEVStack(spacing: 10) {
+            WLStepButton.main(image: .add, text: L10n.topUpYourAccount)
+                .setup { view in
+                    viewModel.outputDriver.map(\.accountStatus)
+                        .map { $0 != .topUpRequired }
+                        .drive(view.rx.isHidden)
+                        .disposed(by: disposeBag)
+                }
+
+            WLStepButton.sub(text: L10n.shareYourSolanaNetworkAddress)
+                .setup { view in
+                    viewModel.outputDriver.map(\.accountStatus)
+                        .map { $0 != .topUpRequired }
+                        .drive(view.rx.isHidden)
+                        .disposed(by: disposeBag)
+                }
+
+            WLStepButton.main(text: "Pay 0.509 USDC & Continue")
+                .setup { view in
+                    viewModel.outputDriver.map(\.accountStatus)
+                        .map { $0 != .payingWalletAvailable }
+                        .drive(view.rx.isHidden)
+                        .disposed(by: disposeBag)
+                }
+        }
+        .padding(.init(only: .bottom, inset: 18))
+    }
+
+    // MARK: - Text builder
+
+    private func createExplanationView(mainString: String, boldString: String) -> UIView {
+        BEHStack(spacing: 10, alignment: .top) {
+            UILabel(text: "•")
+                .withContentHuggingPriority(.required, for: .horizontal)
+                .withContentCompressionResistancePriority(.required, for: .horizontal)
+            UILabel(text: nil, numberOfLines: 0)
+                .withAttributedText(
+                    createExplanationAttributedString(
+                        mainString: mainString,
+                        boldString: boldString
+                    )
+                )
+                .withContentHuggingPriority(.required, for: .horizontal)
+        }
+    }
+
+    private func createExplanationAttributedString(mainString: String, boldString _: String) -> NSAttributedString {
+        NSMutableAttributedString()
+            .text(mainString, size: 15, color: .textBlack)
+    }
+}
--- p2p_wallet/Scenes/Main/ReceiveToken/NetworkSelection/ConfirmReceivingBitcoin/ConfirmReceivingBitcoin.ViewController.swift
+++ p2p_wallet/Scenes/Main/ReceiveToken/NetworkSelection/ConfirmReceivingBitcoin/ConfirmReceivingBitcoin.ViewController.swift
@@ -14,7 +14,7 @@ extension ConfirmReceivingBitcoin {
     class ViewController: WLModalViewController {
         // MARK: - Properties
 
-        private let viewModel: ConfirmReceivingBitcoinViewModelType
+        let viewModel: ConfirmReceivingBitcoinViewModelType
 
         // MARK: - Initializer
 
@@ -99,84 +99,6 @@ extension ConfirmReceivingBitcoin {
             }
         }
 
-        func topUpRequiredView() -> BEVStack {
-            BEVStack(spacing: 12) {
-                createExplanationView(
-                    mainString: L10n.aToReceiveBitcoinsOverTheBitcoinNetwork(L10n.renBTCAccountIsRequired),
-                    boldString: L10n.renBTCAccountIsRequired
-                )
-
-                createExplanationView(
-                    mainString: L10n
-                        .yourWalletListDoesNotContainARenBTCAccountAndToCreateOne(L10n
-                            .youNeedToMakeATransaction),
-                    boldString: L10n
-                        .youNeedToMakeATransaction
-                )
-
-                createExplanationView(
-                    mainString: L10n
-                        .youToPayForAccountCreationButIfSomeoneSendsRenBTCToYourAddressItWillBeCreatedForYou(L10n
-                            .donTHaveFunds),
-                    boldString: L10n
-                        .donTHaveFunds
-                )
-            }
-        }
-
-        func createRenBTCView() -> BEVStack {
-            BEVStack(spacing: 12) {
-                createExplanationView(
-                    mainString: L10n
-                        .yourWalletListDoesNotContainARenBTCAccountAndToCreateOne(L10n
-                            .youNeedToMakeATransaction) + " " + L10n.youCanChooseWhichCurrencyToPayInBelow,
-                    boldString: L10n
-                        .youNeedToMakeATransaction
-                )
-
-                WLCard {
-                    BEHStack(spacing: 12, alignment: .center) {
-                        CoinLogoImageView(size: 44)
-
-                        BEVStack(spacing: 4) {
-                            UILabel(
-                                text: "Account creation fee:",
-                                textSize: 13,
-                                textColor: .textSecondary,
-                                numberOfLines: 0
-                            )
-                            UILabel(text: "0.509 USDC", textSize: 17, weight: .semibold)
-                        }
-
-                        UIView.defaultNextArrow()
-                    }
-                    .padding(.init(x: 18, y: 14))
-                }
-                .padding(.init(only: .bottom, inset: 12))
-
-                createExplanationView(
-                    mainString: L10n
-                        .ThisAddressAccepts.youMayLoseAssetsBySendingAnotherCoin(L10n.onlyBitcoin),
-                    boldString: L10n.onlyBitcoin
-                )
-
-                createExplanationView(
-                    mainString: L10n
-                        .minimumTransactionAmountOf("0.000112 BTC"),
-                    boldString: "0.000112 BTC"
-                )
-
-                createExplanationView(
-                    mainString: L10n.isTheRemainingTimeToSafelySendTheAssets("35:59:59"),
-                    boldString: "35:59:59"
-                )
-            }
-        }
-
-        func buttonsView() -> BEVStack {
-            BEVStack(spacing: 10) {}
-        }
-
         // MARK: - Binding
 
         override func bind() {
@@ -193,29 +115,6 @@ extension ConfirmReceivingBitcoin {
                     self?.updatePresentationLayout(animated: true)
                 })
                 .disposed(by: disposeBag)
-        }
-
-        // MARK: - Text builder
-
-        private func createExplanationView(mainString: String, boldString: String) -> UIView {
-            BEHStack(spacing: 10, alignment: .top) {
-                UILabel(text: "•")
-                    .withContentHuggingPriority(.required, for: .horizontal)
-                    .withContentCompressionResistancePriority(.required, for: .horizontal)
-                UILabel(text: nil, numberOfLines: 0)
-                    .withAttributedText(
-                        createExplanationAttributedString(
-                            mainString: mainString,
-                            boldString: boldString
-                        )
-                    )
-                    .withContentHuggingPriority(.required, for: .horizontal)
-            }
-        }
-
-        private func createExplanationAttributedString(mainString: String, boldString _: String) -> NSAttributedString {
-            NSMutableAttributedString()
-                .text(mainString, size: 15, color: .textBlack)
         }
     }
 }
