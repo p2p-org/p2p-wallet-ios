4dcf2dc6b1decef752e0a03279eb8f65d5feec82
Ivan Babich <ivan.babich@p2p.org>
2022-05-30 14:42:09 +0300

    [iOS] Decimal separation
    PWN-3623
    
    PWN-3625
    
    PWN-3706
    
    iOS Decimal separation - send input does not work properly

--- p2p_wallet/UIKit/Views/TokenAmountTextField.swift
+++ p2p_wallet/UIKit/Views/TokenAmountTextField.swift
@@ -6,11 +6,39 @@
 //
 
 import Foundation
+import RxSwift
 
-class TokenAmountTextField: BEDecimalTextField {
+final class TokenAmountTextField: BEDecimalTextField {
     private var decimals: SolanaSDK.Decimals?
     var value: Double {
         text.map { $0.double ?? 0 } ?? 0
+    }
+
+    private let disposeBag = DisposeBag()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        bind()
+    }
+
+    @available(*, unavailable)
+    required init(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func bind() {
+        rx.text
+            .map { $0?.cryptoCurrencyFormat }
+            .subscribe(onNext: { [weak self] text in
+                self?.text = text
+            })
+            .disposed(by: disposeBag)
+        rx.controlEvent(.editingDidEnd)
+            .asObservable()
+            .subscribe(onNext: { [weak self] in
+                self?.text = self?.text?.withoutLastZeros
+            })
+            .disposed(by: disposeBag)
     }
 
     func setUp(decimals: SolanaSDK.Decimals?) {
