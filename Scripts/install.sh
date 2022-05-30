5ab58eafb82cfe58845dbdddae90c37122f3a5d2
Chung Tran <bigearsenal@gmail.com>
2022-05-30 08:35:28 +0700

    feat: install.sh

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,13 +1,16 @@
+# Install xcodegen
+brew install xcodegen
+
 # Run xcodegen
 xcodegen
 
-# Run pod install
+# Run pod install first to gen swiftgen in Pods
 pod install
 
 # Run swiftgen for the first time
 Pods/swiftgen/bin/swiftgen config run --config swiftgen.yml
 
-# Re-run xcodegen && pod install
+# Re-run xcodegen && pod install to remove build error
 xcodegen && pod install
 
 # Open project
