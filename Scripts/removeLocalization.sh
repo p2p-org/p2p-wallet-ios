7bf3e160d3dc5d3173fea5163107865f8b0569f7
Ivan Babich <ivan.babich@p2p.org>
2022-06-02 19:01:48 +0300

    sync

--- Scripts/removeLocalization.sh
+++
@@ -1,9 +0,0 @@
-echo "Start remove localization script"
-
-PATH="${SRCROOT}/p2p_wallet/Resources"
-
-rm -r "$PATH/ru.lproj/Localizable.strings"
-rm -r "$PATH/fr.lproj/Localizable.strings"
-rm -r "$PATH/vi.lproj/Localizable.strings"
-
-echo "Remove localization script completed"
