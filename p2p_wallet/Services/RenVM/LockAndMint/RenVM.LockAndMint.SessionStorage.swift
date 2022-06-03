d64fdcd9bd6bb28003bcc511fffb0892ad42a422
Chung Tran <bigearsenal@gmail.com>
2022-06-03 14:06:27 +0700

    feat: some fixes

--- p2p_wallet/Services/RenVM/LockAndMint/RenVM.LockAndMint.SessionStorage.swift
+++ p2p_wallet/Services/RenVM/LockAndMint/RenVM.LockAndMint.SessionStorage.swift
@@ -9,191 +9,122 @@ import Foundation
 import RenVMSwift
 import RxCocoa
 
-public extension LockAndMint {
-    struct ProcessingTx: Codable, Hashable {
-        public static let maxVote: UInt64 = 3
-        public var tx: IncomingTransaction
-        public var receivedAt: Date?
-        public var oneVoteAt: Date?
-        public var twoVoteAt: Date?
-        public var threeVoteAt: Date?
-        public var confirmedAt: Date?
-        public var submittedAt: Date?
-        public var mintedAt: Date?
-
-        public var statusString: String? {
-            if mintedAt != nil {
-                return L10n.successfullyMintedRenBTC(
-                    tx.value.convertToBalance(decimals: 8)
-                        .toString(maximumFractionDigits: 9)
-                )
-            }
-
-            if submittedAt != nil {
-                return L10n.minting
-            }
-
-            if confirmedAt != nil {
-                return L10n.submittingToRenVM
-            }
-
-            if receivedAt != nil {
-                return L10n.waitingForDepositConfirmation + " \(tx.vout)/\(Self.maxVote)"
-            }
-
-            return nil
-        }
-
-        public var value: Double {
-            tx.value.convertToBalance(decimals: 8)
-        }
-    }
-}
-
-protocol RenVMLockAndMintSessionStorageType {
-    var processingTxsDriver: Driver<[LockAndMint.ProcessingTx]> { get }
-
-    func loadSession() -> RenVMSwift.Session?
-    func saveSession(_ session: RenVMSwift.Session)
-    func expireCurrentSession()
-
-    func processingTx(tx: IncomingTransaction, didReceiveAt receivedAt: Date)
-    func processingTx(tx: IncomingTransaction, didConfirmAt confirmedAt: Date)
-    func processingTx(tx: IncomingTransaction, didSubmitAt submittedAt: Date)
-    func processingTx(tx: IncomingTransaction, didMintAt mintedAt: Date)
-
-    func isMinted(txid: String) -> Bool
-    func getProcessingTx(txid: String) -> LockAndMint.ProcessingTx?
-    func getAllProcessingTx() -> [LockAndMint.ProcessingTx]
-    func removeMintedTx(txid: String)
-}
-
-extension LockAndMint {
-    class SessionStorage: RenVMLockAndMintSessionStorageType {
-        private let processingTxsSubject = BehaviorRelay<[ProcessingTx]>(value: Defaults.renVMProcessingTxs)
-        private var disposable: DefaultsDisposable!
-        let lock = NSLock()
-
-        init() {
-            disposable = Defaults.observe(\.renVMProcessingTxs, handler: { [weak self] update in
-                self?.processingTxsSubject.accept(update.newValue ?? [])
-            })
-        }
-
-        var processingTxsDriver: Driver<[LockAndMint.ProcessingTx]> {
-            processingTxsSubject.asDriver()
-        }
-
-        func loadSession() -> RenVMSwift.Session? {
-            Defaults.renVMSession
-        }
-
-        func saveSession(_ session: RenVMSwift.Session) {
-            Defaults.renVMSession = session
-        }
-
-        func expireCurrentSession() {
-            Defaults.renVMSession = nil
-            Defaults.renVMProcessingTxs = []
-        }
-
-        func processingTx(tx: IncomingTransaction, didReceiveAt receivedAt: Date) {
-            save { current in
-                guard let index = current.indexOf(tx) else {
-                    current.append(.init(tx: tx, receivedAt: receivedAt))
-                    return true
-                }
-
-                if tx.vout == 3, current[index].threeVoteAt == nil {
-                    current[index].threeVoteAt = receivedAt
-                }
-                if tx.vout == 2, current[index].twoVoteAt == nil {
-                    current[index].twoVoteAt = receivedAt
-                }
-                if tx.vout == 1, current[index].oneVoteAt == nil {
-                    current[index].oneVoteAt = receivedAt
-                }
-                if tx.vout == 0, current[index].receivedAt == nil {
-                    current[index].receivedAt = receivedAt
-                }
-
-                return true
-            }
-        }
-
-        func processingTx(tx: IncomingTransaction, didConfirmAt confirmedAt: Date) {
-            save { current in
-                guard let index = current.indexOf(tx) else {
-                    current.append(.init(tx: tx, confirmedAt: confirmedAt))
-                    return true
-                }
-                current[index].confirmedAt = confirmedAt
-                return true
-            }
-        }
-
-        func processingTx(tx: IncomingTransaction, didSubmitAt submittedAt: Date) {
-            save { current in
-                guard let index = current.indexOf(tx) else {
-                    current.append(.init(tx: tx, submittedAt: submittedAt))
-                    return true
-                }
-                current[index].submittedAt = submittedAt
-                return true
-            }
-        }
-
-        func processingTx(tx: IncomingTransaction, didMintAt mintedAt: Date) {
-            save { current in
-                guard let index = current.indexOf(tx) else {
-                    current.append(.init(tx: tx, mintedAt: mintedAt))
-                    return true
-                }
-                current[index].mintedAt = mintedAt
-                return true
-            }
-        }
-
-        func isMinted(txid: String) -> Bool {
-            getAllProcessingTx().contains(where: { $0.tx.txid == txid && $0.mintedAt != nil })
-        }
-
-        func getProcessingTx(txid: String) -> LockAndMint.ProcessingTx? {
-            Defaults.renVMProcessingTxs.first(where: { $0.tx.txid == txid })
-        }
-
-        func getAllProcessingTx() -> [LockAndMint.ProcessingTx] {
-            Defaults.renVMProcessingTxs
-        }
-
-        func removeMintedTx(txid: String) {
-            lock.lock()
-            defer { lock.unlock() }
-
-            var current = Defaults.renVMProcessingTxs
-            current.removeAll(where: { $0.tx.txid == txid })
-            Defaults.renVMProcessingTxs = current
-        }
-
-        private func save(_ modify: @escaping (inout [LockAndMint.ProcessingTx]) -> Bool) {
-            lock.lock()
-            defer { lock.unlock() }
-
-            var current = Defaults.renVMProcessingTxs
-            let shouldSave = modify(&current)
-            if shouldSave && Defaults.renVMProcessingTxs != current {
-                Defaults.renVMProcessingTxs = current
-            }
-        }
-    }
-}
-
-private extension Array where Element == LockAndMint.ProcessingTx {
-    func hasTx(_ tx: IncomingTransaction) -> Bool {
-        contains(where: { $0.tx.txid == tx.txid })
-    }
-
-    func indexOf(_ tx: IncomingTransaction) -> Int? {
-        firstIndex(where: { $0.tx.txid == tx.txid })
-    }
-}
+// extension LockAndMint {
+//    class SessionStorage: RenVMLockAndMintSessionStorageType {
+//        private let processingTxsSubject = BehaviorRelay<[ProcessingTx]>(value: Defaults.renVMProcessingTxs)
+//        private var disposable: DefaultsDisposable!
+//        let lock = NSLock()
+//
+//        init() {
+//            disposable = Defaults.observe(\.renVMProcessingTxs, handler: { [weak self] update in
+//                self?.processingTxsSubject.accept(update.newValue ?? [])
+//            })
+//        }
+//
+//        var processingTxsDriver: Driver<[LockAndMint.ProcessingTx]> {
+//            processingTxsSubject.asDriver()
+//        }
+//
+//        func loadSession() -> RenVMSwift.Session? {
+//            Defaults.renVMSession
+//        }
+//
+//        func saveSession(_ session: RenVMSwift.Session) {
+//            Defaults.renVMSession = session
+//        }
+//
+//        func expireCurrentSession() {
+//            Defaults.renVMSession = nil
+//            Defaults.renVMProcessingTxs = []
+//        }
+//
+//        func processingTx(tx: IncomingTransaction, didReceiveAt receivedAt: Date) {
+//            save { current in
+//                guard let index = current.indexOf(tx) else {
+//                    current.append(.init(tx: tx, receivedAt: receivedAt))
+//                    return true
+//                }
+//
+//                if tx.vout == 3, current[index].threeVoteAt == nil {
+//                    current[index].threeVoteAt = receivedAt
+//                }
+//                if tx.vout == 2, current[index].twoVoteAt == nil {
+//                    current[index].twoVoteAt = receivedAt
+//                }
+//                if tx.vout == 1, current[index].oneVoteAt == nil {
+//                    current[index].oneVoteAt = receivedAt
+//                }
+//                if tx.vout == 0, current[index].receivedAt == nil {
+//                    current[index].receivedAt = receivedAt
+//                }
+//
+//                return true
+//            }
+//        }
+//
+//        func processingTx(tx: IncomingTransaction, didConfirmAt confirmedAt: Date) {
+//            save { current in
+//                guard let index = current.indexOf(tx) else {
+//                    current.append(.init(tx: tx, confirmedAt: confirmedAt))
+//                    return true
+//                }
+//                current[index].confirmedAt = confirmedAt
+//                return true
+//            }
+//        }
+//
+//        func processingTx(tx: IncomingTransaction, didSubmitAt submittedAt: Date) {
+//            save { current in
+//                guard let index = current.indexOf(tx) else {
+//                    current.append(.init(tx: tx, submittedAt: submittedAt))
+//                    return true
+//                }
+//                current[index].submittedAt = submittedAt
+//                return true
+//            }
+//        }
+//
+//        func processingTx(tx: IncomingTransaction, didMintAt mintedAt: Date) {
+//            save { current in
+//                guard let index = current.indexOf(tx) else {
+//                    current.append(.init(tx: tx, mintedAt: mintedAt))
+//                    return true
+//                }
+//                current[index].mintedAt = mintedAt
+//                return true
+//            }
+//        }
+//
+//        func isMinted(txid: String) -> Bool {
+//            getAllProcessingTx().contains(where: { $0.tx.txid == txid && $0.mintedAt != nil })
+//        }
+//
+//        func getProcessingTx(txid: String) -> LockAndMint.ProcessingTx? {
+//            Defaults.renVMProcessingTxs.first(where: { $0.tx.txid == txid })
+//        }
+//
+//        func getAllProcessingTx() -> [LockAndMint.ProcessingTx] {
+//            Defaults.renVMProcessingTxs
+//        }
+//
+//        func removeMintedTx(txid: String) {
+//            lock.lock()
+//            defer { lock.unlock() }
+//
+//            var current = Defaults.renVMProcessingTxs
+//            current.removeAll(where: { $0.tx.txid == txid })
+//            Defaults.renVMProcessingTxs = current
+//        }
+//
+//        private func save(_ modify: @escaping (inout [LockAndMint.ProcessingTx]) -> Bool) {
+//            lock.lock()
+//            defer { lock.unlock() }
+//
+//            var current = Defaults.renVMProcessingTxs
+//            let shouldSave = modify(&current)
+//            if shouldSave && Defaults.renVMProcessingTxs != current {
+//                Defaults.renVMProcessingTxs = current
+//            }
+//        }
+//    }
+// }
