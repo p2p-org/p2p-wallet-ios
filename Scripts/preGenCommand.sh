3482cd1cf7ee4782f55a3d254b1337682373c51b
Chung Tran <bigearsenal@gmail.com>
2022-06-05 15:55:48 +0700

    feat: update script

--- Scripts/preGenCommand.sh
+++ Scripts/preGenCommand.sh
@@ -1,12 +1,12 @@
 echo "Temporarily ignore localization using preGenCommand.sh and postGenCommand.sh, please remove this script later"
-[ ! -f src ] || rm -rf p2p_wallet/Base.lproj-backup
-[ ! -f src ] || rm -rf p2p_wallet/en.lproj-backup
-[ ! -f src ] || rm -rf p2p_wallet/ru.lproj-backup
-[ ! -f src ] || rm -rf p2p_wallet/fr.lproj-backup
-[ ! -f src ] || rm -rf p2p_wallet/vi.lproj-backup
+rm -rf p2p_wallet/Base.lproj-backup
+rm -rf p2p_wallet/en.lproj-backup
+rm -rf p2p_wallet/ru.lproj-backup
+rm -rf p2p_wallet/fr.lproj-backup
+rm -rf p2p_wallet/vi.lproj-backup
 
-[ ! -f src ] || mv p2p_wallet/Base.lproj p2p_wallet/Base.lproj-backup
-[ ! -f src ] || mv p2p_wallet/en.lproj p2p_wallet/en.lproj-backup
-[ ! -f src ] || mv p2p_wallet/ru.lproj p2p_wallet/ru.lproj-backup
-[ ! -f src ] || mv p2p_wallet/fr.lproj p2p_wallet/fr.lproj-backup
-[ ! -f src ] || mv p2p_wallet/vi.lproj p2p_wallet/vi.lproj-backup
+mv p2p_wallet/Base.lproj p2p_wallet/Base.lproj-backup
+mv p2p_wallet/en.lproj p2p_wallet/en.lproj-backup
+mv p2p_wallet/ru.lproj p2p_wallet/ru.lproj-backup
+mv p2p_wallet/fr.lproj p2p_wallet/fr.lproj-backup
+mv p2p_wallet/vi.lproj p2p_wallet/vi.lproj-backup
