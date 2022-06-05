b4b7fbfef95c3f7e0b5f180b749616828068a2e1
Chung Tran <bigearsenal@gmail.com>
2022-06-05 16:37:10 +0700

    feat: move Localizable files back to Resources

--- Scripts/preGenCommand.sh
+++ Scripts/preGenCommand.sh
@@ -1,12 +1,12 @@
 echo "Temporarily ignore localization using preGenCommand.sh and postGenCommand.sh, please remove this script later"
-rm -rf p2p_wallet/Base.lproj-backup
-rm -rf p2p_wallet/en.lproj-backup
-rm -rf p2p_wallet/ru.lproj-backup
-rm -rf p2p_wallet/fr.lproj-backup
-rm -rf p2p_wallet/vi.lproj-backup
+rm -rf p2p_wallet/Resources/Base.lproj-backup
+rm -rf p2p_wallet/Resources/en.lproj-backup
+rm -rf p2p_wallet/Resources/ru.lproj-backup
+rm -rf p2p_wallet/Resources/fr.lproj-backup
+rm -rf p2p_wallet/Resources/vi.lproj-backup
 
-mv p2p_wallet/Base.lproj p2p_wallet/Base.lproj-backup
-mv p2p_wallet/en.lproj p2p_wallet/en.lproj-backup
-mv p2p_wallet/ru.lproj p2p_wallet/ru.lproj-backup
-mv p2p_wallet/fr.lproj p2p_wallet/fr.lproj-backup
-mv p2p_wallet/vi.lproj p2p_wallet/vi.lproj-backup
+mv p2p_wallet/Resources/Base.lproj p2p_wallet/Resources/Base.lproj-backup
+mv p2p_wallet/Resources/en.lproj p2p_wallet/Resources/en.lproj-backup
+mv p2p_wallet/Resources/ru.lproj p2p_wallet/Resources/ru.lproj-backup
+mv p2p_wallet/Resources/fr.lproj p2p_wallet/Resources/fr.lproj-backup
+mv p2p_wallet/Resources/vi.lproj p2p_wallet/Resources/vi.lproj-backup
