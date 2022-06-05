4ce06150387167e5c400f864244bba2c6773463d
Chung Tran <bigearsenal@gmail.com>
2022-06-05 15:39:04 +0700

    feat: test

---
+++ Scripts/preGenCommand.sh
@@ -0,0 +1,6 @@
+echo "Temporarily ignore localization using preGenCommand.sh and postGenCommand.sh, please remove this script later"
+mv p2p_wallet/Base.lproj p2p_wallet/Base.lproj-backup
+mv p2p_wallet/en.lproj p2p_wallet/en.lproj-backup
+mv p2p_wallet/ru.lproj p2p_wallet/ru.lproj-backup
+mv p2p_wallet/fr.lproj p2p_wallet/fr.lproj-backup
+mv p2p_wallet/vi.lproj p2p_wallet/vi.lproj-backup
