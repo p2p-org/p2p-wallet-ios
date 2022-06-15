76ad53761b3a6a0448fd832bc45a89e479245ac6
Ivan Babich <ivan.babich@p2p.org>
2022-06-15 10:30:25 +0400

    Debug menu

---
+++ p2p_wallet/Scenes/DebugMenu/View/DebugMenuView.swift
@@ -0,0 +1,34 @@
+//
+//  DebugMenuView.swift
+//  p2p_wallet
+//
+//  Created by Ivan on 14.06.2022.
+//
+
+import SwiftUI
+
+struct DebugMenuView: View {
+    @ObservedObject private var viewModel: DebugMenuViewModel
+
+    init(viewModel: DebugMenuViewModel) {
+        self.viewModel = viewModel
+    }
+
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(viewModel.features.indices) { index in
+                    if let feature = viewModel.features[index].feature {
+                        Toggle(viewModel.features[index].title, isOn: $viewModel.features[index].isOn)
+                            .valueChanged(value: viewModel.features[index].isOn) { newValue in
+                                viewModel.setFeature(feature, isOn: newValue)
+                            }
+                    } else {
+                        Text(viewModel.features[index].title)
+                    }
+                }
+            }
+            .navigationBarTitle("Debug Menu", displayMode: .inline)
+        }
+    }
+}
