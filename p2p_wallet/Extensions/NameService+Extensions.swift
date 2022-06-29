36de3a4fa8239c7733073880c879b8644f3822b6
Elizaveta Semenova <thesharkight84@gmail.com>
2022-06-29 17:53:25 +0400

    [PWN-4046] Split FeeRelayer and NameRegister service settings

--- p2p_wallet/Extensions/NameService+Extensions.swift
+++ p2p_wallet/Extensions/NameService+Extensions.swift
@@ -10,7 +10,9 @@ import NameService
 import Resolver
 
 extension NameServiceImpl {
-    static let endpoint = "\(FeeRelayerEndpoint.prodBaseUrl)/name_register"
+    /// Name register service does not depend on Fee relayer service so we don't use FeeRelayerEndpoint here.
+    /// They share single server for now but logically settings should be splitted.
+    static let endpoint = "https://\(String.secretConfig("FEE_RELAYER_ENDPOINT")!)/name_register"
 
     static var captchaAPI1Url: String {
         NameServiceImpl.endpoint + "/auth/gt/register"
