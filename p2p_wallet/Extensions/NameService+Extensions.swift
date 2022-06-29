5f6c71ff260cbdc10234df80461c80cba7fa866a
Elizaveta Semenova <thesharkight84@gmail.com>
2022-06-29 12:10:37 +0400

    [PWN-4046] Return original production key name for backward compatibility

--- p2p_wallet/Extensions/NameService+Extensions.swift
+++ p2p_wallet/Extensions/NameService+Extensions.swift
@@ -11,11 +11,11 @@ import Resolver
 
 extension NameServiceImpl {
     static var endpoint: String {
-        let config: String
+        var config = String.secretConfig("FEE_RELAYER_ENDPOINT")!
         #if DEBUG
-            config = String.secretConfig("FEE_RELAYER_STAGING_ENDPOINT")!
-        #else
-            config = String.secretConfig("FEE_RELAYER_PRODUCTION_ENDPOINT")!
+            if let stagingEndpoint = String.secretConfig("FEE_RELAYER_STAGING_ENDPOINT"), !stagingEndpoint.isEmpty {
+                config = stagingEndpoint
+            }
         #endif
         return "https://\(config)/name_register"
     }
