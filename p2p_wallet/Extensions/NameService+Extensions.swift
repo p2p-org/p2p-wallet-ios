e7cdb501437511da45c843b6b7d4e756e7271b00
Elizaveta Semenova <thesharkight84@gmail.com>
2022-06-30 10:35:00 +0400

    [PWN-4046] Add Name Service Endpoint

--- p2p_wallet/Extensions/NameService+Extensions.swift
+++ p2p_wallet/Extensions/NameService+Extensions.swift
@@ -10,9 +10,7 @@ import NameService
 import Resolver
 
 extension NameServiceImpl {
-    /// Name register service does not depend on Fee relayer service so we don't use FeeRelayerEndpoint here.
-    /// They share single server for now but logically settings should be splitted.
-    static let endpoint = "https://\(String.secretConfig("FEE_RELAYER_ENDPOINT")!)/name_register"
+    static let endpoint = "https://\(String.secretConfig("NAME_SERVICE_ENDPOINT")!)"
 
     static var captchaAPI1Url: String {
         NameServiceImpl.endpoint + "/auth/gt/register"
