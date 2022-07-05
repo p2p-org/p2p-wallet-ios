f71eea14f7648fd3a20801180b30b0a1142f107a
Alexey Sidorov <ody344@gmail.com>
2022-07-05 15:35:26 +0300

    Clear recipient on init

--- p2p_wallet/Scenes/Main/SendToken/SendTokenCoordinator.swift
+++ p2p_wallet/Scenes/Main/SendToken/SendTokenCoordinator.swift
@@ -79,18 +79,17 @@ extension SendToken {
                 pushChooseToken(showAfterConfirmation: showAfterConfirmation)
             case let .chooseRecipientAndNetwork(showAfterConfirmation, preSelectedNetwork):
                 guard let navigationController = navigationController else { return }
-                if coordinator == nil {
-                    let vm = ChooseRecipientAndNetwork.ViewModel(
-                        showAfterConfirmation: showAfterConfirmation,
-                        preSelectedNetwork: preSelectedNetwork,
-                        sendTokenViewModel: viewModel,
-                        relayMethod: viewModel.relayMethod
-                    )
-                    coordinator = ChooseRecipientAndNetwork.Coordinator(
-                        viewModel: vm,
-                        navigationController: navigationController
-                    )
-                }
+
+                let vm = ChooseRecipientAndNetwork.ViewModel(
+                    showAfterConfirmation: showAfterConfirmation,
+                    preSelectedNetwork: preSelectedNetwork,
+                    sendTokenViewModel: viewModel,
+                    relayMethod: viewModel.relayMethod
+                )
+                coordinator = ChooseRecipientAndNetwork.Coordinator(
+                    viewModel: vm,
+                    navigationController: navigationController
+                )
                 coordinator?.start()
             case .confirmation:
                 let vc = ConfirmViewController(viewModel: viewModel)
