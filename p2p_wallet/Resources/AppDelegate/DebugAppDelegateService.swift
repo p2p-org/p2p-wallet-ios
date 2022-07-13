83bb18e22338e98567a0fa8cf43edd8d375af70e
Chung Tran <bigearsenal@gmail.com>
2022-07-13 11:34:11 +0700

    Revert "feat: revert to test"
    This reverts commit 898e9cdcfe0bb0efbbf5c110973d5e1dcdf81e25.

--- p2p_wallet/Resources/AppDelegate/DebugAppDelegateService.swift
+++ p2p_wallet/Resources/AppDelegate/DebugAppDelegateService.swift
@@ -7,16 +7,29 @@
 
 #if !RELEASE
 
+    /* script_delete_flag_start */
     import CocoaDebug
+    /* script_delete_flag_end */
 
     final class DebugAppDelegateService: NSObject, AppDelegateService {
         func applicationDidFinishLaunching(_: UIApplication) {
+            /* script_delete_flag_start */
             CocoaDebugSettings.shared.responseShake = false
+            /* script_delete_flag_end */
             showDebugger(isShown)
         }
     }
 
+#endif
+
+#if !RELEASE
+
     var isShown = CocoaDebugSettings.shared.showBubbleAndWindow
+
+#endif
+
+/* script_delete_flag_start */
+#if !RELEASE
 
     var isShaken = false
 
@@ -40,13 +53,20 @@
         }
     }
 
+#endif
+/* script_delete_flag_end */
+
+#if !RELEASE
+
     func showDebugger(_ isShown: Bool) {
         DispatchQueue.main.async {
+            /* script_delete_flag_start */
             if isShown {
                 CocoaDebug.showBubble()
             } else {
                 CocoaDebug.hideBubble()
             }
+            /* script_delete_flag_end */
         }
     }
 
