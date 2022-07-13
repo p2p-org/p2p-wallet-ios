5d231b9ca1ace8cd136eeab8626fe5e7022986f4
Chung Tran <bigearsenal@gmail.com>
2022-07-13 11:34:11 +0700

    Revert "feat: revert to test"
    This reverts commit 3db08dab886ba6e52f70a1433ba752e49f8a0dd4.

--- p2p_wallet/Scenes/DebugMenu/ViewModel/DebugMenuViewModel.swift
+++ p2p_wallet/Scenes/DebugMenu/ViewModel/DebugMenuViewModel.swift
@@ -5,74 +5,76 @@
 //  Created by Ivan on 14.06.2022.
 //
 
-#if !RELEASE
-    import Combine
-    import FirebaseRemoteConfig
-    import SwiftyUserDefaults
+import Combine
+import FirebaseRemoteConfig
+import SwiftyUserDefaults
 
-    final class DebugMenuViewModel: ObservableObject {
-        @Published var networkLoggerVisible = isShown {
-            didSet {
-                updateNetworkLoggerState()
+final class DebugMenuViewModel: ObservableObject {
+    @Published var networkLoggerVisible = isShown {
+        didSet {
+            updateNetworkLoggerState()
+        }
+    }
+
+    @Published var features: [FeatureItem]
+
+    init() {
+        features = Menu.allCases
+            .map {
+                FeatureItem(
+                    title: $0.title,
+                    feature: $0.feature,
+                    isOn: available($0.feature)
+                )
             }
-        }
+    }
 
-        @Published var features: [FeatureItem]
-
-        init() {
-            features = Menu.allCases
-                .map {
-                    FeatureItem(
-                        title: $0.title,
-                        feature: $0.feature,
-                        isOn: available($0.feature)
-                    )
-                }
-        }
-
-        func setFeature(_ feature: Feature, isOn: Bool) {
-            DebugMenuFeaturesProvider.shared.updateFlag(for: feature, with: isOn)
-            FeatureFlagProvider.shared.fetchFeatureFlags(
-                mainFetcher: MergingFlagsFetcher(
-                    primaryFetcher: DebugMenuFeaturesProvider.shared,
-                    secondaryFetcher: MergingFlagsFetcher(
-                        primaryFetcher: RemoteConfig.remoteConfig(),
-                        secondaryFetcher: defaultFlags
-                    )
+    func setFeature(_ feature: Feature, isOn: Bool) {
+        DebugMenuFeaturesProvider.shared.updateFlag(for: feature, with: isOn)
+        FeatureFlagProvider.shared.fetchFeatureFlags(
+            mainFetcher: MergingFlagsFetcher(
+                primaryFetcher: DebugMenuFeaturesProvider.shared,
+                secondaryFetcher: MergingFlagsFetcher(
+                    primaryFetcher: RemoteConfig.remoteConfig(),
+                    secondaryFetcher: defaultFlags
                 )
             )
-        }
+        )
+    }
 
-        private func updateNetworkLoggerState() {
+    private func updateNetworkLoggerState() {
+        #if !RELEASE
+
             showDebugger(networkLoggerVisible)
-        }
+
+        #endif
     }
+}
 
-    extension DebugMenuViewModel {
-        struct FeatureItem {
-            let title: String
-            let feature: Feature
-            var isOn: Bool
-        }
+extension DebugMenuViewModel {
+    struct FeatureItem {
+        let title: String
+        let feature: Feature
+        var isOn: Bool
     }
+}
 
-    extension DebugMenuViewModel {
-        enum Menu: Int, CaseIterable {
-            case sslPinning
+extension DebugMenuViewModel {
+    enum Menu: Int, CaseIterable {
+        case sslPinning
 
-            var title: String {
-                switch self {
-                case .sslPinning:
-                    return "SSL Pinning"
-                }
-            }
-
-            var feature: Feature {
-                switch self {
-                case .sslPinning:
-                    return .sslPinning
-                }
+        var title: String {
+            switch self {
+            case .sslPinning:
+                return "SSL Pinning"
             }
         }
+
+        var feature: Feature {
+            switch self {
+            case .sslPinning:
+                return .sslPinning
+            }
+        }
     }
-#endif
+}
