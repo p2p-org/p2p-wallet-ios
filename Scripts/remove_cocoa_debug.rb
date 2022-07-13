6d25dbfd1708715a78678a9cddd6560cacdb7a6a
Chung Tran <bigearsenal@gmail.com>
2022-07-13 10:16:21 +0700

    feat: remove CocoaDebug on releasing

--- Scripts/remove_cocoa_debug.rb
+++ Scripts/remove_cocoa_debug.rb
@@ -7,12 +7,3 @@ text = File.read(path)
 new_contents = text.gsub("pod 'CocoaDebug'", "# pod 'CocoaDebug'")
 puts new_contents
 File.open(path, "w") {|file| file.puts new_contents }
-
-# delete extension from DebugAppDelegateService.swift
-path = 'p2p_wallet/Resources/AppDelegate/DebugAppDelegateService.swift'
-text = File.read(path)
-new_contents = text
-    .gsub("/*script_delete_flag_start*/", "/*")
-    .gsub("/*script_delete_flag_end*/", "*/")
-puts new_contents
-File.open(path, "w") {|file| file.puts new_contents }
--- p2p_wallet/Scenes/DebugMenu/ViewModel/DebugMenuViewModel.swift
+++ p2p_wallet/Scenes/DebugMenu/ViewModel/DebugMenuViewModel.swift
@@ -5,76 +5,74 @@
 //  Created by Ivan on 14.06.2022.
 //
 
-import Combine
-import FirebaseRemoteConfig
-import SwiftyUserDefaults
+#if !RELEASE
+    import Combine
+    import FirebaseRemoteConfig
+    import SwiftyUserDefaults
 
-final class DebugMenuViewModel: ObservableObject {
-    @Published var networkLoggerVisible = isShown {
-        didSet {
-            updateNetworkLoggerState()
-        }
-    }
-
-    @Published var features: [FeatureItem]
-
-    init() {
-        features = Menu.allCases
-            .map {
-                FeatureItem(
-                    title: $0.title,
-                    feature: $0.feature,
-                    isOn: available($0.feature)
-                )
+    final class DebugMenuViewModel: ObservableObject {
+        @Published var networkLoggerVisible = isShown {
+            didSet {
+                updateNetworkLoggerState()
             }
-    }
+        }
 
-    func setFeature(_ feature: Feature, isOn: Bool) {
-        DebugMenuFeaturesProvider.shared.updateFlag(for: feature, with: isOn)
-        FeatureFlagProvider.shared.fetchFeatureFlags(
-            mainFetcher: MergingFlagsFetcher(
-                primaryFetcher: DebugMenuFeaturesProvider.shared,
-                secondaryFetcher: MergingFlagsFetcher(
-                    primaryFetcher: RemoteConfig.remoteConfig(),
-                    secondaryFetcher: defaultFlags
+        @Published var features: [FeatureItem]
+
+        init() {
+            features = Menu.allCases
+                .map {
+                    FeatureItem(
+                        title: $0.title,
+                        feature: $0.feature,
+                        isOn: available($0.feature)
+                    )
+                }
+        }
+
+        func setFeature(_ feature: Feature, isOn: Bool) {
+            DebugMenuFeaturesProvider.shared.updateFlag(for: feature, with: isOn)
+            FeatureFlagProvider.shared.fetchFeatureFlags(
+                mainFetcher: MergingFlagsFetcher(
+                    primaryFetcher: DebugMenuFeaturesProvider.shared,
+                    secondaryFetcher: MergingFlagsFetcher(
+                        primaryFetcher: RemoteConfig.remoteConfig(),
+                        secondaryFetcher: defaultFlags
+                    )
                 )
             )
-        )
-    }
+        }
 
-    private func updateNetworkLoggerState() {
-        #if !RELEASE
-
+        private func updateNetworkLoggerState() {
             showDebugger(networkLoggerVisible)
-
-        #endif
-    }
-}
-
-extension DebugMenuViewModel {
-    struct FeatureItem {
-        let title: String
-        let feature: Feature
-        var isOn: Bool
-    }
-}
-
-extension DebugMenuViewModel {
-    enum Menu: Int, CaseIterable {
-        case sslPinning
-
-        var title: String {
-            switch self {
-            case .sslPinning:
-                return "SSL Pinning"
-            }
-        }
-
-        var feature: Feature {
-            switch self {
-            case .sslPinning:
-                return .sslPinning
-            }
         }
     }
-}
+
+    extension DebugMenuViewModel {
+        struct FeatureItem {
+            let title: String
+            let feature: Feature
+            var isOn: Bool
+        }
+    }
+
+    extension DebugMenuViewModel {
+        enum Menu: Int, CaseIterable {
+            case sslPinning
+
+            var title: String {
+                switch self {
+                case .sslPinning:
+                    return "SSL Pinning"
+                }
+            }
+
+            var feature: Feature {
+                switch self {
+                case .sslPinning:
+                    return .sslPinning
+                }
+            }
+        }
+    }
+#endif
