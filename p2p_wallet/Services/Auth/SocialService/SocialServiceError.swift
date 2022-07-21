9a0a2d1c115157480295e621d6929118904ad580
Ivan Babich <ivan.babich@p2p.org>
2022-07-21 11:06:30 +0300

    Create Authentication Service

---
+++ p2p_wallet/Services/Auth/SocialService/SocialServiceError.swift
@@ -0,0 +1,13 @@
+//
+//  SocialServiceError.swift
+//  p2p_wallet
+//
+//  Created by Ivan on 20.07.2022.
+//
+
+import Foundation
+
+enum SocialServiceError: Error {
+    case cancelled
+    case unknown
+}
