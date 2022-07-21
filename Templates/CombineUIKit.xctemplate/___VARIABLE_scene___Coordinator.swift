4771380f91270569115f5f1f4379bd5008963f2f
Chung Tran <bigearsenal@gmail.com>
2022-07-21 11:38:36 +0700

    feat: Combine Template

---
+++ Templates/CombineUIKit.xctemplate/___VARIABLE_scene___Coordinator.swift
@@ -0,0 +1,23 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//
+
+import Foundation
+import CombineCocoa
+
+class ___VARIABLE_scene___Coordinator: BaseCoordinator {
+    // MARK: - Dependencies
+
+    let viewModel: ___VARIABLE_scene___ViewModel
+    
+    // MARK: - Properties
+    
+    // MARK: - Initializer
+
+    init(viewModel: ___VARIABLE_scene___ViewModel) {
+        self.viewModel = viewModel
+    }
+}
