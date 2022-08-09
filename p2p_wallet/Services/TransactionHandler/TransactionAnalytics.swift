0d3c0b9ce104ebc3e48d6bb081d9a22cdba7a740
Chung Tran <bigearsenal@gmail.com>
2022-08-09 12:38:04 +0700

    feat: processing

--- p2p_wallet/Services/TransactionHandler/TransactionAnalytics.swift
+++ p2p_wallet/Services/TransactionHandler/TransactionAnalytics.swift
@@ -3,107 +3,102 @@
 //
 
 import AnalyticsManager
+import Combine
 import Foundation
 import Resolver
-import RxSwift
 
 class SwapTransactionAnalytics {
-    let disposeBag = DisposeBag()
+    private var subscriptions = [AnyCancellable]()
     @Injected private var analyticsManager: AnalyticsManager
     @Injected private var transactionHandler: TransactionHandlerType
 
     init() {
         transactionHandler
             .onNewTransaction
-            .subscribe(onNext: { [weak self] trx, index in
+            .sink { [weak self] trx, index in
                 if trx.rawTransaction.isSwap { self?.observer(index: index) }
-            })
-            .disposed(by: disposeBag)
+            }
+            .store(in: &subscriptions)
     }
 
     func observer(index: TransactionHandler.TransactionIndex) {
-        Observable<PendingTransaction>.create { [unowned self] observer in
-            let localDisposable = transactionHandler.observeTransaction(transactionIndex: index)
-                .subscribe(onNext: { trx in
-                    guard let trx = trx else {
-                        observer.on(.completed)
-                        return
-                    }
-
-                    observer.on(.next(trx))
-                    switch trx.status {
-                    case .finalized, .error:
-                        observer.on(.completed)
-                    default:
-                        break
-                    }
-
-                })
-
-            return Disposables.create {
-                localDisposable.dispose()
-            }
-        }
-        .distinctUntilChanged(at: \.status.rawValue)
-        .withPrevious()
-        .subscribe(onNext: { prevTrx, trx in
-            guard let rawTrx = trx.rawTransaction as? ProcessTransaction.SwapTransaction else { return }
-
-            switch trx.status {
-            case .sending:
-                self.analyticsManager.log(
-                    event: .swapUserConfirmed(
-                        tokenAName: rawTrx.sourceWallet.token.symbol,
-                        tokenBName: rawTrx.destinationWallet.token.symbol,
-                        swapSum: rawTrx.amount,
-                        swapMAX: rawTrx.metaInfo.swapMAX,
-                        swapUSD: rawTrx.metaInfo.swapUSD,
-                        priceSlippage: rawTrx.slippage,
-                        feesSource: rawTrx.payingWallet?.token.name ?? "Unknown"
-                    )
-                )
-            case let .confirmed(confirmation):
-                if confirmation == 0 {
-                    self.analyticsManager.log(
-                        event: .swapStarted(
-                            tokenAName: rawTrx.sourceWallet.token.symbol,
-                            tokenBName: rawTrx.destinationWallet.token.symbol,
-                            swapSum: rawTrx.amount,
-                            swapMAX: rawTrx.metaInfo.swapMAX,
-                            swapUSD: rawTrx.metaInfo.swapUSD,
-                            priceSlippage: rawTrx.slippage,
-                            feesSource: rawTrx.payingWallet?.token.name ?? "Unknown"
-                        )
-                    )
-                } else if prevTrx?.status.numberOfConfirmations == 0 {
-                    self.analyticsManager.log(
-                        event: .swapApprovedByNetwork(
-                            tokenAName: rawTrx.sourceWallet.token.symbol,
-                            tokenBName: rawTrx.destinationWallet.token.symbol,
-                            swapSum: rawTrx.amount,
-                            swapMAX: rawTrx.metaInfo.swapMAX,
-                            swapUSD: rawTrx.metaInfo.swapUSD,
-                            priceSlippage: rawTrx.slippage,
-                            feesSource: rawTrx.payingWallet?.token.name ?? "Unknown"
-                        )
-                    )
+        transactionHandler.observeTransaction(transactionIndex: index)
+            .flatMap { trx -> AnyPublisher<PendingTransaction, Never> in
+                guard let trx = trx else {
+                    return Empty(completeImmediately: true)
+                        .eraseToAnyPublisher()
                 }
-            case .finalized:
-                self.analyticsManager.log(
-                    event: .swapCompleted(
-                        tokenAName: rawTrx.sourceWallet.token.symbol,
-                        tokenBName: rawTrx.destinationWallet.token.symbol,
-                        swapSum: rawTrx.amount,
-                        swapMAX: rawTrx.metaInfo.swapMAX,
-                        swapUSD: rawTrx.metaInfo.swapUSD,
-                        priceSlippage: rawTrx.slippage,
-                        feesSource: rawTrx.payingWallet?.token.name ?? "Unknown"
-                    )
-                )
-            default:
-                break
+                switch trx.status {
+                case .finalized, .error:
+                    return Empty(completeImmediately: true)
+                        .eraseToAnyPublisher()
+                default:
+                    return Just(trx)
+                        .eraseToAnyPublisher()
+                }
             }
-        })
-        .disposed(by: disposeBag)
+            .removeDuplicates { p1, p2 in
+                p1.status.rawValue != p2.status.rawValue
+            }
+            .withPrevious()
+            .sink { prevTrx, trx in
+                guard let rawTrx = trx.rawTransaction as? ProcessTransaction.SwapTransaction else { return }
+
+                switch trx.status {
+                case .sending:
+                    self.analyticsManager.log(
+                        event: .swapUserConfirmed(
+                            tokenAName: rawTrx.sourceWallet.token.symbol,
+                            tokenBName: rawTrx.destinationWallet.token.symbol,
+                            swapSum: rawTrx.amount,
+                            swapMAX: rawTrx.metaInfo.swapMAX,
+                            swapUSD: rawTrx.metaInfo.swapUSD,
+                            priceSlippage: rawTrx.slippage,
+                            feesSource: rawTrx.payingWallet?.token.name ?? "Unknown"
+                        )
+                    )
+                case let .confirmed(confirmation, _):
+                    if confirmation == 0 {
+                        self.analyticsManager.log(
+                            event: .swapStarted(
+                                tokenAName: rawTrx.sourceWallet.token.symbol,
+                                tokenBName: rawTrx.destinationWallet.token.symbol,
+                                swapSum: rawTrx.amount,
+                                swapMAX: rawTrx.metaInfo.swapMAX,
+                                swapUSD: rawTrx.metaInfo.swapUSD,
+                                priceSlippage: rawTrx.slippage,
+                                feesSource: rawTrx.payingWallet?.token.name ?? "Unknown"
+                            )
+                        )
+                    } else if prevTrx?.status.numberOfConfirmations == 0 {
+                        self.analyticsManager.log(
+                            event: .swapApprovedByNetwork(
+                                tokenAName: rawTrx.sourceWallet.token.symbol,
+                                tokenBName: rawTrx.destinationWallet.token.symbol,
+                                swapSum: rawTrx.amount,
+                                swapMAX: rawTrx.metaInfo.swapMAX,
+                                swapUSD: rawTrx.metaInfo.swapUSD,
+                                priceSlippage: rawTrx.slippage,
+                                feesSource: rawTrx.payingWallet?.token.name ?? "Unknown"
+                            )
+                        )
+                    }
+                case .finalized:
+                    self.analyticsManager.log(
+                        event: .swapCompleted(
+                            tokenAName: rawTrx.sourceWallet.token.symbol,
+                            tokenBName: rawTrx.destinationWallet.token.symbol,
+                            swapSum: rawTrx.amount,
+                            swapMAX: rawTrx.metaInfo.swapMAX,
+                            swapUSD: rawTrx.metaInfo.swapUSD,
+                            priceSlippage: rawTrx.slippage,
+                            feesSource: rawTrx.payingWallet?.token.name ?? "Unknown"
+                        )
+                    )
+                default:
+                    break
+                }
+            }
+            .store(in: &subscriptions)
     }
 }
