2186bcd40956e7bea3439567e19781e8ff90389e
Chung Tran <bigearsenal@gmail.com>
2022-08-09 10:49:51 +0700

    fix: UI code commit in background thread

--- p2p_wallet/Scenes/Main/History/History.SceneModel.swift
+++ p2p_wallet/Scenes/Main/History/History.SceneModel.swift
@@ -11,7 +11,6 @@ import SolanaSwift
 import TransactionParser
 
 extension History {
-    @MainActor
     class SceneModel: BEStreamListViewModel<ParsedTransaction> {
         typealias AccountSymbol = (account: String, symbol: String)
 
