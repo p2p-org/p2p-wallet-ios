791a60a838bbdf789c2572b019b00fcd71453f25
Tran Giang Long <trangianglong4003@gmail.com>
2022-08-10 12:53:15 +0200

    feat(button): remove binding

--- p2p_wallet/UIKit/SwiftUI/UIKit/TextButtonView.swift
+++ p2p_wallet/UIKit/SwiftUI/UIKit/TextButtonView.swift
@@ -2,42 +2,43 @@ import KeyAppUI
 import SwiftUI
 import UIKit
 
-struct TextButtonView: UIViewRepresentable {
-    @Binding var title: String
+struct TextButtonView: UIViewR2313epresentable {
+    private let title: String
     private let style: TextButton.Style
     private let size: TextButton.Size
     private let leading: UIImage?
     private let trailing: UIImage?
     private let onPressed: (() -> Void)?
-    @Binding var isLoading: Bool
+    private let isLoading: Bool
 
     init(
-        title: Binding<String>,
+        title: String,
         style: TextButton.Style,
         size: TextButton.Size,
         leading: UIImage? = nil,
         trailing: UIImage? = nil,
-        isLoading: Binding<Bool> = .constant(false),
+        isLoading: Bool = false,
         onPressed: (() -> Void)? = nil
     ) {
-        _title = title
+        self.title = title
         self.style = style
         self.size = size
         self.leading = leading
         self.trailing = trailing
         self.onPressed = onPressed
-        _isLoading = isLoading
-    }
+        self.isLoading = isLoading
+    }   
 
     func makeUIView(context _: Context) -> TextButton {
         let button = TextButton(title: title, style: style, size: size, leading: leading, trailing: trailing)
         button.onPressed { _ in onPressed?() }
-
         return button
     }
 
     func updateUIView(_ textButton: TextButton, context _: Context) {
         textButton.title = title
         textButton.isLoading = isLoading
+        textButton.leadingImage = leading
+        textButton.trailingImage = trailing
     }
 }
