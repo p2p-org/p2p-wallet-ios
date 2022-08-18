b377b9868ee90527c1814cb326887dc393fa633e
Tran Giang Long <trangianglong4003@gmail.com>
2022-08-18 16:04:21 +0200

    feat(restore-icloud): add view and view model

---
+++ p2p_wallet/Scenes/CreateOrRestoreWallet/RestoreWallet/Steps/ICloudRestore/ICloudRestoreView.swift
@@ -0,0 +1,56 @@
+// Copyright 2022 P2P Validator Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style license that can be
+// found in the LICENSE file.
+
+import KeyAppUI
+import SwiftUI
+
+struct ICloudRestoreView: View {
+    @ObservedObject var viewModel: ICloudRestoreViewModel
+
+    var body: some View {
+        VStack {
+            Spacer()
+            content
+            Spacer()
+        }
+        .navigationBarTitle(Text(L10n.createANewWallet), displayMode: .inline)
+        .navigationBarItems(
+            leading: Button(
+                action: { [weak viewModel] in viewModel?.back() },
+                label: {
+                    Image(uiImage: Asset.MaterialIcon.arrowBackIos.image)
+                        .foregroundColor(Color(Asset.Colors.night.color))
+                }
+            ),
+            trailing: Button(
+                action: { [weak viewModel] in viewModel?.info() },
+                label: {
+                    Image(uiImage: Asset.MaterialIcon.helpOutline.image)
+                        .foregroundColor(Color(Asset.Colors.night.color))
+                }
+            )
+        )
+        .background(Color(Asset.Colors.lime.color))
+        .edgesIgnoringSafeArea(.all)
+        .frame(maxHeight: .infinity)
+    }
+
+    var content: some View {
+        OnboardingContentView(
+            data: .init(
+                image: .introWelcomeToP2pFamily,
+                title: "Choose your wallet",
+                subtitle: "3 Wallets found"
+            )
+        ).padding(.horizontal, 40)
+    }
+}
+
+struct ICloudRestoreView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            ICloudRestoreView(viewModel: .init())
+        }
+    }
+}
