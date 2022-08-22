cc139c4b7fdf3ed76ca413525699264ad6b9e9b3
Tran Giang Long <trangianglong4003@gmail.com>
2022-08-22 10:54:08 +0200

    feat(onboarding): feature toggle for mocking

--- p2p_wallet/Scenes/CreateOrRestoreWallet/CreateWallet/CreateWalletViewModel.swift
+++ p2p_wallet/Scenes/CreateOrRestoreWallet/CreateWallet/CreateWalletViewModel.swift
@@ -12,14 +12,37 @@ final class CreateWalletViewModel: BaseViewModel {
 
     @Injected var onboardingService: OnboardingService
 
-    init(tKeyFacade _: TKeyFacade? = nil, initialState: CreateWalletFlowState?) {
+    init(initialState: CreateWalletFlowState?) {
+        let tKeyFacade: TKeyFacade = available(.mockedTKeyFacade) ?
+            TKeyMockupFacade() :
+            TKeyJSFacade(
+                wkWebView: GlobalWebView.requestWebView(),
+                config: .init(
+                    metadataEndpoint: String.secretConfig("META_DATA_ENDPOINT") ?? "",
+                    torusEndpoint: String.secretConfig("TORUS_ENDPOINT") ?? "",
+                    torusVerifierMapping: [
+                        "google": String.secretConfig("TORUS_GOOGLE_VERIFIER") ?? "",
+                        "apple": String.secretConfig("TORUS_APPLE_VERIFIER") ?? "",
+                    ]
+                )
+            )
+
+        #if !RELEASE
+            let apiGatewayEndpoint = String.secretConfig("API_GATEWAY_DEV")!
+        #else
+            let apiGatewayEndpoint = String.secretConfig("API_GATEWAY_PROD")!
+        #endif
+
+        let apiGatewayClient: APIGatewayClient = available(.mockedApiGateway) ?
+            APIGatewayClientImplMock() :
+            APIGatewayClientImpl(endpoint: apiGatewayEndpoint)
+
         onboardingStateMachine = .init(
             initialState: initialState,
             provider: .init(
                 authService: AuthServiceBridge(),
-                apiGatewayClient: APIGatewayClientImplMock(),
-                // apiGatewayClient: APIGatewayClientImpl(endpoint: String.secretConfig("API_GATEWAY")!),
-                tKeyFacade: TKeyMockupFacade(),
+                apiGatewayClient: apiGatewayClient,
+                tKeyFacade: tKeyFacade,
                 securityStatusProvider: Resolver.resolve()
             )
         )
