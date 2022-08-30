fa9879ec739137eacc3021e29f1c87fc54930c5f
Tran Giang Long <trangianglong4003@gmail.com>
2022-08-30 20:28:07 +0200

    feat(onboarding): improve

--- p2p_wallet/Scenes/StartOnboarding/Start/StartCoordinator.swift
+++ p2p_wallet/Scenes/StartOnboarding/Start/StartCoordinator.swift
@@ -1,5 +1,6 @@
 import Combine
 import Onboarding
+import Resolver
 import SwiftUI
 
 struct StartParameters {
@@ -50,9 +51,65 @@ final class StartCoordinator: Coordinator<OnboardingWallet> {
     }
 
     private func openCreateWallet(vc: UIViewController) {
-        coordinate(to: CreateWalletCoordinator(parent: vc)).sink { [weak vc] result in
+        let service: OnboardingService = Resolver.resolve()
+
+        let onFinish = { [weak vc] (result: CreateWalletResult) in
             switch result {
             case let .restore(socialProvider, email):
+                guard let vc = vc else { return }
+                self.openRestoreWallet(vc: vc)
+            case let .success(onboardingWallet):
+                self.subject.send(onboardingWallet)
+            }
+            self.subject.send(completion: .finished)
+        }
+
+        if let lastState = service.lastState {
+            switch lastState {
+            case let .bindingPhoneNumber(email, solPrivateKey, ethPublicKey, deviceShare, innerState):
+                switch innerState {
+                case let .block(until, _, phoneNumber, data):
+                    // Move user to block screen, after expired time move him to enter phone number
+                    if Date() < until {
+                        coordinate(to: CreateWalletCoordinator(
+                            parent: vc,
+                            initialState: CreateWalletFlowState.bindingPhoneNumber(
+                                email: email,
+                                solPrivateKey: solPrivateKey,
+                                ethPublicKey: ethPublicKey,
+                                deviceShare: deviceShare,
+                                BindingPhoneNumberState
+                                    .block(
+                                        until: until,
+                                        reason: .blockEnterPhoneNumber,
+                                        phoneNumber: phoneNumber,
+                                        data: data
+                                    )
+                            )
+                        ))
+                            .sink(receiveValue: onFinish)
+                            .store(in: &subscriptions)
+
+                        return
+                    } else {
+                        // Open continuation
+                        coordinate(to: ContinueCoordinator(window: window))
+                            .sink { result in
+                                self.subject.send(result)
+                                self.subject.send(completion: .finished)
+                            }.store(in: &subscriptions)
+
+                        return
+                    }
+                default: break
+                }
+            default: break
+            }
+        }
+
+        coordinate(to: CreateWalletCoordinator(parent: vc)).sink { [weak vc] result in
+            switch result {
+            case .restore:
                 guard let vc = vc else { return }
                 self.openRestoreWallet(vc: vc)
             case let .success(onboardingWallet):
