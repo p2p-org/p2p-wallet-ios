e2fe97883be27c759d24184adb2e64b3c1fc7c21
Tran Giang Long <trangianglong4003@gmail.com>
2022-09-02 11:43:56 +0200

    feat(onboarding): simulated social error

--- p2p_wallet/Services/Auth/AuthService/AuthServiceImpl.swift
+++ p2p_wallet/Services/Auth/AuthService/AuthServiceImpl.swift
@@ -11,6 +11,17 @@ import KeychainSwift
 final class AuthServiceImpl: AuthService {
     func socialSignIn(_ type: SocialType) async throws -> SocialAuthResponse {
         guard let service = type.authObject() else { throw SocialServiceError.invalidSocialType }
+
+        #if DEBUG
+            if available(.simulatedSocialError) {
+                switch Int.random(in: 0 ... 1) {
+                case 0: throw SocialServiceError.tokenIDIsNil
+                case 1: throw SocialServiceError.unknown
+                default: break
+                }
+            }
+        #endif
+
         return try await service.auth()
     }
 
