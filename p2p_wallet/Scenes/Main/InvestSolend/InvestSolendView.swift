f7722a33b655cbb49b59907cd43e1976b4073296
Tran Giang Long <trangianglong4003@gmail.com>
2022-09-14 01:03:51 +0200

    feat(solend): add invest screen

---
+++ p2p_wallet/Scenes/Main/InvestSolend/InvestSolendView.swift
@@ -0,0 +1,18 @@
+// Copyright 2022 P2P Validator Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style license that can be
+// found in the LICENSE file.
+
+import Solend
+import SwiftUI
+
+struct InvestSolendView: View {
+    var body: some View {
+        Text(")")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        InvestSolendView()
+    }
+}
