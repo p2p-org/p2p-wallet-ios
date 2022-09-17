aea7b86c39eef9e335449b885dbe684af8930d81
Tran Giang Long <trangianglong4003@gmail.com>
2022-09-17 21:30:14 +0200

    refactor(solend)

--- p2p_wallet/Scenes/Main/InvestSolend/InvestSolendView.swift
+++ p2p_wallet/Scenes/Main/InvestSolend/InvestSolendView.swift
@@ -2,19 +2,91 @@
 // Use of this source code is governed by a MIT-style license that can be
 // found in the LICENSE file.
 
+import KeyAppUI
+import Resolver
+import SolanaSwift
 import Solend
 import SwiftUI
 
 struct InvestSolendView: View {
-    let service = SolendServiceImpl(owner: try! .init(secretKey: Data()), lendingMark: "")
+    @StateObject var viewModel: InvestSolendViewModel
 
     var body: some View {
-        Text(")")
+        VStack(alignment: .leading) {
+            // Title
+            HStack {
+                Text(L10n.earnAYield)
+                    .fontWeight(.bold)
+                    .apply(style: .largeTitle)
+                Spacer()
+                Image(uiImage: Asset.MaterialIcon.helpOutline.image)
+            }.padding(.horizontal, 16)
+
+            // Card
+            VStack(alignment: .leading) {
+                // Title
+                Text(L10n.totalRewardsEarned)
+                    .foregroundColor(Color(Asset.Colors.mountain.color))
+                    .apply(style: .text3)
+
+                // Reward
+                Text("$ 0.0000000000")
+                    .fontWeight(.bold)
+                    .apply(style: .title1)
+                    .padding(.top, 8)
+
+                // Show deposit
+                HStack {
+                    Text(L10n.showDeposit("$ \(viewModel.totalDeposit.fixedDecimal(2))"))
+                    Spacer()
+                    Image(uiImage: Asset.MaterialIcon.accountBalanceWalletOutlined.image)
+                }
+                .padding(.all, 16)
+                .background(
+                    Color(Asset.Colors.snow.color)
+                        .cornerRadius(radius: 12, corners: .allCorners)
+                )
+                .padding(.top, 16)
+            }
+            .padding(.all, 20)
+            .frame(maxWidth: .infinity)
+            .background(
+                Color(Asset.Colors.rain.color)
+                    .cornerRadius(radius: 28, corners: .allCorners)
+            )
+            .padding(.horizontal, 16)
+            .padding(.bottom, 16)
+
+            // Market
+            ScrollView {
+                // Title
+                HStack {
+                    Text(L10n.depositToEarnAYield)
+                        .fontWeight(.semibold)
+                        .apply(style: .text1)
+                    Spacer()
+                    Text(L10n.apy)
+                        .fontWeight(.semibold)
+                        .apply(style: .text1)
+                }.padding(.horizontal, 16)
+
+                // Cells
+                ForEach(viewModel.market, id: \.asset.symbol) { asset, market, userDeposit in
+                    NavigationLink(destination: DepositSolendView(viewModel: try! .init(initialAsset: asset))) {
+                        InvestSolendCell(
+                            asset: asset,
+                            deposit: userDeposit?.depositedAmount,
+                            apy: market?.supplyInterest
+                        )
+                    }
+                }
+            }
+        }.onAppear { Task { try await viewModel.update() } }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        InvestSolendView()
+        InvestSolendView(viewModel: try! .init(mocked: true))
     }
 }
---
+++ p2p_wallet/Scenes/Main/InvestSolend/InvestSolendViewModel.swift
@@ -0,0 +1,68 @@
+// Copyright 2022 P2P Validator Authors. All rights reserved.
+// Use of this source code is governed by a MIT-style license that can be
+// found in the LICENSE file.
+
+import Combine
+import FeeRelayerSwift
+import Resolver
+import SolanaSwift
+import Solend
+
+typealias Invest = (asset: SolendConfigAsset, market: SolendMarketInfo?, userDeposit: SolendUserDeposit?)
+
+@MainActor
+class InvestSolendViewModel: ObservableObject {
+    private let service: SolendDataService
+    var subscriptions = Set<AnyCancellable>()
+
+    @Published var loading: Bool = false
+    @Published var market: [Invest] = []
+    @Published var totalDeposit: Double = 0
+
+    init(mocked: Bool) throws {
+        service = mocked ? SolendDataServiceMock() : Resolver.resolve(SolendDataService.self)
+
+        service.availableAssets
+            .combineLatest(service.marketInfo, service.deposits)
+            .map { (assets: [SolendConfigAsset], marketInfo: [SolendMarketInfo], userDeposits: [SolendUserDeposit]) -> [Invest] in
+                assets.map { asset -> Invest in
+                    (
+                        asset: asset,
+                        market: marketInfo.first(where: { $0.symbol == asset.symbol }),
+                        userDeposit: userDeposits.first(where: { $0.symbol == asset.symbol })
+                    )
+                }.sorted { (v1: Invest, v2: Invest) -> Bool in
+                    let apy1: Double = .init(v1.market?.supplyInterest ?? "") ?? 0
+                    let apy2: Double = .init(v2.market?.supplyInterest ?? "") ?? 0
+                    return apy1 > apy2
+                }
+            }
+            .sink { [weak self] value in self?.market = value }
+            .store(in: &subscriptions)
+
+        service.deposits
+            .map { deposits -> Double in
+                deposits.reduce(0) { (partialResult: Double, deposit: SolendUserDeposit) in
+                    partialResult + (Double(deposit.depositedAmount) ?? 0)
+                }
+            }
+            .sink { [weak self] (totalDeposit: Double) in
+                self?.totalDeposit = totalDeposit
+            }
+            .store(in: &subscriptions)
+
+        service.status
+            .sink { [weak self] status in
+                guard let self = self else { return }
+                switch status {
+                case .initialized, .ready: self.loading = false
+                case .updating: self.loading = true
+                }
+            }
+            .store(in: &subscriptions)
+    }
+
+    func update() async throws {
+        try await service.update()
+    }
+}
