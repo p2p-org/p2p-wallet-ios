fc77912989fc09f27fff2df449c9c565cef4b217
Chung Tran <bigearsenal@gmail.com>
2022-09-20 11:16:54 +0700

    feat: implementing SolendTutorial

---
+++ p2p_wallet/Scenes/Main/InvestSolend/Tutorial/SolendTutorialData.swift
@@ -0,0 +1,14 @@
+//
+//  SolendTutorialData.swift
+//  p2p_wallet
+//
+//  Created by Chung Tran on 20/09/2022.
+//
+
+import Foundation
+
+struct SolendTutorialContentData {
+    let image: UIImage
+    let title: String
+    let subtitle: String
+}
---
+++ p2p_wallet/Scenes/Main/InvestSolend/Tutorial/SolendTutorialView.swift
@@ -0,0 +1,54 @@
+import Combine
+import KeyAppUI
+import SolanaSwift
+import SwiftUI
+
+struct SolendTutorialView: View {
+    @ObservedObject var viewModel: StartViewModel
+    @State private var isShowing = false
+
+    var body: some View {
+        ZStack {
+            Color(Asset.Colors.lime.color)
+                .edgesIgnoringSafeArea(.all)
+            VStack(spacing: .zero) {
+                if isShowing {
+                    PagingView(
+                        index: $viewModel.currentDataIndex.animation(),
+                        maxIndex: viewModel.data.count - 1,
+                        fillColor: Color(Asset.Colors.night.color)
+                    ) {
+                        ForEach(viewModel.data, id: \.id) { data in
+                            OnboardingContentView(data: data)
+                        }
+                    }
+                    .transition(.opacity.combined(with: .move(edge: .top)))
+
+                    bottomActionsView
+                        .transition(.opacity.combined(with: .move(edge: .bottom)))
+                }
+            }
+            .edgesIgnoringSafeArea(.bottom)
+        }
+    }
+}
+
+extension SolendTutorialView {
+    private var bottomActionsView: some View {
+        BottomActionContainer {
+            VStack(spacing: .zero) {
+                // Create a wallet
+                TextButtonView(
+                    title: L10n.createANewWallet,
+                    style: .inverted,
+                    size: .large,
+                    trailing: Asset.MaterialIcon.arrowForward.image
+                ) { [weak viewModel] in
+                    viewModel?.createWalletDidTap.send()
+                }
+                    .frame(height: 56)
+                    .frame(maxWidth: .infinity)
+            }
+        }
+    }
+}
