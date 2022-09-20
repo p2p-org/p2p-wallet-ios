a29905021d00ac7bad6f5b6431669c3dbbb62efc
Chung Tran <bigearsenal@gmail.com>
2022-09-20 14:45:13 +0700

    feat: some fixes

--- p2p_wallet/Scenes/Main/InvestSolend/Tutorial/SolendTutorialViewModel.swift
+++ p2p_wallet/Scenes/Main/InvestSolend/Tutorial/SolendTutorialViewModel.swift
@@ -9,10 +9,8 @@ final class SolendTutorialViewModel: BaseViewModel {
     let skipDidTap = PassthroughSubject<Void, Never>()
     let nextDidTap = PassthroughSubject<Void, Never>()
     let continueDidTap = PassthroughSubject<Void, Never>()
-    
-    private var subscriptions = [AnyCancellable]()
 
-    init() {
+    override init() {
         data = [
             SolendTutorialContentData(
                 image: .coins,
