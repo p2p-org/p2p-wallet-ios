904c97d5777a3aa4bcbf5938ecbd1c35e3ec2192
Chung Tran <bigearsenal@gmail.com>
2022-09-20 15:21:59 +0700

    fix: add SolendTutorialView

--- p2p_wallet/Scenes/Main/InvestSolend/Tutorial/SolendTutorialView.swift
+++ p2p_wallet/Scenes/Main/InvestSolend/Tutorial/SolendTutorialView.swift
@@ -5,28 +5,25 @@ import SwiftUI
 
 struct SolendTutorialView: View {
     @ObservedObject var viewModel: SolendTutorialViewModel
-    @State private var isShowing = false
 
     var body: some View {
         ZStack {
-            Color(Asset.Colors.lime.color)
+            Color(.e0dbff)
                 .edgesIgnoringSafeArea(.all)
             VStack(spacing: .zero) {
-                if isShowing {
-                    PagingView(
-                        index: $viewModel.currentDataIndex.animation(),
-                        maxIndex: viewModel.data.count - 1,
-                        fillColor: Color(Asset.Colors.night.color)
-                    ) {
-                        ForEach(viewModel.data) { data in
-                            SolendTutorialSlideView(data: data)
-                        }
+                PagingView(
+                    index: $viewModel.currentDataIndex.animation(),
+                    maxIndex: viewModel.data.count - 1,
+                    fillColor: Color(Asset.Colors.night.color)
+                ) {
+                    ForEach(viewModel.data) { data in
+                        SolendTutorialSlideView(data: data)
                     }
-                    .transition(.opacity.combined(with: .move(edge: .top)))
-
-                    bottomActionsView
-                        .transition(.opacity.combined(with: .move(edge: .bottom)))
                 }
+                .transition(.opacity.combined(with: .move(edge: .top)))
+
+                bottomActionsView
+                    .transition(.opacity.combined(with: .move(edge: .bottom)))
             }
             .edgesIgnoringSafeArea(.bottom)
         }
