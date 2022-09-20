79a461014fa8794ed54d76a4b04b32b22bd90a9c
Chung Tran <bigearsenal@gmail.com>
2022-09-20 16:14:44 +0700

    feat: skip button

--- p2p_wallet/Scenes/Main/InvestSolend/Tutorial/SolendTutorialView.swift
+++ p2p_wallet/Scenes/Main/InvestSolend/Tutorial/SolendTutorialView.swift
@@ -28,11 +28,28 @@ struct SolendTutorialView: View {
                     .transition(.opacity.combined(with: .move(edge: .bottom)))
             }
             .edgesIgnoringSafeArea(.bottom)
+            
+            HStack {
+                Spacer()
+                VStack {
+                    Button(L10n.skip.uppercaseFirst) {
+                        markAsReadAndDismiss()
+                    }
+                    .foregroundColor(Color(Asset.Colors.night.color))
+                    .font(.system(size: UIFont.fontSize(of: .text1), weight: .bold))
+                    .padding(.init(top: max(safeAreaInsets.top, 20), leading: 0, bottom: 0, trailing: 20))
+                    Spacer()
+                }
+            }
         }
     }
-}
-
-extension SolendTutorialView {
+    
+    // MARK: - Actions
+    private func markAsReadAndDismiss() {
+        presentationMode.wrappedValue.dismiss()
+    }
+    
+    // MARK: - ViewBuilders
     private var bottomActionsView: some View {
         TextButtonView(
             title: viewModel.isLastPage ? L10n.continue: L10n.next.uppercaseFirst,
@@ -41,7 +58,7 @@ extension SolendTutorialView {
             isEnabled: .constant(true)
         ) { [weak viewModel] in
             if viewModel?.isLastPage == true {
-                presentationMode.wrappedValue.dismiss()
+                markAsReadAndDismiss()
             } else {
                 withAnimation {
                     viewModel?.next()
