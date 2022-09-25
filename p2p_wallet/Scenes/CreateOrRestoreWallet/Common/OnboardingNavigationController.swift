f03acac64b2ab6527e3d97d29c7bfa94aa63d4ee
Tran Giang Long <trangianglong4003@gmail.com>
2022-09-25 23:03:28 +0700

    fix(navigation)

--- p2p_wallet/Scenes/CreateOrRestoreWallet/Common/OnboardingNavigationController.swift
+++ p2p_wallet/Scenes/CreateOrRestoreWallet/Common/OnboardingNavigationController.swift
@@ -3,6 +3,10 @@ import UIKit
 class OnboardingNavigationController: UINavigationController {
     override init(nibName: String? = nil, bundle: Bundle? = nil) {
         super.init(nibName: nibName, bundle: bundle)
+
+        navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationBar.backgroundColor = .clear
+
         modalPresentationStyle = .fullScreen
         modalTransitionStyle = .crossDissolve
     }
