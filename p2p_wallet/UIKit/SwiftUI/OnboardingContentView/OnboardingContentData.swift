c1c275725243eca66a6d435f37915cd1f2082416
Elizaveta Semenova <thesharkight84@gmail.com>
2022-09-27 13:36:51 +0300

    [PWN-5191] Add ability to put inseparable string inside OnboardingContentView at any position

--- p2p_wallet/UIKit/SwiftUI/OnboardingContentView/OnboardingContentData.swift
+++ p2p_wallet/UIKit/SwiftUI/OnboardingContentView/OnboardingContentData.swift
@@ -1,14 +1,29 @@
 struct OnboardingContentData: Identifiable, Equatable {
+    struct Subtitle: Identifiable, Equatable {
+        var id: String { text }
+        let text: String
+        let isLimited: Bool
+
+        init(text: String, isLimited: Bool = false) {
+            self.text = text
+            self.isLimited = isLimited
+        }
+    }
+
     var id: String { title }
     let image: UIImage?
     let title: String
-    let email: String?
-    let subtitle: String?
+    let subtitles: [Subtitle]
 
-    init(image: UIImage, title: String, email: String? = nil, subtitle: String? = nil) {
+    init(image: UIImage, title: String, subtitles: [Subtitle] = []) {
         self.image = image
         self.title = title
-        self.email = email
-        self.subtitle = subtitle
+        self.subtitles = subtitles
+    }
+
+    init(image: UIImage, title: String, subtitle: String) {
+        self.image = image
+        self.title = title
+        subtitles = [Subtitle(text: subtitle, isLimited: false)]
     }
 }
