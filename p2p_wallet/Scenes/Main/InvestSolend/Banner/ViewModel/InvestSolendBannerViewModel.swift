7da81992291f0099b69e79b715a011990e499c6d
Tran Giang Long <trangianglong4003@gmail.com>
2022-09-30 00:42:04 +0700

    feat(solend): update banner logic

--- p2p_wallet/Scenes/Main/InvestSolend/Banner/ViewModel/InvestSolendBannerViewModel.swift
+++ p2p_wallet/Scenes/Main/InvestSolend/Banner/ViewModel/InvestSolendBannerViewModel.swift
@@ -8,41 +8,59 @@ import Resolver
 import Solend
 
 class InvestSolendBannerViewModel: ObservableObject {
+    @Injected private var priceService: PricesServiceType
+
     @Published var state: InvestSolendBannerState = .pending
 
     private var cancellables = Set<AnyCancellable>()
     private let solendService: SolendDataService
 
-    init(mocked: Bool = true) {
+    init(mocked: Bool = false) {
         solendService = mocked ? SolendDataServiceMock() : Resolver.resolve(SolendDataService.self)
 
         solendService.availableAssets
             .combineLatest(solendService.deposits)
-            .sink(receiveValue: { [weak self] assets, deposits in
+            .sink(receiveValue: { [weak self] (assets: [SolendConfigAsset]?, deposits: [SolendUserDeposit]?) in
                 self?.state = .learnMore
+
+                guard let assets = assets else {
+                    self?.state = .failure(title: L10n.somethingWentWrong, subtitle: L10n.pleaseTryAgainLater)
+                    return
+                }
+
+                guard let deposits = deposits else {
+                    self?.state = .learnMore
+                    return
+                }
+
                 if deposits.isEmpty {
                     self?.state = .learnMore
                 } else {
-                    let urls = assets.map { asset -> URL? in
-                        var asset = asset
-                        if asset.symbol == "USDT" {
-                            asset = .init(
-                                name: asset.name,
-                                symbol: asset.symbol,
-                                decimals: asset.decimals,
-                                mintAddress: asset.mintAddress,
-                                logo: "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/BQcdHdAQW1hczDbBi9hiegXAR7A98Q9jx3X3iBBBDiq4/logo.png"
-                            )
-                        }
-                        return URL(string: asset.logo ?? "")
-                    }.compactMap { $0 }
+                    let urls = assets
+                        .map { asset -> URL? in URL(string: asset.logo ?? "") }
+                        .compactMap { $0 }
+
+                    let total = self?.calculateBalance(assets: assets, deposits: deposits) ?? 0
                     self?.state = .withBalance(model: .init(
-                        balance: "$89.5762413036",
+                        balance: "$ \(total.fixedDecimal(9))",
                         depositUrls: urls
                     ))
                 }
             })
             .store(in: &cancellables)
+    }
+
+    func calculateBalance(assets: [SolendConfigAsset], deposits: [SolendUserDeposit]) -> Double {
+        deposits
+            .map { (deposit: SolendUserDeposit) -> (amount: Double, symbol: String) in
+                (deposit.depositedAmount.double ?? 0, deposit.symbol)
+            }
+            .map { [priceService] (amount: Double, symbol: String) -> Double in
+                guard let asset: SolendConfigAsset = assets.first(where: { $0.symbol == symbol }) else { return 0 }
+                let currentPrice = priceService.currentPrice(for: symbol)?.value ?? 0
+                return currentPrice * amount
+            }
+            .reduce(0, +)
     }
 }
 
