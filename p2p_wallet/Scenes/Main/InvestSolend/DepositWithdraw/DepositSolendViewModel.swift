8d6ab838071af9fe948625d2e6f2baa90e02fc6a
Alexey Sidorov <ody344@gmail.com>
2022-10-12 14:09:38 +0400

    Keyboard after lock fix

--- p2p_wallet/Scenes/Main/InvestSolend/DepositWithdraw/DepositSolendViewModel.swift
+++ p2p_wallet/Scenes/Main/InvestSolend/DepositWithdraw/DepositSolendViewModel.swift
@@ -311,12 +311,26 @@ class DepositSolendViewModel: ObservableObject {
                 }
             }.store(in: &subscriptions)
 
-        NotificationCenter.default.addObserver(forName: UIApplication.willResignActiveNotification, object: nil, queue: nil) { _ in
+        NotificationCenter.default.addObserver(
+            forName: UIApplication.willResignActiveNotification,
+            object: nil,
+            queue: nil
+        ) { _ in
             DispatchQueue.main.async {
-                _ = UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                _ = UIApplication.shared.sendAction(
+                    #selector(UIResponder.resignFirstResponder),
+                    to: nil,
+                    from: nil,
+                    for: nil
+                )
             }
         }
-        NotificationCenter.default.addObserver(forName: UIApplication.willEnterForegroundNotification, object: nil, queue: nil) { _ in
+
+        NotificationCenter.default.addObserver(
+            forName: UIApplication.willEnterForegroundNotification,
+            object: nil,
+            queue: nil
+        ) { _ in
             DispatchQueue.main.async {
                 self.focusSide = .left
             }
