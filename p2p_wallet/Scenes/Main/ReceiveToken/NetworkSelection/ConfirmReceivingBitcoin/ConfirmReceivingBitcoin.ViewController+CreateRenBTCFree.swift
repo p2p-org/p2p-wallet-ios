1e2020c7af5312f9578c6b6fc63664725059f39c
Chung Tran <bigearsenal@gmail.com>
2022-10-13 11:24:54 +0700

    fix: ui

---
+++ p2p_wallet/Scenes/Main/ReceiveToken/NetworkSelection/ConfirmReceivingBitcoin/ConfirmReceivingBitcoin.ViewController+CreateRenBTCFree.swift
@@ -0,0 +1,40 @@
+//
+//  ConfirmReceivingBitcoin.ViewController+CreateRenBTCFree.swift
+//  p2p_wallet
+//
+//  Created by Chung Tran on 13/10/2022.
+//
+
+import Foundation
+import RxCocoa
+import RxSwift
+
+extension ConfirmReceivingBitcoin.ViewController {
+    func createRenBTCFreeView() -> BEVStack {
+        BEVStack(spacing: 12) {
+            ReceiveToken.textBuilder(
+                text: L10n.aToReceiveBitcoinsOverTheBitcoinNetwork(L10n.renBTCAccountIsRequired)
+                    .asMarkdown()
+            )
+            ReceiveToken.textBuilder(
+                text: L10n.yourWalletListDoesNotContainARenBTCAccountAndToCreateOne(L10n
+                    .youNeedToMakeATransaction)
+                    .asMarkdown()
+            )
+            ReceiveToken.textBuilder(
+                text: L10n.youToPayForAccountCreationButIfSomeoneSendsRenBTCToYourAddressItWillBeCreatedForYou(L10n
+                    .donTHaveFunds)
+                    .asMarkdown()
+            )
+        }
+    }
+
+    func createRenBTCFreeButton() -> UIView {
+        WLStepButton.main(image: .buttonBuy.withTintColor(.white), text: L10n.free)
+            .onTap { [unowned self] in
+                self.dismiss(animated: true) {
+                    self.viewModel.dismissAndTopUp()
+                }
+            }
+    }
+}
