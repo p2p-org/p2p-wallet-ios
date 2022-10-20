50c30d9375bb7adba1b7aa94ede42a3883c5249a
Elizaveta Semenova <thesharkight84@gmail.com>
2022-10-20 13:14:05 +0300

    [PWN-5601] Apply validation to username field and change logic of requests

--- p2p_wallet/Services/CreateName/CreateNameService.swift
+++ p2p_wallet/Services/CreateName/CreateNameService.swift
@@ -4,33 +4,45 @@ import Resolver
 import SolanaSwift
 
 protocol CreateNameService {
-    var transactionDetails: AnyPublisher<(isSuccess: Bool, transaction: String), Never> { get }
-    func send(transaction: String, name: String, owner: String)
+    var transactionDetails: AnyPublisher<Bool, Never> { get }
+    func create(username: String)
 }
 
 final class CreateNameServiceImpl: CreateNameService {
-    var transactionDetails: AnyPublisher<(isSuccess: Bool, transaction: String), Never> {
+    var transactionDetails: AnyPublisher<Bool, Never> {
         transactionDetailsSubject.eraseToAnyPublisher()
     }
 
     @Injected private var solanaAPIClient: SolanaAPIClient
     @Injected private var nameCache: NameServiceCacheType
+    @Injected private var nameService: NameService
+    @Injected private var storage: AccountStorageType
 
-    private let transactionDetailsSubject = PassthroughSubject<(isSuccess: Bool, transaction: String),
-        Never>()
+    private let transactionDetailsSubject = PassthroughSubject<Bool, Never>()
 
-    func send(transaction: String, name: String, owner: String) {
+    func create(username: String) {
         Task {
             do {
+                guard let account = storage.account else {
+                    transactionDetailsSubject.send(false)
+                    return
+                }
+
+                let createResult = try await self.nameService.create(
+                    name: username,
+                    publicKey: account.publicKey.base58EncodedString,
+                    privateKey: account.secretKey
+                )
+
                 _ = try await solanaAPIClient.sendTransaction(
-                    transaction: transaction,
+                    transaction: createResult.transaction,
                     configs: RequestConfiguration(encoding: "base64")!
                 )
-                nameCache.save(name, for: owner)
-                transactionDetailsSubject.send((isSuccess: true, transaction: transaction))
+
+                nameCache.save(username, for: account.publicKey.base58EncodedString)
+                transactionDetailsSubject.send(true)
             } catch {
-                debugPrint(error)
-                transactionDetailsSubject.send((isSuccess: false, transaction: transaction))
+                transactionDetailsSubject.send(false)
             }
         }
     }
