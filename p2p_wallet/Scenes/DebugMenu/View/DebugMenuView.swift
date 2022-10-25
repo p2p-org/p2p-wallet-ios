830a65a5d48f49e34cf4c15a4877dbe18ef841b1
Ivan Babich <babichivanya@gmail.com>
2022-10-25 13:19:29 +0300

    [PWN-5658] Не работает pullToRefresh на главной

--- p2p_wallet/Scenes/DebugMenu/View/DebugMenuView.swift
+++ p2p_wallet/Scenes/DebugMenu/View/DebugMenuView.swift
@@ -37,8 +37,9 @@ struct DebugMenuView: View {
                 Section(header: Text("Fee relayer")) {
                     Toggle("Disable free transaction", isOn: $feeRelayerConfig.disableFeeTransaction)
                         .valueChanged(value: feeRelayerConfig.disableFeeTransaction) { newValue in
+                            #if !RELEASE
                             showDebugger(false)
-                            
+                            #endif
                             let app: AppEventHandlerType = Resolver.resolve()
                             app.delegate?.refresh()
                         }
