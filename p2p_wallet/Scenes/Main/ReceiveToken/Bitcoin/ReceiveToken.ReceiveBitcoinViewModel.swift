7c6bb04a5bd2878dac95e991c019508099ed86a7
Chung Tran <bigearsenal@gmail.com>
2022-10-27 19:49:45 +0700

    feat: dynamic minimum transaction amount on receiving btc

--- p2p_wallet/Scenes/Main/ReceiveToken/Bitcoin/ReceiveToken.ReceiveBitcoinViewModel.swift
+++ p2p_wallet/Scenes/Main/ReceiveToken/Bitcoin/ReceiveToken.ReceiveBitcoinViewModel.swift
@@ -10,9 +10,11 @@ import RenVMSwift
 import Resolver
 import RxCocoa
 import RxSwift
+import SolanaSwift
 
 protocol ReceiveTokenBitcoinViewModelType: AnyObject {
     var addressDriver: Driver<String?> { get }
+    var minimumTransactionAmountSignal: Signal<Double?> { get }
     var timerSignal: Signal<Void> { get }
     var processingTxsDriver: Driver<[LockAndMint.ProcessingTx]> { get }
     var hasExplorerButton: Bool { get }
@@ -37,6 +39,7 @@ extension ReceiveToken {
 
         @Injected private var persistentStore: LockAndMintServicePersistentStore
         @Injected private var lockAndMintService: LockAndMintService
+        @Injected private var renVMRpcClient: RenVMRpcClientType
         @Injected private var analyticsManager: AnalyticsManager
         @Injected private var clipboardManager: ClipboardManagerType
         @Injected private var imageSaver: ImageSaverType
@@ -46,6 +49,7 @@ extension ReceiveToken {
 
         private let isLoadingSubject = BehaviorRelay<Bool>(value: false)
         private let timerSubject = PublishRelay<Void>()
+        private let minimumTransactionAmountSubject = PublishRelay<Double?>()
         private let navigationSubject: PublishRelay<NavigatableScene?>
         private let addressSubject = BehaviorRelay<String?>(value: nil)
         private let processingTransactionsSubject = BehaviorRelay<[LockAndMint.ProcessingTx]>(value: [])
@@ -116,6 +120,14 @@ extension ReceiveToken {
                     self?.addressSubject.accept(address)
                 }
             }
+            
+            Task {
+                let fee = try await renVMRpcClient.estimateTransactionFee()?
+                    .convertToBalance(decimals: Token.renBTC.decimals)
+                await MainActor.run { [weak self] in
+                    self?.minimumTransactionAmountSubject.accept(fee * 2)
+                }
+            }
         }
         
         private func updateSessionEndDate() async {
@@ -162,6 +174,10 @@ extension ReceiveToken.ReceiveBitcoinViewModel: ReceiveTokenBitcoinViewModelType
 
     var timerSignal: Signal<Void> {
         timerSubject.asSignal()
+    }
+    
+    var minimumTransactionAmountSignal: Signal<Double?> {
+        minimumTransactionAmountSubject.asSignal()
     }
 
     var processingTxsDriver: Driver<[LockAndMint.ProcessingTx]> {
