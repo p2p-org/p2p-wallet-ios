4e19038478f53054365f1bdac31574973448e1eb
Chung Tran <bigearsenal@gmail.com>
2022-11-10 17:47:16 +0700

    feat: forward BlockchainClient

--- p2p_wallet/Services/RenVM/RenVMSolanaChainProvider.swift
+++ p2p_wallet/Services/RenVM/RenVMSolanaChainProvider.swift
@@ -9,12 +9,12 @@ import Foundation
 import RenVMSwift
 import Resolver
 import SolanaSwift
+import FeeRelayerSwift
 
 class RenVMSolanaChainProvider: ChainProvider {
     @Injected private var accountStorage: SolanaAccountStorage
     @Injected private var rpcClient: RenVMRpcClientType
     @Injected private var apiClient: SolanaAPIClient
-    @Injected private var blockchainClient: SolanaBlockchainClient
 
     /// Get authorized account from chain
     func getAccount() async throws -> (publicKey: Data, secret: Data) {
@@ -29,7 +29,62 @@ class RenVMSolanaChainProvider: ChainProvider {
         try await SolanaChain.load(
             client: rpcClient,
             apiClient: apiClient,
-            blockchainClient: blockchainClient
+            blockchainClient: RenVMFeeRelayerSolanaBlockchainClient()
         )
+    }
+}
+
+private class RenVMFeeRelayerSolanaBlockchainClient: SolanaBlockchainClient {
+    @Injected private var blockchainClient: SolanaBlockchainClient
+    @Injected private var feeRelayerAPIClient: FeeRelayerAPIClient
+    @Injected private var feeRelayerContextManager: FeeRelayerContextManager
+    @Injected private var feeRelayer: FeeRelayer
+    
+    var apiClient: SolanaAPIClient {
+        get { blockchainClient.apiClient }
+        set { blockchainClient.apiClient = newValue }
+    }
+    
+    func prepareTransaction(
+        instructions: [SolanaSwift.TransactionInstruction],
+        signers: [SolanaSwift.Account],
+        feePayer: SolanaSwift.PublicKey,
+        feeCalculator: SolanaSwift.FeeCalculator?
+    ) async throws -> SolanaSwift.PreparedTransaction {
+        let feePayer = try await feeRelayerAPIClient.getFeePayerPubkey()
+        
+        class FreeFeeCalculator: FeeCalculator {
+            func calculateNetworkFee(transaction: Transaction) throws -> FeeAmount {
+                .zero
+            }
+        }
+        
+        return try await blockchainClient.prepareTransaction(
+            instructions: instructions,
+            signers: [],
+            feePayer: try PublicKey(string: feePayer),
+            feeCalculator: FreeFeeCalculator()
+        )
+    }
+    
+    func sendTransaction(
+        preparedTransaction: PreparedTransaction
+    ) async throws -> String {
+        let context = try await feeRelayerContextManager.getCurrentContext()
+        return try await feeRelayer.topUpAndRelayTransaction(
+            context,
+            preparedTransaction,
+            fee: nil,
+            config: .init(
+                operationType: .transfer,
+                currency: PublicKey.renBTCMint.base58EncodedString
+            )
+        )
+    }
+    
+    func simulateTransaction(
+        preparedTransaction: PreparedTransaction
+    ) async throws -> SimulationResult {
+        fatalError()
     }
 }
