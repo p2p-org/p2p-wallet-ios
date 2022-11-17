ecfc9ce06d90e661bba7708943b975b9f1a335b9
Tran Giang Long <trangianglong4003@gmail.com>
2022-11-17 16:37:08 +0700

    feat(send): prepare input view

--- p2p_wallet/Scenes/Main/Send/Input/SendInputView.swift
+++ p2p_wallet/Scenes/Main/Send/Input/SendInputView.swift
@@ -8,13 +8,32 @@
 import SwiftUI
 
 struct SendInputView: View {
+    @ObservedObject var viewModel: SendInputViewModel
+
+    @State var detailModal: Bool = false
+    @State var freeTransactionModal: Bool = false
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Input screen")
+            Button { detailModal = true } label: { Text("Open detail") }
+            Button { freeTransactionModal = true } label: { Text("Open free transaction") }
+        }
+        .sheet(isPresented: $detailModal) {
+            Text("Detail View")
+        }
+        .sheet(isPresented: $freeTransactionModal) {
+            Text("Free transaction modal")
+        }
     }
 }
 
 struct SendInputView_Previews: PreviewProvider {
     static var previews: some View {
-        SendInputView()
+        SendInputView(viewModel: .init(recipient: .init(
+            address: "8JmwhgewSppZ2sDNqGZoKu3bWh8wUKZP8mdbP4M1XQx1",
+            category: .solanaAddress,
+            hasFunds: false
+        )))
     }
 }
