68fbc2722dfaa1eb8e8ec96406de3d48f58a2830
Chung Tran <bigearsenal@gmail.com>
2022-12-08 21:18:33 +0700

    feat: DecimalTextField

---
+++ p2p_wallet/SwiftUI/DecimalTextField.swift
@@ -0,0 +1,87 @@
+//
+//  DecimalTextField.swift
+//  p2p_wallet
+//
+//  Created by Chung Tran on 08/12/2022.
+//
+
+import Foundation
+import UIKit
+import SwiftUI
+import BEPureLayout
+import KeyAppUI
+
+struct DecimalTextField: UIViewRepresentable {
+    @Binding private var value: Double?
+    @Binding private var textColor: UIColor
+    private var configuration = { (_: BEDecimalTextField) in }
+
+    init(
+        value: Binding<Double?>,
+        textColor: Binding<UIColor> = Binding.constant(Asset.Colors.night.color),
+        configuration: @escaping (BEDecimalTextField) -> Void = { _ in }
+    ) {
+        self.configuration = configuration
+        _value = value
+        _textColor = textColor
+    }
+
+    func makeUIView(context: Context) -> BEDecimalTextField {
+        let view = BEDecimalTextField()
+        view.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+        view.delegate = context.coordinator
+        view.addTarget(
+            context.coordinator,
+            action: #selector(context.coordinator.textViewDidChange),
+            for: .editingChanged
+        )
+        view.textColor = textColor
+        return view
+    }
+
+    func updateUIView(_ uiView: BEDecimalTextField, context _: Context) {
+        if let value = value {
+            if let text = uiView.text,
+               let double = Double(text),
+               double == value
+            {
+                
+            } else {
+                uiView.text = value.toString(maximumFractionDigits: 9)
+            }
+        } else {
+            uiView.text = nil
+        }
+        
+        configuration(uiView)
+        uiView.textColor = textColor
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator($value)
+    }
+
+    class Coordinator: NSObject, UITextFieldDelegate {
+        var value: Binding<Double?>
+
+        init(_ value: Binding<Double?>) {
+            self.value = value
+        }
+
+        @objc func textViewDidChange(_ textField: UITextField) {
+            if let text = textField.text,
+               let double = Double(text)
+            {
+                value.wrappedValue = double
+            } else {
+                value.wrappedValue = nil
+            }
+        }
+
+        func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange,
+                       replacementString string: String) -> Bool
+        {
+            (textField as! BEDecimalTextField).shouldChangeCharactersInRange(range, replacementString: string)
+        }
+    }
+}
