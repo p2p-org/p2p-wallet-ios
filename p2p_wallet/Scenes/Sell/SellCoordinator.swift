9d14e9f4cb0ccfd7073e908d932112455333264d
Chung Tran <bigearsenal@gmail.com>
2022-12-08 11:32:22 +0700

    feat: remind of making coordinator

--- p2p_wallet/Scenes/Sell/SellCoordinator.swift
+++ p2p_wallet/Scenes/Sell/SellCoordinator.swift
@@ -40,5 +40,7 @@ final class SellCoordinator: Coordinator<SellCoordinatorResult> {
         let vc = SFSafariViewController(url: url)
         vc.modalPresentationStyle = .automatic
         navigationController.present(vc, animated: true)
+        
+        // TODO - Make coordinator
     }
 }
