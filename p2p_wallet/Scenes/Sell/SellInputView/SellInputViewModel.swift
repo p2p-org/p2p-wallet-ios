ba5b00332b588b346b2c983129b531f2671e2755
Chung Tran <bigearsenal@gmail.com>
2022-12-08 21:37:45 +0700

    feat: add logic for exchange rate

--- p2p_wallet/Scenes/Sell/SellInputView/SellInputViewModel.swift
+++ p2p_wallet/Scenes/Sell/SellInputView/SellInputViewModel.swift
@@ -13,5 +13,45 @@ final class SellInputViewModel: BaseViewModel, ObservableObject {
     // MARK: - Properties
     
     @Published var baseAmount: Double?
+    @Published var isEnteringBaseAmount: Bool = false
+    
     @Published var quoteAmount: Double?
+    @Published var isEnteringQuoteAmount: Bool = false
+    
+    @Published var exchangeRate: Double = .random(in: 13...13.99)
+    
+    // MARK: - Initializer
+    
+    override init() {
+        super.init()
+        bind()
+    }
+    
+    // MARK: - Binding
+    
+    private func bind() {
+        // enter base amount
+        Publishers.CombineLatest($baseAmount, $exchangeRate)
+            .filter {[weak self] _ in
+                self?.isEnteringBaseAmount == true
+            }
+            .map { baseAmount, exchangeRate in
+                guard let baseAmount else {return nil}
+                return baseAmount * exchangeRate
+            }
+            .assign(to: \.quoteAmount, on: self)
+            .store(in: &subscriptions)
+        
+        // enter quote amount
+        Publishers.CombineLatest($quoteAmount, $exchangeRate)
+            .filter {[weak self] _ in
+                self?.isEnteringQuoteAmount == true
+            }
+            .map { quoteAmount, exchangeRate in
+                guard let quoteAmount, exchangeRate != 0 else {return nil}
+                return quoteAmount / exchangeRate
+            }
+            .assign(to: \.baseAmount, on: self)
+            .store(in: &subscriptions)
+    }
 }
