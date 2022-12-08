e04e011dfa5c926ab51e5062de32de0c2fb966d8
Chung Tran <bigearsenal@gmail.com>
2022-12-08 08:54:12 +0700

    feat: open webview

--- p2p_wallet/Scenes/Sell/SellCoordinator.swift
+++ p2p_wallet/Scenes/Sell/SellCoordinator.swift
@@ -2,6 +2,7 @@ import Combine
 import Foundation
 import SwiftUI
 import UIKit
+import SafariServices
 
 typealias SellCoordinatorResult = Void
 
@@ -13,9 +14,31 @@ final class SellCoordinator: Coordinator<SellCoordinatorResult> {
     }
 
     override func start() -> AnyPublisher<SellCoordinatorResult, Never> {
+        // create SellViewModel
         let viewModel = SellViewModel()
+        
+        // buy tapped
+        viewModel.$subscene
+            .sink { [unowned self] scene in
+                switch scene {
+                case .moonpayWebpage(let url):
+                    navigateToMoonpay(url: url)
+                default:
+                    break
+                }
+            }
+            .store(in: &subscriptions)
+        
+        // create viewController
         let vc = UIHostingController(rootView: SellView(viewModel: viewModel))
         navigationController.pushViewController(vc, animated: true)
         return vc.deallocatedPublisher()
+    }
+    
+    // MARK: - Navigation
+    private func navigateToMoonpay(url: URL) {
+        let vc = SFSafariViewController(url: url)
+        vc.modalPresentationStyle = .automatic
+        navigationController.present(vc, animated: true)
     }
 }
--- p2p_wallet/Scenes/Sell/SellViewModel.swift
+++ p2p_wallet/Scenes/Sell/SellViewModel.swift
@@ -1,7 +1,48 @@
 import Foundation
+import Combine
+import Resolver
 
 class SellViewModel: BaseViewModel, ObservableObject {
+    
+    // MARK: - Dependencies
+    
+    @Injected private var userWalletManager: UserWalletManager
+
+    // MARK: - Properties
 
 //    @Published var state: SellStateMachine.State
+    @Published private(set) var subscene: SellSubScene?
+    
+    private let externalTransactionId: String = UUID().uuidString
 
+    // MARK: - Actions
+    
+    func openMoonPayWebView(
+        quoteCurrencyCode: String,
+        baseCurrencyAmount: Double
+    ) throws {
+        #if !RELEASE
+        let endpoint = String.secretConfig("MOONPAY_STAGING_SELL_ENDPOINT")!
+        let apiKey = String.secretConfig("MOONPAY_STAGING_API_KEY")!
+        #else
+        let endpoint = String.secretConfig("MOONPAY_PRODUCTION_SELL_ENDPOINT")!
+        let apiKey = String.secretConfig("MOONPAY_PRODUCTION_API_KEY")!
+        #endif
+        
+        var components = URLComponents(string: endpoint)!
+        components.queryItems = [
+            .init(name: "apiKey", value: apiKey),
+            .init(name: "baseCurrencyCode", value: "SOL"),
+            .init(name: "refundWalletAddress", value: userWalletManager.wallet?.account.publicKey.base58EncodedString),
+            .init(name: "quoteCurrencyCode", value: quoteCurrencyCode),
+            .init(name: "baseCurrencyAmount", value: baseCurrencyAmount.toString()),
+            .init(name: "externalTransactionId", value: externalTransactionId)
+        ]
+        
+        guard let url = components.url else {
+            throw SellError.invalidURL
+        }
+        
+        subscene = .moonpayWebpage(url: url)
+    }
 }
