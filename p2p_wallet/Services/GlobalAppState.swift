3c14d88a62930054e6c57d9d6e9d28f5fd8ebc08
Elizaveta Semenova <thesharkight84@gmail.com>
2022-12-14 13:24:44 +0600

    [PWN-6358] Store forced fee relayer endpoint if it is set in debug menu

--- p2p_wallet/Services/GlobalAppState.swift
+++ p2p_wallet/Services/GlobalAppState.swift
@@ -18,8 +18,9 @@ class GlobalAppState: ObservableObject {
     
     // Debug features
     @Published var forcedWalletAddress: String = ""
-    @Published var forcedFeeRelayerEndpoint: String = "" {
+    @Published var forcedFeeRelayerEndpoint: String? = Defaults.forcedFeeRelayerEndpoint {
         didSet {
+            Defaults.forcedFeeRelayerEndpoint = forcedFeeRelayerEndpoint
             ResolverScope.session.reset()
         }
     }
