824029e3c7e5281a092ad50f565020b9261ca73d
Chung Tran <bigearsenal@gmail.com>
2022-12-17 17:56:09 +0700

    feat: ActivityIndicator

--- p2p_wallet/UIKit/SwiftUI/ActivityIndicator/ActivityIndicator.swift
+++ p2p_wallet/UIKit/SwiftUI/ActivityIndicator/ActivityIndicator.swift
@@ -7,23 +7,23 @@
 
 import SwiftUI
 
-public struct ActivityIndicator: UIViewRepresentable {
-    public typealias UIView = UIActivityIndicatorView
-    public var isAnimating: Bool = true
-    public var configuration = { (_: UIView) in }
+struct ActivityIndicator: UIViewRepresentable {
+    typealias UIView = UIActivityIndicatorView
+    var isAnimating: Bool = true
+    var configuration = { (_: UIView) in }
 
-    public init(isAnimating: Bool, configuration: ((UIView) -> Void)? = nil) {
+    init(isAnimating: Bool, configuration: ((UIView) -> Void)? = nil) {
         self.isAnimating = isAnimating
         if let configuration = configuration {
             self.configuration = configuration
         }
     }
 
-    public func makeUIView(context _: UIViewRepresentableContext<Self>) -> UIView {
+    func makeUIView(context _: UIViewRepresentableContext<Self>) -> UIView {
         UIView()
     }
 
-    public func updateUIView(_ uiView: UIView, context _: UIViewRepresentableContext<Self>) {
+    func updateUIView(_ uiView: UIView, context _: UIViewRepresentableContext<Self>) {
         isAnimating ? uiView.startAnimating() : uiView.stopAnimating()
         configuration(uiView)
     }
