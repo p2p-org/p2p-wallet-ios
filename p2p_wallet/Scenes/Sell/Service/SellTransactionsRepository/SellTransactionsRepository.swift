9448ef5de19db5485a89a70b4fa566e025b55df4
Chung Tran <bigearsenal@gmail.com>
2022-12-19 11:55:31 +0700

    feat: SellTransactionsRepositoryImpl

--- p2p_wallet/Scenes/Sell/Service/SellTransactionsRepository/SellTransactionsRepository.swift
+++ p2p_wallet/Scenes/Sell/Service/SellTransactionsRepository/SellTransactionsRepository.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import Combine
+import Resolver
 
 /// Repository that control the flow of sell transactions
 protocol SellTransactionsRepository {
@@ -19,10 +20,19 @@ protocol SellTransactionsRepository {
 
 /// Mock SellTransactionsRepository
 final class SellTransactionsRepositoryImpl: SellTransactionsRepository {
+    
+    // MARK: - Dependencies
+    
+    @Injected private var sellDataService: any SellDataService
+    @Injected private var userWalletManager: UserWalletManager
 
     // MARK: - Properties
+    
+    private let refreshingRate: TimeInterval = 5.0 // 5 sec
 
     let transactions = CurrentValueSubject<[SellDataServiceTransaction], Never>([])
+    
+    var updatingTask: Task<Void, Never>?
     
     var transactionsPublisher: AnyPublisher<[SellDataServiceTransaction], Never> {
         transactions.eraseToAnyPublisher()
@@ -35,56 +45,21 @@ final class SellTransactionsRepositoryImpl: SellTransactionsRepository {
     // MARK: - Initializer
 
     init() {
-        // FIXME: - Replace mock later
-        let solPrice = 13.1
-        let fakeDepositWalletPrefix = "CMNyyCXkAQ5cfFS2zQEg6YPzd8fpHvMFbbbmUfjoPp1"
-        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1)) { [unowned self] in
-            transactions.send([
-                .init(
-                    id: UUID().uuidString,
-                    createdAt: Date().addingTimeInterval(-2.0 * 60), // 2 minutes ago
-                    status: .waitingForDeposit,
-                    baseCurrencyAmount: 2,
-                    quoteCurrencyAmount: 2 * solPrice,
-                    usdRate: solPrice,
-                    eurRate: solPrice,
-                    gbpRate: solPrice,
-                    depositWallet: fakeDepositWalletPrefix + "2"
-                ),
-                .init(
-                    id: UUID().uuidString,
-                    createdAt: Date().addingTimeInterval(-3.0 * 60), // 3 minutes ago
-                    status: .pending,
-                    baseCurrencyAmount: 3,
-                    quoteCurrencyAmount: 3 * solPrice,
-                    usdRate: solPrice,
-                    eurRate: solPrice,
-                    gbpRate: solPrice,
-                    depositWallet: fakeDepositWalletPrefix + "3"
-                ),
-                .init(
-                    id: UUID().uuidString,
-                    createdAt: Date().addingTimeInterval(-4.0 * 60), // 4 minutes ago
-                    status: .failed,
-                    baseCurrencyAmount: 4,
-                    quoteCurrencyAmount: 4 * solPrice,
-                    usdRate: solPrice,
-                    eurRate: solPrice,
-                    gbpRate: solPrice,
-                    depositWallet: fakeDepositWalletPrefix + "4"
-                ),
-                .init(
-                    id: UUID().uuidString,
-                    createdAt: Date().addingTimeInterval(-5.0 * 60), // 5 minutes ago
-                    status: .completed,
-                    baseCurrencyAmount: 5,
-                    quoteCurrencyAmount: 5 * solPrice,
-                    usdRate: solPrice,
-                    eurRate: solPrice,
-                    gbpRate: solPrice,
-                    depositWallet: fakeDepositWalletPrefix + "5"
-                )
-            ])
+        Timer.scheduledTimer(withTimeInterval: refreshingRate, repeats: true) { [weak self] _ in
+            self?.update()
+        }
+    }
+    
+    private func update() {
+        // cancel previous task
+        updatingTask?.cancel()
+        
+        // update
+        updatingTask = Task { [weak self] in
+            guard let self = self, let id = self.userWalletManager.wallet?.moonpayExternalClientId else {return}
+            guard let transactions = try? await self.sellDataService.transactions(id: id)
+            else { return }
+            self.transactions.send(transactions)
         }
     }
 }
