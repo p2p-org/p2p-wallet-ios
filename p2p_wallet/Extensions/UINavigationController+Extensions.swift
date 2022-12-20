5fc7ad98599564c14222fee3e383a1ba55e9e272
Chung Tran <bigearsenal@gmail.com>
2022-12-20 14:50:59 +0700

    fix: tabbar hidden after interupting

--- p2p_wallet/Extensions/UINavigationController+Extensions.swift
+++ p2p_wallet/Extensions/UINavigationController+Extensions.swift
@@ -18,3 +18,18 @@ extension UINavigationController {
         }
     }
 }
+
+extension UINavigationController {
+    // True if `hidesBottomBarWhenPushed` can be set to true, otherwise false.
+    // Workaround for iOS 14 bug.
+    var canHideBottomForNextPush: Bool {
+        // There is a bug in iOS 14 that hides the bottom bar
+        // when popping multiple navigation controllers from the stack,
+        // and one of them has hidesBottomBarWhenPushed set to true.
+        // https://developer.apple.com/forums/thread/660750
+        guard #available(iOS 14, *) else {
+            return true
+        }
+        return viewControllers.count == 1
+    }
+}
