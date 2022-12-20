0836a815a7ddc23c44c56712122224fc3950592f
Elizaveta Semenova <thesharkight84@gmail.com>
2022-12-20 12:52:36 +0300

    [PWN-6295] Do not close fee prompt screen after choosing token

--- p2p_wallet/Scenes/Main/Send/Input/Details/FeePrompt/SendInputFeePromptViewModel.swift
+++ p2p_wallet/Scenes/Main/Send/Input/Details/FeePrompt/SendInputFeePromptViewModel.swift
@@ -2,24 +2,34 @@ import Combine
 import Resolver
 import SolanaSwift
 
-final class SendInputFeePromptViewModel: ObservableObject {
+final class SendInputFeePromptViewModel: BaseViewModel, ObservableObject {
 
     let close = PassthroughSubject<Void, Never>()
     let chooseToken = PassthroughSubject<Void, Never>()
 
-    @Published var title = ""
-    @Published var description = ""
-    @Published var isChooseTokenAvailable = false
-    @Published var continueTitle = ""
+    @Published var title: String
+    @Published var description: String
+    @Published var isChooseTokenAvailable: Bool
+    @Published var continueTitle: String
+    @Published var feeToken: Wallet
 
-    init(feeToken: Token, availableFeeTokens: [Wallet]) {
+    init(feeToken: Wallet, availableFeeTokens: [Wallet]) {
         title = L10n.thisAddressDoesnTHaveAnAccountForThisToken
         description = L10n.YouWillHaveToPayAOneTimeFee0._03ToCreateAnAccountForThisAddress
-        continueTitle = L10n.continueWith(feeToken.symbol)
+        continueTitle = L10n.continueWith(feeToken.token.symbol)
+        isChooseTokenAvailable = availableFeeTokens.count > 1
+        self.feeToken = feeToken
 
-        if availableFeeTokens.count > 1 {
+        super.init()
+
+        if isChooseTokenAvailable {
             description.append(". \(L10n.youCanChooseInWhichCurrencyToPayWithBelow)")
-            isChooseTokenAvailable = true
         }
+
+        $feeToken
+            .sink { [weak self] value in
+                self?.continueTitle = L10n.continueWith(value.token.symbol)
+            }
+            .store(in: &subscriptions)
     }
 }
