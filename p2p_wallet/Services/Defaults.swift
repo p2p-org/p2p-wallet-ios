b8e1612dafda24c7e3db36fd9a22b8b663d138e5
Chung Tran <bigearsenal@gmail.com>
2022-12-21 18:20:45 +0700

    feat: add NameService endpoint to Debug menu

--- p2p_wallet/Services/Defaults.swift
+++ p2p_wallet/Services/Defaults.swift
@@ -43,6 +43,7 @@ extension DefaultsKeys {
         )
     }
     var forcedFeeRelayerEndpoint: DefaultsKey<String?> { .init(#function, defaultValue: nil) }
+    var forcedNameServiceEndpoint: DefaultsKey<String?> { .init(#function, defaultValue: nil) }
 
     var isCoingeckoProviderDisabled: DefaultsKey<Bool> { .init(#function, defaultValue: false) }
     var didBackupOffline: DefaultsKey<Bool> { .init(#function, defaultValue: false) }
