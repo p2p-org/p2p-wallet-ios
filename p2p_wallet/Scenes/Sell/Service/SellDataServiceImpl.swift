03cb193c962489916d3332503e1f622ca37c0f40
Chung Tran <bigearsenal@gmail.com>
2022-12-21 15:00:44 +0700

    feat: add second version of data service

--- p2p_wallet/Scenes/Sell/Service/SellDataServiceImpl.swift
+++ p2p_wallet/Scenes/Sell/Service/SellDataServiceImpl.swift
@@ -3,6 +3,162 @@ import Foundation
 import Resolver
 import SwiftyUserDefaults
 
+enum SellDataServiceError: Error {
+    case userIdNotFound
+    case couldNotLoadSellData
+}
+
+struct SellDataServiceData {
+    var currency: ProviderCurrency
+    var fiat: Fiat
+}
+
+enum SellDataServiceStatus2 {
+    case initialized
+    case updating
+    case ready(SellDataServiceData)
+    case error(Error)
+}
+
+protocol SellDataService2 {
+    associatedtype Provider: SellDataServiceProvider
+
+    var statusPublisher: AnyPublisher<SellDataServiceStatus2, Never> { get }
+//    var lastUpdateDate: AnyPublisher<Date, Never> { get }
+//
+    /// Request for pendings, rates, min amounts
+    func update() async
+    
+    /// Retrieve all incompleted transactions
+    func retrieveAllIncompletedTransactions() async throws
+    
+//    /// Return incomplete transactions
+//    var incompleteTransactions: [SellDataServiceTransaction] { get }
+//    /// Weather service available
+//    func isAvailable() async -> Bool
+//    func deleteTransaction(id: String) async throws
+//    func transactions(id: String) async throws -> [SellDataServiceTransaction]
+}
+
+final class SellDataServiceImpl2: SellDataService2 {
+    
+    // MARK: - Associated type
+
+    typealias Provider = MoonpaySellDataServiceProvider
+    
+    // MARK: - Dependencies
+
+    @Injected private var priceService: PricesService
+    @Injected private var userWalletManager: UserWalletManager
+    @Injected private var sellTransactionsRepository: SellTransactionsRepository2
+    
+    // MARK: - Properties
+
+    private var provider = Provider()
+    
+    @SwiftyUserDefault(keyPath: \.isSellAvailable, options: .cached)
+    private var cachedIsAvailable: Bool?
+    
+    @Published private var status: SellDataServiceStatus2 = .initialized
+    var statusPublisher: AnyPublisher<SellDataServiceStatus2, Never> {
+        $status.eraseToAnyPublisher()
+    }
+    
+    // MARK: - Methods
+    
+    func update() async {
+        // get user id
+        guard let userId = userWalletManager.wallet?.moonpayExternalClientId else {
+            status = .error(SellDataServiceError.userIdNotFound)
+            return
+        }
+        
+        // mark as updating
+        status = .updating
+        
+        // get currency
+        do {
+            let (currency, fiat, incompletedTransactions) = try await(
+                provider.currencies().filter({ $0.code.uppercased() == "SOL" }).first,
+                provider.fiat(),
+                retrieveAllIncompletedTransactions()
+            )
+            if currency == nil {
+                throw SellDataServiceError.couldNotLoadSellData
+            }
+            status = .ready(.init(currency: currency!, fiat: fiat))
+        } catch {
+            status = .error(SellDataServiceError.couldNotLoadSellData)
+            return
+        }
+    }
+    
+    func retrieveAllIncompletedTransactions() async throws {
+        let txs = try await provider.sellTransactions(externalTransactionId: userId)
+
+        let incompletedTransactions = try await txs.asyncMap { transaction in
+            let detailed = try await provider.detailSellTransaction(id: transaction.id)
+            let quoteCurrencyAmount = detailed.quoteCurrencyAmount ?? (self.priceService.currentPrice(for: "SOL")?.value ?? 0) * detailed.baseCurrencyAmount
+            guard
+                let usdRate = detailed.usdRate,
+                let eurRate = detailed.eurRate,
+                let gbpRate = detailed.gbpRate,
+                let depositWallet = detailed.depositWallet?.walletAddress,
+                let status = SellDataServiceTransaction.Status(rawValue: detailed.status.rawValue)
+            else { return nil }
+            
+            let dateFormatter = ISO8601DateFormatter()
+            dateFormatter.formatOptions =  [.withInternetDateTime, .withFractionalSeconds]
+            let createdAt = dateFormatter.date(from: detailed.createdAt)
+            
+            return SellDataServiceTransaction(
+                id: detailed.id,
+                createdAt: createdAt,
+                status: status,
+                baseCurrencyAmount: detailed.baseCurrencyAmount,
+                quoteCurrencyAmount: quoteCurrencyAmount,
+                usdRate: usdRate,
+                eurRate: eurRate,
+                gbpRate: gbpRate,
+                depositWallet: depositWallet
+            )
+        }.compactMap { $0 }
+        
+        await sellTransactionsRepository.setTransactions(incompletedTransactions)
+    }
+    
+    func isAvailable() async -> Bool {
+        return true
+        guard cachedIsAvailable == nil else {
+            defer {
+                Task {
+                    do {
+                        cachedIsAvailable = try await provider.isAvailable()
+                    } catch {}
+                }
+            }
+            return cachedIsAvailable ?? false
+        }
+        do {
+            cachedIsAvailable = try await provider.isAvailable()
+        } catch {
+            return false
+        }
+        return (cachedIsAvailable ?? false)
+    }
+    
+    // MARK: - Helpers
+
+    func transaction(id: String) async throws -> Provider.Transaction {
+        try await provider.detailSellTransaction(id: id)
+    }
+
+    func deleteTransaction(id: String) async throws {
+        try await provider.deleteSellTransaction(id: id)
+        incompleteTransactions.removeAll { $0.id == id }
+    }
+}
+
 class SellDataServiceImpl: SellDataService {
     typealias Provider = MoonpaySellDataServiceProvider
     private var provider = Provider()
