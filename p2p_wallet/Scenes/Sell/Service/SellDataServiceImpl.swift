6c577eb8410054d23bda3020dca502eb6543c972
Chung Tran <bigearsenal@gmail.com>
2022-12-21 18:48:16 +0700

    feat: speed up loading by throwingTaskGroup

--- p2p_wallet/Scenes/Sell/Service/SellDataServiceImpl.swift
+++ p2p_wallet/Scenes/Sell/Service/SellDataServiceImpl.swift
@@ -98,34 +98,49 @@ final class SellDataServiceImpl: SellDataService {
         }
         
         let txs = try await provider.sellTransactions(externalTransactionId: userId)
-
-        let incompletedTransactions: [SellDataServiceTransaction] = try await txs.asyncMap { transaction in
-            let detailed = try await provider.detailSellTransaction(id: transaction.id)
-            let quoteCurrencyAmount = detailed.quoteCurrencyAmount ?? (self.priceService.currentPrice(for: "SOL")?.value ?? 0) * detailed.baseCurrencyAmount
-            guard
-                let usdRate = detailed.usdRate,
-                let eurRate = detailed.eurRate,
-                let gbpRate = detailed.gbpRate,
-                let depositWallet = detailed.depositWallet?.walletAddress,
-                let status = SellDataServiceTransaction.Status(rawValue: detailed.status.rawValue)
-            else { return nil }
+        
+        let incompletedTransactions = try await withThrowingTaskGroup(of: SellDataServiceTransaction?.self) { group in
+            var transactions = [SellDataServiceTransaction?]()
             
-            let dateFormatter = ISO8601DateFormatter()
-            dateFormatter.formatOptions =  [.withInternetDateTime, .withFractionalSeconds]
-            let createdAt = dateFormatter.date(from: detailed.createdAt)
+            for id in txs.map(\.id) {
+                group.addTask { [weak self] in
+                    guard let self = self else {return nil}
+                    let detailed = try await self.provider.detailSellTransaction(id: id)
+                    
+                    let quoteCurrencyAmount = detailed.quoteCurrencyAmount ?? (self.priceService.currentPrice(for: "SOL")?.value ?? 0) * detailed.baseCurrencyAmount
+                    guard
+                        let usdRate = detailed.usdRate,
+                        let eurRate = detailed.eurRate,
+                        let gbpRate = detailed.gbpRate,
+                        let depositWallet = detailed.depositWallet?.walletAddress,
+                        let status = SellDataServiceTransaction.Status(rawValue: detailed.status.rawValue)
+                    else { return nil }
+                    
+                    let dateFormatter = ISO8601DateFormatter()
+                    dateFormatter.formatOptions =  [.withInternetDateTime, .withFractionalSeconds]
+                    let createdAt = dateFormatter.date(from: detailed.createdAt)
+                    
+                    return SellDataServiceTransaction(
+                        id: detailed.id,
+                        createdAt: createdAt,
+                        status: status,
+                        baseCurrencyAmount: detailed.baseCurrencyAmount,
+                        quoteCurrencyAmount: quoteCurrencyAmount,
+                        usdRate: usdRate,
+                        eurRate: eurRate,
+                        gbpRate: gbpRate,
+                        depositWallet: depositWallet
+                    )
+                }
+            }
             
-            return SellDataServiceTransaction(
-                id: detailed.id,
-                createdAt: createdAt,
-                status: status,
-                baseCurrencyAmount: detailed.baseCurrencyAmount,
-                quoteCurrencyAmount: quoteCurrencyAmount,
-                usdRate: usdRate,
-                eurRate: eurRate,
-                gbpRate: gbpRate,
-                depositWallet: depositWallet
-            )
-        }.compactMap { $0 }
+            // grab results
+            for try await tx in group {
+                transactions.append(tx)
+            }
+            
+            return transactions
+        }.compactMap {$0}
         
         await sellTransactionsRepository.setTransactions(incompletedTransactions)
         transactions = await sellTransactionsRepository.transactions
