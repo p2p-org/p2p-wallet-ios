c4aefb8d29bb2366b866fc36d83bb11aba59964d
Chung Tran <bigearsenal@gmail.com>
2022-12-22 10:56:46 +0700

    fix: merging

--- p2p_wallet/Scenes/Main/NewHome/HomeWithTokens/View/HomeWithTokensView.swift
+++ p2p_wallet/Scenes/Main/NewHome/HomeWithTokens/View/HomeWithTokensView.swift
@@ -62,16 +62,7 @@ struct HomeWithTokensView: View {
 
     private var header: some View {
         ActionsPanelView(
-            actionsPublisher: viewModel.navigation
-                .compactMap { scene in
-                    switch scene {
-                    case .actions(let actions):
-                        return actions
-                    default:
-                        return nil
-                    }
-                }
-                .eraseToAnyPublisher(),
+            actionsPublisher: viewModel.actions,
             balancePublisher: viewModel.balance,
             action: {
                 viewModel.actionClicked($0)
