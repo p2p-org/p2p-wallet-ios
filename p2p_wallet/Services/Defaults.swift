97fbedac84cd455c7c5df4d1d2d98674c049da49
Elizaveta Semenova <thesharkight84@gmail.com>
2023-01-17 17:59:21 +0400

    [PWN-6517] Remember send input type choice and reset it on logout

--- p2p_wallet/Services/Defaults.swift
+++ p2p_wallet/Services/Defaults.swift
@@ -84,6 +84,9 @@ extension DefaultsKeys {
     var isSolendTutorialShown: DefaultsKey<Bool> { .init(#function, defaultValue: false) }
     var isEarnBannerClosed: DefaultsKey<Bool> { .init(#function, defaultValue: false) }
 
+    // Send
+    var isTokenInputTypeChosen: DefaultsKey<Bool> { .init(#function, defaultValue: false) }
+
     var solanaNegativeStatusFrequency: DefaultsKey<String?> {
         .init(
             #function,
