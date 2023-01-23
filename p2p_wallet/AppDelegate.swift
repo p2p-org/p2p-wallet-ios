8a4b29f321d1bed601663a447bcc1b690cfdd6f2
Ivan Babich <babichivanya@gmail.com>
2023-01-23 16:07:23 +0300

    [PWN-6776] Интеграция Localise

--- p2p_wallet/AppDelegate.swift
+++ p2p_wallet/AppDelegate.swift
@@ -13,6 +13,7 @@ import Firebase
 import Intercom
 import KeyAppKitLogger
 import KeyAppUI
+import Lokalise
 import Resolver
 import Sentry
 import SolanaSwift
@@ -64,6 +65,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         AppsFlyerLib.shared().appsFlyerDevKey = String.secretConfig("APPSFLYER_DEV_KEY")!
         AppsFlyerLib.shared().appleAppID = String.secretConfig("APPSFLYER_APP_ID")!
         AppsFlyerLib.shared().waitForATTUserAuthorization(timeoutInterval: 60)
+        
+        Lokalise.shared.setProjectID(
+            String.secretConfig("LOKALISE_PROJECT_ID")!,
+            token: String.secretConfig("LOKALISE_TOKEN")!
+        )
+        Lokalise.shared.swizzleMainBundle()
 
         // set app coordinator
         appCoordinator = AppCoordinator()
