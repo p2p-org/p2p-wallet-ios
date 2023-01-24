e8173c7d2f1459ab4f8c3e2a97e954b5ee3b97c3
Elizaveta Semenova <thesharkight84@gmail.com>
2023-01-24 17:50:26 +0400

    [PWN-6881] Rename shouldHideRemoveButtonOnFirstAppearance for navigatedFromMoonpay

--- p2p_wallet/Scenes/Main/Sell/Pending/SellPendingView.swift
+++ p2p_wallet/Scenes/Main/Sell/Pending/SellPendingView.swift
@@ -45,7 +45,7 @@ struct SellPendingView: View {
                     )
                     .disabled(viewModel.isRemoving)
 
-                    if !viewModel.model.shouldHideRemoveButtonOnFirstAppearance {
+                    if !viewModel.model.navigatedFromMoonpay {
                         Button(
                             action: {
                                 viewModel.removeClicked()
@@ -112,7 +112,7 @@ struct SellPendingView: View {
 
     private var textView: some View {
         HStack {
-            Text(viewModel.model.shouldHideRemoveButtonOnFirstAppearance ? L10n.send : "\(L10n.send) \(viewModel.model.tokenSymbol)")
+            Text(viewModel.model.navigatedFromMoonpay ? L10n.send : "\(L10n.send) \(viewModel.model.tokenSymbol)")
                 .foregroundColor(Color(Asset.Colors.night.color))
                 .font(uiFont: .font(of: .text3))
             Spacer()
@@ -145,7 +145,7 @@ struct SellPendingView_Previews: PreviewProvider {
                     fiatAmount: 300.05,
                     currency: MoonpaySellDataServiceProvider.MoonpayFiat.eur,
                     receiverAddress: "FfRBerfgeritjg43fBeJEr",
-                    shouldHideRemoveButtonOnFirstAppearance: false
+                    navigatedFromMoonpay: false
                 )
             )
         )
