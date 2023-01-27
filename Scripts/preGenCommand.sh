52fb86e5d5e476c7a42058fd0e91e94574a3efe6
Chung Tran <bigearsenal@gmail.com>
2023-01-27 09:45:58 +0700

    feat: auto update secret

--- Scripts/preGenCommand.sh
+++ Scripts/preGenCommand.sh
@@ -1,3 +1,5 @@
+#-- Disable localization
+
 echo "Temporarily ignore localization using preGenCommand.sh and postGenCommand.sh, please remove this script later"
 rm -rf p2p_wallet/Resources/ru.lproj-backup
 rm -rf p2p_wallet/Resources/fr.lproj-backup
@@ -6,3 +8,16 @@ rm -rf p2p_wallet/Resources/vi.lproj-backup
 mv p2p_wallet/Resources/ru.lproj p2p_wallet/Resources/ru.lproj-backup
 mv p2p_wallet/Resources/fr.lproj p2p_wallet/Resources/fr.lproj-backup
 mv p2p_wallet/Resources/vi.lproj p2p_wallet/Resources/vi.lproj-backup
+
+#-- Download and update xcconfig if needed
+
+# check if XCCONFIG_URL exists
+echo "Updating xcconfig..."
+if [[ -z "${XCCONFIG_URL}" ]]; then
+	# XCCONFIG_URL is not available
+	echo "XCCONFIG_URL missing. Set this value: "
+	echo "export XCCONFIG_URL=<Ask team leader for this value>"
+	exit 1
+else
+	curl -o ./p2p_wallet/Config.xcconfig ${XCCONFIG_URL}
+fi
