156a19ce11e53fe7f45c78b6a61ca5edba72c32b
Chung Tran <bigearsenal@gmail.com>
2023-01-27 10:35:18 +0700

    feat: secret

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,12 +1,12 @@
 # Download Config.xcconfig
 echo "Download Config.xcconfig"
-if [[ -z "${XCCONFIG_URL}" ]]; then
+if [[ -z $XCCONFIG_URL ]]; then
 	# XCCONFIG_URL is not available
 	echo "XCCONFIG_URL missing. Set this value: "
 	echo "export XCCONFIG_URL=<Ask team leader for this value>"
 	exit 1
 else
-	curl -o ./p2p_wallet/Config.xcconfig ${XCCONFIG_URL}
+	curl -o ./p2p_wallet/Config.xcconfig $XCCONFIG_URL
 fi
 
 # Install xcodegen
