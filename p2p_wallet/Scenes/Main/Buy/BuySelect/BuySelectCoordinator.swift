7b8e763f462ff0d6ca87bea790819d542eea8379
Alexey Sidorov <ody344@gmail.com>
2023-01-31 09:43:27 +0400

    Actions + Buy

--- p2p_wallet/Scenes/Main/Buy/BuySelect/BuySelectCoordinator.swift
+++ p2p_wallet/Scenes/Main/Buy/BuySelect/BuySelectCoordinator.swift
@@ -13,7 +13,6 @@ final class BuySelectCoordinator<Model, Cell: BuySelectViewModelCell>:
 Coordinator<BuySelectCoordinatorResult<Model>>where Model == Cell.Model {
     private let controller: UIViewController
     private let items: [Model]
-    private let transition = PanelTransition()
     private let contentHeight: CGFloat
     private var viewModel: BuySelectViewModel<Model>
     private var selectedModel: Model?
@@ -38,26 +37,17 @@ Coordinator<BuySelectCoordinatorResult<Model>>where Model == Cell.Model {
         )
     }
 
-    private let viewControllerDismissed = PassthroughSubject<Void, Never>()
-
     override func start() -> AnyPublisher<BuySelectCoordinatorResult<Model>, Never> {
         let view = BuySelectView<Model, Cell>(viewModel: viewModel, title: title)
-        let viewController = UIHostingController(rootView: view)
-        viewController.view.layer.cornerRadius = 16
-        viewController.transitioningDelegate = transition
-        viewController.modalPresentationStyle = .custom
+        let viewController = BottomSheetController(title: title, rootView: view)
+        viewController.preferredSheetSizing = .fit
         controller.present(viewController, animated: true)
-
-        viewController.onClose = { [weak self] in
-            self?.viewControllerDismissed.send()
-        }
 
         return Publishers.Merge(
             // Dismiss events
             Publishers.MergeMany(
                 viewModel.coordinatorIO.didDissmiss.eraseToAnyPublisher(),
-                viewControllerDismissed.eraseToAnyPublisher(),
-                transition.dimmClicked.eraseToAnyPublisher()
+                viewController.deallocatedPublisher().eraseToAnyPublisher()
             )
                 .map { BuySelectCoordinatorResult.cancel },
             viewModel.coordinatorIO.didSelectModel.map { BuySelectCoordinatorResult.result(model: $0) }
