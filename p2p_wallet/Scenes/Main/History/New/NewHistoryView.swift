3ca6787e8ab431a0ab9d38e9af0ef09a445edffa
Tran Giang Long <trangianglong4003@gmail.com>
2023-02-03 18:17:46 +0700

    feat(history): add UI

--- p2p_wallet/Scenes/Main/History/New/NewHistoryView.swift
+++ p2p_wallet/Scenes/Main/History/New/NewHistoryView.swift
@@ -6,7 +6,7 @@
 //
 
 import KeyAppUI
-import Kingfisher
+
 import SwiftUI
 
 struct NewHistoryView: View {
@@ -14,102 +14,48 @@ struct NewHistoryView: View {
 
     var body: some View {
         VStack {
-            TextField("Hello", text: .constant(""))
             ScrollView {
-                LazyVStack {
-                    ForEach(viewModel.historyItems, id: \.id) { item in
-                        NewHistoryItemView(item: item)
-                    }
+                LazyVStack(alignment: .leading, spacing: 0) {
+                    ForEach(viewModel.sections) { (section: NewHistorySection) in
+                        
+                        Text(section.title)
+                            .apply(style: .text4)
+                            .foregroundColor(Color(Asset.Colors.mountain.color))
+                            .padding(.top, 24)
+                            .padding(.bottom, 12)
+                        
+                        Color(Asset.Colors.snow.color)
+                            .frame(height: 12)
+                            .cornerRadius(radius: 16, corners: [.topLeft, .topRight])
+                        ForEach(section.items, id: \.id) { (item: NewHistoryItem) in
+                            Group {
+                                switch item {
+                                case let .rendable(rendableItem):
+                                    NewHistoryItemView(item: rendableItem) {}
+                                case .placeHolder:
+                                    SwiftUI.EmptyView()
+                                case let .button(_, title, action):
+                                    TextButtonView(
+                                        title: title,
+                                        style: .second,
+                                        size: .large,
+                                        onPressed: action
+                                    )
+                                    .frame(height: TextButton.Size.large.height)
+                                }
+                            }
+                            .background(Color(Asset.Colors.snow.color))
+                        }
+                        Color(Asset.Colors.snow.color)
+                            .frame(height: 12)
+                            .cornerRadius(radius: 16, corners: [.bottomLeft, .bottomRight])
+
+                    }.padding(.horizontal, 16)
                 }
+                .padding(.vertical, 8)
             }
         }
-    }
-}
-
-struct NewHistoryItemView: View {
-    let item: any NewHistoryItem
-
-    var titleColor: Color {
-        switch item.status {
-        case .success: return Color(Asset.Colors.night.color)
-        case .failed: return Color(Asset.Colors.rose.color)
-        }
-    }
-    
-    var detailColor: Color {
-        switch item.status {
-        case .success:
-            switch item.change {
-            case .positive: return Color(Asset.Colors.mint.color)
-            case .negative: return Color(Asset.Colors.night.color)
-            }
-        case .failed: return Color(Asset.Colors.rose.color)
-        }
-    }
-
-    var body: some View {
-        HStack {
-            NewHistoryIconView(icon: item.icon)
-            VStack(spacing: 4) {
-                HStack(spacing: 5) {
-                    if item.status == .failed {
-                        Image(uiImage: .crossIcon)
-                            .renderingMode(.template)
-                            .foregroundColor(titleColor)
-                    }
-                    Text(item.title)
-                        .foregroundColor(titleColor)
-                        .fontWeight(.semibold)
-                        .apply(style: .text2)
-                    Spacer()
-                    Text(item.detail)
-                        .fontWeight(.semibold)
-                        .apply(style: .text2)
-                        .foregroundColor(detailColor)
-                }
-                
-
-                HStack {
-                    Text(item.subtitle)
-                        .foregroundColor(Color(Asset.Colors.mountain.color))
-                        .apply(style: .label1)
-                    Spacer()
-                    Text(item.subdetail)
-                        .foregroundColor(Color(Asset.Colors.mountain.color))
-                        .apply(style: .label1)
-                }
-            }
-        }
-        .padding(.horizontal, 16)
-        .frame(height: 72)
-    }
-}
-
-struct NewHistoryIconView: View {
-    private let size: CGFloat = 46
-
-    let icon: NewHistoryItemIcon
-
-    var body: some View {
-        Group {
-            switch icon {
-            case let .single(url):
-                KFImage
-                    .url(url)
-
-                    .setProcessor(
-                        DownsamplingImageProcessor(size: .init(width: size*2, height: size*2))
-                            |> RoundCornerImageProcessor(cornerRadius: size)
-                    )
-                    .resizable()
-                    .cacheMemoryOnly()
-                    .fade(duration: 0.25)
-            case let .double(from, to):
-                // TODO: Implement design
-                KFImage(from)
-            }
-        }
-        .frame(width: 46, height: 46)
+        .background(Color(Asset.Colors.smoke.color))
     }
 }
 
@@ -117,13 +63,15 @@ struct NewHistoryView_Previews: PreviewProvider {
     static var previews: some View {
         NewHistoryView(
             viewModel: .init(
-                initialHistoryItem: [
-                    BaseHistoryItem.send(),
-                    BaseHistoryItem.failedSend(),
-                    BaseHistoryItem.receive(),
-                    BaseHistoryItem.send(),
-                    BaseHistoryItem.send(),
-                    BaseHistoryItem.send()
+                initialSections: [
+                    .init(
+                        title: "Today",
+                        items: NewHistoryItemView_Previews.items.map { .rendable($0) }
+                    ),
+                    .init(
+                        title: "Yesterday",
+                        items: NewHistoryItemView_Previews.items.map { .rendable($0) }
+                    )
                 ]
             )
         )
