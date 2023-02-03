09690947e490658bebdf92a0a9ee4c1f5f46f54e
Chung Tran <bigearsenal@gmail.com>
2023-02-03 15:36:33 +0700

    feat: some fixes

--- p2p_wallet/Analytics/KeyAppAnalyticsEvent+Extensions.swift
+++ p2p_wallet/Analytics/KeyAppAnalyticsEvent+Extensions.swift
@@ -2,44 +2,48 @@ import Foundation
 import AnalyticsManager
 
 extension KeyAppAnalyticsEvent: MirrorableEnum {
+
+    /// The name of the event to send
     var eventName: String? {
         // By default, name of the event will be converted from `camelCase` to `Uppercased_Snake_Case` format
         // For example: `KeyAppAnalyticsEvent.mainScreenSwapOpen` will be converted to "Main_Screen_Swap_Open" automatically.
         // Example: modify the name manually and prevent default behavior
-//        switch self {
-//        case .login:
-//            return "UserLoggedIn"
-//        default:
-//            break
-//        }
+        // switch self {
+        // case .login:
+        //     return "UserLoggedIn"
+        // default:
+        //     break
+        // }
         
         // Default converter from `camelCase` to `Uppercased_Snake_Case` format
         return mirror.label.snakeAndFirstUppercased
     }
 
+    /// Params sent with event
     var params: [String: Any]? {
         guard !mirror.params.isEmpty else { return nil }
         
         // The same for params, params key & value can be customized too, if not, it will be automatically converted to `Uppercased_Snake_Case`
         
         // Example: modify the key & value manually and prevent default behavior if needed
-//        switch self {
-//        case let .login(userId: String):
-//            return ["Id": userId] // by default userId -> User_Id
-//        default:
-//            break
-//        }
+        // switch self {
+        // case let .login(userId: String):
+        //     return ["Id": userId] // by default userId -> User_Id
+        // default:
+        //     break
+        // }
         
         // Default converter from `camelCase` to `Uppercased_Snake_Case` format
         let formatted = mirror.params.map { ($0.key.snakeAndFirstUppercased ?? "", $0.value) }
         return Dictionary(uniqueKeysWithValues: formatted)
     }
-    
+
+    /// Array of sending providers, even will be sent to only these defined providers
     var providerIds: [AnalyticsProviderId] {
         let ids: [KeyAppAnalyticsProviderId] = [
             .amplitude,
-            .appsFlyer,
-            .firebaseAnalytics
+//            .appsFlyer,
+//            .firebaseAnalytics
         ]
         return ids.map(\.rawValue)
     }
