80535e8a8e2bf83c93e67b80cf909d689a53c70a
Alexey Sidorov <ody344@gmail.com>
2023-02-03 10:28:34 +0400

    New History ErrorView + EmptyView

---
+++ p2p_wallet/Scenes/Main/History/New/NewHistoryEmptyView.swift
@@ -0,0 +1,30 @@
+import SwiftUI
+import KeyAppUI
+
+struct NewHistoryEmptyView: View {
+    var body: some View {
+        ZStack {
+            Color(Asset.Colors.smoke.color)
+            VStack(spacing: 20) {
+                Image(uiImage: .moneybox)
+                    .padding(.bottom, 24) // 24 + 16
+                Text("Your history will appear here.\nTo get started you can:")
+                    .apply(style: .text1)
+                    .multilineTextAlignment(.center)
+                TextButtonView(title: L10n.buyCrypto, style: .primary, size: .large)
+                    .frame(height: TextButton.Size.large.height)
+                TextButtonView(title: L10n.receive, style: .second, size: .large)
+                    .frame(height: TextButton.Size.large.height)
+                Spacer()
+            }
+            .padding(.top, 40)
+            .padding(.horizontal, 20)
+        }.edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct NewHistoryEmptyView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewHistoryEmptyView()
+    }
+}
