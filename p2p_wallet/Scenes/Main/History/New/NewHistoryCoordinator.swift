b2a520127b00b478831e258ff87877850bba8021
Tran Giang Long <trangianglong4003@gmail.com>
2023-02-03 18:17:46 +0700

    feat(history): add UI

--- p2p_wallet/Scenes/Main/History/New/NewHistoryCoordinator.swift
+++ p2p_wallet/Scenes/Main/History/New/NewHistoryCoordinator.swift
@@ -7,10 +7,33 @@
 
 import Foundation
 import SwiftUI
+import KeyAppUI
 
 class NewHistoryCoordinator: SmartCoordinator<Void> {
     override func build() -> UIViewController {
-        let view = NewHistoryView(viewModel: .init(initialHistoryItem: []))
-        return UIHostingController(rootView: view)
+        let view = NewHistoryView(
+            viewModel: .init(
+                initialSections: [
+                    .init(
+                        title: "Today",
+                        items: NewHistoryItemView_Previews.items.map { .rendable($0) }
+                    ),
+                    .init(
+                        title: "Yesterday",
+                        items: NewHistoryItemView_Previews.items.map { .rendable($0) }
+                    )
+                ]
+            )
+        )
+        let vc = UIHostingControllerWithoutNavigation(rootView: view)
+        vc.navigationIsHidden = false
+        vc.title = L10n.history
+        vc.view.backgroundColor = Asset.Colors.smoke.color
+        
+        vc.viewDidAppear.sink {
+            vc.navigationItem.largeTitleDisplayMode = .always
+        }.store(in: &subscriptions)
+
+        return vc
     }
 }
