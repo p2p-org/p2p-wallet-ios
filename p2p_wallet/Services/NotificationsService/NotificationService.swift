14ec48ce413d2289898cd839cacdc82b4eb29eb8
Chung Tran <bigearsenal@gmail.com>
2023-02-06 13:06:05 +0700

    Update NotificationService.swift

--- p2p_wallet/Services/NotificationsService/NotificationService.swift
+++ p2p_wallet/Services/NotificationsService/NotificationService.swift
@@ -85,7 +85,7 @@ final class NotificationServiceImpl: NSObject, NotificationService {
         UNUserNotificationCenter.current()
             .requestAuthorization(options: [.alert, .sound, .badge]) { [weak self] granted, _ in
                 Defaults.didSetEnableNotifications = granted
-                self?.analyticsManager.log(event: AmplitudeEvent.pushAllowed(value: granted))
+                self?.analyticsManager.log(event: .pushAllowed(value: granted))
                 if granted {
                     self?.analyticsManager.log(event: .pushApprove)
                 }
