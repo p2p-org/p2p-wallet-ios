85940c7570a973e86eb112da5b03134bb8da3f8c
Chung Tran <bigearsenal@gmail.com>
2023-02-06 15:39:40 +0700

    feat: remove getCurrentPrices by symbol

--- p2p_wallet/Scenes/Main/Buy/BuyViewModel.swift
+++ p2p_wallet/Scenes/Main/Buy/BuyViewModel.swift
@@ -54,7 +54,7 @@ final class BuyViewModel: ObservableObject {
     @SwiftyUserDefault(keyPath: \.buyMinPrices, options: .cached)
     var buyMinPrices: [String: [String: Double]]
 
-    private var tokenPrices: [Fiat: [TokenPriceKey: Double?]] = [:]
+    private var tokenPrices: [Fiat: [String: Double?]] = [:]
 
     // Defaults
     private static let defaultMinAmount = Double(40)
@@ -288,7 +288,7 @@ final class BuyViewModel: ObservableObject {
             tokens.map {
                 TokenCellViewItem(
                     token: $0,
-                    amount: tokenPrices[fiat]?[.init(token: token)] ?? 0,
+                    amount: tokenPrices[fiat]?[token.address] ?? 0,
                     fiat: fiat
                 )
             }
