e4c092894e86b25c9653536af0f3d417713cff36
Tran Giang Long <trangianglong4003@gmail.com>
2023-02-09 18:33:14 +0700

    feat(history): history

--- p2p_wallet/Scenes/Main/History/New/NewHistoryCoordinator.swift
+++ p2p_wallet/Scenes/Main/History/New/NewHistoryCoordinator.swift
@@ -6,44 +6,39 @@
 //
 
 import Foundation
-import SwiftUI
 import KeyAppUI
+import SwiftUI
 
 class NewHistoryCoordinator: SmartCoordinator<Void> {
     override func build() -> UIViewController {
-        let vm = NewHistoryViewModel(
-            initialSections: [
-                .init(
-                    title: "Today",
-                    items: NewHistoryItemView_Previews.items.map { .rendable($0) }
-                ),
-                .init(
-                    title: "Yesterday",
-                    items: NewHistoryItemView_Previews.items.map { .rendable($0) }
-                )
-            ]
-        )
-        
+        let vm = NewHistoryViewModel()
+
         vm.actionSubject
             .sink { [weak self] action in
                 guard let self = self else { return }
                 switch action {
                 case let .openDetailByRendableItem(item):
-                    self.coordinate(to: TransactionDetailCoordinator(transaction: item, presentingViewController: self.presentation.presentingViewController))
-                        .sink { _ in }
-                        .store(in: &self.subscriptions)
+                    self.coordinate(
+                        to: TransactionDetailCoordinator(
+                            transaction: item,
+                            style: .passive,
+                            presentingViewController: self.presentation.presentingViewController
+                        )
+                    )
+                    .sink { _ in }
+                    .store(in: &self.subscriptions)
                 default:
                     break
                 }
             }
             .store(in: &subscriptions)
-        
+
         let view = NewHistoryView(viewModel: vm)
         let vc = UIHostingControllerWithoutNavigation(rootView: view)
         vc.navigationIsHidden = false
         vc.title = L10n.history
         vc.view.backgroundColor = Asset.Colors.smoke.color
-        
+
         vc.viewDidAppear.sink {
             vc.navigationItem.largeTitleDisplayMode = .always
         }.store(in: &subscriptions)
