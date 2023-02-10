88164588f48f7e632912c153dddc7d695aa1af89
Chung Tran <bigearsenal@gmail.com>
2023-02-10 13:32:41 +0700

    feat: add comment

--- p2p_wallet/Services/Repositories/BaseRepository/ItemRepository.swift
+++ p2p_wallet/Services/Repositories/BaseRepository/ItemRepository.swift
@@ -1,61 +1,76 @@
 import Foundation
 import Combine
 
+/// Repository to manage some kind of Item
 @MainActor
-class ItemRepository<T: Hashable>: ObservableObject {
-    // MARK: - Properties
+class ItemRepository<ItemType: Hashable>: ObservableObject {
+    // MARK: - Private properties
+    
+    /// Initial data for initializing state
+    private let initialData: ItemType
+    
+    /// Current running task
+    private var loadingTask: Task<Void, Error>?
+    
+    // MARK: - Public properties
+    
+    /// The current data
+    @Published var data: ItemType
 
-    let initialData: T
-    
-    var task: Task<Void, Error>?
-    
-    @Published var data: T
+    /// The current loading state of the data
     @Published var state: LoadingState = .initializing
+
+    /// Optional error if occurred
     @Published var error: Error?
     
     // MARK: - Initializer
-
-    init(initialData: T) {
+    
+    /// ItemRepository's initializer
+    /// - Parameter initialData: initial data for begining state of the Repository
+    init(initialData: ItemType) {
         self.initialData = initialData
         data = initialData
-        bind()
     }
     
-    func bind() {}
+    // MARK: - Asynchronous request handler
+    
+    /// The request to retrieve data into repository
+    /// - Returns: item
+    func createRequest() async throws -> ItemType {
+        fatalError("Must override")
+    }
     
     // MARK: - Actions
 
+    /// Erase data and reset repository to its initial state
     func flush() {
         data = initialData
         state = .initializing
         error = nil
     }
     
+    /// Erase and reload all data
     func reload() {
         flush()
-        request(reload: true)
+        request()
     }
     
-    // MARK: - Asynchronous request handler
-
-    func createRequest() async throws -> T {
-        fatalError("Must override")
+    /// Refresh data without erasing current data
+    func refresh() {
+        request()
     }
     
-    func shouldRequest() -> Bool {
-        state == .loading
-    }
-    
-    func request(reload: Bool = false) {
+    /// Request data from outside to get new data
+    func request() {
         // cancel previous request
-        task?.cancel()
+        loadingTask?.cancel()
         
         // mark as loading
         state = .loading
         error = nil
         
-        // assign and execute task
-        task = Task {
+        // assign and execute loadingTask
+        loadingTask = Task {
             do {
                 let newData = try await createRequest()
                 handleNewData(newData)
@@ -68,22 +83,18 @@ class ItemRepository<T: Hashable>: ObservableObject {
         }
     }
     
-    func refresh() {
-        request(reload: false)
-    }
-    
-    func handleNewData(_ newData: T) {
+    /// Handle new data that just received
+    /// - Parameter newData: the new data received
+    func handleNewData(_ newData: ItemType) {
         data = newData
         error = nil
         state = .loaded
     }
     
-    func handleError(_ error: Error) {
-        self.error = error
+    /// Handle error when received
+    /// - Parameter err: the error received
+    func handleError(_ err: Error) {
+        error = err
         state = .error
-    }
-    
-    var dataDidChange: AnyPublisher<Void, Never> {
-        $data.map {_ in ()}.eraseToAnyPublisher()
     }
 }
--- p2p_wallet/Services/Repositories/BaseRepository/LoadingState.swift
+++ p2p_wallet/Services/Repositories/BaseRepository/LoadingState.swift
@@ -1,8 +1,13 @@
 import Foundation
 
+/// Loading state for a specific time-consuming operation
 enum LoadingState {
+    /// Nothing loaded
     case initializing
+    /// Data is loading
     case loading
+    /// Data is loaded
     case loaded
+    /// Error
     case error
 }
