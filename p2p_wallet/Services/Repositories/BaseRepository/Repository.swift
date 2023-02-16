a35d650ddbd235a9632739d650aa86080fb78eff
Chung Tran <bigearsenal@gmail.com>
2023-02-16 12:39:07 +0700

    feat: some fixes

--- p2p_wallet/Services/Repositories/BaseRepository/Repository.swift
+++ p2p_wallet/Services/Repositories/BaseRepository/Repository.swift
@@ -10,6 +10,43 @@ protocol AnyRepository {
     func fetch() async throws -> ItemType?
 }
 
+protocol AnyListRepository: AnyRepository {
+    /// Pagination strategy
+    var paginationStrategy: PaginationStrategy? { get }
+    /// Fetch list of item from outside
+    func fetch() async throws -> [ItemType]?
+    /// Reload
+    func reload()
+    
+//    /// Indicate if should fetch new data to prevent unwanted request
+//    /// - Returns: should fetch new data
+//    func shouldFetch() -> Bool {
+//        var shouldRequest = super.shouldRequest()
+//
+//        // check if isLastPageLoaded
+//        if let paginationStrategy {
+//            shouldRequest = shouldRequest && !paginationStrategy.isLastPageLoaded
+//        }
+//
+//        return shouldRequest
+//    }
+}
+
+//class ListRepository<ItemType: Hashable>: AnyRepository {
+//    // MARK: - Properties
+//
+//    /// Strategy that indicates how pagination works, nil if pagination is disabled
+//    let paginationStrategy: PaginationStrategy?
+//
+//    func shouldFetch() -> Bool {
+//        true
+//    }
+//
+//    func fetch() async throws -> [ItemType]? {
+//        nil
+//    }
+//}
+
 //extension AsyncSequence: Repository {
 //    func fetch() async throws {
 //        let iterator = makeAsyncIterator()
