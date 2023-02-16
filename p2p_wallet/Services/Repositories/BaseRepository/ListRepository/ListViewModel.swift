37ed6df656e2cd04b1237cb1694fefe7af875b14
Chung Tran <bigearsenal@gmail.com>
2023-02-16 13:07:17 +0700

    feat: mapper

--- p2p_wallet/Services/Repositories/BaseRepository/ListRepository/ListViewModel.swift
+++ p2p_wallet/Services/Repositories/BaseRepository/ListRepository/ListViewModel.swift
@@ -38,43 +38,6 @@ class ListViewModel<Repository: AnyListRepository>: ItemViewModel<Repository> {
         super.request()
     }
 
-    /// Handle new data that just received
-    /// - Parameter newData: the new data received
-    override func handleNewData(_ newItems: ItemType?) {
-        var newData = data
-
-        // for pagination
-        if let paginationStrategy = repository.paginationStrategy {
-            // append data that is currently not existed in current data array
-            
-            newData += newItems.filter {!data.contains($0)}
-
-            // resign state
-            paginationStrategy.moveToNextPage()
-            paginationStrategy.checkIfLastPageLoaded(lastSnapshot: newItems)
-        }
-
-        // without pagination
-        else {
-            // replace the current data
-            newData = newItems
-        }
-
-        super.handleNewData(newData)
-    }
-
-    /// Handle error when received
-    /// - Parameter err: the error received
-    override func handleError(_ err: Error) {
-        super.handleError(err)
-    }
-
-    /// Force override current data with newData
-    /// - Parameter newData: newData to be overriden
-    func overrideData(by newData: [ItemType]) {
-        super.handleNewData(newData)
-    }
-
 //    func updateFirstPage(onSuccessFilterNewData: (([ItemType]) -> [ItemType])? = nil) {
 //        let originalOffset = offset
 //        offset = 0
@@ -94,78 +57,78 @@ class ListViewModel<Repository: AnyListRepository>: ItemViewModel<Repository> {
 //        offset = originalOffset
 //    }
 
-    /// Update multiple records with a closure
-    /// - Parameter closure: updating closure
-    func batchUpdate(closure: ([ItemType]) -> [ItemType]) {
-        let newData = closure(data)
-        overrideData(by: newData)
-    }
-
-    /// Update item that matchs predicate
-    /// - Parameters:
-    ///   - predicate: predicate to find item
-    ///   - transform: transform item before udpate
-    /// - Returns: true if updated, false if not
-    @discardableResult
-    func updateItem(where predicate: (ItemType) -> Bool, transform: (ItemType) -> ItemType?) -> Bool {
-        // modify items
-        var itemsChanged = false
-        if let index = data.firstIndex(where: predicate),
-           let item = transform(data[index]),
-           item != data[index]
-        {
-            itemsChanged = true
-            var data = self.data
-            data[index] = item
-            overrideData(by: data)
-        }
-
-        return itemsChanged
-    }
-
-    /// Insert item into list or update if needed
-    /// - Parameters:
-    ///   - item: item to be inserted
-    ///   - predicate: predicate to find item
-    ///   - shouldUpdate: should update instead
-    /// - Returns: true if inserted, false if not
-    @discardableResult
-    func insert(_ item: ItemType, where predicate: ((ItemType) -> Bool)? = nil, shouldUpdate: Bool = false) -> Bool
-    {
-        var items = data
-
-        // update mode
-        if let predicate = predicate {
-            if let index = items.firstIndex(where: predicate), shouldUpdate {
-                items[index] = item
-                overrideData(by: items)
-                return true
-            }
-        }
-
-        // insert mode
-        else {
-            items.append(item)
-            overrideData(by: items)
-            return true
-        }
-
-        return false
-    }
-
-    /// Remove item that matches a predicate from list
-    /// - Parameter predicate: predicate to find item
-    /// - Returns: removed item
-    @discardableResult
-    func removeItem(where predicate: (ItemType) -> Bool) -> ItemType? {
-        var result: ItemType?
-        var data = self.data
-        if let index = data.firstIndex(where: predicate) {
-            result = data.remove(at: index)
-        }
-        if result != nil {
-            overrideData(by: data)
-        }
-        return nil
-    }
+//    /// Update multiple records with a closure
+//    /// - Parameter closure: updating closure
+//    func batchUpdate(closure: ([ItemType]) -> [ItemType]) {
+//        let newData = closure(data)
+//        overrideData(by: newData)
+//    }
+//
+//    /// Update item that matchs predicate
+//    /// - Parameters:
+//    ///   - predicate: predicate to find item
+//    ///   - transform: transform item before udpate
+//    /// - Returns: true if updated, false if not
+//    @discardableResult
+//    func updateItem(where predicate: (ItemType) -> Bool, transform: (ItemType) -> ItemType?) -> Bool {
+//        // modify items
+//        var itemsChanged = false
+//        if let index = data.firstIndex(where: predicate),
+//           let item = transform(data[index]),
+//           item != data[index]
+//        {
+//            itemsChanged = true
+//            var data = self.data
+//            data[index] = item
+//            overrideData(by: data)
+//        }
+//
+//        return itemsChanged
+//    }
+//
+//    /// Insert item into list or update if needed
+//    /// - Parameters:
+//    ///   - item: item to be inserted
+//    ///   - predicate: predicate to find item
+//    ///   - shouldUpdate: should update instead
+//    /// - Returns: true if inserted, false if not
+//    @discardableResult
+//    func insert(_ item: ItemType, where predicate: ((ItemType) -> Bool)? = nil, shouldUpdate: Bool = false) -> Bool
+//    {
+//        var items = data
+//
+//        // update mode
+//        if let predicate = predicate {
+//            if let index = items.firstIndex(where: predicate), shouldUpdate {
+//                items[index] = item
+//                overrideData(by: items)
+//                return true
+//            }
+//        }
+//
+//        // insert mode
+//        else {
+//            items.append(item)
+//            overrideData(by: items)
+//            return true
+//        }
+//
+//        return false
+//    }
+//
+//    /// Remove item that matches a predicate from list
+//    /// - Parameter predicate: predicate to find item
+//    /// - Returns: removed item
+//    @discardableResult
+//    func removeItem(where predicate: (ItemType) -> Bool) -> ItemType? {
+//        var result: ItemType?
+//        var data = self.data
+//        if let index = data.firstIndex(where: predicate) {
+//            result = data.remove(at: index)
+//        }
+//        if result != nil {
+//            overrideData(by: data)
+//        }
+//        return nil
+//    }
 }
