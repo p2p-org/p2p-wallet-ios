7e1cb78d3252310a73635246683b00d83cd7a573
Chung Tran <bigearsenal@gmail.com>
2023-02-17 10:04:45 +0700

    feat: shouldFetch

--- p2p_wallet/Services/Repositories/BaseRepository/BooksExample.swift
+++ p2p_wallet/Services/Repositories/BaseRepository/BooksExample.swift
@@ -10,4 +10,25 @@ import Foundation
 // Start with a model
 struct Book: Hashable & Identifiable {
     var id = UUID()
+    let name: String
+}
+
+// Then a Repository
+final class BooksRepository: ListRepository<Book> {
+    override func fetch() async throws -> [Book] {
+        [
+            .init(name: "Book1"),
+            .init(name: "Book2")
+        ]
+    }
+}
+
+// Then form viewModel
+@MainActor class Test {
+    func test() {
+        let vm = ListViewModel(
+            initialData: nil,
+            repository: BooksRepository(paginationStrategy: nil)
+        )
+    }
 }
