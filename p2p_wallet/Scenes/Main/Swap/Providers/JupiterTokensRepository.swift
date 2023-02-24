bae7eaee254ed3f4c1870bfa349ccb21b535d183
Chung Tran <bigearsenal@gmail.com>
2023-02-24 15:55:50 +0700

    feat: update jupiterTokens when userWallets changes

--- p2p_wallet/Scenes/Main/Swap/Providers/JupiterTokensRepository.swift
+++ p2p_wallet/Scenes/Main/Swap/Providers/JupiterTokensRepository.swift
@@ -30,6 +30,7 @@ final class JupiterTokensRepositoryImpl: JupiterTokensRepository {
     private let jupiterClient: JupiterAPI
     private let localProvider: JupiterTokensProvider
     @Injected private var walletsRepository: WalletsRepository
+    private var subscriptions = Set<AnyCancellable>()
 
     // MARK: - Private params
     private var statusSubject = CurrentValueSubject<JupiterDataStatus, Never>(.initial)
@@ -37,6 +38,8 @@ final class JupiterTokensRepositoryImpl: JupiterTokensRepository {
     init(provider: JupiterTokensProvider, jupiterClient: JupiterAPI) {
         self.localProvider = provider
         self.jupiterClient = jupiterClient
+        
+        bind()
     }
 
     func load() async {
@@ -53,16 +56,40 @@ final class JupiterTokensRepositoryImpl: JupiterTokensRepository {
                 try localProvider.save(tokens: jupiterTokens, routeMap: routeMap)
             }
             
-            let wallets = walletsRepository.getWallets()
-            let swapTokens = jupiterTokens.map { jupiterToken in
-                if let userWallet = wallets.first(where: { $0.mintAddress == jupiterToken.address }) {
-                    return SwapToken(token: jupiterToken, userWallet: userWallet)
-                }
-                return SwapToken(token: jupiterToken, userWallet: nil)
-            }
-            statusSubject.send(.ready(swapTokens: swapTokens, routeMap: routeMap))
+            mapTokens()
         } catch {
             statusSubject.send(.failed)
         }
+    }
+    
+    // MARK: - Helpers
+
+    private func bind() {
+        walletsRepository.dataPublisher
+            .sink { [weak self] _ in
+                self?.mapTokens()
+            }
+            .store(in: &subscriptions)
+    }
+    
+    private func mapTokens() {
+        // get catchedData
+        guard let cachedData = localProvider.getCachedData()
+        else {
+            return
+        }
+        
+        let jupiterTokens = cachedData.tokens
+        let routeMap = cachedData.routeMap
+        
+        // get user wallets
+        let wallets = walletsRepository.getWallets()
+        let swapTokens = jupiterTokens.map { jupiterToken in
+            if let userWallet = wallets.first(where: { $0.mintAddress == jupiterToken.address }) {
+                return SwapToken(token: jupiterToken, userWallet: userWallet)
+            }
+            return SwapToken(token: jupiterToken, userWallet: nil)
+        }
+        statusSubject.send(.ready(swapTokens: swapTokens, routeMap: routeMap))
     }
 }
