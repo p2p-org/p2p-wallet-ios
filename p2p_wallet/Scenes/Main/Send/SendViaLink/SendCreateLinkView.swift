8f3078fb8d14004b7100ef0a88155a84356ecc85
Chung Tran <bigearsenal@gmail.com>
2023-03-01 13:26:25 +0700

    feat: SendCreateLinkView

---
+++ p2p_wallet/Scenes/Main/Send/SendViaLink/SendCreateLinkView.swift
@@ -0,0 +1,48 @@
+import SwiftUI
+import KeyAppUI
+
+struct SendCreateLinkView: View {
+
+    let onAppear: () -> Void
+    private let mainColor = Color(Asset.Colors.night.color)
+    private let animationSize = CGSize(width: 272, height: 204)
+
+    var body: some View {
+        ZStack {
+            Color(Asset.Colors.smoke.color)
+                .edgesIgnoringSafeArea(.all)
+            VStack(spacing: 0) {
+                LottieView(lottieFile: "walletAnimation", loopMode: .loop)
+                    .scaledToFill()
+                    .frame(width: animationSize.width, height: animationSize.height)
+                    .padding(.bottom, 36)
+
+                Text(L10n.creatingYourLink)
+                    .apply(style: .largeTitle)
+                    .foregroundColor(mainColor)
+                    .transition(.opacity.animation(.linear))
+                    .padding(.bottom, 16)
+                
+                Text(L10n.thisWillTakeUpToSeconds(5))
+                    .apply(style: .title3)
+                    .foregroundColor(mainColor)
+                    .padding(.bottom, 60)
+
+                IndeterminateProgressBar(indicatorColor: mainColor)
+                    .frame(width: 113)
+            }
+            .edgesIgnoringSafeArea(.top)
+            .padding(.horizontal, 20)
+            .padding(.bottom, animationSize.height / 2)
+        }
+        .onAppear {
+            onAppear()
+        }
+    }
+}
+
+struct SendCreateLinkView_Previews: PreviewProvider {
+    static var previews: some View {
+        SendCreateLinkView {}
+    }
+}
---
+++ p2p_wallet/Scenes/Main/Send/SendViaLink/SendCreateLinkViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  SendCreateLinkViewModel.swift
+//  p2p_wallet
+//
+//  Created by Chung Tran on 01/03/2023.
+//
+
+import Foundation
+
+final class SendCreateLinkViewModel: BaseViewModel, ObservableObject {
+}
