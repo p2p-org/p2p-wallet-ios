29f9d62c4103c98b8ebeb8776be0478243ee1db3
Chung Tran <bigearsenal@gmail.com>
2023-03-02 12:45:05 +0700

    feat: add rows

--- p2p_wallet/Scenes/Main/Swap/SwapSettings/View/SwapSettingsView.swift
+++ p2p_wallet/Scenes/Main/Swap/SwapSettings/View/SwapSettingsView.swift
@@ -16,12 +16,138 @@ struct SwapSettingsView: View {
     
     var body: some View {
         List {
+            Section {
+                fisrtSectionRows
+            }
+            
+            Section {
+                commonRow(
+                    title: L10n.minimumReceived,
+                    subtitle: viewModel.minimumReceived?.amountDescription
+                )
+            }
+            
             Section(header: Text(L10n.slippage)) {
                 slippageRows
             }
         }
     }
     
+    // MARK: - First section
+
+    private var fisrtSectionRows: some View {
+        Group {
+            // Route
+            commonRow(
+                title: L10n.swappingThrough,
+                subtitle: viewModel.currentRoute?.tokens,
+                trailingSubtitle: viewModel.currentRoute?.description,
+                trailingView: Image(uiImage: .nextArrow)
+                    .resizable()
+                    .frame(width: 7.41, height: 12)
+                    .padding(.vertical, (20-12)/2)
+                    .padding(.horizontal, (20-7.41)/2)
+                    .castToAnyView()
+            )
+            
+            // Network fee
+            feeRow(
+                title: L10n.networkFee,
+                fee: viewModel.networkFee,
+                canBePaidByKeyApp: true
+            )
+            
+            // Account creation fee
+            feeRow(
+                title: L10n.accountCreationFee,
+                fee: viewModel.accountCreationFee,
+                canBePaidByKeyApp: false
+            )
+            
+            // Liquidity fee
+            feeRow(
+                title: L10n.liquidityFee,
+                fees: viewModel.liquidityFee
+            )
+            
+            // Estimated fee
+            HStack {
+                Text(L10n.estimatedFees)
+                    .fontWeight(.semibold)
+                    .apply(style: .text3)
+                    .padding(.vertical, 10)
+                
+                Spacer()
+                
+                Text(viewModel.estimatedFees)
+                    .fontWeight(.semibold)
+                    .apply(style: .text3)
+                    .padding(.vertical, 10)
+            }
+                .frame(maxWidth: .infinity)
+        }
+            .listRowInsets(EdgeInsets(top: 14, leading: 20, bottom: 14, trailing: 20))
+    }
+    
+    private func feeRow(
+        title: String,
+        fee: SwapSettingsViewModel.FeeInfo?,
+        canBePaidByKeyApp: Bool
+    ) -> some View {
+        commonRow(
+            title: title,
+            subtitle: fee?.amountDescription,
+            subtitleColor: fee?.shouldHighlightAmountDescription == true ? Asset.Colors.mint.color: Asset.Colors.mountain.color,
+            trailingSubtitle: fee?.amountInFiatDescription
+        )
+    }
+    
+    private func feeRow(
+        title: String,
+        fees: [SwapSettingsViewModel.FeeInfo]
+    ) -> some View {
+        commonRow(
+            title: title,
+            subtitle: fees.compactMap(\.amountDescription).joined(separator: ", "),
+            trailingSubtitle: "≈ " + fees.compactMap(\.amountInFiat).reduce(0.0, +).fiatAmountFormattedString()
+        )
+    }
+    
+    private func commonRow(
+        title: String,
+        subtitle: String?,
+        subtitleColor: UIColor = Asset.Colors.mountain.color,
+        trailingSubtitle: String? = nil,
+        trailingView: AnyView = Image(uiImage: .infoStraight)
+            .resizable()
+            .foregroundColor(Color(Asset.Colors.mountain.color))
+            .frame(width: 20, height: 20)
+            .castToAnyView()
+    ) -> some View {
+        HStack {
+            VStack(alignment: .leading, spacing: 4) {
+                Text(title)
+                    .apply(style: .text3)
+                Text(subtitle)
+                    .apply(style: .label1)
+                    .foregroundColor(Color(subtitleColor))
+            }
+            
+            Spacer()
+            
+            Text(trailingSubtitle)
+                .apply(style: .label1)
+                .foregroundColor(Color(Asset.Colors.mountain.color))
+                .layoutPriority(1)
+                .padding(.trailing, 10)
+            
+            trailingView
+        }
+        .frame(maxWidth: .infinity)
+    }
+    
+    // MARK: - Slippage section
+
     private var slippageRows: some View {
         ForEach(Array(zip(viewModel.slippages.indices, viewModel.slippages)), id: \.0) { index, slippage in
             Button(
@@ -87,5 +213,50 @@ struct SwapSettingsView: View {
             textFieldColor = !failureSlippage ? Asset.Colors.night.color : Asset.Colors.rose.color
             self.failureSlippage = failureSlippage
         }
+    }
+}
+
+struct SwapSettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwapSettingsView(
+            viewModel: .init(
+                currentRoute: .init(
+                    name: "Raydium",
+                    description: "Best price",
+                    tokens: "SOL→CLMM→USDC→CRAY"
+                ),
+                networkFee: .init(
+                    amount: 0,
+                    token: nil,
+                    amountInFiat: nil,
+                    canBePaidByKeyApp: true
+                ),
+                accountCreationFee: .init(
+                    amount: 0.8,
+                    token: "Token A",
+                    amountInFiat: 6.1,
+                    canBePaidByKeyApp: false
+                ),
+                liquidityFee: [
+                    .init(
+                        amount: 0.991,
+                        token: "TokenC",
+                        amountInFiat: 0.05,
+                        canBePaidByKeyApp: false
+                    ),
+                    .init(
+                        amount: 0.991,
+                        token: "TokenD",
+                        amountInFiat: 0.05,
+                        canBePaidByKeyApp: false
+                    )
+                ],
+                minimumReceived: .init(
+                    amount: 0.91,
+                    token: "TokenB"
+                ),
+                slippage: 0.5
+            )
+        )
     }
 }
