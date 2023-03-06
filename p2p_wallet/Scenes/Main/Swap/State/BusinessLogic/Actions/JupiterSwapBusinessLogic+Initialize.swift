1e8d9023f4ffa3d2bdef94ed5dca3cc079af2370
Chung Tran <bigearsenal@gmail.com>
2023-03-06 12:45:29 +0700

    feat: some fixes

--- p2p_wallet/Scenes/Main/Swap/State/BusinessLogic/Actions/JupiterSwapBusinessLogic+Initialize.swift
+++ p2p_wallet/Scenes/Main/Swap/State/BusinessLogic/Actions/JupiterSwapBusinessLogic+Initialize.swift
@@ -10,36 +10,28 @@ extension JupiterSwapBusinessLogic {
         fromToken: SwapToken?,
         toToken: SwapToken?
     ) async -> JupiterSwapState {
-        do {
-            let tokens: (fromToken: SwapToken, toToken: SwapToken)
-            if let fromToken, let toToken {
-                tokens = (fromToken, toToken)
-            } else if let fromToken, let toToken = autoChooseToToken(for: fromToken, from: swapTokens) {
-                tokens = (fromToken, toToken)
-            } else if let chosenTokens = autoChoose(swapTokens: swapTokens) {
-                tokens = chosenTokens
-            } else {
-                return .zero.modified {
-                    $0.status = .ready
-                    $0.routeMap = routeMap
-                    $0.swapTokens = swapTokens
-                }
-            }
-            
+        let tokens: (fromToken: SwapToken, toToken: SwapToken)
+        if let fromToken, let toToken {
+            tokens = (fromToken, toToken)
+        } else if let fromToken, let toToken = autoChooseToToken(for: fromToken, from: swapTokens) {
+            tokens = (fromToken, toToken)
+        } else if let chosenTokens = autoChoose(swapTokens: swapTokens) {
+            tokens = chosenTokens
+        } else {
             return .zero.modified {
                 $0.status = .ready
                 $0.routeMap = routeMap
                 $0.swapTokens = swapTokens
-                $0.fromToken = tokens.fromToken
-                $0.toToken = tokens.toToken
-                $0.slippageBps = Int(Defaults.slippage * 100)
             }
-        } catch {
-            return .zero.modified {
-                $0.status = .error(reason: .initializationFailed)
-                $0.routeMap = routeMap
-                $0.swapTokens = swapTokens
-            }
+        }
+        
+        return .zero.modified {
+            $0.status = .ready
+            $0.routeMap = routeMap
+            $0.swapTokens = swapTokens
+            $0.fromToken = tokens.fromToken
+            $0.toToken = tokens.toToken
+            $0.slippageBps = Int(Defaults.slippage * 100)
         }
     }
 }
