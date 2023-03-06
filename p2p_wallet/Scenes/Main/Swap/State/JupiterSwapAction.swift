0cf5f0f81d7f9b0e250232ddd28745fbb8d49d17
Chung Tran <bigearsenal@gmail.com>
2023-03-06 23:32:10 +0700

    fix: binding

--- p2p_wallet/Scenes/Main/Swap/State/JupiterSwapAction.swift
+++ p2p_wallet/Scenes/Main/Swap/State/JupiterSwapAction.swift
@@ -17,4 +17,31 @@ enum JupiterSwapAction: Equatable {
     
     case chooseRoute(Route)
     case changeSlippageBps(Int)
+    
+    // MARK: - Helper
+
+    var description: String {
+        switch self {
+        case .initialize:
+            return "initialize"
+        case .update:
+            return "update"
+        case .changeAmountFrom(let double):
+            return "changeAmountFrom(\(double))"
+        case .changeFromToken(let swapToken):
+            return "changeFromToken(\(swapToken.token.symbol))"
+        case .changeToToken(let swapToken):
+            return "changeToToken(\(swapToken.token.symbol))"
+        case .switchFromAndToTokens:
+            return "switchFromAndToTokens"
+        case .updateUserWallets:
+            return "updateUserWallets"
+        case .updateTokensPriceMap:
+            return "updateTokensPriceMap"
+        case .chooseRoute(let route):
+            return "chooseRoute(\(route.id))"
+        case .changeSlippageBps(let int):
+            return "changeSlippageBps(\(int))"
+        }
+    }
 }
