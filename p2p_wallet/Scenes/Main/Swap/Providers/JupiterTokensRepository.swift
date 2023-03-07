2cebf2c492c44c621598c3a718835dafe9623ea2
Chung Tran <bigearsenal@gmail.com>
2023-03-07 12:13:33 +0700

    fix: network call error caching

--- p2p_wallet/Scenes/Main/Swap/Providers/JupiterTokensRepository.swift
+++ p2p_wallet/Scenes/Main/Swap/Providers/JupiterTokensRepository.swift
@@ -27,12 +27,16 @@ final class JupiterTokensRepositoryImpl: JupiterTokensRepository {
         statusSubject.eraseToAnyPublisher()
     }
     // MARK: - Dependencies
+
     private let jupiterClient: JupiterAPI
     private let localProvider: JupiterTokensProvider
     @Injected private var walletsRepository: WalletsRepository
 
     // MARK: - Private params
+
     private var statusSubject = CurrentValueSubject<JupiterDataStatus, Never>(.initial)
+    private var task: Task<Void, Never>?
+    private var isInitialized = false
 
     init(provider: JupiterTokensProvider, jupiterClient: JupiterAPI) {
         self.localProvider = provider
@@ -40,19 +44,48 @@ final class JupiterTokensRepositoryImpl: JupiterTokensRepository {
     }
 
     func load() async {
+        // cancel previous task
+        task?.cancel()
+        task = nil
+        
+        // assign task
+        task = Task { [weak self] in
+            await self?.fetch()
+        }
+        
+        await task?.value
+    }
+    
+    private func fetch() async {
         statusSubject.send(.loading)
         do {
             let jupiterTokens: [Token]
             let routeMap: RouteMap
-            if let cachedData = localProvider.getCachedData() {
-                jupiterTokens = cachedData.tokens
-                routeMap = cachedData.routeMap
-            } else {
-                jupiterTokens = try await jupiterClient.getTokens()
-                routeMap = try await jupiterClient.routeMap()
+            
+            try Task.checkCancellation()
+            
+            // if it is the first time user initialize the repository
+            if !isInitialized {
+                (jupiterTokens, routeMap) = try await(
+                    jupiterClient.getTokens(),
+                    jupiterClient.routeMap()
+                )
                 try localProvider.save(tokens: jupiterTokens, routeMap: routeMap)
+                isInitialized = true
             }
             
+            // if it is not the first time, use cached data to reduce network calls
+            else if let cachedData = localProvider.getCachedData() {
+                jupiterTokens = cachedData.tokens
+                routeMap = cachedData.routeMap
+            }
+
+            // return error if no cached data found
+            else {
+                throw JupiterError.invalidResponse
+            }
+            
+            // map userWallets with jupiter tokens
             let wallets = walletsRepository.getWallets()
             let swapTokens = jupiterTokens.map { jupiterToken in
                 if let userWallet = wallets.first(where: { $0.mintAddress == jupiterToken.address }) {
@@ -60,8 +93,12 @@ final class JupiterTokensRepositoryImpl: JupiterTokensRepository {
                 }
                 return SwapToken(token: jupiterToken, userWallet: nil)
             }
+            try Task.checkCancellation()
             statusSubject.send(.ready(swapTokens: swapTokens, routeMap: routeMap))
         } catch {
+            guard !(error is CancellationError) else {
+                return
+            }
             statusSubject.send(.failed)
         }
     }
