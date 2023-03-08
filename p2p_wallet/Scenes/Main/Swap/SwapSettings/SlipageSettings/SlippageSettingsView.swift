44a5c937793ede2c934f98bed8130d8f89cfaac1
Chung Tran <bigearsenal@gmail.com>
2023-03-08 22:04:05 +0700

    refactor: some fixes

---
+++ p2p_wallet/Scenes/Main/Swap/SwapSettings/SlipageSettings/SlippageSettingsView.swift
@@ -0,0 +1,116 @@
+//
+//  SlippageSettingsView.swift
+//  p2p_wallet
+//
+//  Created by Chung Tran on 08/03/2023.
+//
+
+import SwiftUI
+import KeyAppUI
+
+struct SlippageSettingsView: View {
+    @ObservedObject private var viewModel: SlippageSettingsViewModel
+    
+    @State private var textFieldColor: UIColor = Asset.Colors.night.color
+    
+    init(viewModel: SlippageSettingsViewModel) {
+        self.viewModel = viewModel
+    }
+    
+    var body: some View {
+        ForEach(Array(zip(viewModel.slippages.indices, viewModel.slippages)), id: \.0) { index, slippage in
+            Button(
+                action: {
+                    viewModel.selectRow(at: index)
+                },
+                label: {
+                    cell(index: index, slippage: slippage)
+                }
+            )
+        }
+        .onChange(of: viewModel.isCustomSlippageValid) { isSlippageValid in
+            textFieldColor = isSlippageValid ? Asset.Colors.night.color : Asset.Colors.rose.color
+        }
+    }
+    
+    @ViewBuilder
+    func cell(index: Int, slippage: Double?) -> some View {
+        if let slippage = slippage {
+            staticSelectionCell(index: index, slippage: slippage)
+        } else {
+            customInput(index: index)
+        }
+    }
+    
+    @ViewBuilder
+    func staticSelectionCell(index: Int, slippage: Double) -> some View {
+        HStack {
+            Text("\(String(format: "%.1f", slippage))% \(viewModel.selectedSlippage ?? 0)")
+                .foregroundColor(Color(Asset.Colors.night.color))
+                .font(uiFont: .font(of: .text3))
+            Spacer()
+            if index == viewModel.selectedIndex {
+                Image(systemName: "checkmark")
+            }
+        }
+        .padding(.vertical, 16)
+    }
+    
+    @ViewBuilder
+    func customInput(index: Int) -> some View {
+        VStack {
+            HStack {
+                Text(L10n.custom)
+                    .foregroundColor(Color(Asset.Colors.night.color))
+                    .font(uiFont: .font(of: .text3))
+                Spacer()
+                if index == viewModel.selectedIndex {
+                    Image(systemName: "checkmark")
+                }
+            }
+            if index == viewModel.selectedIndex {
+                VStack(alignment: .leading, spacing: 4) {
+                    ZStack {
+                        Color(Asset.Colors.rain.color)
+                            .frame(height: 44)
+                            .cornerRadius(12)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 12)
+                                    .stroke(
+                                        Color(Asset.Colors.rose.color),
+                                        lineWidth: viewModel.isCustomSlippageValid ? 0 : 1
+                                    )
+                            )
+                        
+                        DecimalTextField(
+                            value: $viewModel.customSlippage,
+                            isFirstResponder: $viewModel.isCustomSlippageSelected,
+                            textColor: $textFieldColor
+                        ) { textField in
+                            textField.font = .font(of: .text3)
+                            textField.keyboardType = .decimalPad
+                            textField.placeholder = "0"
+                            textField.maximumFractionDigits = 2
+                            textField.decimalSeparator = "."
+                        }
+                        .padding(.horizontal, 16)
+                    }
+                    Text("\(L10n.theSlippageCouldBe) 0.01-50%")
+                        .foregroundColor(viewModel.isCustomSlippageValid ? Color(Asset.Colors.mountain.color): Color(Asset.Colors.rose.color))
+                        .font(uiFont: .font(of: .label1))
+                }
+            }
+        }
+        .padding(.vertical, 16)
+    }
+}
+
+struct SlippageSettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        List {
+            Section {
+                SlippageSettingsView(viewModel: .init(slippage: 0.5))
+            }
+        }
+    }
+}
