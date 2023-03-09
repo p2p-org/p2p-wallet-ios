31310084bce662ea5d535c2dcca9328bf91614fc
Ivan Babich <babichivanya@gmail.com>
2023-03-09 17:29:59 +0300

    [PWN-7479] Неверное отображение списка токенов В

--- p2p_wallet/Scenes/Main/Swap/ChooseSwapItem/Service/ChooseSwapTokenService.swift
+++ p2p_wallet/Scenes/Main/Swap/ChooseSwapItem/Service/ChooseSwapTokenService.swift
@@ -2,44 +2,80 @@ import Resolver
 
 final class ChooseSwapTokenService: ChooseItemService {
 
-    var chosenTokenTitle: String = L10n.chosenToken.uppercased()
-    var otherTokensTitle: String = L10n.allTokens.uppercased()
+    let chosenTokenTitle: String = L10n.chosenToken.uppercased()
+    let otherTokensTitle: String = L10n.allTokens.uppercased()
 
     private let swapTokens: [SwapToken]
-    init(swapTokens: [SwapToken]) {
+    private let fromToken: Bool
+
+    private let preferTokens: [String]
+
+    init(swapTokens: [SwapToken], fromToken: Bool) {
         self.swapTokens = swapTokens
+        self.fromToken = fromToken
+
+        if fromToken {
+            preferTokens = ["USDC", "USDT"]
+        } else {
+            preferTokens = SwapToken.preferTokens
+        }
     }
 
     func fetchItems() async throws -> [ChooseItemListSection] {
-        let firstSection = swapTokens.filter({ $0.userWallet != nil })
-        let secondSection = swapTokens.filter({ $0.userWallet == nil })
-        return [ChooseItemListSection(items: firstSection), ChooseItemListSection(items: secondSection)]
+        var firstSection = [SwapToken]()
+        var secondSection = [SwapToken]()
+        if fromToken {
+            firstSection = swapTokens.filter { $0.userWallet != nil }
+            secondSection = swapTokens.filter { $0.userWallet == nil }
+        } else {
+            let preferTokens = Set(self.preferTokens)
+            swapTokens.forEach {
+                if preferTokens.contains($0.token.symbol) {
+                    firstSection.append($0)
+                } else {
+                    secondSection.append($0)
+                }
+            }
+        }
+        return [
+            ChooseItemListSection(items: firstSection),
+            ChooseItemListSection(items: secondSection)
+        ]
     }
 
     func sort(items: [ChooseItemListSection]) -> [ChooseItemListSection] {
         let newItems = items.map { section in
             guard let tokens = section.items as? [SwapToken] else { return section }
-            return ChooseItemListSection(items: tokens.sorted())
+            return ChooseItemListSection(items: tokens.sorted(
+                preferTokens: preferTokens,
+                sortByName: !fromToken
+            ))
         }
-        let isEmpty = newItems.flatMap({ $0.items }).isEmpty
+        let isEmpty = newItems.flatMap { $0.items }.isEmpty
         return isEmpty ? [] : newItems
     }
 }
 
+// MARK: - Sort Rules
+
 private extension Array where Element == SwapToken {
-    func sorted() -> Self {
-        let preferOrder: [String: Int] = ["USDC": 1, "USDT": 2]
-        return self
-            .sorted { (lhs: SwapToken, rhs: SwapToken) -> Bool in
-                if preferOrder[lhs.token.symbol] != nil || preferOrder[rhs.token.symbol] != nil {
-                    return (preferOrder[lhs.token.symbol] ?? 3) < (preferOrder[rhs.token.symbol] ?? 3)
-                } else if let lhsWallet = lhs.userWallet, let rhsWallet = rhs.userWallet {
-                    return lhsWallet.amountInCurrentFiat > rhsWallet.amountInCurrentFiat
-                } else if lhs.userWallet != nil || rhs.userWallet != nil {
-                    return false
-                } else {
-                    return lhs.token.name < rhs.token.name
-                }
+    func sorted(preferTokens: [String], sortByName: Bool) -> Self {
+        var preferOrder = [String: Int]()
+        preferTokens.enumerated().forEach {
+            preferOrder[$0.1] = $0.0 + 1
+        }
+        return sorted { (lhs: SwapToken, rhs: SwapToken) -> Bool in
+            if preferOrder[lhs.token.symbol] != nil || preferOrder[rhs.token.symbol] != nil {
+                return (preferOrder[lhs.token.symbol] ?? 3) < (preferOrder[rhs.token.symbol] ?? 3)
+            } else if sortByName {
+                return lhs.token.name < rhs.token.name
+            } else if let lhsWallet = lhs.userWallet, let rhsWallet = rhs.userWallet {
+                return lhsWallet.amountInCurrentFiat > rhsWallet.amountInCurrentFiat
+            } else if lhs.userWallet != nil || rhs.userWallet != nil {
+                return false
+            } else {
+                return lhs.token.name < rhs.token.name
             }
+        }
     }
 }
