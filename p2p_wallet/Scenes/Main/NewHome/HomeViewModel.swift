1653beda1c5d3004b3d938586c76027af87671b2
Tran Giang Long <trangianglong4003@gmail.com>
2023-03-10 17:43:49 +0700

    fix(wormhole): home screen

--- p2p_wallet/Scenes/Main/NewHome/HomeViewModel.swift
+++ p2p_wallet/Scenes/Main/NewHome/HomeViewModel.swift
@@ -123,7 +123,7 @@ private extension HomeViewModel {
                 guard let self else { return }
 
                 let hasAnyTokenWithPositiveBalance =
-                    solanaState.value.contains(where: { account in (account.data.lamports ?? 0) > 0 }) &&
+                    solanaState.value.contains(where: { account in (account.data.lamports ?? 0) > 0 }) ||
                     ethereumState.value.contains(where: { account in account.balance > 0 })
 
                 // TODO: Bad place
