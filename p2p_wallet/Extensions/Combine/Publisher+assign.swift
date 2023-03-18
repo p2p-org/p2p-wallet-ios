f1cf87ff924e814b309cacf1125c2ebf675e95de
Chung Tran <bigearsenal@gmail.com>
2023-03-18 08:24:57 +0700

    feat: remove warning

--- p2p_wallet/Extensions/Combine/Publisher+assign.swift
+++ p2p_wallet/Extensions/Combine/Publisher+assign.swift
@@ -2,7 +2,6 @@ import Foundation
 import Combine
 
 extension Publisher where Failure == Never {
-    @available(*, deprecated, renamed: "assign(to:)")
     func assign<Root: AnyObject>(to keyPath: ReferenceWritableKeyPath<Root, Output>, on root: Root) -> AnyCancellable {
         sink { [weak root] in
             root?[keyPath: keyPath] = $0
