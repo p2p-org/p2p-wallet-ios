8c76578f68fec277eba8032d48de275720d73ddb
Chung Tran <bigearsenal@gmail.com>
2023-03-18 08:03:30 +0700

    feat: remove extra extension

--- p2p_wallet/Scenes/CreateOrRestoreWallet/CreateWallet/Steps/EnterPhoneNumber/EnterPhoneNumberViewModel.swift
+++ p2p_wallet/Scenes/CreateOrRestoreWallet/CreateWallet/Steps/EnterPhoneNumber/EnterPhoneNumberViewModel.swift
@@ -296,11 +296,3 @@ private extension String {
         self.filter("0123456789+".contains)
     }
 }
-
-extension Publisher where Failure == Never {
-    func assign<Root: AnyObject>(to keyPath: ReferenceWritableKeyPath<Root, Output>, on root: Root) -> AnyCancellable {
-        sink { [weak root] in
-            root?[keyPath: keyPath] = $0
-        }
-    }
-}
