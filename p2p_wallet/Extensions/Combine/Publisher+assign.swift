724965db4e0186f85532134222dc565cdcc6510b
Chung Tran <bigearsenal@gmail.com>
2023-03-18 08:21:58 +0700

    fix: typo

--- p2p_wallet/Extensions/Combine/Publisher+assign.swift
+++ p2p_wallet/Extensions/Combine/Publisher+assign.swift
@@ -1,7 +1,8 @@
 import Foundation
-import Publisher
+import Combine
 
 extension Publisher where Failure == Never {
+    @available(*, deprecated, renamed: "assign(to:)")
     func assign<Root: AnyObject>(to keyPath: ReferenceWritableKeyPath<Root, Output>, on root: Root) -> AnyCancellable {
         sink { [weak root] in
             root?[keyPath: keyPath] = $0
