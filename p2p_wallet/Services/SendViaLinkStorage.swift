5279e1cb7d303620c6986e412851689f15fc80e8
Chung Tran <bigearsenal@gmail.com>
2023-03-21 15:14:27 +0700

    refactor: SendViaLinkStorage

--- p2p_wallet/Services/SendViaLinkStorage.swift
+++ p2p_wallet/Services/SendViaLinkStorage.swift
@@ -2,6 +2,9 @@ import Foundation
 import Resolver
 import SolanaSwift
 import SwiftyUserDefaults
+import Combine
+
+// MARK: - TransactionInfo
 
 struct SendViaLinkTransactionInfo: Codable {
     let amount: Double
@@ -10,23 +13,68 @@ struct SendViaLinkTransactionInfo: Codable {
     let seed: String
 }
 
+// MARK: - Protocols
+
+/// Storage that handle SendViaLinkTransactions
 protocol SendViaLinkStorage {
+    
+    /// Save transaction to storage
     @discardableResult
     func save(transaction: SendViaLinkTransactionInfo) -> Bool
+    
+    /// Remove transaction with seed from storage
     @discardableResult
     func remove(seed: String) -> Bool
+    
+    /// Get all transactions
     func getTransactions() -> [SendViaLinkTransactionInfo]
+    
+    /// Transaction publisher to handle transaction
+    var transactionsPublisher: AnyPublisher<[SendViaLinkTransactionInfo], Never> { get }
 }
 
 final class SendViaLinkStorageImpl: SendViaLinkStorage {
+    // MARK: - Dependencies
+    
     @Injected private var userWalletManager: UserWalletManager
+    
+    // MARK: - Properties
+    
+    private var subscription: DefaultsDisposable?
+    private let locker = NSLock()
+    private let transactionsSubject = CurrentValueSubject<[SendViaLinkTransactionInfo], Never>([])
+    
+    // MARK: - Computed properties
     
     var userPubkey: String? {
         userWalletManager.wallet?.account.publicKey.base58EncodedString
     }
     
+    var transactionsPublisher: AnyPublisher<[SendViaLinkTransactionInfo], Never> {
+        transactionsSubject.eraseToAnyPublisher()
+    }
+    
+    // MARK: - Initializer
+
+    init() {
+        // retrieve transaction
+        transactionsSubject.send(getTransactions())
+
+        // observe changes
+        subscription = Defaults.observe(\.sendViaLinkTransactions) { [weak self] transactions in
+            guard let userPubkey = self?.userPubkey,
+                  let newValue = transactions.newValue?[userPubkey]
+            else { return }
+            self?.transactionsSubject.send(newValue)
+        }
+    }
+    
+    // MARK: - Actions
+    
     @discardableResult
     func save(transaction: SendViaLinkTransactionInfo) -> Bool {
+        locker.lock(); defer { locker.unlock() }
+        
         // get seeds
         var seeds = getTransactions()
         
@@ -43,6 +91,8 @@ final class SendViaLinkStorageImpl: SendViaLinkStorage {
     }
     
     func remove(seed: String) -> Bool {
+        locker.lock(); defer { locker.unlock() }
+        
         // get seeds from keychain
         var seeds = getTransactions()
         
@@ -54,6 +104,8 @@ final class SendViaLinkStorageImpl: SendViaLinkStorage {
     }
     
     func getTransactions() -> [SendViaLinkTransactionInfo] {
+        locker.lock(); defer { locker.unlock() }
+        
         guard let userPubkey else {
             return []
         }
