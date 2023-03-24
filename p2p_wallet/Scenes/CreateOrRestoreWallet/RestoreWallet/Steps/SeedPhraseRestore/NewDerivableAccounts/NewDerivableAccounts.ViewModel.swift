880ce7415ed0da69b992051bee42ebc7d7ff9f32
Chung Tran <bigearsenal@gmail.com>
2023-03-24 10:20:22 +0700

    feat: allow user select wallet with index other than 0

--- p2p_wallet/Scenes/CreateOrRestoreWallet/RestoreWallet/Steps/SeedPhraseRestore/NewDerivableAccounts/NewDerivableAccounts.ViewModel.swift
+++ p2p_wallet/Scenes/CreateOrRestoreWallet/RestoreWallet/Steps/SeedPhraseRestore/NewDerivableAccounts/NewDerivableAccounts.ViewModel.swift
@@ -15,9 +15,9 @@ protocol NewDrivableAccountsViewModelType {
     var selectedDerivablePathPublisher: AnyPublisher<DerivablePath, Never> { get }
 
     func getCurrentSelectedDerivablePath() -> DerivablePath
-    func chooseDerivationPath()
-    func selectDerivationPath(_ path: DerivablePath)
-    func restoreAccount()
+    func navigateToSelectDerivableType()
+    func selectDerivablePath(_ path: DerivablePath)
+    func selectDerivableType(_ type: DerivablePath.DerivableType)
     func onBack()
 }
 
@@ -78,19 +78,28 @@ extension NewDerivableAccounts.ViewModel: NewDrivableAccountsViewModelType {
         selectedDerivablePath
     }
 
-    func chooseDerivationPath() {
-        navigatableScene = .selectDerivationPath
+    func navigateToSelectDerivableType() {
+        navigatableScene = .selectDerivableType
+    }
+    
+    func selectDerivablePath(_ path: DerivablePath) {
+        selectedDerivablePath = path
+        restoreAccount()
     }
 
-    func selectDerivationPath(_ path: DerivablePath) {
-        selectedDerivablePath = path
+    func selectDerivableType(_ type: DerivablePath.DerivableType) {
+        accountsListViewModel.cancelRequest()
+        accountsListViewModel.setDerivableType(type)
+        accountsListViewModel.reload()
     }
 
     func onBack() {
         coordinatorIO.back.send()
     }
 
-    func restoreAccount() {
+    // MARK: - Helpers
+
+    private func restoreAccount() {
         // cancel any requests
         accountsListViewModel.cancelRequest()
 
