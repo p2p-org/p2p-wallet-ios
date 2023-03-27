a9d65cea4a52122fcca5ff1509eb50f4d6a5ad25
Chung Tran <bigearsenal@gmail.com>
2023-03-28 04:41:44 +0700

    feat: onWeak assign

--- p2p_wallet/Extensions/Combine/Publisher+assign.swift
+++ p2p_wallet/Extensions/Combine/Publisher+assign.swift
@@ -2,7 +2,7 @@ import Foundation
 import Combine
 
 extension Publisher where Failure == Never {
-    func assign<Root: AnyObject>(to keyPath: ReferenceWritableKeyPath<Root, Output>, on root: Root) -> AnyCancellable {
+    func assign<Root: AnyObject>(to keyPath: ReferenceWritableKeyPath<Root, Output>, onWeak root: Root) -> AnyCancellable {
         sink { [weak root] in
             root?[keyPath: keyPath] = $0
         }
