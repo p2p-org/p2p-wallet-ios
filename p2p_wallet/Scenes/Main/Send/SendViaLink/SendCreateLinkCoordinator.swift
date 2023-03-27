ff557782b9bd0a78245766ba274e415bb77fe540
Chung Tran <bigearsenal@gmail.com>
2023-03-27 22:21:09 +0700

    Revert "fix: send transaction"
    This reverts commit c313ebf15554d5690cfb095f485b21ac20690d5d.

--- p2p_wallet/Scenes/Main/Send/SendViaLink/SendCreateLinkCoordinator.swift
+++ p2p_wallet/Scenes/Main/Send/SendViaLink/SendCreateLinkCoordinator.swift
@@ -32,38 +32,24 @@ final class SendCreateLinkCoordinator: Coordinator<SendCreateLinkCoordinator.Res
     }
     
     func bind() {
-        Task {
-            do {
-                _ = try await transaction.createRequest()
-                await MainActor.run { [weak self] in
+        let transactionHandler = Resolver.resolve(TransactionHandlerType.self)
+        let index = transactionHandler.sendTransaction(transaction)
+        
+        transactionHandler.observeTransaction(transactionIndex: index)
+            .compactMap {$0}
+            .filter {
+                $0.status.error != nil || $0.status.isFinalized || ($0.status.numberOfConfirmations ?? 0) > 0
+            }
+            .prefix(1)
+            .receive(on: RunLoop.main)
+            .sink { [weak self] tx in
+                if tx.status.error != nil {
+                    self?.showErrorView()
+                } else {
                     self?.showSendLinkCreatedView()
                 }
-            } catch {
-                await MainActor.run { [weak self] in
-                    self?.showErrorView()
-                }
             }
-        }
-        
-        // TODO: - TransactionHandler did not return the transaction, why?
-//        let transactionHandler = Resolver.resolve(TransactionHandlerType.self)
-//        let index = transactionHandler.sendTransaction(transaction)
-//
-//        transactionHandler.observeTransaction(transactionIndex: index)
-//            .compactMap {$0}
-//            .filter {
-//                $0.status.error != nil || $0.status.isFinalized || ($0.status.numberOfConfirmations ?? 0) > 0
-//            }
-//            .prefix(1)
-//            .receive(on: RunLoop.main)
-//            .sink { [weak self] tx in
-//                if tx.status.error != nil {
-//                    self?.showErrorView()
-//                } else {
-//                    self?.showSendLinkCreatedView()
-//                }
-//            }
-//            .store(in: &subscriptions)
+            .store(in: &subscriptions)
     }
     
     // MARK: - Builder
