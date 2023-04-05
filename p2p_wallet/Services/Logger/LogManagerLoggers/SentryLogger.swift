aa10ba4a608f5036dad832102dfdb64e18aaa985
Chung Tran <bigearsenal@gmail.com>
2023-04-05 11:39:18 +0700

    fix: logger

--- p2p_wallet/Services/Logger/LogManagerLoggers/SentryLogger.swift
+++ p2p_wallet/Services/Logger/LogManagerLoggers/SentryLogger.swift
@@ -1,15 +1,15 @@
-import FeeRelayerSwift
 import Foundation
-import KeyAppKitLogger
 import Sentry
+import FeeRelayerSwift
+import KeyAppKitLogger
 import SolanaSwift
 import LoggerSwift
 
 class SentryLogger: LogManagerLogger {
     private var queue = DispatchQueue(label: "SentryLogger", qos: .utility)
-
+    
     var supportedLogLevels: [LogLevel] = [.error]
-
+    
     func log(event: String, logLevel: LogLevel, data: String?) {
         queue.sync {
             let sentryEvent = Event(level: sentryLevel(logLevel: logLevel))
@@ -22,9 +22,9 @@ class SentryLogger: LogManagerLogger {
             }
         }
     }
-
+    
     // MARK: -
-
+    
     private func sentryLevel(logLevel: LogLevel) -> SentryLevel {
         switch logLevel {
         case .info:
@@ -56,9 +56,9 @@ extension SentryLogger: SolanaSwiftLogger {
         case .debug:
             newLogLevel = .debug
         }
-
+        
         guard supportedLogLevels.contains(newLogLevel) else { return }
-
+        
         log(event: event, logLevel: newLogLevel, data: data)
     }
 }
@@ -76,9 +76,9 @@ extension SentryLogger: FeeRelayerSwiftLogger {
         case .debug:
             newLogLevel = .debug
         }
-
+        
         guard supportedLogLevels.contains(newLogLevel) else { return }
-
+        
         log(event: event, logLevel: newLogLevel, data: data)
     }
 }
@@ -96,105 +96,9 @@ extension SentryLogger: KeyAppKitLoggerType {
         case .debug:
             newLogLevel = .debug
         }
-
+        
         guard supportedLogLevels.contains(newLogLevel) else { return }
-
-        log(event: event, logLevel: newLogLevel, data: data)
-    }
-}
-
-class LoggerSwiftLogger: LogManagerLogger {
-    private var queue = DispatchQueue(label: "LoggerSwiftLogger", qos: .utility)
-
-    var supportedLogLevels: [LogLevel] = [.error, .info, .request, .response, .event, .warning, .debug]
-
-    func log(event: String, logLevel: LogLevel, data: String?) {
-        queue.async { [unowned self] in
-            LoggerSwift.Logger.log(
-                event: mapEventLogLeverToLoggerSwiftEvent(logLevel),
-                message: event + " " + (data ?? "")
-            )
-        }
-    }
-    
-    private func mapEventLogLeverToLoggerSwiftEvent(
-        _ event: LogLevel
-    ) -> LoggerSwift.LoggerEvent {
-        switch event {
-        case .info:
-            return .info
-        case .error:
-            return .error
-        case .request:
-            return .request
-        case .response:
-            return .response
-        case .event:
-            return .event
-        case .warning:
-            return .warning
-        case .debug:
-            return .debug
-        }
-    }
-}
-
-extension LoggerSwiftLogger: SolanaSwiftLogger {
-    func log(event: String, data: String?, logLevel: SolanaSwiftLoggerLogLevel) {
-        var newLogLevel: LogLevel = .info
-        switch logLevel {
-        case .info:
-            newLogLevel = .info
-        case .error:
-            newLogLevel = .error
-        case .warning:
-            newLogLevel = .warning
-        case .debug:
-            newLogLevel = .debug
-        }
-
-        guard supportedLogLevels.contains(newLogLevel) else { return }
-
-        log(event: event, logLevel: newLogLevel, data: data)
-    }
-}
-
-extension LoggerSwiftLogger: FeeRelayerSwiftLogger {
-    func log(event: String, data: String?, logLevel: FeeRelayerSwiftLoggerLogLevel) {
-        var newLogLevel: LogLevel = .info
-        switch logLevel {
-        case .info:
-            newLogLevel = .info
-        case .error:
-            newLogLevel = .error
-        case .warning:
-            newLogLevel = .warning
-        case .debug:
-            newLogLevel = .debug
-        }
-
-        guard supportedLogLevels.contains(newLogLevel) else { return }
-
-        log(event: event, logLevel: newLogLevel, data: data)
-    }
-}
-
-extension LoggerSwiftLogger: KeyAppKitLoggerType {
-    func log(event: String, data: String?, logLevel: KeyAppKitLoggerLogLevel) {
-        var newLogLevel: LogLevel = .info
-        switch logLevel {
-        case .info:
-            newLogLevel = .info
-        case .error:
-            newLogLevel = .error
-        case .warning:
-            newLogLevel = .warning
-        case .debug:
-            newLogLevel = .debug
-        }
-
-        guard supportedLogLevels.contains(newLogLevel) else { return }
-
+        
         log(event: event, logLevel: newLogLevel, data: data)
     }
 }
