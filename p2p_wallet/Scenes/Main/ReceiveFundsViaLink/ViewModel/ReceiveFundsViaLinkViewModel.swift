996da1746b8c83fc82937c73fed01a9229c007c9
Chung Tran <bigearsenal@gmail.com>
2023-04-11 16:21:54 +0700

    fix: native solana token

--- p2p_wallet/Scenes/Main/ReceiveFundsViaLink/ViewModel/ReceiveFundsViaLinkViewModel.swift
+++ p2p_wallet/Scenes/Main/ReceiveFundsViaLink/ViewModel/ReceiveFundsViaLinkViewModel.swift
@@ -184,18 +184,19 @@ final class ReceiveFundsViaLinkViewModel: BaseViewModel, ObservableObject {
         Task {
             do {
                 let claimableToken = try await self.sendViaLinkDataService.getClaimableTokenInfo(url: self.url)
-                let token = try await self.tokensRepository.getTokensList(useCache: true)
-                    .first {
-                        // Native SOL token
-                        if claimableToken.mintAddress == PublicKey.wrappedSOLMint.base58EncodedString {
-                            return $0.isNativeSOL
-                        }
-                        
-                        // Other tokens
-                        return $0.address == claimableToken.mintAddress
-                    }
-
-                guard let token = token else { return }
+                
+                // Native solana token
+                let token: Token
+                if claimableToken.mintAddress == PublicKey.wrappedSOLMint.base58EncodedString {
+                    token = .nativeSolana
+                }
+                
+                // Other spl tokens
+                else {
+                    token = try await self.tokensRepository.getTokensList(useCache: true)
+                        .first { $0.address == claimableToken.mintAddress } ??
+                        .unsupported(mint: claimableToken.mintAddress, decimals: claimableToken.decimals)
+                }
 
                 let cryptoAmount = claimableToken.lamports
                     .convertToBalance(decimals: claimableToken.decimals)
