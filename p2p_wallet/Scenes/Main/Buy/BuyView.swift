19075fd39cb1286aafd8cb9f96e7e7fba272cb35
Alexey Sidorov <ody344@gmail.com>
2023-04-28 11:09:17 +0200

    Added missing assets

--- p2p_wallet/Scenes/Main/Buy/BuyView.swift
+++ p2p_wallet/Scenes/Main/Buy/BuyView.swift
@@ -270,14 +270,14 @@ struct BuyView: View, KeyboardVisibilityReadable {
                 }
                 Spacer()
                 if viewModel?.selectedPayment == item.type {
-                    Image("checkmark-filled")
+                    Image(uiImage: .checkmarkFilled)
                         .resizable()
                         .scaledToFill()
                         .frame(width: 25, height: 25)
                         .padding(.trailing, 13)
                         .padding(.top, -3)
                 } else {
-                    Image("checkmark-empty")
+                    Image(uiImage: .checkmarkEmpty)
                         .resizable()
                         .scaledToFill()
                         .frame(width: 25, height: 25)
