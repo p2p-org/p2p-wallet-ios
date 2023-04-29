8a7d78f33b261e10544c662d8655f880568d53b0
Chung Tran <bigearsenal@gmail.com>
2023-04-29 16:30:45 +0700

    fix: non-m1 mac

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -4,6 +4,24 @@ brew install xcodegen swiftgen swiftlint swiftformat
 # Install periphery
 brew install peripheryapp/periphery/periphery
 
+# Resolve homebrew installation folder for non-m1 mac (ci)
+# For non-m1, homebrew tools would be located in /usr/local/bin
+# So we create symbolic links from /usr/local/bin to /opt/homebrew/bin for each tool
+if [[ ! $(uname -m) == 'arm64' ]]; then
+	# log
+	echo "Create a symbolic link from /usr/local/bin to /opt/homebrew/bin"
+
+	# create /opt/homebrew/bin if not exists
+	mkdir -p /opt/homebrew/bin
+
+	# Create symbolic links
+	ln -s /usr/local/bin/xcodegen /opt/homebrew/bin/xcodegen
+	ln -s /usr/local/bin/swiftgen /opt/homebrew/bin/swiftgen
+	ln -s /usr/local/bin/swiftlint /opt/homebrew/bin/swiftlint
+	ln -s /usr/local/bin/swiftformat /opt/homebrew/bin/swiftformat
+	ln -s /usr/local/bin/periphery /opt/homebrew/bin/periphery
+fi
+
 # Run xcodegen
 xcodegen
 
