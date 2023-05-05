b110ce895f9487f86f7946e129de9b3b1733ac3e
Ivan Babich <babichivanya@gmail.com>
2023-05-05 15:17:09 +0300

    [PWN-8345] Filtration of SCAM NFTs

--- p2p_wallet/Scenes/Main/NewHome/Subview/AccountList/HomeAccountsViewModel+Helper.swift
+++ p2p_wallet/Scenes/Main/NewHome/Subview/AccountList/HomeAccountsViewModel+Helper.swift
@@ -2,10 +2,15 @@ import Foundation
 import SolanaSwift
 import KeyAppBusiness
 
+// As NFT Address
+private let scamAddresses: Set<String> = [
+    "XzR7CUMqhDBzbAm4aUNvwhVCxjWGn1KEvqTp3Y8fFCD"
+]
+
 extension Wallet {
     // Hide NFT TODO: $0.token.supply == 1 is also a condition for NFT but skipped atm
     var isNFTToken: Bool {
-        token.decimals == 0
+        (token.decimals == 0) || scamAddresses.contains(token.address)
     }
 }
 
