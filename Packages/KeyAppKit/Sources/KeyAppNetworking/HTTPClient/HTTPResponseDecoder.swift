b6063d8bb79a48ebede33bb8c5c21e20ae6e35c5
Chung Tran <bigearsenal@gmail.com>
2023-05-19 15:30:40 +0700

    feat: add documentation

--- Packages/KeyAppKit/Sources/KeyAppNetworking/HTTPClient/HTTPResponseDecoder.swift
+++ Packages/KeyAppKit/Sources/KeyAppNetworking/HTTPClient/HTTPResponseDecoder.swift
@@ -2,18 +2,39 @@ import Foundation
 
 /// Decoder for response from `HTTPClient`
 public protocol HTTPResponseDecoder {
+    /// Decode data and response to needed type
+    /// - Parameters:
+    ///   - type: object type to be decoded to
+    ///   - data: data to decode
+    ///   - response: httpURLResponse from network
+    /// - Returns: object of predefined type
     func decode<T: Decodable>(_ type: T.Type, data: Data, httpURLResponse response: HTTPURLResponse) throws -> T
 }
 
 /// ResponseDecoder for JSON type
 public class JSONResponseDecoder: HTTPResponseDecoder {
     
+    // MARK: - Properties
+    
+    /// Default native `JSONDecoder`
     private let jsonDecoder: JSONDecoder
     
+    // MARK: - Initializers
+    
+    /// `JSONResponseDecoder` initializer
+    /// - Parameter jsonDecoder: Default native `JSONDecoder`
     public init(jsonDecoder: JSONDecoder = .init()) {
         self.jsonDecoder = jsonDecoder
     }
     
+    // MARK: - Methods
+
+    /// Decode data and response to needed type
+    /// - Parameters:
+    ///   - type: object type to be decoded to
+    ///   - data: data to decode
+    ///   - response: httpURLResponse from network
+    /// - Returns: object of predefined type
     public func decode<T: Decodable>(_ type: T.Type, data: Data, httpURLResponse response: HTTPURLResponse) throws -> T
     {
         switch response.statusCode {
