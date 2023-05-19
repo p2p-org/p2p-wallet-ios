af047e62e4be28e0820eb103b8c19cc1fcaad042
Chung Tran <bigearsenal@gmail.com>
2023-05-19 22:30:44 +0700

    fix: some fixes

--- Packages/KeyAppKit/Sources/Wormhole/Model/WormholeStatus.swift
+++ Packages/KeyAppKit/Sources/Wormhole/Model/WormholeStatus.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public enum WormholeStatus: String,  Codable, Hashable {
+public enum WormholeStatus: String, Codable, Hashable {
     case failed
     case pending
     case expired
