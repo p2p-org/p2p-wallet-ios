0d94a3e5f6d54338bc71216e005cdf8a22d24fa4
Alexey Sidorov <ody344@gmail.com>
2023-05-19 10:59:29 +0200

    format fix

--- p2p_wallet/Scenes/CreateOrRestoreWallet/CreateWallet/CreateWalletAlertLoggerErrorMessage.swift
+++ p2p_wallet/Scenes/CreateOrRestoreWallet/CreateWallet/CreateWalletAlertLoggerErrorMessage.swift
@@ -2,8 +2,14 @@ import Foundation
 
 struct CreateWalletAlertLoggerErrorMessage: Codable {
     let error: String
+    var platform: String = "iOS \(UIDevice.current.systemVersion)"
+    var appVersion: String = AppInfo.appVersionDetail
+    var timestamp: String = "\(Int64(Date().timeIntervalSince1970 * 1000))"
 
     enum CodingKeys: String, CodingKey {
+        case platform
+        case appVersion
+        case timestamp
         case error = "web3_error"
     }
 }
--- p2p_wallet/Services/Logger/LogManagerLoggers/AlertLogger/Models/CreateNameAlertLoggerErrorMessage.swift
+++ p2p_wallet/Services/Logger/LogManagerLoggers/AlertLogger/Models/CreateNameAlertLoggerErrorMessage.swift
@@ -3,8 +3,14 @@ import Foundation
 struct CreateNameAlertLoggerErrorMessage: Codable {
     let name: String
     let error: String
+    var platform: String = "iOS \(UIDevice.current.systemVersion)"
+    var appVersion: String = AppInfo.appVersionDetail
+    var timestamp: String = "\(Int64(Date().timeIntervalSince1970 * 1000))"
 
     enum CodingKeys: String, CodingKey {
+        case platform
+        case appVersion
+        case timestamp
         case name
         case error = "name_service_error"
     }
