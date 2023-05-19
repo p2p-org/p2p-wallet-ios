298797994b8a425ac9ed37211104403d779f3c3a
Chung Tran <bigearsenal@gmail.com>
2023-05-19 14:37:00 +0700

    fix: some fixes

--- Packages/KeyAppKit/Sources/KeyAppNetworking/JsonRpc/JsonRpcRequestDto.swift
+++ Packages/KeyAppKit/Sources/KeyAppNetworking/JsonRpc/JsonRpcRequestDto.swift
@@ -11,17 +11,33 @@ public struct JsonRpcRequestDto<T: Encodable>: Encodable {
     let jsonrpc: String
     let id: String
     let method: String
-    let params: [T]
+    let params: [T]?
     
     public init(
         jsonrpc: String = "2.0",
         id: String = UUID().uuidString,
         method: String,
-        params: [T]
+        params: [T]? = nil
     ) {
         self.jsonrpc = jsonrpc
         self.id = id
         self.method = method
         self.params = params
+    }
+}
+
+
+public extension JsonRpcRequestDto where T == String { /*T == String, or what ever confirmed to Encodable to fix ambiguous type*/
+    init(
+        jsonrpc: String = "2.0",
+        id: String = UUID().uuidString,
+        method: String
+    ) {
+        self.init(
+            jsonrpc: jsonrpc,
+            id: id,
+            method: method,
+            params: nil
+        )
     }
 }
