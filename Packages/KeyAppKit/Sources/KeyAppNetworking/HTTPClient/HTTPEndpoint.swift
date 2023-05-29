4ce0621fb0c61db84b17bae44682c58ed9d28812
Chung Tran <bigearsenal@gmail.com>
2023-05-29 09:46:14 +0700

    feat: Add KeyAppNetworking

---
+++ Packages/KeyAppKit/Sources/KeyAppNetworking/HTTPClient/HTTPEndpoint.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+/// Endpoint for specific `HTTPClient` network call
+public protocol HTTPEndpoint {
+    var baseURL: String { get }
+    var path: String { get }
+    var method: HTTPMethod { get }
+    var header: [String: String] { get }
+    var body: String? { get }
+}
