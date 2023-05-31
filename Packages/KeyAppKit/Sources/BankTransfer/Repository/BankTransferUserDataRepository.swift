e3ed90f826769139b849b46c1366c18e2d4aa948
Chung Tran <bigearsenal@gmail.com>
2023-05-31 14:07:03 +0700

    feat: getKYC

--- Packages/KeyAppKit/Sources/BankTransfer/Repository/BankTransferUserDataRepository.swift
+++ Packages/KeyAppKit/Sources/BankTransfer/Repository/BankTransferUserDataRepository.swift
@@ -8,6 +8,8 @@ public protocol BankTransferUserDataRepository {
     func createUser(registrationData: BankTransferRegistrationData) async throws -> StrigaCreateUserResponse
     func updateUser(registrationData: BankTransferRegistrationData) async throws
     func resendSMS(userId: String) async throws
+    
+    func getKYCToken(userId: String) async throws -> String
 
     func updateUserLocally(registrationData: BankTransferRegistrationData) async throws
     func clearCache() async
