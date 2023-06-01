8fe96d4ef6a1b679a9a4a9e56c6b5d4876b53b27
Alexey Sidorov <ody344@gmail.com>
2023-06-01 08:36:41 +0200

    OTP

--- Packages/KeyAppKit/Sources/Onboarding/Utils.swift
+++ Packages/KeyAppKit/Sources/Onboarding/Utils.swift
@@ -9,7 +9,7 @@ public typealias None = Void
 public class Wrapper<T: Codable & Equatable>: Codable, Equatable {
     public internal(set) var value: T
 
-    internal init(_ value: T) { self.value = value }
+    public init(_ value: T) { self.value = value }
 
     public static func == (lhs: Wrapper<T>, rhs: Wrapper<T>) -> Bool {
         lhs.value == rhs.value
