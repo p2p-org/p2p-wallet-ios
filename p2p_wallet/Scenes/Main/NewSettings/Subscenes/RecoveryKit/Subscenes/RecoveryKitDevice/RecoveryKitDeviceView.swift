2af31a014c04efc65facdbf2972a26d02650378e
Giang Long Tran <darklong@Giangs-MacBook-Pro.local>
2023-06-04 18:51:21 +0700

    feat: add DeviceShareMigrationView

---
+++ p2p_wallet/Scenes/Main/NewSettings/Subscenes/RecoveryKit/Subscenes/RecoveryKitDevice/RecoveryKitDeviceView.swift
@@ -0,0 +1,20 @@
+//
+//  RecoveryKitDeviceView.swift
+//  p2p_wallet
+//
+//  Created by Giang Long Tran on 04/06/2023.
+//
+
+import SwiftUI
+
+struct RecoveryKitDeviceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecoveryKitDeviceView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecoveryKitDeviceView()
+    }
+}
