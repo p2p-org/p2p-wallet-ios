3acb945f87d98f5db8ba47c20c1febe32caa8218
Tran Giang Long <trangianglong4003@gmail.com>
2023-06-06 16:02:33 +0700

    feat(web3auth): improve

---
+++ p2p_wallet/Scenes/DebugMenu/View/DebugTextField.swift
@@ -0,0 +1,27 @@
+//
+//  DebugTextField.swift
+//  p2p_wallet
+//
+//  Created by Giang Long Tran on 06.06.2023.
+//
+
+import SwiftUI
+
+struct DebugTextField: View {
+    let title: String
+    let content: Binding<String>
+
+    var body: some View {
+        HStack {
+            Text(title)
+            TextEditor(text: content)
+        }
+    }
+}
+
+
+struct DebugTextField_Previews: PreviewProvider {
+    static var previews: some View {
+        DebugTextField(title: "Property", content: .constant("Value"))
+    }
+}
---
+++ p2p_wallet/Scenes/DebugMenu/View/OnboardingDebugView.swift
@@ -0,0 +1,63 @@
+//
+//  OnboardingDebugView.swift
+//  p2p_wallet
+//
+//  Created by Giang Long Tran on 06.06.2023.
+//
+
+import Resolver
+import SwiftUI
+
+struct OnboardingDebugView: View {
+    @ObservedObject private var onboardingConfig = OnboardingConfig.shared
+
+    var body: some View {
+        List {
+            Section(header: Text("Info")) {
+                DebugText(title: "Network", value: onboardingConfig.torusNetwork)
+                DebugText(title: "GG Verifier", value: onboardingConfig.torusGoogleVerifier)
+                DebugText(title: "GG Subverifier", value: onboardingConfig.torusGoogleSubVerifier)
+                DebugText(title: "AP Verifier", value: onboardingConfig.torusAppleVerifier)
+            }
+
+            Section(header: Text("Onboarding configurations")) {
+                DebugTextField(title: "Torus:", content: $onboardingConfig.torusEndpoint)
+                DebugTextField(title: "OTP Resend", content: $onboardingConfig.enterOTPResend)
+            }
+
+            Section(header: Text("Mocked device share")) {
+                Toggle("Enabled", isOn: $onboardingConfig.isDeviceShareMocked)
+                    .valueChanged(value: onboardingConfig.isDeviceShareMocked) { newValue in
+                        onboardingConfig.isDeviceShareMocked = newValue
+                    }
+                DebugTextField(title: "Share:", content: $onboardingConfig.mockDeviceShare)
+                    .disabled(!onboardingConfig.isDeviceShareMocked)
+                    .foregroundColor(!onboardingConfig.isDeviceShareMocked ? Color.gray : Color.black)
+
+                HStack {
+                    Text("Delete current share")
+                    Spacer()
+                    Button {
+                        do {
+                            try Resolver.resolve(KeychainStorage.self).save(deviceShare: "")
+                        } catch { print(error) }
+                    } label: { Text("Delete") }
+                }
+
+                HStack {
+                    Text("Delete last progress")
+                    Spacer()
+                    Button {
+                        Resolver.resolve(OnboardingService.self).lastState = nil
+                    } label: { Text("Delete") }
+                }
+            }
+        }.navigationTitle("Web3Auth")
+    }
+}
+
+struct OnboardingDebugView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingDebugView()
+    }
+}
