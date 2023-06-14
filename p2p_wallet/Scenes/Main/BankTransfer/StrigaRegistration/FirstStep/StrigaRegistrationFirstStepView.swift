3cc53c648c6bed6115b22ec9235402655ad03dd2
Alexey Sidorov <ody344@gmail.com>
2023-06-14 22:12:09 +0200

    UI fixes

--- p2p_wallet/Scenes/Main/BankTransfer/StrigaRegistration/FirstStep/StrigaRegistrationFirstStepView.swift
+++ p2p_wallet/Scenes/Main/BankTransfer/StrigaRegistration/FirstStep/StrigaRegistrationFirstStepView.swift
@@ -18,126 +18,116 @@ struct StrigaRegistrationFirstStepView: View {
         if viewModel.isLoading {
             ProgressView()
         } else {
-            content
-        }
-    }
-    
-    var content: some View {
-        List {
-            InfoView(appearance: .credentials)
-                .listRowBackground(Color(Asset.Colors.smoke.color))
-                .listRowInsets(EdgeInsets.infoInset)
-            
-            contactsSection
-            credentialsSection
-            dateOfBirthSection
+            ZStack(alignment: .bottom) {
+                Color(Asset.Colors.smoke.color)
+                    .edgesIgnoringSafeArea(.all)
 
-            NewTextButton(
-                title: viewModel.actionTitle.uppercaseFirst,
-                style: .primaryWhite,
-                isEnabled: viewModel.isDataValid,
-                trailing: viewModel.isDataValid ? .arrowForward : nil,
-                action: viewModel.actionPressed.send
-            )
-            .listRowInsets(EdgeInsets.infoInset)
-            .listRowBackground(Color(Asset.Colors.smoke.color))
+                ScrollView {
+                    VStack(alignment: .leading, spacing: 20) {
+                        InfoView(appearance: .credentials)
+                            .padding(.top, 22)
+
+                        contactsSection
+                        ListSpacerCellView(height: 10, backgroundColor: .clear)
+                        credentialsSection
+                        ListSpacerCellView(height: 10, backgroundColor: .clear)
+                        dateOfBirthSection
+                        ListSpacerCellView(height: 28, backgroundColor: .clear)
+
+                        NewTextButton(
+                            title: viewModel.actionTitle.uppercaseFirst,
+                            style: .primaryWhite,
+                            isEnabled: viewModel.isDataValid,
+                            trailing: viewModel.isDataValid ? .arrowForward : nil,
+                            action: viewModel.actionPressed.send
+                        )
+                            .padding(.bottom, 20)
+                    }
+                        .padding(.horizontal, 16)
+                }
+            }
         }
-        .listStyle(.insetGrouped)
-        .modifier(ListBackgroundModifier(separatorColor: .clear))
-        .background(
-            Color(asset: Asset.Colors.smoke).ignoresSafeArea()
-        )
     }
 
     var contactsSection: some View {
-        Section(header: section(header: L10n.contacts)) {
-            Cell(
-                title: L10n.email,
-                status: viewModel.fieldsStatuses[.email]
-            ) {
-                TextField(placeholder: L10n.enter, text: $viewModel.email, isEnabled: false)
-            }
+        VStack(alignment: .leading, spacing: 14) {
+            StrigaRegistrationSectionView(title: L10n.contacts)
+                .padding(.horizontal, 9)
+            VStack(spacing: 23) {
+                Cell(
+                    title: L10n.email,
+                    status: viewModel.fieldsStatuses[.email]
+                ) {
+                    TextField(placeholder: L10n.enter, text: $viewModel.email, isEnabled: false)
+                }
 
-            Cell(
-                title: L10n.phoneNumber,
-                status: viewModel.fieldsStatuses[.phoneNumber]
-            ) {
-                StrigaRegistrationPhoneTextField(
-                    text: $viewModel.phoneNumber,
-                    phoneNumber: $viewModel.phoneNumberModel,
-                    country: $viewModel.selectedPhoneCountryCode,
-                    action: { [weak viewModel] in
-                        guard let viewModel else { return }
-                        viewModel.choosePhoneCountryCode.send(viewModel.selectedPhoneCountryCode)
-                    }
-                )
+                Cell(
+                    title: L10n.phoneNumber,
+                    status: viewModel.fieldsStatuses[.phoneNumber]
+                ) {
+                    StrigaRegistrationPhoneTextField(
+                        text: $viewModel.phoneNumber,
+                        phoneNumber: $viewModel.phoneNumberModel,
+                        country: $viewModel.selectedPhoneCountryCode,
+                        action: { [weak viewModel] in
+                            guard let viewModel else { return }
+                            viewModel.choosePhoneCountryCode.send(viewModel.selectedPhoneCountryCode)
+                        }
+                    )
+                }
             }
         }
-        .styled()
     }
 
     var credentialsSection: some View {
-        Section(header: section(header: L10n.credentials)) {
-            Cell(
-                title: L10n.firstName,
-                status: viewModel.fieldsStatuses[.firstName]
-            ) {
-                TextField(placeholder: L10n.enter, text: $viewModel.firstName)
-            }
+        VStack(alignment: .leading, spacing: 14) {
+            StrigaRegistrationSectionView(title: L10n.credentials)
+                .padding(.horizontal, 9)
+            VStack(spacing: 23) {
+                Cell(
+                    title: L10n.firstName,
+                    status: viewModel.fieldsStatuses[.firstName]
+                ) {
+                    TextField(placeholder: L10n.enter, text: $viewModel.firstName)
+                }
 
-            Cell(
-                title: L10n.surname,
-                status: viewModel.fieldsStatuses[.surname]
-            ) {
-                TextField(placeholder: L10n.enter, text: $viewModel.surname)
+                Cell(
+                    title: L10n.surname,
+                    status: viewModel.fieldsStatuses[.surname]
+                ) {
+                    TextField(placeholder: L10n.enter, text: $viewModel.surname)
+                }
             }
         }
-        .styled()
     }
 
     var dateOfBirthSection: some View {
-        Section(header: section(header: L10n.dateOfBirth)) {
-            Cell(
-                title: L10n.dateOfBirth,
-                status: viewModel.fieldsStatuses[.dateOfBirth]
-            ) {
-                StrigaRegistrationDateTextField(text: $viewModel.dateOfBirth)
-            }
+        VStack(alignment: .leading, spacing: 14) {
+            StrigaRegistrationSectionView(title: L10n.dateOfBirth)
+                .padding(.horizontal, 9)
+            VStack(spacing: 23) {
+                Cell(
+                    title: L10n.dateOfBirth,
+                    status: viewModel.fieldsStatuses[.dateOfBirth]
+                ) {
+                    StrigaRegistrationDateTextField(text: $viewModel.dateOfBirth)
+                }
 
-            Cell(
-                title: L10n.countryOfBirth,
-                status: viewModel.fieldsStatuses[.countryOfBirth]
-            ) {
-                DetailedButton(
-                    value: $viewModel.countryOfBirth,
-                    action: { [weak viewModel] in
-                        guard let viewModel else { return }
-                        viewModel.chooseCountry.send(viewModel.selectedCountryOfBirth)
-                    }
-                )
+                Cell(
+                    title: L10n.countryOfBirth,
+                    status: viewModel.fieldsStatuses[.countryOfBirth]
+                ) {
+                    DetailedButton(
+                        value: $viewModel.countryOfBirth,
+                        action: { [weak viewModel] in
+                            guard let viewModel else { return }
+                            viewModel.chooseCountry.send(viewModel.selectedCountryOfBirth)
+                        }
+                    )
+                }
             }
         }
-        .styled()
     }
-}
-
-private extension Section where Parent: View, Content: View, Footer == EmptyView {
-    func styled() -> some View {
-        return self.listRowBackground(Color(Asset.Colors.smoke.color))
-            .listRowInsets(EdgeInsets.rowInset)
-            .modifier(ListBackgroundModifier(separatorColor: .clear))
-    }
-}
-
-private extension StrigaRegistrationFirstStepView {
-    func section(header: String) -> some View {
-        Text(header).foregroundColor(Color(asset: Asset.Colors.night)).padding(.leading, 8)
-    }
-}
-
-private extension EdgeInsets {
-    static let rowInset = EdgeInsets(top: 0, leading: 0, bottom: 12, trailing: 0)
-    static let infoInset = EdgeInsets(top: 0, leading: 0, bottom: 20, trailing: 0)
 }
 
 struct StrigaRegistrationFirstStepView_Previews: PreviewProvider {
@@ -145,5 +135,19 @@ struct StrigaRegistrationFirstStepView_Previews: PreviewProvider {
         StrigaRegistrationFirstStepView(
             viewModel: StrigaRegistrationFirstStepViewModel(country: Country(name: "France", code: "FR", dialCode: "", emoji: "🇫🇷", alpha3Code: "FRA"))
         )
+    }
+}
+
+struct StrigaRegistrationSectionView: View {
+    var title: String
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            Spacer()
+            Text(title.uppercased())
+                .apply(style: .caps)
+                .foregroundColor(Color(Asset.Colors.night.color))
+        }
+            .frame(minHeight: 33)
     }
 }
