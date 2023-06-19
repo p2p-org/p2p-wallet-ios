f4da91e71e7eb9070c6b5d9bcf9da76f6413db5c
Chung Tran <bigearsenal@gmail.com>
2023-06-19 16:47:02 +0700

    feat: remove unused init

--- Packages/KeyAppKit/Sources/Onboarding/Common/Models/WalletMetaData.swift
+++ Packages/KeyAppKit/Sources/Onboarding/Common/Models/WalletMetaData.swift
@@ -251,8 +251,4 @@ private extension Date {
     var millisecondsSince1970: Int64 {
         Int64((self.timeIntervalSince1970 * 1000.0).rounded())
     }
-    
-    init(milliseconds:Int64) {
-        self = Date(timeIntervalSince1970: TimeInterval(milliseconds) / 1000)
-    }
 }
