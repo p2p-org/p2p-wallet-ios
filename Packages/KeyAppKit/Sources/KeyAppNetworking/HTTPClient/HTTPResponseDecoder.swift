d3295df7c48cc94c891046977a191ecababbe0a0
Chung Tran <bigearsenal@gmail.com>
2023-06-20 11:24:06 +0700

    feat: response string

--- Packages/KeyAppKit/Sources/KeyAppNetworking/HTTPClient/HTTPResponseDecoder.swift
+++ Packages/KeyAppKit/Sources/KeyAppNetworking/HTTPClient/HTTPResponseDecoder.swift
@@ -42,8 +42,10 @@ extension JSONResponseDecoder: HTTPResponseDecoder {
         switch response.statusCode {
         case 200 ... 299:
             // Special case when return type is string
-            if type == String.self {
-                return String(data: data, encoding: .utf8) as! T
+            if type == String.self,
+               let string = String(data: data, encoding: .utf8) as? T
+            {
+                return string
             }
             return try JSONDecoder().decode(type, from: data)
         default:
