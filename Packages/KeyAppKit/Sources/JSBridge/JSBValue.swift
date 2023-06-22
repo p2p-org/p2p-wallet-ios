5c05d4c7ce3adfcfab3756819ab0dcdb1cfaca26
Tran Giang Long <trangianglong4003@gmail.com>
2023-06-22 16:15:10 +0700

    Update JSBValue.swift

--- Packages/KeyAppKit/Sources/JSBridge/JSBValue.swift
+++ Packages/KeyAppKit/Sources/JSBridge/JSBValue.swift
@@ -100,8 +100,6 @@ public class JSBValue: JSBridge, CustomStringConvertible {
                      );
                  0;
                 """
-                
-                print(script)
 
                 await context.wkWebView.evaluateJavaScript(script) { _, error in
                     guard let error = error else { return }
