9af3ec499a200176e574d6fbe52ee6c2e9914d84
Tran Giang Long <trangianglong4003@gmail.com>
2023-07-04 15:56:28 +0700

    feat(web3auth): fix

--- p2p_wallet/Scenes/Web3Auth/Reauthentication/SubFlow/CustomShare/ReAuthCustomShareDelegatedCoordinator.swift
+++ p2p_wallet/Scenes/Web3Auth/Reauthentication/SubFlow/CustomShare/ReAuthCustomShareDelegatedCoordinator.swift
@@ -64,7 +64,7 @@ class ReAuthCustomShareDelegatedCoordinator: DelegatedCoordinator<ReAuthCustomSh
                 title = L10n.itSOkayToBeWrong
                 subtitle = L10n.YouUsedTooMuchNumbers.forYourSafetyWeHaveFrozenYourAccountFor
             case .blockResend:
-                title = L10n.soLetSBreathe
+                title = L10n.confirmationCodeLimitHit
                 subtitle = { (_: Any) in L10n.YouVeUsedAll5Codes.TryAgainLater.forHelpContactSupport }
             }
 
