b95493d058afa766486f0b477c1298c8fa734e7d
Chung Tran <bigearsenal@gmail.com>
2023-07-05 16:33:01 +0700

    feat: Event

--- Packages/KeyAppKit/Sources/StateMachine/State.swift
+++ Packages/KeyAppKit/Sources/StateMachine/State.swift
@@ -1,8 +1,9 @@
 import Foundation
 
-/// A state struct that is managed by a `StateMachine`.
+/// A state struct that represent a state in a `StateMachine`.
 /// State should be self-immutable, every actions/event will receive old state, copy it, calculate and return new state.
 /// State needs to be **lightweight**. It contains mutable properties and computed properties. Every time you want to add a property to a state, you need to clearly define weather it is mutable property or computed property.
+/// State should be Equatable (to support reverse, comparison, ignoring duplication, etc.).
 public protocol State: Equatable {
     
 }
