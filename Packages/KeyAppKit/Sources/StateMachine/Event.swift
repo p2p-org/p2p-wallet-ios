10c729a0eadb74680b7ef9f8a52db32075c108c0
Chung Tran <bigearsenal@gmail.com>
2023-07-06 10:25:57 +0700

    feat: rename Event to Action

--- Packages/KeyAppKit/Sources/StateMachine/Event.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-/// A event struct that represent an event in a `StateMachine`.
-/// Event needs to be lightweight that holds the name of the event and parameters needed for performing an action.
-/// Event should be Equatable (to support reverse, comparison, ignoring duplication, etc.).
-public protocol Event: Equatable {
-    
-}
