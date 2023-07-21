6dd85b484eef602cb811f710fd57f6a933baa815
Tran Giang Long <trangianglong4003@gmail.com>
2023-07-21 15:43:34 +0700

    feat: fix swap not update after operation

--- p2p_wallet/Common/Services/SendViaLinkStorage/SendViaLinkTransactionInfo.swift
+++ p2p_wallet/Common/Services/SendViaLinkStorage/SendViaLinkTransactionInfo.swift
@@ -63,7 +63,7 @@ extension Array where Element == SendViaLinkTransactionInfo {
                 seed: "UOO8ZTPqlwY5bJqE",
                 timestamp: Date()
                     .addingTimeInterval(-60*60*24*2) // 2 days ago
-            )
+            ),
         ]
     }
 }
