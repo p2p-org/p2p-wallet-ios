38e2bd282f31c381f2e2985566e3cb757c9d0fc4
Zafar Ivaev <zafarivaev@gmail.com>
2023-07-25 13:36:48 +0500

    feat: refactor formatting

--- p2p_wallet/Common/Extensions/Double+Extensions.swift
+++ p2p_wallet/Common/Extensions/Double+Extensions.swift
@@ -108,36 +108,6 @@ extension Double {
         let number = showMinus ? self : abs(self)
         return formatter.string(from: number as NSNumber) ?? "0"
     }
-    
-    public func formattedAsUnit() -> String {
-        
-        let formatter = NumberFormatter()
-        formatter.numberStyle = .decimal
-        formatter.roundingMode = .down
-        formatter.groupingSeparator = ""
-        
-        let value = abs(self)
-        
-        if value < 0.01 {
-            return "0"
-        } else if value >= 0.01 && value < 1 {
-            formatter.maximumFractionDigits = 2
-        } else if value >= 1 && value < 10_000 {
-            formatter.maximumFractionDigits = 0
-        } else if value >= 10_000 && value < 1_000_000 {
-            formatter.multiplier = 0.001
-            formatter.positiveSuffix = "k"
-            formatter.maximumFractionDigits = 1
-        } else if value >= 1_000_000 && value < 1_000_000_000 {
-            formatter.multiplier = 0.000001
-            formatter.positiveSuffix = "M"
-            formatter.maximumFractionDigits = 0
-        } else if value >= 1_000_000_000 {
-            return "999M+"
-        }
-        
-        return formatter.string(from: self as NSNumber) ?? "0"
-    }
 
     func fiatAmountFormattedString(
         maximumFractionDigits: Int = 2,
--- p2p_wallet/Scenes/TabBar/TabBarViewModel.swift
+++ p2p_wallet/Scenes/TabBar/TabBarViewModel.swift
@@ -162,7 +162,7 @@ extension TabBarViewModel {
                 let equityValue: Double = state.value
                     .filter { $0.isUSDC }
                     .reduce(0) { $0 + $1.amountInFiatDouble }
-                return "\(Defaults.fiat.symbol)\(equityValue.formattedAsUnit())"
+                return "\(Defaults.fiat.symbol)\(NumberFormatter.unit(for: equityValue))"
             }
             .receive(on: RunLoop.main)
             .eraseToAnyPublisher()
