4f1b0f6c136ab14ae91f9100646c49741d43065b
Chung Tran <bigearsenal@gmail.com>
2023-08-04 16:07:23 +0700

    fix: script

--- Scripts/download-and-create-bundle-lokalise.sh
+++ Scripts/download-and-create-bundle-lokalise.sh
@@ -19,10 +19,15 @@ timestamp=$(get_creation_timestamp)
 
 # Convert timestamp to the desired date format (YYYY-MM-DD)
 creation_date=$(python -c "import datetime; print(datetime.datetime.fromtimestamp($timestamp).strftime('%Y-%m-%d'))")
+echo "$creation_date"
+
+# Read the MARKETING_VERSION from project.yml and extract only the number
+marketing_version=$(grep "MARKETING_VERSION" project.yml | cut -d ':' -f 2 | tr -d '[:space:]' | tr -d '"')
 
 # Function to download localization files in JSON format from Lokalise
 download_json_files() {
   local date_param="$1"
+  local supported_languages="${SUPPORTED_LANGUAGUES}"
 
   # Download JSON files
   response=$(curl -X "POST" "https://api.lokalise.com/api2/projects/${LOKALISE_PROJECT_ID}/files/download" \
@@ -31,16 +36,24 @@ download_json_files() {
        -d $'{
               "format": "strings",
               "export_empty_as": "base",
-              "filter_data": {
-                "created_after": "'"$date_param"'"
-              }
+              "all_platforms": false,
+              "filter_lang": "'"$supported_languages"'",
+              "replace_breaks": true,
+              "escape_percent": true,
+              "include_tags": ["'"$marketing_version"'"]
             }')
 
   # Extract the bundle_url from the JSON response
   bundle_url=$(echo "$response" | jq -r '.bundle_url')
 
   # Download the file and save it as a .zip bundle
-  curl -L "$bundle_url" -o "localization_bundle_${date_param}.zip"
+  curl -L "$bundle_url" -o "localization_bundle.zip"
+
+  # Extract the ZIP file to p2p_wallet/Resources
+  unzip "localization_bundle.zip" -d "p2p_wallet/Resources"
+
+  # Remove downloaded file
+  rm "localization_bundle.zip"
 }
 
 # Download JSON files and create the .zip bundle
