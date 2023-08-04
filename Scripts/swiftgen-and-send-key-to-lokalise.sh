ad8b4d728658c81898e93222b00ce9aae159d594
Chung Tran <bigearsenal@gmail.com>
2023-08-04 11:46:41 +0700

    fix: test

--- Scripts/swiftgen-and-send-key-to-lokalise.sh
+++ Scripts/swiftgen-and-send-key-to-lokalise.sh
@@ -3,8 +3,14 @@
 # Read the MARKETING_VERSION from project.yml and extract only the number
 marketing_version=$(grep "MARKETING_VERSION" project.yml | cut -d ':' -f 2 | tr -d '[:space:]' | tr -d '"')
 
+# Check if 'origin/lokalise/synced' tag exists
+if ! git show-ref --tags | grep -q "refs/tags/lokalise/synced"; then
+  echo "Error: 'origin/lokalise/synced' tag does not exist."
+  exit 1
+fi
+
 # Get the new lines from Localizable.strings using git diff with the previous commit
-changes=$(git diff HEAD^ -- p2p_wallet/Resources/Base.lproj/Localizable.strings)
+changes=$(git diff lokalise/synced -- p2p_wallet/Resources/Base.lproj/Localizable.strings)
 new_lines=$(echo "$changes" | grep "^[+]" | grep -v "^+++" | cut -c2-)
 
 # Parse values from the new lines and build the JSON payload
@@ -27,8 +33,8 @@ json_payload=${json_payload%,}"]}"
 echo "$json_payload"
 
 # Read the API token and project ID from Config.xcconfig
-lokalise_api_token=$(grep "LOKALISE_API_TOKEN" p2p_wallet/Config.xcconfig | cut -d '=' -f 2 | tr -d ' ')
-project_id=$(grep "LOKALISE_PROJECT_ID" p2p_wallet/Config.xcconfig | cut -d '=' -f 2 | tr -d ' ')
+lokalise_api_token=$LOKALISE_API_TOKEN
+project_id=$LOKALISE_PROJECT_ID
 
 # Send the keys and values to Lokalise API
 curl "https://api.lokalise.com/api2/projects/$project_id/keys" \
