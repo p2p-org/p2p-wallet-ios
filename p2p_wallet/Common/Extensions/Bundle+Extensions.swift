9038c558a716fb08f9178f341425d589bf42e47e
Chung Tran <bigearsenal@gmail.com>
2023-08-08 15:07:24 +0700

    feat: wrong commit

--- p2p_wallet/Common/Extensions/Bundle+Extensions.swift
+++ p2p_wallet/Common/Extensions/Bundle+Extensions.swift
@@ -4,6 +4,12 @@ import UIKit
 
 extension Bundle {
     // MARK: - Build number, marketing number
+    
+    
+    
+    
+    
+    
 
     var releaseVersionNumber: String? {
         infoDictionary?["CFBundleShortVersionString"] as? String
