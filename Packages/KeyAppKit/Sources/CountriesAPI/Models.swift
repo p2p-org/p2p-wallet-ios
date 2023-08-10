000b99fe43cf094cfc99228566daa2243fcbe4c8
Alexey Sidorov <ody344@gmail.com>
2023-08-10 12:37:00 +0100

    Removed HTML entiteles

--- Packages/KeyAppKit/Sources/CountriesAPI/Models.swift
+++ Packages/KeyAppKit/Sources/CountriesAPI/Models.swift
@@ -36,9 +36,101 @@ public struct Region: Codable, Equatable, Hashable {
     public var name: String
     public let alpha2: String
     public let alpha3: String
-    public let flagEmoji: String?
+    public var flagEmoji: String?
     public var isStrigaAllowed: Bool
     public var isMoonpayAllowed: Bool
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.name = try container.decode(String.self, forKey: .name)
+        self.alpha2 = try container.decode(String.self, forKey: .alpha2)
+        self.alpha3 = try container.decode(String.self, forKey: .alpha3)
+        self.flagEmoji = try container.decodeIfPresent(String.self, forKey: .flagEmoji)?.stringByDecodingHTMLEntities
+        self.isStrigaAllowed = try container.decode(Bool.self, forKey: .isStrigaAllowed)
+        self.isMoonpayAllowed = try container.decode(Bool.self, forKey: .isMoonpayAllowed)
+    }
 }
 
 public typealias Countries = [Country]
+
+
+private extension String {
+    /// Returns a new string made by replacing in the `String`
+    /// all HTML character entity references with the corresponding
+    /// character.
+    var stringByDecodingHTMLEntities : String {
+
+        // ===== Utility functions =====
+
+        // Convert the number in the string to the corresponding
+        // Unicode character, e.g.
+        //    decodeNumeric("64", 10)   --> "@"
+        //    decodeNumeric("20ac", 16) --> "€"
+        func decodeNumeric(_ string : Substring, base : Int) -> Character? {
+            guard let code = UInt32(string, radix: base),
+                let uniScalar = UnicodeScalar(code) else { return nil }
+            return Character(uniScalar)
+        }
+
+        // Decode the HTML character entity to the corresponding
+        // Unicode character, return `nil` for invalid input.
+        //     decode("&#64;")    --> "@"
+        //     decode("&#x20ac;") --> "€"
+        //     decode("&lt;")     --> "<"
+        //     decode("&foo;")    --> nil
+        func decode(_ entity : Substring) -> Character? {
+
+            if entity.hasPrefix("&#x") || entity.hasPrefix("&#X") {
+                return decodeNumeric(entity.dropFirst(3).dropLast(), base: 16)
+            } else if entity.hasPrefix("&#") {
+                return decodeNumeric(entity.dropFirst(2).dropLast(), base: 10)
+            } else {
+                return characterEntities[entity]
+            }
+        }
+
+        // ===== Method starts here =====
+
+        var result = ""
+        var position = startIndex
+
+        // Find the next '&' and copy the characters preceding it to `result`:
+        while let ampRange = self[position...].range(of: "&") {
+            result.append(contentsOf: self[position ..< ampRange.lowerBound])
+            position = ampRange.lowerBound
+
+            // Find the next ';' and copy everything from '&' to ';' into `entity`
+            guard let semiRange = self[position...].range(of: ";") else {
+                // No matching ';'.
+                break
+            }
+            let entity = self[position ..< semiRange.upperBound]
+            position = semiRange.upperBound
+
+            if let decoded = decode(entity) {
+                // Replace by decoded character:
+                result.append(decoded)
+            } else {
+                // Invalid entity, copy verbatim:
+                result.append(contentsOf: entity)
+            }
+        }
+        // Copy remaining characters to `result`:
+        result.append(contentsOf: self[position...])
+        return result
+    }
+}
+
+private let characterEntities : [ Substring : Character ] = [
+    // XML predefined entities:
+    "&quot;"    : "\"",
+    "&amp;"     : "&",
+    "&apos;"    : "'",
+    "&lt;"      : "<",
+    "&gt;"      : ">",
+
+    // HTML character entity references:
+    "&nbsp;"    : "\u{00a0}",
+    // ...
+    "&diams;"   : "♦",
+]
--- p2p_wallet/Common/Extensions/String+Extensions.swift
+++ p2p_wallet/Common/Extensions/String+Extensions.swift
@@ -128,24 +128,3 @@ extension String {
 }
 
 // MARK: - HTML Entiteles
-
-extension String {
-    func decodeHTMLEntities() -> String? {
-        guard let data = data(using: .utf8) else {
-            return nil
-        }
-
-        let options: [NSAttributedString.DocumentReadingOptionKey: Any] = [
-            .documentType: NSAttributedString.DocumentType.html,
-            .characterEncoding: String.Encoding.utf8.rawValue,
-        ]
-
-        do {
-            let attributedString = try NSAttributedString(data: data, options: options, documentAttributes: nil)
-            return attributedString.string
-        } catch {
-            print("Error decoding HTML entities: \(error)")
-            return nil
-        }
-    }
-}
