8cf1a982c02df0ae609e6e2c256db909c8c3bdf8
Zafar Ivaev <zafarivaev@gmail.com>
2023-08-11 13:10:56 +0500

    fix: add isTokenCellVisibleOnWS handling

--- p2p_wallet/Scenes/Main/NewHome/Subview/AccountList/Aggregator/HomeSolanaAccountsAggregator.swift
+++ p2p_wallet/Scenes/Main/NewHome/Subview/AccountList/Aggregator/HomeSolanaAccountsAggregator.swift
@@ -18,7 +18,8 @@ struct HomeSolanaAccountsAggregator: DataAggregator {
         let (accounts, favourites, ignores, hideZeroBalance) = input
 
         return accounts
-            .filter { !$0.isNFTToken }
+            .filter { !$0.isNFTToken && $0.token.keyAppExtensions.isPositionOnWS == true }
+            .filter { $0.token.keyAppExtensions.isTokenCellVisibleOnWS == true }
             .sorted(by: Self.defaultSorter)
             .map { account in
                 var tags: AccountTags = []
