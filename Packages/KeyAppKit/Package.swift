d93725efc493083c11b9240b58d393db520cdf79
Chung Tran <bigearsenal@gmail.com>
2024-01-26 13:48:45 +0700

    fix: typo

--- Packages/KeyAppKit/Package.swift
+++ Packages/KeyAppKit/Package.swift
@@ -333,7 +333,7 @@ let package = Package(
         .target(
             name: "PnLService",
             dependencies: [
-                "PnLService",
+                "KeyAppNetworking",
             ]
         ),
 
